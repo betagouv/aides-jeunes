f80a0e434dcc1ab7943d8593af0425b61dad45f2
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2014-06-20 11:35:17 +0200

    Add some tests

---
+++ tests/rsa.js
@@ -0,0 +1,33 @@
+var should = require('should');
+var openfisca = require('../lib/simulation/openfisca');
+var expand = require('../lib/situation').expand;
+
+describe('rsa', function() {
+
+    describe('etudiant-seul', function() {
+        var data = require('./rsa-tests/etudiant-seul.json');
+        var expectedValue = data.expectedResults.rsa;
+
+        it('should return ' + expectedValue, function(done) {
+            openfisca.simulate(expand(data), function(err, result) {
+                if (err) throw err;
+                result.rsa.should.equal(expectedValue);
+                done();
+            });
+        });
+    });
+
+    describe('chomeur-1enfant-locataire', function() {
+        var data = require('./rsa-tests/chomeur-1enfant-locataire.json');
+        var expectedValue = data.expectedResults.rsa;
+
+        it('should return ' + expectedValue, function(done) {
+            openfisca.simulate(expand(data), function(err, result) {
+                if (err) throw err;
+                result.rsa.should.equal(expectedValue);
+                done();
+            });
+        });
+    });
+
+});

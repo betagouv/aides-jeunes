5eeb1af4ee025c6feb1f74d5a3015e75406f6b82
Arnaud Kleinpeter <arnaud.kleinpeter@gmail.com>
2014-06-20 14:50:42 +0200

    premiers tests sur le front

--- karma.conf.js
+++ karma.conf.js
@@ -13,23 +13,23 @@ module.exports = function(config) {
     files: [
       'app/bower_components/angular/angular.js',
       'app/bower_components/angular-mocks/angular-mocks.js',
-      'app/bower_components/angular-resource/angular-resource.js',
-      'app/bower_components/angular-cookies/angular-cookies.js',
-      'app/bower_components/angular-sanitize/angular-sanitize.js',
       'app/bower_components/angular-route/angular-route.js',
       'app/js/*.js',
       'app/js/**/*.js',
+      'test/client/spec/**/*.js'
     ],
 
     // list of files / patterns to exclude
-    exclude: [],
+    exclude: [
+      'app/js/marqueblanche.js'
+    ],
 
     // web server port
     port: 8001,
 
     // level of logging
     // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-    logLevel: config.LOG_INFO,
+    logLevel: config.LOG_DEBUG,
 
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: false,
@@ -47,6 +47,6 @@ module.exports = function(config) {
 
     // Continuous Integration mode
     // if true, it capture browsers, run tests and exit
-    singleRun: false
+    singleRun: true
   });
 };
---
+++ test/client/spec/controllers/marqueBlancheCtrl.js
@@ -0,0 +1,32 @@
+'use strict';
+
+describe('Controller: marqueBlancheCtrl', function () {
+
+  beforeEach(module('ddsApp'));
+
+  it('Should attach the secours populaire logo to the scope when path name corresponds', function () {
+    var scope;
+    inject(function($rootScope, $controller) {
+      scope = $rootScope.$new();
+      var _window_ = { location: { pathname: '/secours-populaire/' }};
+      $controller('marqueBlancheCtrl', {
+        $scope: scope,
+        $window: _window_
+      });
+    });
+    expect(scope.logo).toBeDefined();
+  });
+
+  it('Should attach no logo when path name is /', function () {
+    var scope;
+    inject(function($rootScope, $controller) {
+      scope = $rootScope.$new();
+      var _window_ = { location: { pathname: '/' }};
+      $controller('marqueBlancheCtrl', {
+        $scope: scope,
+        $window: _window_
+      });
+    });
+    expect(scope.logo).toBeUndefined();
+  });
+});

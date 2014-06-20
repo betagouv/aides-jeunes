1d803bef553dfc4ad28c181c201ea8b9c031a407
Arnaud Kleinpeter <arnaud.kleinpeter@gmail.com>
2014-06-20 15:50:56 +0200

    exécution tests front dans chrome

--- karma.conf.js
+++ karma.conf.js
@@ -29,7 +29,7 @@ module.exports = function(config) {
 
     // level of logging
     // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-    logLevel: config.LOG_DEBUG,
+    logLevel: config.LOG_INFO,
 
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: false,
@@ -42,7 +42,7 @@ module.exports = function(config) {
     // - Safari (only Mac)
     // - PhantomJS
     // - IE (only Windows)
-    browsers: ['PhantomJS'],
+    browsers: ['Chrome'],
 
 
     // Continuous Integration mode

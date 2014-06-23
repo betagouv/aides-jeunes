166bf69a34029c22f75775d76a66c7126bc11891
Arnaud Kleinpeter <arnaud.kleinpeter@gmail.com>
2014-06-23 16:47:50 +0200

    bootstrap back part 2

---
+++ app/js/back.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var ddsApp = angular.module('ddsBackend', ['ngRoute']);
+
+ddsApp.config(function($routeProvider, $locationProvider) {
+    $locationProvider.html5Mode(true);
+    $routeProvider
+        .when('/', {
+            templateUrl: '/back/partials/taskList.html',
+            controller: 'BackendCtrl'
+        });
+});

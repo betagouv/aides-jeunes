361d1939fc39fc60a195a5568be2733904fa85c8
Arnaud Kleinpeter <arnaud.kleinpeter@gmail.com>
2014-06-25 15:51:27 +0200

    login back-office côté angulat

--- app/js/back.js
+++ app/js/back.js
@@ -8,5 +8,23 @@ ddsApp.config(function($routeProvider, $locationProvider) {
         .when('/', {
             templateUrl: '/back/partials/taskList.html',
             controller: 'TaskListCtrl'
+        })
+        .when('/login', {
+            templateUrl: '/back/partials/login.html',
+            controller: 'LoginCtrl'
         });
+});
+
+ddsApp.run(function($rootScope, $location, UserService) {
+    var routeChangeLoginListener = function() {
+        var isRouteLogin = ('/login' === $location.path());
+        if (!UserService.isLoggedIn() && !isRouteLogin) {
+            $location.path('/login');
+        } else if (isRouteLogin && UserService.isLoggedIn()) {
+            $location.path('/');
+        }
+    };
+
+    $rootScope.$on('$routeChangeStart', routeChangeLoginListener);
+    routeChangeLoginListener();
 });
---
+++ app/js/controllers/login.js
@@ -0,0 +1,4 @@
+'use strict';
+
+angular.module('ddsBackend').controller('LoginCtrl', function($scope, $http) {
+});

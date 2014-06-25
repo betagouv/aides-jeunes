236ad9dd097c911fb7bccbd053a23437780fbe99
Arnaud Kleinpeter <arnaud.kleinpeter@gmail.com>
2014-06-25 17:08:20 +0200

    formulaire de login

--- app/js/controllers/login.js
+++ app/js/controllers/login.js
@@ -1,4 +1,18 @@
 'use strict';
 
-angular.module('ddsBackend').controller('LoginCtrl', function($scope, $http) {
+angular.module('ddsBackend').controller('LoginCtrl', function($scope, UserService) {
+    $scope.badCredentials = false;
+    $scope.username = 'test@test.com';
+    $scope.password = 'coucou';
+
+    $scope.connect = function() {
+        $scope.badCredentials = false;
+        UserService.authenticate($scope.username, $scope.password)
+            .then(function() {
+                $location.path('/');
+            }, function() {
+                $scope.password = '';
+                $scope.badCredentials = true;
+            });
+    };
 });

6e88cc639dfcd74a876c741a7d533f4088bdea00
Arnaud Kleinpeter <arnaud.kleinpeter@gmail.com>
2014-07-10 15:23:21 +0200

    directive selectOnFocus

---
+++ app/js/directives/selectOnFocus.js
@@ -0,0 +1,12 @@
+'use strict';
+
+angular.module('ddsApp').directive('selectOnFocus', function() {
+    return {
+        restrict: 'A',
+        link: function(scope, element) {
+            element.on('click', function () {
+                this.select();
+            });
+        }
+    };
+});

eac9a52a8bf64d0dd4d62036359db526d97f126e
Arnaud Kleinpeter <arnaud.kleinpeter@gmail.com>
2014-07-10 10:24:27 +0200

    situations specifiques meileur affichage

--- app/js/controllers/situationsSpecifiques.js
+++ app/js/controllers/situationsSpecifiques.js
@@ -2,6 +2,10 @@
 
 angular.module('ddsApp').controller('SituationsSpecifiquesCtrl', function($scope, SituationService) {
     $scope.situations = SituationService.statutsSpecifiquesLabels;
+    $scope.displaySituation = function(situation) {
+        return situation.charAt(0).toUpperCase() + situation.substring(1);
+    };
+
     var situation = SituationService.restoreLocal();
     $scope.individus = SituationService.createIndividusList(situation);
 

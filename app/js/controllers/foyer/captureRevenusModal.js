00edeb71e913be9d2db9a44554a58b03e291b925
Arnaud Kleinpeter <arnaud.kleinpeter@gmail.com>
2014-07-11 11:08:16 +0200

    modale sélection revenus

--- app/js/controllers/foyer/captureRevenusModal.js
+++ app/js/controllers/foyer/captureRevenusModal.js
@@ -4,25 +4,75 @@ angular.module('ddsApp').controller('CaptureRevenusModalCtrl', function($scope, 
     $scope.modalTitle = modalTitle;
     $scope.individu = individu;
 
+    $scope.cleanSelectedRessources = function() {
+        for (var i in individu.selectedRessources) {
+            if (!individu.selectedRessources[i]) {
+                delete individu.selectedRessources[i];
+            }
+        }
+    };
+
     if (!individu.selectedRessources) {
         individu.selectedRessources = {};
+    } else {
+        $scope.ressourcesSelected = true;
+        $scope.cleanSelectedRessources();
     }
 
-    $scope.submit = function() {
-        if (!individu.ressources) {
-            individu.ressources = [];
-            $scope.revenus = {};
-            for (var ressourceType in individu.selectedRessources) {
-                $scope.revenus[ressourceType] = new Array(0, 0, 0);
+    $scope.initMonths = function() {
+        var month1 = moment().subtract('months', 3);
+        var month2 = moment().subtract('months', 2);
+        var month3 = moment().subtract('months', 1);
+
+        $scope.month1 = month1.format('YYYY-MM');
+        $scope.month2 = month2.format('YYYY-MM');
+        $scope.month3 = month3.format('YYYY-MM');
+
+        $scope.month1Label = month1.format('MMMM YYYY');
+        $scope.month2Label = month2.format('MMMM YYYY');
+        $scope.month3Label = month3.format('MMMM YYYY');
+    };
+
+    $scope.initMonths();
+
+    $scope.initRevenusFromIndividu = function() {
+        $scope.revenus = {};
+
+        // remplissage des cases des types de revenus sélectionnés avec des 0
+        for (var ressourceType in individu.selectedRessources) {
+            var ressource = $scope.revenus[ressourceType] = {};
+            ressource[$scope.month1] = 0;
+            ressource[$scope.month2] = 0;
+            ressource[$scope.month3] = 0;
+        }
+
+        // récupération des éventuelles valeurs rentrées précédemment
+        for (var i in individu.ressources) {
+            var ressource = individu.ressources[i];
+            var revenu = $scope.revenus[ressource.type]
+            if (!!revenu && angular.isDefined(revenu[ressource.periode])) {
+                revenu[ressource.periode] = ressource.montant;
             }
+        }
+    };
+
+    $scope.initRevenusFromIndividu();
+
+    $scope.submit = function() {
+        if (!$scope.ressourcesSelected) {
+            $scope.ressourcesSelected = true;
+            $scope.cleanSelectedRessources();
+            $scope.mergeRevenusWithNewRessources();
         } else {
+            individu.ressources = [];
             for (var ressourceType in $scope.revenus) {
                 var montants = $scope.revenus[ressourceType];
-                for (var i = 0; i < 3; i++) {
+                for (var month in montants) {
+                    var montant = montants[month];
                     var ressource = {
                         type: ressourceType,
-                        periode: '2014-04',
-                        montant: montants[i]
+                        periode: month,
+                        montant: montant
                     };
                     individu.ressources.push(ressource);
                 }
@@ -31,11 +81,37 @@ angular.module('ddsApp').controller('CaptureRevenusModalCtrl', function($scope, 
         }
     };
 
+    $scope.mergeRevenusWithNewRessources = function() {
+        var previousRevenus = $scope.revenus;
+        $scope.revenus = {};
+
+        // remplissage des cases des types de revenus sélectionnés avec des 0
+        for (var ressourceType in individu.selectedRessources) {
+            var ressource = $scope.revenus[ressourceType] = {};
+            ressource[$scope.month1] = 0;
+            ressource[$scope.month2] = 0;
+            ressource[$scope.month3] = 0;
+        }
+
+        // récupération des éventuelles valeurs rentrées précédemment
+        for (var i in previousRevenus) {
+            var ressource = previousRevenus[i];
+            var revenu = $scope.revenus[i];
+            if (!!revenu) {
+                for (var j in ressource) {
+                    var montant = ressource[j];
+                    if (angular.isDefined(revenu[j])) {
+                        revenu[j] = montant;
+                    }
+                }
+            }
+        }
+    };
+
     $scope.sections = [
         {
             name: 'revenusActivite',
             label: 'Revenus d\'activité',
-            open: true,
             subsections: [
                 {
                     name: 'revenusSalarie',

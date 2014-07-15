5a9393e652f00cb0b84063256cd0c6b43fe17e16
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2014-07-15 23:13:25 +0200

    Add tests for simulation

--- test/server/rsa.js
+++ test/server/rsa.js
@@ -1,32 +1,35 @@
 var should = require('should');
+var path = require('path');
+var fs = require('fs');
 var openfisca = require('../../lib/simulation/openfisca');
 
+var testsPath = path.join(__dirname, 'rsa-tests');
+
 describe('rsa', function() {
-
-    describe('etudiant-seul', function() {
-        var data = require('./rsa-tests/etudiant-seul.json');
-        var expectedValue = data.expectedResults.rsa;
-
-        it('should return ' + expectedValue, function(done) {
-            openfisca.simulate(data, function(err, result) {
-                if (err) throw err;
-                result.rsa.should.equal(expectedValue);
-                done();
+    fs.readdirSync(testsPath).forEach(function (file) {
+        if (/(.*)\.(json$)/.test(file)) {
+            describe(file, function() {
+                var data = require(testsPath + '/' + file);
+                if (data.expectedResults.exception) {
+                    var expectedException = data.expectedResults.exception;
+                    it('should throw ' + expectedException, function(done) {
+                        openfisca.simulate(data, function(err, result) {
+                            should.exist(err);
+                            err.message.should.equal(expectedException);
+                            done();
+                        });
+                    });
+                } else {
+                    var expectedValue = data.expectedResults.rsa;
+                    it('should return ' + expectedValue, function(done) {
+                        openfisca.simulate(data, function(err, result) {
+                            if (err) throw err;
+                            result.rsa.should.be.approximately(expectedValue, 0.1);
+                            done();
+                        });
+                    });
+                }
             });
-        });
+        }
     });
-
-    describe('chomeur-1enfant-locataire', function() {
-        var data = require('./rsa-tests/chomeur-1enfant-locataire.json');
-        var expectedValue = data.expectedResults.rsa;
-
-        it('should return ' + expectedValue, function(done) {
-            openfisca.simulate(data, function(err, result) {
-                if (err) throw err;
-                result.rsa.should.equal(expectedValue);
-                done();
-            });
-        });
-    });
-
 });

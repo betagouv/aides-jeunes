c41552a885cae6153f4f88a70eeb217f902aca43
Arnaud Kleinpeter <arnaud.kleinpeter@gmail.com>
2014-07-16 16:06:05 +0200

    amélioration rendu blocs de situations spécifiques

--- app/js/controllers/foyer/situationsSpecifiquesModal.js
+++ app/js/controllers/foyer/situationsSpecifiquesModal.js
@@ -1,17 +1,30 @@
 'use strict';
 
 angular.module('ddsApp').controller('FoyerSituationsSpecifiquesModalCtrl', function($scope, $modalInstance, SituationService) {
-    $scope.situations = SituationService.statutsSpecifiquesLabels;
+    var statuts = SituationService.statutsSpecifiquesLabels;
     var situation = SituationService.restoreLocal();
     $scope.individus = SituationService.createIndividusList(situation);
 
     $scope.selectedSituations = {};
     _.forEach($scope.individus, function(individu) {
-        _.forEach($scope.situations, function(situation, k) {
+        _.forEach(statuts, function(situation, k) {
             if (individu.individu[k]) {
                 $scope.selectedSituations[k] = true;
             }
         });
+    });
+
+    // création d'un array avec deux situations par ligne
+    $scope.situations = [];
+    var i = 0;
+    var arr;
+    _.forEach(statuts, function(statut, key) {
+        if (i % 2 === 0) {
+            arr = {};
+            $scope.situations.push(arr);
+        }
+        arr[key] = statut;
+        i++;
     });
 
     $scope.submit = function() {

a1cba10fa753ac4c5c353ce9b59c97a32c80769b
Arnaud Kleinpeter <arnaud.kleinpeter@gmail.com>
2014-07-17 15:20:38 +0200

    transformation côté front de l'objet situation pour qu'il corresponde avec l'api

--- app/js/controllers/resultat.js
+++ app/js/controllers/resultat.js
@@ -1,22 +1,12 @@
 'use strict';
 
-angular.module('ddsApp').controller('ResultatCtrl', function($scope, $http, SituationService) {
+angular.module('ddsApp').controller('ResultatCtrl', function($scope, SituationService) {
     $scope.awaitingResults = true;
-    var situation = SituationService.restoreLocal();
-    situation.demandeur.dateDeNaissance = moment(situation.demandeur.birthDate, 'DD/MM/YYYY').format('YYYY-MM-DD');
-    if (situation.conjoint) {
-        situation.conjoint.dateDeNaissance = moment(situation.conjoint.birthDate, 'DD/MM/YYYY').format('YYYY-MM-DD');
-    }
-
-    $http.post('/api/situations', situation).then(function(result) {
-        console.log(situation);
-        $http.get('/api/situations/' + result.data._id + '/simulation').then(function() {
-        }, function() {
-            $scope.error = true;
-        }).finally(function() {
-            $scope.awaitingResults = false;
-        });
+    SituationService.simulate().then(function(result) {
+        console.log(result);
     }, function() {
         $scope.error = true;
+    }).finally(function() {
+        $scope.awaitingResults = false;
     });
 });

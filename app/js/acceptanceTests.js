3f8a2a1a71e99cd44e008037951728a961cebf7d
Arnaud Kleinpeter <arnaud.kleinpeter@gmail.com>
2014-07-18 16:28:21 +0200

    création d'un cas de test à la volée

--- app/js/acceptanceTests.js
+++ app/js/acceptanceTests.js
@@ -2,16 +2,31 @@
 
 var ddsApp = angular.module('acceptanceTests', ['ui.router', 'ngAnimate', 'ui.bootstrap', 'ngStorage']);
 
-ddsApp.config(function($locationProvider, $stateProvider) {
+ddsApp.config(function($locationProvider, $stateProvider, $urlRouterProvider) {
     moment.lang('fr');
 
     $locationProvider.html5Mode(true);
+    $urlRouterProvider.otherwise('/');
 
     $stateProvider
         .state('index', {
             url: '/',
             templateUrl: '/acceptance-tests/partials/index.html',
             controller: 'IndexCtrl'
+        })
+        .state('index.new', {
+            url: '/new/:situationId',
+            onEnter: ['$state', '$modal', function($state, $modal) {
+                $modal.open({
+                    templateUrl: '/acceptance-tests/partials/new.html',
+                    controller: 'FoyerSituationsSpecifiquesModalCtrl',
+                    size: 'lg',
+                    backdrop: 'static',
+                    keyboard: false
+                }).result.then(function() {
+                    return $state.go('foyer');
+                });
+            }]
         });
 });
 
--- app/js/controllers/resultat.js
+++ app/js/controllers/resultat.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('ddsApp').controller('ResultatCtrl', function($scope, SimulationService) {
+angular.module('ddsApp').controller('ResultatCtrl', function($scope, $window, SimulationService) {
     $scope.awaitingResults = true;
     SimulationService.simulate().then(function(simulation) {
         $scope.situationId = simulation.situationId;
@@ -10,4 +10,8 @@ angular.module('ddsApp').controller('ResultatCtrl', function($scope, SimulationS
     }).finally(function() {
         $scope.awaitingResults = false;
     });
+
+    $scope.createTest = function() {
+        $window.location.href = '/acceptance-tests/new';
+    };
 });

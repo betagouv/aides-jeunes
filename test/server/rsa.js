f0d6e72347b40730d12c84560c70e277c696968d
Arnaud Kleinpeter <arnaud.kleinpeter@gmail.com>
2014-07-18 09:57:15 +0200

    flag comme désactivé les tests d'integ qui ne passent pas encore

--- test/server/rsa.js
+++ test/server/rsa.js
@@ -5,29 +5,39 @@ var openfisca = require('../../lib/simulation/openfisca');
 
 var testsPath = path.join(__dirname, 'rsa-tests');
 
-xdescribe('rsa', function() {
+describe('rsa', function() {
     fs.readdirSync(testsPath).forEach(function (file) {
         if (/(.*)\.(json$)/.test(file)) {
             describe(file, function() {
                 var data = require(testsPath + '/' + file);
                 if (data.expectedResults.exception) {
                     var expectedException = data.expectedResults.exception;
-                    it('should throw ' + expectedException, function(done) {
+                    var test = function(done) {
                         openfisca.simulate(data, function(err, result) {
                             should.exist(err);
                             err.message.should.equal(expectedException);
                             done();
                         });
-                    });
+                    };
+                    if (data.desactive) {
+                        xit('should throw ' + expectedException, test);
+                    } else {
+                        it('should throw ' + expectedException, test);
+                    }
                 } else {
                     var expectedValue = data.expectedResults.rsa;
-                    it('should return ' + expectedValue, function(done) {
+                    var test = function(done) {
                         openfisca.simulate(data, function(err, result) {
                             if (err) throw err;
                             result.rsa.should.be.approximately(expectedValue, 0.1);
                             done();
                         });
-                    });
+                    };
+                    if (data.desactive) {
+                        xit('should return ' + expectedValue, test);
+                    } else {
+                        it('should return ' + expectedValue, test);
+                    }
                 }
             });
         }

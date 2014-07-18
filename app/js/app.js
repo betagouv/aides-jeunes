f5a5c486454804a0fb1298f211f4f06a8e13b61c
Arnaud Kleinpeter <arnaud.kleinpeter@gmail.com>
2014-07-18 09:56:53 +0200

    page de teasing pour 'envoi en ligne de la demande

--- app/js/app.js
+++ app/js/app.js
@@ -147,7 +147,7 @@ ddsApp.config(function($locationProvider, $stateProvider, $urlRouterProvider) {
         })
         .state('envoi_demande', {
             url: '/envoi-demande/:situationId',
-            templateUrl: '/partials/envoi-demande.html',
+            templateUrl: '/partials/envoi-demande-teaser.html',
             controller: 'EnvoiDemandeCtrl'
         });
 });

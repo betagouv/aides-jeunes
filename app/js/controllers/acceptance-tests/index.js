eeda61ce6b31c39ede7151b98b906a53e800601e
Arnaud Kleinpeter <arnaud.kleinpeter@gmail.com>
2014-07-21 11:34:10 +0200

    amélioration page de tests d'acceptance

--- app/js/controllers/acceptance-tests/index.js
+++ app/js/controllers/acceptance-tests/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('acceptanceTests').controller('IndexCtrl', function($scope, $http) {
+angular.module('acceptanceTests').controller('IndexCtrl', function($scope, $http, $timeout) {
     $http.get('/api/acceptance-tests').then(function(result) {
         $scope.tests = result.data;
     });
@@ -28,6 +28,7 @@ angular.module('acceptanceTests').controller('IndexCtrl', function($scope, $http
         var promise = $http.get('/api/situations/' + test.situation + '/simulation');
         promise.then(function(result) {
             var droits = result.data;
+            test.status = 'ok';
             test.droitsAttendus.forEach(function(droit) {
                 var actualValue = droits[droit.name];
                 if (angular.isDefined(actualValue)) {
@@ -39,19 +40,22 @@ angular.module('acceptanceTests').controller('IndexCtrl', function($scope, $http
                         droit.status = 'ok';
                     } else {
                         droit.status = 'ko';
+                        test.status = 'ko';
                     }
                 }
             });
             _.forEach(droits, function(value, name) {
                 if (value) {
-                    test.droitsAttendus.push({name: name, expectedValue: undefined, actualValue: value, status: 'unknown'});
+                    test.droitsAttendus.push({ name: name, expectedValue: undefined, actualValue: value, status: 'unknown' });
                 }
             });
             _.where(test.droitsAttendus, {status: undefined}).forEach(function(droit) {
                 droit.status = 'ko';
+                test.status = 'ko';
                 droit.actualValue = false;
             });
         }, function() {
+            test.status = 'ko';
             test.droitsAttendus.forEach(function(droit) {
                 droit.status = 'ko';
             });

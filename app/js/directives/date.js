2049e58be8d51dfcf6bb303e8c9a4311eb0640a9
Arnaud Kleinpeter <arnaud.kleinpeter@gmail.com>
2014-08-21 17:32:12 +0200

    fix directive ddsDate pour autoriser les dates vides

--- app/js/directives/date.js
+++ app/js/directives/date.js
@@ -5,14 +5,18 @@ angular.module('ddsApp').directive('ddsDate', function() {
         require: 'ngModel',
         link: function(scope, elm, attrs, ctrl) {
             ctrl.$parsers.unshift(function(viewValue) {
-                var date = moment(viewValue, 'DD/MM/YYYY', true);
-                if (date.isValid()) {
-                    ctrl.$setValidity('ddsDate', true);
-                    return viewValue;
-                } else {
-                    ctrl.$setValidity('ddsDate', false);
-                    return undefined;
+                if (angular.isString(viewValue) && viewValue.length > 0) {
+                    var date = moment(viewValue, 'DD/MM/YYYY', true);
+                    if (!date.isValid()) {
+                        ctrl.$setValidity('ddsDate', true);
+
+                        return viewValue;
+                    }
                 }
+
+                ctrl.$setValidity('ddsDate', false);
+
+                return undefined;
             });
         }
     };

8eb8d1d062091bfd16eb544ca2e393b1c3fccf98
Arnaud Kleinpeter <arnaud.kleinpeter@gmail.com>
2014-09-10 14:58:11 +0200

    ajout disclaimer capture ressources 2012 si on a pas droit à l'AL

--- app/js/controllers/resultat.js
+++ app/js/controllers/resultat.js
@@ -3,6 +3,10 @@
 angular.module('ddsApp').controller('ResultatCtrl', function($scope, $window, $http, $state, $stateParams, $timeout, SimulationService, CerfaService, situation) {
     $scope.situation = situation;
 
+    $scope.yearMoins2 = moment().subtract('years', 2).format('YYYY');
+    $scope.debutPeriode = moment().startOf('month').subtract('years', 1).format('MMMM YYYY');
+    $scope.finPeriode = moment().startOf('month').subtract('months', 1).format('MMMM YYYY');
+
     var launchSimulation = function() {
         $scope.awaitingResults = true;
         $scope.error = false;
@@ -33,6 +37,17 @@ angular.module('ddsApp').controller('ResultatCtrl', function($scope, $window, $h
 
     $scope.isDroitAllocationLogement = function(droit) {
         return _.contains(['als', 'alf', 'apl'], droit.id);
+    };
+
+    $scope.hasDroitAllocationLogement = function() {
+        var result = false;
+        $scope.droits.forEach(function(droit) {
+            if ($scope.isDroitAllocationLogement(droit)) {
+                result = true;
+            }
+        });
+
+        return result;
     };
 
     $scope.$on('ressourcesCaptured', function(ressourcesN2) {

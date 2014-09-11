c2403ccb447b7f99e660fd5eee419d1f1536326f
Arnaud Kleinpeter <arnaud.kleinpeter@gmail.com>
2014-09-11 11:32:05 +0200

    capture plus robuste de la ville du logement

--- app/js/controllers/foyer/logement.js
+++ app/js/controllers/foyer/logement.js
@@ -28,20 +28,35 @@ angular.module('ddsApp').controller('FoyerLogementCtrl', function($scope, $http,
     $scope.primoAccedantTooltip = 'Un primo-accédant est une personne (ou un ménage) qui n’a pas été propriétaire de sa résidence principale dans les deux années qui viennent de s’écouler.';
 
     $scope.updateCities = function() {
+        $scope.unknownCodePostal = false;
         $scope.cities = [];
         var codePostal = situation.logement.adresse.codePostal;
+        $scope.retrievingCities = false;
+        $scope.situation.logement.adresse.ville = null;
+
         if (9999 < codePostal && 100000 > codePostal) {
+            $scope.retrievingCities = true;
             var baseApi = '//public.opendatasoft.com/api/records/1.0/search?dataset=correspondance-code-insee-code-postal&format=jsonp&callback=JSON_CALLBACK&rows=1000&q=';
             $http.jsonp(baseApi + situation.logement.adresse.codePostal).then(function(result) {
-                $scope.cities = [];
-                result.data.records.forEach(function(record) {
-                    var field = 'nom_comm';
-                    $scope.cities.push(record.fields[field]);
-                });
-                situation.logement.adresse.ville = $scope.cities[0];
+                var records = result.data.records;
+                if (!records.length) {
+                    $scope.unknownCodePostal = true;
+                } else {
+                    $scope.unknownCodePostal = false;
+                    $scope.cities = [];
+                    result.data.records.forEach(function(record) {
+                        var field = 'nom_comm';
+                        $scope.cities.push(record.fields[field]);
+                    });
+                    situation.logement.adresse.ville = $scope.cities[0];
+                }
+            }, function() {
+                $scope.unknownCodePostal = true;
+            }).finally(function() {
+                $scope.retrievingCities = false;
             });
-        } else {
-            $scope.situation.logement.adresse.ville = null;
+        } else if (100000 <= codePostal) {
+            $scope.unknownCodePostal = true;
         }
     };
 

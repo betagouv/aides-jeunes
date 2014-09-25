017eacdcb1eeceb602e0e71837074da9d3bf1a9f
Arnaud Kleinpeter <arnaud.kleinpeter@gmail.com>
2014-09-25 15:08:09 +0200

    unglobalize marque blanche

--- app/js/marqueblanche.js
+++ app/js/marqueblanche.js
@@ -1,7 +1,10 @@
-var baseUrl = '/';
-if (window.location.pathname.indexOf('/secours-populaire') === 0) {
-    baseUrl += 'secours-populaire/';
-}
+(function() {
+    'use strict';
+    var baseUrl = '/';
+    if (window.location.pathname.indexOf('/secours-populaire') === 0) {
+        baseUrl += 'secours-populaire/';
+    }
 
-var docHead = document.getElementsByTagName('head')[0];
-docHead.insertAdjacentHTML('beforeend', '<base href="' + baseUrl + '">');
+    var docHead = document.getElementsByTagName('head')[0];
+    docHead.insertAdjacentHTML('beforeend', '<base href="' + baseUrl + '">');
+})();

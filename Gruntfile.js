46975650a9a70d11bb6df75baed26a34a4af0833
Arnaud Kleinpeter <arnaud.kleinpeter@gmail.com>
2014-09-29 11:13:08 +0200

    utilisation de ngAnnotate au lieu de ngmin

--- Gruntfile.js
+++ Gruntfile.js
@@ -310,7 +310,7 @@ module.exports = function (grunt) {
 
     // Allow the use of non-minsafe AngularJS files. Automatically makes it
     // minsafe compatible so Uglify does not destroy the ng references
-    ngmin: {
+    ngAnnotate: {
       dist: {
         files: [{
           expand: true,
@@ -511,7 +511,7 @@ module.exports = function (grunt) {
     'concurrent:dist',
     'autoprefixer',
     'concat',
-    'ngmin',
+    'ngAnnotate',
     'copy:dist',
     'cssmin',
     'uglify',

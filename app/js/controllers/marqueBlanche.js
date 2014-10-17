bd78e0bd9063c603e9efcef833cab79995d37bbb
Florian Delezenne <del.florian@gmail.com>
2014-10-17 17:13:16 +0200

    Ajout phrase d'intro service public de demain

--- app/js/controllers/marqueBlanche.js
+++ app/js/controllers/marqueBlanche.js
@@ -1,7 +1,20 @@
 'use strict';
 
-angular.module('ddsApp').controller('MarqueBlancheCtrl', function ($scope, $window) {
+angular.module('ddsApp').controller('MarqueBlancheCtrl', function ($scope, $window, $sessionStorage, $timeout) {
     if ($window.location.pathname.indexOf('/secours-populaire/') === 0) {
         $scope.logo = 'logo_secours_populaire.png';
     }
+
+    $scope.$storage = $sessionStorage;
+
+    angular.element(document).ready(function() {
+      $timeout(function() {
+        $scope.showIntro = !$scope.$storage.hideIntro;
+      }, 500);
+    });
+
+    $scope.hideIntro = function() {
+      $scope.showIntro = false;
+      $scope.$storage.hideIntro = true;
+    };
 });

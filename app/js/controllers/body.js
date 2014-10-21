0a12ee5e8290f694226d6841ef10515c5c9cfdf1
Arnaud Kleinpeter <arnaud.kleinpeter@gmail.com>
2014-10-21 15:22:59 +0200

    racourcissement header

---
+++ app/js/controllers/body.js
@@ -0,0 +1,20 @@
+'use strict';
+
+angular.module('ddsApp').controller('BodyCtrl', function($scope, $window, $sessionStorage, $timeout) {
+    if ($window.location.pathname.indexOf('/secours-populaire/') === 0) {
+        $scope.logo = 'logo_secours_populaire.png';
+    }
+
+    $scope.showIntro = false;
+
+    if (false !== $sessionStorage.showIntro) {
+        $timeout(function() {
+            $scope.showIntro = true;
+        }, 500);
+    }
+
+    $scope.hideIntro = function() {
+        $scope.showIntro = false;
+        $sessionStorage.showIntro = false;
+    };
+});

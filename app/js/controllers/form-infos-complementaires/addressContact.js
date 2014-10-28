421eaf107707da7308216c22cc7a5f33c970e91a
Florian Delezenne <del.florian@gmail.com>
2014-10-28 11:04:01 +0100

    Skip last step for cerfas

--- app/js/controllers/form-infos-complementaires/addressContact.js
+++ app/js/controllers/form-infos-complementaires/addressContact.js
@@ -4,6 +4,11 @@ angular.module('ddsApp').controller('FormInfosComplementairesAddressContactCtrl'
     $scope.situation = SituationService.restoreLocal();
 
     $scope.submit = function() {
-        $state.go('form_infos_complementaires_situation_pro', {droit: $stateParams.droit});
+        SituationService.update($scope.situation).then(function() {
+            $scope.situation.infosComplementairesCaptured = true;
+            $state.go('download_cerfa', {droit: $stateParams.droit});
+        });
+
+      // $state.go('form_infos_complementaires_situation_pro', {droit: $stateParams.droit});
     };
 });

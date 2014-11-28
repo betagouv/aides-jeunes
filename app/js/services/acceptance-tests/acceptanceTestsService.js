386a6a33519da1000f5729e896a479238734c803
Arnaud Kleinpeter <arnaud.kleinpeter@gmail.com>
2014-11-28 17:22:04 +0100

    migration du front pour la nouvelle présentation des résultats de la simulation dans les tests d'acceptance

--- app/js/services/acceptance-tests/acceptanceTestsService.js
+++ app/js/services/acceptance-tests/acceptanceTestsService.js
@@ -46,13 +46,13 @@ angular.module('acceptanceTests').factory('AcceptanceTestsService', function($q,
 
             var promise = $http.post('/api/acceptance-tests/' + test._id + '/executions', {});
             promise.then(function(result) {
-                var droits = result.data.droitsCalcules;
+                var droits = _.indexBy(result.data.droitsCalcules, 'code');
                 test.status = 'ok';
                 test.droitsAttendus.forEach(function(droit) {
                     var actualValue = droits[droit.id];
                     if (angular.isDefined(actualValue)) {
                         delete droits[droit.id];
-                        droit.actualValue = actualValue;
+                        droit.actualValue = actualValue.value;
                         if (_.isUndefined(droit.expectedValue)) {
                             droit.status = 'unknown';
                         } else if (droit.actualValue === droit.expectedValue) {
@@ -61,23 +61,33 @@ angular.module('acceptanceTests').factory('AcceptanceTestsService', function($q,
                             droit.status = 'ok';
                         } else if ((Math.abs(droit.actualValue - droit.expectedValue) / droit.expectedValue) < 0.1) {
                             droit.status = 'near';
-                            if (test.status !== 'ko') test.status = 'near';
+                            if (test.status !== 'ko') {
+                                test.status = 'near';
+                            }
                         } else {
                             droit.status = 'ko';
                             test.status = 'ko';
                         }
                     }
                 });
+
                 _.forEach(droits, function(value, id) {
-                    if (value) {
-                        test.droitsAttendus.push({ id: id, expectedValue: undefined, actualValue: value, status: 'unknown' });
+                    if (value.value) {
+                        test.droitsAttendus.push({
+                            id: id,
+                            expectedValue: undefined,
+                            actualValue: value.value,
+                            status: 'unknown'
+                        });
                     }
                 });
+
                 _.where(test.droitsAttendus, { status: undefined }).forEach(function(droit) {
                     droit.status = 'ko';
                     test.status = 'ko';
                     droit.actualValue = false;
                 });
+
                 if (test.status === 'ko') {
                     deferred.reject();
                 } else {

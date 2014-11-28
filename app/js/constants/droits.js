fe3da2078b60e8922e4bc35b8f11581c8f84fe87
Arnaud Kleinpeter <arnaud.kleinpeter@gmail.com>
2014-11-28 16:11:23 +0100

    ajout du droit bourse de collège

--- app/js/constants/droits.js
+++ app/js/constants/droits.js
@@ -98,5 +98,13 @@ angular.module('ddsCommon').constant('droitsDescription', [
         'description':'L\'allocation de solidarité spécifique (ASS) est une aide financière destinée aux personnes ayant épuisé leurs droits aux allocations chômage.',
         'imgSrc':'logo_ass.png',
         'link':'http://vosdroits.service-public.fr/particuliers/F12484.xhtml'
+    },
+    {
+        'id':'bourse_college',
+        'label':'Bourse des collèges',
+        'shortLabel':'Bourse collège',
+        'description':'La bourse des collèges est une aide destinée à favoriser la scolarité des collégiens. Elle est versée sous conditions de ressources.',
+        'imgSrc':'logo_education_nationale.png',
+        'link':'http://www.education.gouv.fr/cid88/les-aides-financieres-au-college.html#Les%20bourses%20de%20coll%C3%A8ge'
     }
 ]);

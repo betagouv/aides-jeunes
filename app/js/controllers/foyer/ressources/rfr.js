1b8561ed9dc9ec1b26a31b325a0d7106272b0c55
Arnaud Kleinpeter <arnaud.kleinpeter@gmail.com>
2014-12-22 16:54:27 +0100

    récap rfr 2012

--- app/js/controllers/foyer/ressources/rfr.js
+++ app/js/controllers/foyer/ressources/rfr.js
@@ -3,7 +3,7 @@
 angular.module('ddsApp').controller('FoyerRessourceRfrCtrl', function($scope, $state) {
     $scope.yearMoins2 = moment().subtract('years', 2).format('YYYY');
     $scope.yearMoins1 = moment().subtract('years', 1).format('YYYY');
-    $scope.rfr = 0;
+    $scope.rfr = $scope.situation.rfr || 0;
     $scope.situationFamilialeChanged = false;
 
     $scope.submit = function() {

2e0b85a2dcf45abb2b24f2c0116676aaa9fe9e1f
Arnaud Kleinpeter <arnaud.kleinpeter@gmail.com>
2015-01-08 16:22:43 +0100

    affichage de la valeur obtenue à coté de la valeur attendue dans le formulaire des tests

--- app/js/controllers/acceptance-tests/form.js
+++ app/js/controllers/acceptance-tests/form.js
@@ -19,11 +19,27 @@ angular.module('acceptanceTests').controller('FormCtrl', function($scope, $http,
     } else {
         $scope.test = { situation: $stateParams.situationId, expectedResults: [] };
         _.forEach(droitsObtenus, function(value, name) {
-            if (_.isBoolean(value) || (_.isNumber(value) && 0 !== value)) {
-                $scope.test.expectedResults.push({ ref: _.find($scope.droitsChoices, {id: name}), value: value });
+            if ((_.isBoolean(value) && true === value) || (_.isNumber(value) && 0 !== value)) {
+                $scope.test.expectedResults.push({
+                    ref: _.find($scope.droitsChoices, { id: name }),
+                    actualValue: value,
+                    value: value
+                });
             }
         });
     }
+
+    $scope.droitSelected = function(droit) {
+        droit.actualValue = droitsObtenus[droit.ref.id];
+        droit.value = droit.actualValue;
+    };
+
+    $scope.formatDroitValue = function(value) {
+        if (_.isNumber(value)) {
+            return '' + value + ' €';
+        }
+        return value ? 'Oui' : 'Non';
+    };
 
     $scope.removeDroit = function(droit) {
         var index = $scope.test.expectedResults.indexOf(droit);

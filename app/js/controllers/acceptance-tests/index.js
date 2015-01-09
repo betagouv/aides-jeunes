d993d8ad7aea948c1befc8a476d81c55d43f9390
Florian Delezenne <del.florian@gmail.com>
2015-01-09 18:06:14 +0100

    Feature flipping

--- app/js/controllers/acceptance-tests/index.js
+++ app/js/controllers/acceptance-tests/index.js
@@ -17,9 +17,9 @@ angular.module('acceptanceTests').controller('IndexCtrl', function($scope, $stat
     $scope.user = UserService.user();
 
     $scope.tabs = [
-        { heading: 'Liste des tests', route:'index.list' },
+        { heading: 'Liste des tests', route:'index.list' } /*,
         { heading: 'Timeline', route:'index.timeline' },
-        { heading: 'Statistiques', route:'index.stats' }
+        { heading: 'Statistiques', route:'index.stats' } */
     ];
 
     var extractSelectedFilters = function(selection) {

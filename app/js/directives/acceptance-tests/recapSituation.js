da16e064af467f170a4a7960955f76180a2d8165
Arnaud Kleinpeter <arnaud.kleinpeter@gmail.com>
2015-01-20 11:21:11 +0100

    réactivation récap ressources dans les tests

--- app/js/directives/acceptance-tests/recapSituation.js
+++ app/js/directives/acceptance-tests/recapSituation.js
@@ -139,6 +139,7 @@ angular.module('ddsCommon').directive('recapSituation', function($timeout, resso
                 $scope.logement.ville = logement.adresse.ville;
             };
 
+            debugger;
             $scope.individus = _.map(situation.individus, mapIndividu);
 
             if (situation.patrimoine) {

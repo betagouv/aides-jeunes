c27cdda96035442a09bffb31588633958a65a258
Arnaud Kleinpeter <arnaud.kleinpeter@gmail.com>
2015-02-13 14:50:39 +0100

    fix build fichiers pour la prod

--- Gruntfile.js
+++ Gruntfile.js
@@ -225,7 +225,7 @@ module.exports = function (grunt) {
     // concat, minify and revision files. Creates configurations in memory so
     // additional tasks can operate on them
     useminPrepare: {
-      html: ['<%= yeoman.app %>/views/front.html', '<%= yeoman.app %>/views/back.html', '<%= yeoman.app %>/views/acceptance-tests.html'],
+      html: ['<%= yeoman.app %>/views/front.html', '<%= yeoman.app %>/views/embed.html'],
       options: {
         dest: '<%= yeoman.dist %>'
       }

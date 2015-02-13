d46c76420d4eee4b1999f5bc93ed52042b96a8dc
Florian Delezenne <del.florian@gmail.com>
2015-02-13 14:19:56 +0100

    Fix constants

--- app/js/ludwig-constants.js
+++ app/js/ludwig-constants.js
@@ -2,6 +2,6 @@
 
  angular.module('ludwigConstants', [])
 
-.constant('config', {scenarioTemplate:'app/js/directives/tests/scenario.js',baseUrl:'/tests',constants:'app/ludwig-constants.js',showUrls:true})
+.constant('config', {scenarioTemplate:'app/js/directives/tests/scenario.js',baseUrl:'/tests',constants:'app/js/ludwig-constants.js',showUrls:true})
 
 ;

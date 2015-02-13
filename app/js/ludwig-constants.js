2d1c6cbe5929014662e722f00296ac2605505988
Arnaud Kleinpeter <arnaud.kleinpeter@gmail.com>
2015-02-13 14:37:30 +0100

    ignore fichier généré ludwig-constants.js

--- app/js/ludwig-constants.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
- angular.module('ludwigConstants', [])
-
-.constant('config', {scenarioTemplate:'app/js/directives/ludwigEmbed.js',baseUrl:'/tests',constants:'app/js/ludwig-constants.js',showUrls:true})
-
-;

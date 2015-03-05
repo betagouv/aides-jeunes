810b3aae60ee124af6bd2f8917abbc00c3f3cc8e
Arnaud Kleinpeter <arnaud.kleinpeter@gmail.com>
2015-03-05 16:53:56 +0100

    fix oubli ajout controller contribuez

---
+++ app/js/controllers/contribuez.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('ddsApp').controller('ContribuezCtrl', function() {
+  // copié-collé code tweeter
+  (function(d,s,id){
+    var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';
+    if(!d.getElementById(id)){
+      js=d.createElement(s);
+      js.id=id;
+      js.src=p+'://platform.twitter.com/widgets.js';
+      fjs.parentNode.insertBefore(js,fjs);
+    }
+  })(document,'script','twitter-wjs');
+});

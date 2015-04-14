db6f60eb36b618d1b5ad1afe442904c4b086432f
Matti Schneider <matti.schneider@data.gouv.fr>
2015-04-14 08:36:57 +0200

    Update Ludwig to v1.0

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var ludwigConfig = require('./ludwig.json');
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-ludwigConfig.serverConfig = { mesAidesRootUrl: process.env.MES_AIDES_ROOT_URL || 'http://localhost:9000' };
+ludwigConfig.mesAidesRootUrl = process.env.MES_AIDES_ROOT_URL || 'http://localhost:9000';
 
 // Setup Express
 var app = express();
@@ -21,7 +21,7 @@ if ('production' === env) {
 
 // Setup app
 app.use('/api', require('sgmap-mes-aides-api'));
-require('ludwig-ui')(app, __dirname, ludwigConfig);
+app.use(ludwigConfig.baseUrl, require('ludwig-ui')(ludwigConfig));
 require('./')(app);
 
 if ('development' === env) {

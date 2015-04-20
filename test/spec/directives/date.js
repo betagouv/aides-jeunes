1201e37887e229d89f5baf49cf53d31337a8dc8a
Arnaud Kleinpeter <arnaud.kleinpeter@gmail.com>
2015-04-20 15:29:14 +0200

    ajout tests directive dds-date

---
+++ test/spec/directives/date.js
@@ -0,0 +1,32 @@
+describe('directives', function() {
+  var $scope, form;
+
+  beforeEach(module('ddsApp'));
+  beforeEach(module('templates'));
+
+  beforeEach(inject(function($compile, $rootScope) {
+    $scope = $rootScope;
+    var element = angular.element(
+      '<form name="form">' +
+      '<input ng-model="model.date" name="date" dds-date>' +
+      '</form>'
+    );
+    $scope.model = { date: null }
+    $compile(element)($scope);
+    form = $scope.form;
+  }));
+
+  describe('integer', function() {
+    it('should pass with a valid date', function() {
+      form.date.$setViewValue('12/08/1980');
+      $scope.$digest();
+      expect(form.date.$valid).toBe(true);
+    });
+
+    it('should not pass with an invalid date', function() {
+      form.date.$setViewValue('99/99/1980');
+      $scope.$digest();
+      expect(form.date.$valid).toBe(false);
+    });
+  });
+});

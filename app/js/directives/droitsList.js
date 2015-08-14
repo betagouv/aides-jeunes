43a15370191caecb8dc8ea6e8585a37847b5a086
fpagnoux <fpagnoux@octo.com>
2015-08-14 13:14:34 +0200

    Create droitsList component

---
+++ app/js/directives/droitsList.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('ddsApp').directive('droitsList', function() {
+    return {
+        restrict: 'E',
+        templateUrl: 'partials/droits-list.html',
+        scope: {
+            droits: '='
+        }
+    };
+});

29013154c031796b8e542b5844fff62f5f32ed07
Matti Schneider <matti.schneider@data.gouv.fr>
2015-09-21 15:55:19 +0200

    Extract computed aides from OpenFisca response

--- openfisca/parse.js
+++ openfisca/parse.js
@@ -1,17 +1,36 @@
 import AIDES from '../config/aides';
 
 
-export function getGivenAides(situation) {
-    return [];
+export function alreadyHasAide(aideId, situation) {
+    var declared = situation.scenarios[0].test_case.familles[0];
+
+    if (! declared[aideId])
+        return false;
+
+    for (let period in declared[aideId])
+        if (declared[aideId][period])
+            return true;
+}
+
+export function getRequestedPeriod(situation) {
+    return situation.scenarios[0].period  // navigate OpenFisca structure
+                    .match(/[\d-]+/)[0];  // could be formatted as 'month:2015-01', and we just want '2015-01'
 }
 
 export function reverseMap(openFiscaResponse, situation) {
     let computed = openFiscaResponse.value[0].familles[0],
+        period = getRequestedPeriod(situation),
         result = {};
 
-    return result;
-};
+    for (let aideId in AIDES) {
+        if (alreadyHasAide(aideId, situation))
+            continue;
 
-export function toOpenFiscaPeriod(date) {
-    return moment(date).format('YYYY-MM');
+        let value = computed[aideId] && computed[aideId][period];
+
+        if (value)
+            result[aideId] = value;
+    }
+
+    return result;
 }
--- test/unit/parse.js
+++ test/unit/parse.js
@@ -2,16 +2,14 @@ import expect from 'expect.js';
 
 import AIDES from '../../config/aides';
 import {
-    getGivenAides,
+    alreadyHasAide,
     reverseMap,
-    toOpenFiscaPeriod,
 } from '../../openfisca/parse';
 
 
 describe('parse', () => {
     const OPENFISCA_RESPONSE = require('../data/openfisca-response.json'),
-          SITUATION = require('../data/situation.json'),
-          SITUATION_WITH_AIDES = require('../data/situation-with-aspa.json');
+          SITUATION = require('../data/situation.json');
 
     describe('reverseMap', () => {
         let subject;
@@ -33,23 +31,16 @@ describe('parse', () => {
         });
     });
 
-    describe('getGivenAides', () => {
+    describe('alreadyHasAide', () => {
+        const SITUATION_WITH_AIDE = require('../data/situation-with-aspa.json');
+
         it('should ignore aides that are set to 0 in a situation', () => {
-            expect(getGivenAides(SITUATION)).to.eql([]);
+            for (let aideId in AIDES)
+                expect(alreadyHasAide(aideId, SITUATION)).to.not.be.ok();
         });
 
         it('should extract aides that are not set to 0 from a situation', () => {
-            expect(getGivenAides(SITUATION_WITH_AIDES)).to.eql([ 'aspa' ]);
-        });
-    });
-
-    describe('toOpenFiscaPeriod', () => {
-        it('should format a date', () => {
-            expect(toOpenFiscaPeriod(new Date('November 12, 2015'))).to.equal('2015-11');
-        });
-
-        it('should pad months', () => {
-            expect(toOpenFiscaPeriod(new Date('January 2, 2015'))).to.equal('2015-01');
+            expect(alreadyHasAide('aspa', SITUATION_WITH_AIDE)).to.be.ok();
         });
     });
 });

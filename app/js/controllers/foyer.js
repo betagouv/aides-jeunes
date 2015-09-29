2bdf3ee30d6960f966f07820b67d570ba089de01
Florian Pagnoux <Florian.Pagnoux@gmail.com>
2015-09-29 15:17:08 +0200

    Move comment

--- app/js/controllers/foyer.js
+++ app/js/controllers/foyer.js
@@ -32,7 +32,8 @@ angular.module('ddsApp').controller('FoyerCtrl', function($scope, $state, $state
             situation.individus = _.filter(situation.individus, function(individu) {
                 return 'conjoint' !== individu.role;
             });
-        } // En cas de parent isolé, on pose une question supplémentaire
+        }
+        // En cas de parent isolé, on pose une question supplémentaire
         if (! SituationService.hasEnfant($scope.situation)) {
             $state.go('foyer.logement');
         }

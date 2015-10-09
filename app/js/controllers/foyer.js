ae27bf87d02d7b7f5222bf9df422bca2f5a0bdb5
Florian Pagnoux <Florian.Pagnoux@gmail.com>
2015-10-09 11:53:18 +0200

    Split events individu.pasDeConjoint and individu.conjoint

--- app/js/controllers/foyer.js
+++ app/js/controllers/foyer.js
@@ -21,18 +21,16 @@ angular.module('ddsApp').controller('FoyerCtrl', function($scope, $state, $state
     });
 
     $scope.$on('individu.conjoint', function(e, conjoint) {
-        if (conjoint) {
-            // si le conjoint existait déjà avant, on l'écrase
-            if (_.find(situation.individus, { role: 'conjoint' })) {
-                situation.individus[1] = conjoint;
-            } else { // on insère le conjoint juste derrière le demandeur dans l'array des individus
-                situation.individus.splice(1, 0, conjoint);
-            }
-        } else { // on supprime l'éventuel conjoint qui existait avant
-            situation.individus = _.filter(situation.individus, function(individu) {
-                return 'conjoint' !== individu.role;
-            });
-        }
+        SituationService.setConjoint(situation, conjoint);
+        $state.go('foyer.logement');
+    });
+
+    $scope.$on('individu.pasDeConjoint', function() {
+        // on supprime l'éventuel conjoint qui existait avant
+        situation.individus = _.filter(situation.individus, function(individu) {
+            return 'conjoint' !== individu.role;
+        });
+
         // En cas de parent isolé, on pose une question supplémentaire
         if (! SituationService.hasEnfant($scope.situation)) {
             $state.go('foyer.logement');

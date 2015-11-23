0c13404937ec7974139555f838d91b991b8e1b8b
Matti Schneider <matti.schneider@data.gouv.fr>
2015-11-23 18:17:44 +0100

    Set birth date in situation

--- routes.js
+++ routes.js
@@ -1,3 +1,5 @@
+import moment from 'moment';
+
 import loadConstYaml from './lib/loadConstYaml';
 import compute from './openfisca/compute';
 import { reverseMap } from './openfisca/parse';
@@ -32,7 +34,15 @@ export default [
     method: 'POST',
     path: '/situation',
     handler: (request, reply) => {
-        let situation = require('./test/mock/situation.json');
+        const situation = require('./test/mock/situation.json');
+
+        let birthdate = moment(request.payload.birthdate, [ 'DD/MM/YY', 'DD/MM/YYYY' ], true);
+
+        if (! birthdate.isValid()) {
+            return reply.redirect('/date-naissance');
+        }
+
+        situation.scenarios[0].test_case.individus[0].birth = birthdate.format('YYYY-MM-DD');
 
         view(reply, 'situation', {
             situation: JSON.stringify(situation),

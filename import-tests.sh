8c4670baa966ae4c2fa92f2dfbaeb4c9a937a215
Matti Schneider <matti.schneider@data.gouv.fr>
2015-12-17 18:20:47 +0100

    Do not drop database contents on import

--- import-tests.sh
+++ import-tests.sh
@@ -11,7 +11,7 @@ LOCAL_DUMP_FOLDER='./dump'
 
 cd `dirname $0`
 
-read -n 1 -p "Are you POSITIVE you want to replace ALL contents of the $DATABASE database with contents from the $USER one?  [y/N]" sure
+read -n 1 -p "Are you POSITIVE you want to merge contents of the local $DATABASE database with contents from the distant $USER one?  [y/N]" sure
 
 if [[ $sure != 'y' ]]
 then
@@ -25,4 +25,4 @@ ssh $USER@sgmap.fr "mongodump --db $DATABASE && mv dump $DISTANT_DUMP_FOLDER && 
 rm -rf $LOCAL_DUMP_FOLDER
 scp -r $USER@sgmap.fr:$DISTANT_DUMP_FOLDER/$DATABASE $LOCAL_DUMP_FOLDER
 gunzip -r $LOCAL_DUMP_FOLDER
-mongorestore --drop --db $DATABASE $LOCAL_DUMP_FOLDER
+mongorestore --db $DATABASE $LOCAL_DUMP_FOLDER

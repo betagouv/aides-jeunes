f350ddcf245878b52678393c4b105e1365d07f8f
Matti Schneider <matti.schneider@data.gouv.fr>
2016-01-18 15:55:31 +0100

    Support birthdate on standards-abiding browsers
    Chrome formats dates properly, broke Moment parsing

--- front/birthdate.js
+++ front/birthdate.js
@@ -8,7 +8,11 @@ import store from './store';
 
 
 export function set(date) {
-    const birthdate = moment(date, [ 'DD/MM/YY', 'DD/MM/YYYY' ], true);
+    const birthdate = moment(date, [
+        'DD/MM/YY',  // support shortcut; TODO: should interpret 20 as 1920, not 2020 (Moment defaults to 21st century before 69)
+        'DD/MM/YYYY',
+        'YYYY-MM-DD',  // browsers that actually support the date element will always format date in ISO format
+    ], true);  // strict: don't let Moment be ambiguous and parse partially-typed dates
 
     if (! birthdate.isValid()) {
         return store.dispatch(setError('individus[0].birth', 'invalid', date));

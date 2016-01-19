414ab6e52493402bbf8e2f45404f59bf705dab95
Matti Schneider <matti.schneider@data.gouv.fr>
2016-01-19 18:29:13 +0100

    Make questions required

--- front/housing.js
+++ front/housing.js
@@ -2,6 +2,7 @@ import objectPath from 'object-path-immutable';
 
 import {
     updateOpenfiscaSituation,
+    setError,
 } from './actions';
 import store from './store';
 
@@ -9,6 +10,12 @@ import store from './store';
 const NAME = 'menages.0.statut_occupation';
 
 export function set(value) {
+    if (! value)
+        return store.dispatch(setError(NAME, 'required', value));
+
+    if (! (value > 0 && value <= 8))
+        return store.dispatch(setError(NAME, 'invalid', value));
+
     const situation = objectPath.set(store.getState().openfiscaSituation, NAME, value);
 
     return store.dispatch(updateOpenfiscaSituation(situation));

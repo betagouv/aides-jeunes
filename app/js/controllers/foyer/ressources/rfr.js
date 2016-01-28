b22e5e982461cdf5126966bdfbf34173cdd0ad55
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2016-01-27 19:27:45 -0500

    Don't force rfr to zero

--- app/js/controllers/foyer/ressources/rfr.js
+++ app/js/controllers/foyer/ressources/rfr.js
@@ -1,9 +1,10 @@
+
 'use strict';
 
 angular.module('ddsApp').controller('FoyerRessourceRfrCtrl', function($scope) {
     $scope.yearMoins2 = moment($scope.situation.dateDeValeur).subtract('years', 2).format('YYYY');
     $scope.yearMoins1 = moment($scope.situation.dateDeValeur).subtract('years', 1).format('YYYY');
-    $scope.rfr = $scope.situation.rfr || 0;
+    $scope.rfr = $scope.situation.rfr;
 
     $scope.submit = function() {
         $scope.$emit('rfr', $scope.rfr);

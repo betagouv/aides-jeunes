f8b75a7a47e708b580e56901d36f233daa76e4a9
Matti Schneider <matti.schneider@data.gouv.fr>
2016-02-03 10:45:06 +0100

    Improve readability of validation messages updates

--- front/lib/redux-semantic-form.js
+++ front/lib/redux-semantic-form.js
@@ -2,10 +2,10 @@ store.subscribe(function updateErrorMessages() {
     const state = store.getState();
 
     Array.prototype.forEach.call(document.querySelectorAll('[data-for]'), message => {
+        const shouldBeVisible = state.error && (state.error.id == message.attributes['data-for'].value);
+
         message.classList[
-            message.attributes['data-for'].value == (state.error && state.error.id)
-            ? 'add'
-            : 'remove'
+            shouldBeVisible ? 'add' : 'remove'
         ]('ui', 'form', 'error');
     });
 });

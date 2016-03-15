64ed3b8d9bca9c2d1f170350e990d9624bb8b6e9
Matti Schneider <matti.schneider@data.gouv.fr>
2016-03-15 15:27:55 +0100

    Switch nationality to Question model
    Use proper routing

--- front/questions/nationality.js
+++ front/questions/nationality.js
@@ -1,27 +1,13 @@
-import objectPath from 'object-path-immutable';
-
-import store from '../store';
-import {
-    createOpenfiscaSituationUpdateAction,
-    createErrorAction,
-} from '../actions';
+import Question from './Question';
 
 
-/**
- * @param {String} property  Property path of the OpenFisca situation.
- * @param {String} nationality  Enum value to set.
- * @return {Action} A Redux action to be dispatched to the store.
- */
-export function update(property, nationality) {
-    if (nationality === '')  // have to validate this manually because "required" attribute cannot be set on a radio group
-        return createErrorAction(name, 'required', nationality);
+export default new Question({
+    format(ressortissant_eee) {
+        return ressortissant_eee == 'true';
+    },
 
-    const situation = objectPath.set(store.getState().openfiscaSituation, property, nationality == 'true');
-
-    return createOpenfiscaSituationUpdateAction(situation);
-}
-
-export function next(state = store.getState()) {
-    if (! state.openfiscaSituation.individus[0].ressortissant_eee)
-        return 'titre-sejour';
-}
+    route(state) {
+        if (! state.openfiscaSituation.individus[0].ressortissant_eee)
+            return 'titre-sejour';
+    },
+});

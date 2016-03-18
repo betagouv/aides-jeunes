d1013405a6a28dad8596bad40238e80012c22ba4
Matti Schneider <matti.schneider@data.gouv.fr>
2016-03-18 18:22:05 +0100

    Switch stay permit to Question model

--- front/questions/stay-permit.js
+++ front/questions/stay-permit.js
@@ -1,19 +1,4 @@
-import objectPath from 'object-path-immutable';
-
-import store from '../store';
-import {
-    createOpenfiscaSituationUpdateAction,
-    createErrorAction,
-} from '../actions';
+import Question from './Question';
 
 
-/**
- * @param {String} property  Property path of the OpenFisca situation.
- * @param {String} stayPermitYearsCount  Amount of years since a valid stay permit has been owned by the user.
- * @return {Action} A Redux action to be dispatched to the store.
- */
-export function update(property, stayPermitYearsCount) {
-    const situation = objectPath.set(store.getState().openfiscaSituation, property, stayPermitYearsCount);
-
-    return createOpenfiscaSituationUpdateAction(situation);
-}
+export default new Question();

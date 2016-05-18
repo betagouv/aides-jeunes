644df5e165a36086e86f2ca3109dd8822f6da8c4
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2016-05-18 17:31:37 +0200

    Transmit result to the API

--- app/js/services/impactStudyService.js
+++ app/js/services/impactStudyService.js
@@ -2,6 +2,8 @@
 
 angular.module('ddsApp').service('ImpactStudyService', function($http, $sessionStorage) {
 
+    var ENDPOINT = 'https://mes-droits.fr/sr';
+
     function getSessionId() {
             $sessionStorage.sessionId = $sessionStorage.sessionId ? $sessionStorage.sessionId : uuid.v1();
             return $sessionStorage.sessionId;
@@ -16,6 +18,20 @@ angular.module('ddsApp').service('ImpactStudyService', function($http, $sessionS
             delete openfiscaResults['paris_logement_aspeh'];
             delete openfiscaResults['paris_logement_plfm'];
             delete openfiscaResults['paris_logement_psol'];
+
+            var result = _.merge(openfiscaResults, {
+                'session_id' : getSessionId(),
+                'eid': eid,
+                'postCode': situation.logement.adresse.codePostal
+            });
+
+            $http.post(ENDPOINT, result).success(function(data) {
+               console.log("Results transmitted to the Survey API");
+               console.log(data);
+            }).error(function(error) {
+                console.log("Error while transmitting results to the Survey API");
+                console.log(error);
+            });
         }
     };
 });

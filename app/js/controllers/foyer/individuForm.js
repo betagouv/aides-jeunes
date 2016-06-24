015e11a77751cce2dffcf7111ac6cdd018d3fc28
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2016-06-24 18:56:48 +0200

    Revert "Set hasRessources to false by default for enfants"
    7b9a09e59217ce582b9c6581f6462698be0a3ce8

--- app/js/controllers/foyer/individuForm.js
+++ app/js/controllers/foyer/individuForm.js
@@ -37,30 +37,24 @@ angular.module('ddsApp').controller('FoyerIndividuFormCtrl', function($scope, in
         }
     ];
 
-    function getDefaultIndividu(role) {
-        var default_individu = {
-            nationalite: 'fr',
-            assPreconditionRemplie: false,
-            scolarite: 'college',
-            tauxIncapacite: 'plus80',
-            boursier: false,
-            aCharge: true,
-            place: false,
-            role: role,
-            autresRevenusTnsActiviteType: 'bic',
-            microEntrepriseActiviteType: 'bic',
-            perteAutonomie: false,
-            autoEntrepreneurActiviteType: 'bic',
-            specificSituations: []
-        };
-        if (role == 'enfant') {
-            default_individu.hasRessources = false;
-        }
-        return default_individu;
-    }
+    var DEFAULT_INDIVIDU = {
+        nationalite: 'fr',
+        assPreconditionRemplie: false,
+        scolarite: 'college',
+        tauxIncapacite: 'plus80',
+        boursier: false,
+        aCharge: true,
+        place: false,
+        role: individuRole,
+        autresRevenusTnsActiviteType: 'bic',
+        microEntrepriseActiviteType: 'bic',
+        perteAutonomie: false,
+        autoEntrepreneurActiviteType: 'bic',
+        specificSituations: []
+    };
 
     var isIndividuParent = IndividuService.isRoleParent(individuRole);
-    $scope.individu = isIndividuParent && _.find($scope.situation.individus, { role: individuRole }) || getDefaultIndividu(individuRole);
+    $scope.individu = isIndividuParent && _.find($scope.situation.individus, { role: individuRole }) || _.cloneDeep(DEFAULT_INDIVIDU);
 
     $scope.individu.specificSituations.forEach(function(specificSituation) {
         $scope.selectedStatuts[specificSituation.situation] = true;

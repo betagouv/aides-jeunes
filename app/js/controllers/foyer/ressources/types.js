1f37ac3b9ecd1505f127af0964dbdae82a84c507
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2016-06-27 14:56:05 +0200

    Minor refactor

--- app/js/controllers/foyer/ressources/types.js
+++ app/js/controllers/foyer/ressources/types.js
@@ -11,8 +11,32 @@ angular.module('ddsApp').controller('FoyerRessourceTypesCtrl', function($scope, 
     });
     $scope.ressourceTypesByCategories = _.groupBy(filteredRessourceTypes, 'category');
 
+    var DEFAULT_RESOURCE = {
+        montantAnnuel: 0,
+        caAnnuel: 0,
+        montantsMensuels: [0, 0, 0],
+        onGoing: true,
+    };
+
+    $scope.applySelectedRessources = function (selectedRessourceTypes) {
+        var currentRessources = _.clone($scope.ressources);
+        // $scope.ressources is referenced by parent controller, we need to keep the reference
+        $scope.ressources.length = 0;
+        ressourceTypes.forEach(function(ressourceType) {
+            if (! selectedRessourceTypes[ressourceType.id]) {
+                return;
+            }
+            var ressource = _.find(currentRessources, { type: ressourceType });
+            if (! ressource) {
+                ressource = _.cloneDeep(DEFAULT_RESOURCE);
+                ressource.type = ressourceType;
+            }
+            $scope.ressources.push(ressource);
+        });
+    };
+
     $scope.submit = function() {
-        $scope.applySelectedRessources($scope.selectedRessourceTypes, $scope.ressources);
+        $scope.applySelectedRessources($scope.selectedRessourceTypes);
         if (_.some($scope.selectedRessourceTypes)) {
             $state.go('foyer.ressources.individu.montants');
         } else {

35c8b1e109c4dc704f67903e14b2db5d5a9a249d
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2016-06-28 17:41:32 +0200

    Display ressources recap progressively

--- app/js/controllers/foyer/ressources/ressources.js
+++ app/js/controllers/foyer/ressources/ressources.js
@@ -12,10 +12,11 @@ angular.module('ddsApp').controller('FoyerRessourcesCtrl', function($scope, $sta
     };
 
     $scope.declareNextIndividuResources = function (lastIndividuDeclaredIndex) {
+        $scope.$parent.$broadcast('ressourcesUpdated');
         var nextIndividuIndex = lastIndividuDeclaredIndex + 1;
         var nextIndividu = $scope.sortedIndividus[nextIndividuIndex];
         if (! nextIndividu) {
-            $scope.$emit('ressources');
+            $state.go('foyer.pensionsAlimentaires');
             return;
         }
         if (nextIndividu.role == 'conjoint' || nextIndividu.hasRessources) {

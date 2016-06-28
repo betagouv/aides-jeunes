71bf1f258308a9360cf2c417afb575b1764078cb
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2016-06-28 18:57:26 +0200

    Don't register empty resources

--- app/js/services/ressourceService.js
+++ app/js/services/ressourceService.js
@@ -8,25 +8,28 @@ angular.module('ddsApp').factory('RessourceService', function(SituationService, 
             [2, 1, 0].forEach(function(i) {
                 var montant = ressource.montantsMensuels[i];
                 somme3DerniersMois += montant;
-                individu.ressources.push({
+                if (montant) {
+                    individu.ressources.push({
                     type: ressource.type.id,
                     periode: months[i].id,
                     montant: montant
                 });
-
+                }
             });
 
             // injection du montant annuel étalé sur les 9 mois restants
             var montantMensuelEtale = (ressource.montantAnnuel - somme3DerniersMois) / 9;
-            for (var j = 0; j < 9; j++) {
-                var periode = moment(dateDeValeur).subtract(4 + j, 'months').format('YYYY-MM');
-                individu.ressources.push({
-                    type: ressource.type.id,
-                    periode: periode,
-                    montant: montantMensuelEtale
-                });
-                individu.ressourcesYearlyApproximation = individu.ressourcesYearlyApproximation || {};
-                individu.ressourcesYearlyApproximation[ressource.type.id] = ressource.montantAnnuel;
+            if (montantMensuelEtale) {
+                for (var j = 0; j < 9; j++) {
+                    var periode = moment(dateDeValeur).subtract(4 + j, 'months').format('YYYY-MM');
+                    individu.ressources.push({
+                        type: ressource.type.id,
+                        periode: periode,
+                        montant: montantMensuelEtale
+                    });
+                    individu.ressourcesYearlyApproximation = individu.ressourcesYearlyApproximation || {};
+                    individu.ressourcesYearlyApproximation[ressource.type.id] = ressource.montantAnnuel;
+                }
             }
 
             if (! ressource.onGoing) {
@@ -37,14 +40,16 @@ angular.module('ddsApp').factory('RessourceService', function(SituationService, 
     function applyYearlyRessource (individu, ressource, dateDeValeur) {
             var montant = ressource.montantAnnuel;
             var periode = moment(dateDeValeur).subtract(1, 'year').format('YYYY');
-            individu.ressources.push({
-                type: ressource.type.id,
-                periode: periode,
-                montant: montant
-            });
+            if (montant) {
+                individu.ressources.push({
+                    type: ressource.type.id,
+                    periode: periode,
+                    montant: montant
+                });
+            }
 
             // For  'professions libérales et entrepreneurs', we capture the 'CA' as well as the 'benefice'
-            if (ressource.type.id == 'autresRevenusTns') {
+            if (ressource.type.id == 'autresRevenusTns' && ressource.caAnnuel) {
                 individu.ressources.push({
                     type: 'caAutresRevenusTns',
                     periode: periode,

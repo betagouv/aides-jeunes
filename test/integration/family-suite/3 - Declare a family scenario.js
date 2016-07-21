fc780d9b4329347a98f1956e6705bc6220752ee3
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2016-07-21 11:51:54 +0200

    Use fixtures for birthdates

--- test/integration/family-suite/3 - Declare a family scenario.js
+++ test/integration/family-suite/3 - Declare a family scenario.js
@@ -2,24 +2,24 @@ description: 'Declare a family with 2 parents and 2 children.',
 
 steps: [
     EnfantsFormComponent.addChild(),
-    IndividualFormComponent.setBirthDateInput('01/04/2010'),
+    IndividualFormComponent.setBirthDateInput(FIRST_CHILD_BIRTHDATE),
     IndividualFormComponent.setFirstNameInput('Alice'),
     IndividualFormComponent.submit(),
 
     EnfantsFormComponent.addChild(),
-    IndividualFormComponent.setBirthDateInput('01/08/2012'),
+    IndividualFormComponent.setBirthDateInput(SECOND_CHILD_BIRTHDATE),
     IndividualFormComponent.setFirstNameInput('Bob'),
     IndividualFormComponent.submit(),
     EnfantsFormComponent.submit(),
 
     ConjointFormComponent.declareCouple(),
-    IndividualFormComponent.setBirthDateInput('04/05/1979'),
+    IndividualFormComponent.setBirthDateInput(CONJOINT_BIRTHDATE),
     IndividualFormComponent.submit(),
 
     {
-        'RecapComponent.firstPersonBirthdate': '21 janvier 1981',
-        'RecapComponent.secondPersonBirthdate': '1 avril 2010',
-        'RecapComponent.thirdPersonBirthdate': '1 août 2012',
-        'RecapComponent.fourthPersonBirthdate': '4 mai 1979',
+        'RecapComponent.firstPersonBirthdate': DEMANDEUR_BIRTHDATE_PLAIN_TEXT,
+        'RecapComponent.secondPersonBirthdate': FIRST_CHILD_BIRTHDATE_PLAIN_TEXT,
+        'RecapComponent.thirdPersonBirthdate': SECOND_CHILD_BIRTHDATE_PLAIN_TEXT,
+        'RecapComponent.fourthPersonBirthdate': CONJOINT_BIRTHDATE_PLAIN_TEXT,
     }
 ]
---
+++ test/integration/family-suite/BirthdateFixture.js
@@ -0,0 +1,1 @@
+../base-suite/BirthdateFixture.js

51988200d80aefb67bcfcc6cb810092d645e4bb6
Matti Schneider <matti.schneider@beta.gouv.fr>
2016-08-04 15:47:38 +0200

    Pass integration tests under Edge
    Edge WebDriver adds trailing space to result link texts

--- test/integration/base-suite/8 - Results scenario.js
+++ test/integration/base-suite/8 - Results scenario.js
@@ -5,7 +5,7 @@ steps: [
         'ResultComponent.title': /Résultats/,
         'ResultComponent.prestationName': PRESTATION_NAME,
         'ResultComponent.prestationMontant': /\d+ €/,
-        'ResultComponent.prestationMontantDetail': '/ mois',
+        'ResultComponent.prestationMontantDetail': /mois/,
         'ResultComponent.prestationYM2Warning': HAS_PRESTATION_WARNING,
         'ResultComponent.greyedPrestation': HAS_PRESTATION_WARNING,
         'ResultComponent.declareYM2ResourcesLink': true,

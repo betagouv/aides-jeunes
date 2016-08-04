00a31174434097eaf14a5f329b19e0cf53c0e6b8
Matti Schneider <matti.schneider@beta.gouv.fr>
2016-08-04 14:32:32 +0200

    Replace homemade date live formatter by Cleave

--- app/js/directives/dateAutocomplete.js
+++ app/js/directives/dateAutocomplete.js
@@ -3,39 +3,10 @@
 angular.module('ddsApp').directive('dateAutocomplete', function() {
     return {
         restrict: 'A',
-        require: 'ngModel',
-        link: function(scope, element, attrs, ngModel) {
-            var lastViewValue = '';
-            ngModel.$parsers.unshift(function(viewValue) {
-                if (viewValue.length > 10) {
-                    var maxLengthValue = viewValue.substring(0, 10);
-                    ngModel.$setViewValue(maxLengthValue);
-                    ngModel.$render();
-
-                    return maxLengthValue;
-                }
-
-                var previousValue = lastViewValue;
-                lastViewValue = viewValue;
-
-                var index = viewValue.lastIndexOf('//');
-                if (-1 !== index && index === viewValue.length - 2) {
-                    var newValue = viewValue.substring(0, viewValue.length - 1);
-                    ngModel.$setViewValue(newValue);
-                    ngModel.$render();
-
-                    return newValue;
-                }
-
-                if (! _.contains([2, 5], viewValue.length)) return viewValue;
-                if (viewValue.length !== previousValue.length + 1) return viewValue;
-                if (previousValue.lastIndexOf('/') === previousValue.length - 1) return viewValue;
-                if (viewValue.lastIndexOf('/') === viewValue.length - 1) return viewValue;
-
-                ngModel.$setViewValue(viewValue + '/');
-                ngModel.$render();
-
-                return viewValue + '/';
+        link: function(scope, element) {
+            new Cleave(element, {
+                date: true,
+                datePattern: ['d', 'm', 'Y']
             });
         }
     };

b13b4276f8f94b550176d7844f2ff96762c8df45
Matti Schneider <matti.schneider@beta.gouv.fr>
2016-08-11 18:06:54 +0200

    Don't leak Cleave event listeners

--- app/js/directives/dateAutocomplete.js
+++ app/js/directives/dateAutocomplete.js
@@ -4,10 +4,12 @@ angular.module('ddsApp').directive('dateAutocomplete', function() {
     return {
         restrict: 'A',
         link: function(scope, element) {
-            new Cleave(element, {
+            var formatter = new Cleave(element, {
                 date: true,
                 datePattern: ['d', 'm', 'Y']
             });
+
+            element.on('$destroy', formatter.destroy.bind(formatter));  // don't leak event listeners
         }
     };
 });

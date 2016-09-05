189a2b66761643a1f63c4cf8f52a481cdfa9c46c
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2016-09-05 18:25:22 +0200

    Merge datedirectives
    (ddsDate, ddsDateMonthYear and dateAutocomplete)

--- app/js/directives/date.js
+++ app/js/directives/date.js
@@ -1,13 +1,31 @@
 'use strict';
 
+var FORMATS = {
+    'JJ/MM/AAAA': {
+        acceptedFormats: ['DD/MM/YY', 'DD/MM/YYYY'],
+        outputFormat: 'DD/MM/YYYY',
+        cleaveFormat: ['d', 'm', 'Y'],
+    },
+    'MM/AAAA': {
+        acceptedFormats: ['MM/YY', 'MM/YYYY'],
+        outputFormat: 'MM/YYYY',
+        cleaveFormat: ['m', 'Y'],
+    }
+}
+
 angular.module('ddsApp').directive('ddsDate', function() {
     return {
         require: 'ngModel',
-        link: function(scope, elm, attrs, ctrl) {
-            ctrl.$parsers.push(function(viewValue) {
-                var result = moment(viewValue, ['DD/MM/YY', 'L', 'LL'], true);
+        restrict: 'A',
+        link: function(scope, element, attributes, ctrl) {
+            var format = attributes.format;
+            element.attr('placeholder', format);
+            element.attr('maxlength', format.length);
+            element.attr('type', 'text');
 
-                ctrl.$setValidity('ddsDate', result.isValid());
+            ctrl.$parsers.push(function(viewValue) {
+                var result = moment(viewValue, FORMATS[format].acceptedFormats, true);
+                ctrl.$setValidity('format', result.isValid());
 
                 return result;
             });
@@ -15,8 +33,15 @@ angular.module('ddsApp').directive('ddsDate', function() {
             ctrl.$formatters.push(function(momentInstance) {
                 if (! momentInstance) return;
 
-                return momentInstance.format('L');
+                return momentInstance.format(FORMATS[format].outputFormat);
             });
+
+            var formatter = new Cleave(element, {
+                date: true,
+                datePattern: (FORMATS[format].cleaveFormat),
+            });
+
+            element.on('$destroy', formatter.destroy.bind(formatter));  // don't leak event listeners
         }
     };
 });

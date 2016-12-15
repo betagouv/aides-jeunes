0b801ed6be56a5d44eebc82b75da3c8a20372976
Matti Schneider <matti.schneider@beta.gouv.fr>
2016-12-15 12:24:09 +0100

    Refactor children page scrolling

--- app/js/controllers/foyer/enfants.js
+++ app/js/controllers/foyer/enfants.js
@@ -16,10 +16,7 @@ angular.module('ddsApp').controller('FoyerEnfantsCtrl', function($scope, $locati
 
     $scope.newEnfant = function() {
         $scope.displayForm = true;
-        $timeout(function() {
-            $location.hash('form-scroll');
-            $anchorScroll();
-        });
+        $timeout(function() { $anchorScroll('new'); });
     };
 
     $scope.removeEnfant = function(enfant) {

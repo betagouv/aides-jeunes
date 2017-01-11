f78b0c1c70bd7dfa87def32220900a4de1cfba75
Matti Schneider <matti.schneider@beta.gouv.fr>
2017-01-11 16:59:50 +0100

    Remove experimental header

--- app/js/controllers/body.js
+++ app/js/controllers/body.js
@@ -1,19 +1,6 @@
 'use strict';
 
 angular.module('ddsApp').controller('BodyCtrl', function($scope, $localStorage, $timeout, SituationService) {
-    $scope.showIntro = false;
-
-    if (false !== $localStorage.showIntro) {
-        $timeout(function() {
-            $scope.showIntro = true;
-        }, 500);
-    }
-
-    $scope.hideIntro = function() {
-        $scope.showIntro = false;
-        $localStorage.showIntro = false;
-    };
-
     $scope.newSituation = function() {
         SituationService.newSituation();
     };

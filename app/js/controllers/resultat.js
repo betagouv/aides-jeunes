b8718145c9ffb5a396ee28c4e95756049b05988a
Matti Schneider <matti.schneider@beta.gouv.fr>
2017-01-25 21:10:25 +0100

    Dump technical information on bug occasions

--- app/js/controllers/resultat.js
+++ app/js/controllers/resultat.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('ddsApp').controller('ResultatCtrl', function($scope, $rootScope, $window, $http, $state, $stateParams, $timeout, SituationService, ResultatService, ImpactStudyService, droitsDescription) {
+angular.module('ddsApp').controller('ResultatCtrl', function($scope, $rootScope, $window, $http, $state, $stateParams, $timeout, SituationService, ResultatService, ImpactStudyService, droitsDescription, $analytics) {
 
     $scope.error = false;
     $scope.awaitingResults = true;
@@ -26,8 +26,8 @@ angular.module('ddsApp').controller('ResultatCtrl', function($scope, $rootScope,
         $scope.noDroits = _.isEmpty($scope.droits.prestationsNationales) && _.isEmpty($scope.droits.partenairesLocaux);
         ImpactStudyService.sendResults($scope.situation, droits.raw);
     })
-    .catch(function() {
-        $scope.error = true;
+    .catch(function(error) {
+        $scope.error = error && error.data;
     })
     .finally(function() {
         $scope.awaitingResults = false;

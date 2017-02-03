8ef12709185a1fd3cf1000983e8a9b62e06a9d6d
Matti Schneider <matti.schneider@beta.gouv.fr>
2017-02-03 19:11:52 +0100

    Unify local and national aides display

--- app/js/controllers/homepage.js
+++ app/js/controllers/homepage.js
@@ -1,15 +1,11 @@
 'use strict';
 
 angular.module('ddsApp').controller('HomepageCtrl', function($scope, droitsDescription) {
-    $scope.countPrestations = function(provider) {
-        return Object.keys(provider.prestations).length;
-    };
-
     [ 'prestationsNationales', 'partenairesLocaux' ].forEach(function(type) {
         $scope[type] = droitsDescription[type];
 
         $scope[type + 'Count'] = Object.keys(droitsDescription[type]).reduce(function(total, provider) {
-            return total + $scope.countPrestations(droitsDescription[type][provider]);
+            return total + Object.keys(droitsDescription[type][provider].prestations).length;
         }, 0);
     });
 });

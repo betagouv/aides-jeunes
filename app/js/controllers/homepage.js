b61cc0e7565c2c1bd4d5666c571c5e08960e37ca
Matti Schneider <matti.schneider@beta.gouv.fr>
2017-02-03 19:52:18 +0100

    Improve CTA discoverability

--- app/js/controllers/homepage.js
+++ app/js/controllers/homepage.js
@@ -1,10 +1,14 @@
 'use strict';
 
-angular.module('ddsApp').controller('HomepageCtrl', function($scope, droitsDescription) {
+angular.module('ddsApp').controller('HomepageCtrl', function($scope, droitsDescription, $timeout) {
     $scope.prestationsNationales = droitsDescription.prestationsNationales;
     $scope.partenairesLocaux = droitsDescription.partenairesLocaux;
 
     $scope.countPrestations = function(provider) {
         return Object.keys(provider.prestations).length;
     };
+
+    $timeout(function() {
+        document.querySelector('#valueProposition a').focus();
+    }, 1500);
 });

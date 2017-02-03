6a011690696b3054b55a450fa3315b1f04739aee
Matti Schneider <matti.schneider@beta.gouv.fr>
2017-02-03 19:09:57 +0100

    Count computed aides

--- app/js/controllers/homepage.js
+++ app/js/controllers/homepage.js
@@ -1,8 +1,13 @@
 'use strict';
 
 angular.module('ddsApp').controller('HomepageCtrl', function($scope, droitsDescription, $timeout) {
-    $scope.prestationsNationales = droitsDescription.prestationsNationales;
-    $scope.partenairesLocaux = droitsDescription.partenairesLocaux;
+    [ 'prestationsNationales', 'partenairesLocaux' ].forEach(function(type) {
+        $scope[type] = droitsDescription[type];
+
+        $scope[type + 'Count'] = Object.keys(droitsDescription[type]).reduce(function(total, provider) {
+            return total + $scope.countPrestations(droitsDescription[type][provider]);
+        }, 0);
+    });
 
     $scope.countPrestations = function(provider) {
         return Object.keys(provider.prestations).length;

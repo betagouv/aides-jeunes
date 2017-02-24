95e279b6da810d718e167a1da2c97a42aa2ac59d
Matti Schneider <matti.schneider@beta.gouv.fr>
2017-02-24 18:31:17 +0100

    Remove unused loading animation
    Could not find a case where it would activate for more than 200ms

--- app/js/controllers/body.js
+++ app/js/controllers/body.js
@@ -4,38 +4,4 @@ angular.module('ddsApp').controller('BodyCtrl', function($scope, $timeout, Situa
     $scope.newSituation = function() {
         SituationService.newSituation();
     };
-
-    $scope.loading = false;
-    var pendingChanges = 0;
-    var changingState = false;
-    var stateChangeStart = function() {
-        pendingChanges++;
-        $timeout(function() {
-            if (0 < pendingChanges) {
-                $scope.loading = true;
-            }
-        }, 100);
-    };
-    $scope.$on('$stateChangeStart', function() {
-        if (! changingState) {
-            changingState = true;
-            stateChangeStart();
-        }
-    });
-
-    var stateChangeEnd = function() {
-        pendingChanges--;
-        if (0 === pendingChanges) {
-            $scope.loading = false;
-        }
-    };
-
-    $scope.$on('$stateChangeSuccess', function() {
-        changingState = false;
-        stateChangeEnd();
-    });
-    $scope.$on('$stateChangeError', function() {
-        changingState = false;
-        stateChangeEnd();
-    });
 });

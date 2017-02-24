4df01497dc3b100395e1af14b9e857791c9eea06
Matti Schneider <matti.schneider@beta.gouv.fr>
2017-02-24 18:30:48 +0100

    Replace spinner loading animation by logo fade

--- app/js/controllers/body.js
+++ app/js/controllers/body.js
@@ -1,7 +1,41 @@
 'use strict';
 
-angular.module('ddsApp').controller('BodyCtrl', function($scope, $localStorage, $timeout, SituationService) {
+angular.module('ddsApp').controller('BodyCtrl', function($scope, $timeout, SituationService) {
     $scope.newSituation = function() {
         SituationService.newSituation();
     };
+
+    $scope.loading = false;
+    var pendingChanges = 0;
+    var changingState = false;
+    var stateChangeStart = function() {
+        pendingChanges++;
+        $timeout(function() {
+            if (0 < pendingChanges) {
+                $scope.loading = true;
+            }
+        }, 100);
+    };
+    $scope.$on('$stateChangeStart', function() {
+        if (! changingState) {
+            changingState = true;
+            stateChangeStart();
+        }
+    });
+
+    var stateChangeEnd = function() {
+        pendingChanges--;
+        if (0 === pendingChanges) {
+            $scope.loading = false;
+        }
+    };
+
+    $scope.$on('$stateChangeSuccess', function() {
+        changingState = false;
+        stateChangeEnd();
+    });
+    $scope.$on('$stateChangeError', function() {
+        changingState = false;
+        stateChangeEnd();
+    });
 });

21ac4bdbfe1e4801a0f7f074b79f978ff2b6b079
Matti Schneider <matti.schneider@beta.gouv.fr>
2017-03-23 16:55:09 +0100

    Refactor echelon bourse input

--- app/js/controllers/foyer/individuForm.js
+++ app/js/controllers/foyer/individuForm.js
@@ -80,10 +80,6 @@ angular.module('ddsApp').controller('FoyerIndividuFormCtrl', function($scope, in
                 delete $scope.individu.tauxIncapacite;
             }
 
-            if (! $scope.captureEtudiantBoursier()) {
-                delete $scope.individu.echelonBourse;
-            }
-
             if (! $scope.captureScolarite(form)) {
                 delete $scope.individu.scolarite;
             }
@@ -107,10 +103,6 @@ angular.module('ddsApp').controller('FoyerIndividuFormCtrl', function($scope, in
 
     $scope.captureEnfantPlace = function() {
         return ! isIndividuParent && $scope.selectedStatuts.handicap;
-    };
-
-    $scope.captureEtudiantBoursier = function() {
-        return $scope.selectedStatuts.etudiant;
     };
 
     $scope.captureDemandeurACharge = function() {

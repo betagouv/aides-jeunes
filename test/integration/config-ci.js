28eae7d7be998449f29b5b79db84efbc97776b9b
Thomas Guillet <guillet.thomas@gmail.com>
2017-04-11 10:28:04 +0200

    Integrate Watai
    * https://github.com/MattiSG/Watai/wiki/Set-up-parallel-continuous-testing

---
+++ test/integration/config-ci.js
@@ -0,0 +1,16 @@
+// this file is for use in CircleCI continuous integration environment
+module.exports = {
+    seleniumServerURL: {
+        hostname    : 'ondemand.saucelabs.com',
+        port        : 80,
+    },
+    driverCapabilities: {
+        platform            : 'Windows 7',
+        'tunnel-identifier' : 'circle-' + process.env.CIRCLE_BUILD_NUM + '-' + process.env.CIRCLE_NODE_INDEX,
+    },
+    tags        : [ 'circle-ci', '#' + process.env.CIRCLE_BUILD_NUM ],
+    views       : [ 'Verbose', 'SauceLabs' ],
+    quit        : 'always', // avoid wasting 90 seconds on SauceLabs
+    bail        : true,
+    build       : 'CircleCI#' + process.env.CIRCLE_BUILD_NUM,
+}

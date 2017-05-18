234375e55885c5b0971f9a7dafbfcc7accfeb754
Thomas Guillet <guillet.thomas@gmail.com>
2017-05-18 17:48:52 +0200

    Handle raw response from OpenFisca

--- app/js/services/resultatService.js
+++ app/js/services/resultatService.js
@@ -1,6 +1,45 @@
 'use strict';
 
 angular.module('ddsApp').service('ResultatService', function($http, droitsDescription) {
+
+    var PRESTATIONS = { aspa: { type: Number }, asi: { type: Number }, acs: { type: Number }, cmu_c: { type: Boolean }, apl: { type: Number }, als: { type: Number }, alf: { type: Number }, aide_logement: { type: Number }, aide_logement_non_calculable: { type: String }, af: { type: Number }, rsa: { type: Number }, rsa_non_calculable: { type: String }, asf: { type: Number }, cf: { type: Number }, ass: { type: Number }, paje_base: { type: Number }, bourse_college: { type: Number }, bourse_lycee: { type: Number }, paris_logement_familles: { type: Number }, paris_forfait_famille: { type: Number }, paris_logement_psol: { type: Number }, paris_logement: { type: Number }, paris_logement_plfm: { type: Number }, paris_logement_aspeh: { type: Number }, paris_energie_famille: { type: Number }, paris_complement_sante: { type: Number }, adpa: { type: Number }, rennes_metropole_transport: { type: Number, prestationIndividuelle: true }, aah: { type: Number, prestationIndividuelle: true }, aah_non_calculable: { type: String, prestationIndividuelle: true }, ppa: { type: Number } };
+
+    function preprocessOpenfiscaResult(situation, openfiscaResponse) {
+        var period = moment(situation.dateDeValeur).format('YYYY-MM');
+
+        var injectedRessources = _.uniq(_.flatten(_.map(situation.individus, function(individu) {
+            return _.map(individu.ressources, 'type');
+        })));
+
+        var injectedPrestations = _.keys(PRESTATIONS).filter(function(prestationOpenFiscaId) {
+            return injectedRessources.indexOf(prestationOpenFiscaId) >= 0;
+        });
+
+        // Computed prestations can be in the 'familles' or 'individus' openfisca entity
+        var openfiscaResults = _.merge(openfiscaResponse.value[0].familles[0], openfiscaResponse.value[0].individus[0]);
+
+        var calculatedPrestations = _.pickBy(PRESTATIONS, function(format, prestationName) {
+            return injectedPrestations.indexOf(prestationName) < 0;
+        });
+
+        return {
+            injectedPrestations: injectedPrestations,
+            calculatedPrestations: _.mapValues(calculatedPrestations, function(format, prestationName) {
+                var result = openfiscaResults[prestationName] && openfiscaResults[prestationName][period],
+                    uncomputabilityReason = openfiscaResults[prestationName + '_non_calculable'] && openfiscaResults[prestationName + '_non_calculable'][period];
+
+                if (uncomputabilityReason) {
+                    return uncomputabilityReason;
+                }
+
+                if (format.type == Number) {
+                    result = Math.round(Number(result) * 100) / 100;
+                }
+                return result;
+            }),
+        };
+    }
+
     function processOpenfiscaResult(openfiscaResult) {
         var droitsEligibles = {};
         var prestationsNationales = extractMontants(droitsDescription.prestationsNationales, openfiscaResult.calculatedPrestations);
@@ -51,12 +90,13 @@ angular.module('ddsApp').service('ResultatService', function($http, droitsDescri
     }
 
     return {
+        preprocessOpenfiscaResult: preprocessOpenfiscaResult,
         processOpenfiscaResult: processOpenfiscaResult,
         simulate: function(situation) {
             return $http.get('/api/situations/' + situation._id + '/simulation', {
                 params: { cacheBust: Date.now() }
             }).then(function(response) {
-                return processOpenfiscaResult(response.data);
+                return processOpenfiscaResult(preprocessOpenfiscaResult(situation, response.data));
             });
         }
     };
--- test/spec/services/resultatService.js
+++ test/spec/services/resultatService.js
@@ -36,5 +36,164 @@ describe('ResultatService', function () {
         it('should extract injected droits', function() {
             expect(droits.droitsInjectes).toEqual([{ shortLabel: 'APL' }]);
         });
+
+        describe('Preprocessing', function() {
+            var INDIVIDU, OPENFISCA_FAMILLE, OPENFISCA_RESPONSE, SITUATION, actual;
+            OPENFISCA_FAMILLE = {
+                id: 0,
+                aspa: {
+                    '2014-11': 1.199
+                },
+                asi: {
+                    '2014-11': 1
+                },
+                acs: {
+                    '2014-11': 1
+                },
+                cmu_c: {
+                    '2014-11': false
+                },
+                apl: {
+                    '2014-11': 1
+                },
+                als: {
+                    '2014-11': 1
+                },
+                alf: {
+                    '2014-11': 1
+                },
+                aide_logement: {
+                    '2014-11': 1
+                },
+                aide_logement_non_calculable: {
+                    '2014-11': ''
+                },
+                af: {
+                    '2014-11': 1
+                },
+                rsa: {
+                    '2014-11': 1
+                },
+                rsa_non_calculable: {
+                    '2014-11': ''
+                },
+                asf: {
+                    '2014-11': 1
+                },
+                cf: {
+                    '2014-11': 1
+                },
+                ass: {
+                    '2014-11': 1
+                },
+                paje_base: {
+                    '2014-11': 1
+                },
+                bourse_college: {
+                    '2014-11': 1
+                },
+                bourse_lycee: {
+                    '2014-11': 1
+                },
+                paris_logement_familles: {
+                    '2014-11': 1
+                },
+                paris_forfait_famille: {
+                    '2014-11': 1
+                },
+                  paris_logement_psol: {
+                    '2014-11': 1
+                },
+                paris_logement: {
+                    '2014-11': 1
+                },
+                paris_logement_plfm: {
+                    '2014-11': 1
+                },
+                paris_logement_aspeh: {
+                    '2014-11': 1
+                },
+                paris_energie_famille: {
+                    '2014-11': 1
+                },
+                paris_complement_sante: {
+                    '2014-11': 1
+                },
+                adpa: {
+                    '2014-11': 1
+                },
+                rennes_metropole_transport: {
+                    '2014-11': 1
+                },
+                ppa: {
+                    '2014-11': 1
+                }
+            };
+            INDIVIDU = {
+                aah: {
+                    '2014-11': 9
+                },
+                aah_non_calculable: {
+                    '2014-11': ''
+                },
+                specificSituations: []
+            };
+            OPENFISCA_RESPONSE = {
+                value: [
+                {
+                    familles: [OPENFISCA_FAMILLE],
+                    individus: [INDIVIDU]
+                }
+            ]};
+            SITUATION = {
+                dateDeValeur: new Date('2014-11'),
+                individus: [
+                {
+                    ressources: []
+                }
+            ]};
+
+            beforeEach(function() {
+                actual = service.preprocessOpenfiscaResult(SITUATION, OPENFISCA_RESPONSE);
+            });
+
+            it('should extract individual prestations', function() {
+                expect(actual.calculatedPrestations.aah).toEqual(9);
+            });
+
+            describe('of an amount', function() {
+                it('should round', function() {
+                    expect(actual.calculatedPrestations.aspa).toEqual(1.2);
+                });
+            });
+
+            describe('of a boolean', function() {
+                it('should be a pass-through', function() {
+                    expect(actual.calculatedPrestations.cmu_c).toEqual(false);
+                });
+            });
+
+            describe('of an uncomputable value', function() {
+                var REASON, familleWithUncomputableRSA, openfiscaResponseWithUncomputableRSA;
+                REASON = 'tns';
+                familleWithUncomputableRSA = _.clone(OPENFISCA_FAMILLE);
+                familleWithUncomputableRSA.rsa_non_calculable = {
+                    '2014-11': REASON
+                };
+
+                openfiscaResponseWithUncomputableRSA = {
+                    value: [
+                      {
+                        familles: [familleWithUncomputableRSA],
+                        individus: []
+                      }
+                ]};
+
+                it('should set the value to the identifier of the uncomputability', function() {
+                    var res = service.preprocessOpenfiscaResult(SITUATION, openfiscaResponseWithUncomputableRSA);
+                    expect(res.calculatedPrestations.rsa).toEqual(REASON);
+                });
+            });
+        });
     });
 });

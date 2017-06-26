8a33710ce801431d5a51da787451c4fa3894a5eb
Thomas Guillet <guillet.thomas@gmail.com>
2017-06-26 17:45:22 +0200

    Compute variable list in UI

--- app/js/services/mappingService.js
+++ app/js/services/mappingService.js
@@ -1,6 +1,18 @@
 'use strict';
 
-angular.module('ddsApp').service('MappingService', function($q, $http) {
+angular.module('ddsApp').service('MappingService', function($q, $http, droitsDescription) {
+    function generateRequestedVariables() {
+        var structuredPrestations = _.values(droitsDescription).map(function(level) {
+            return _.values(level).map(function(provider) {
+                return _.values(_.mapValues(provider.prestations, function(prestation, prestationName) {
+                    return prestation.uncomputability ? [ prestationName, prestationName + '_non_calculable'] : [ prestationName ];
+                }));
+            });
+        });
+
+        return _.chain(structuredPrestations).flatten().flatten().flatten().value();
+    }
+
     function buildOpenFiscaRequest(situation) {
         return $q(function(resolve, reject) {
             return $http.get('/api/situations/' + situation._id + '/openfisca-request').then(function(simulation) {
@@ -8,7 +20,7 @@ angular.module('ddsApp').service('MappingService', function($q, $http) {
                     intermediate_variables: true,
                     labels: true,
                     scenarios: [ simulation.data.scenarios[0] ],
-                    variables: simulation.data.variables,
+                    variables: generateRequestedVariables(),
                 };
 
                 resolve(request);

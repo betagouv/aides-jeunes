a08a8f3cb16f40e8b5e9d57305e90ce55b90df4b
Thomas Guillet <guillet.thomas@gmail.com>
2017-10-31 07:16:21 +0100

    Use D3JS to generate chart

--- app/js/controllers/stats.js
+++ app/js/controllers/stats.js
@@ -1,90 +1,172 @@
 'use strict';
 
-angular.module('ddsApp').controller('StatsCtrl', function($scope, $http, $q) {
-    $scope.statPeriods = [/*{
-        label: 'Aujourd‘hui',
-        period: 'day',
-    }, */{
-        label: 'Hier',
-        period: 'day',
-        date: function() { return moment().subtract('days', 1).format('YYYY-MM-DD'); },
-    },/* {
-        label: 'Cette semaine',
-        period: 'week',
-    }, {
-        label: 'La semaine dernière',
-        period: 'day',
-        date: function() { return moment().subtract('days', 7).format('YYYY-MM-DD'); },
-    }, {
-        label: 'Ce mois-ci',
-        period: 'month'
-    }, */{
-        label: 'Le mois dernier',
-        period: 'month',
-        date: function() { return moment().subtract('days', 7).format('YYYY-MM-DD'); },
-    }, {
-        label: 'Depuis le début de l‘année',
-        period: 'year'
-    }];
+angular.module('ddsApp').controller('StatsCtrl', function($scope, $http) {
 
-    $scope.retrievalDoneCount = 0;
+    // Define margins
+    var margin = {top: 20, right: 80, bottom: 30, left: 50},
+    width = parseInt(d3.select("#chart").style("width")) - margin.left - margin.right,
+    height = parseInt(d3.select("#chart").style("height")) - margin.top - margin.bottom;
 
-    $scope.statPeriods.forEach(function(period) {
-        var date = period.date && period.date() || 'today';
-        var uri = 'http://stats.data.gouv.fr/index.php?module=API&method=API.get&format=json&idSite=9&period=month&date=' + date;
-        //$http.get(uri)
-        $q(function(resolve, reject) {
-            var data = {
-    nb_uniq_visitors: 32336,
-    nb_visits: 36118,
-    nb_users: 0,
-    nb_actions: 420933,
-    max_actions: 962,
-    bounce_count: 1254,
-    sum_visit_length: 8794143,
-    nb_visits_returning: 8539,
-    nb_actions_returning: 86220,
-    nb_uniq_visitors_returning: 6799,
-    nb_users_returning: 0,
-    max_actions_returning: 558,
-    bounce_rate_returning: "7%",
-    nb_actions_per_visit_returning: 10.1,
-    avg_time_on_site_returning: 217,
-    nb_conversions: 12888,
-    nb_visits_converted: 11105,
-    revenue: 0,
-    conversion_rate: "30.75%",
-    nb_conversions_new_visit: 9874,
-    nb_visits_converted_new_visit: 8440,
-    revenue_new_visit: 0,
-    conversion_rate_new_visit: "30.6%",
-    nb_conversions_returning_visit: 3014,
-    nb_visits_converted_returning_visit: 2665,
-    revenue_returning_visit: 0,
-    conversion_rate_returning_visit: "31.21%",
-    nb_pageviews: 420299,
-    nb_uniq_pageviews: 149216,
-    nb_downloads: 1,
-    nb_uniq_downloads: 1,
-    nb_outlinks: 94,
-    nb_uniq_outlinks: 90,
-    nb_searches: 1,
-    nb_keywords: 1,
-    nb_hits_with_time_generation: 410615,
-    avg_time_generation: 0.126,
-    bounce_rate: "3%",
-    nb_actions_per_visit: 11.7,
-    avg_time_on_site: 243
-};
-            resolve({ data: data});
-        })
-        .then(function(response) {
-            period.data = response.data;
-            $scope.retrievalDoneCount = $scope.retrievalDoneCount + 1;
+    // Define date parser
+    var parseDate = d3.timeParse("%Y-%m-%d");
 
-            $scope.allStatsRetrieved = $scope.retrievalDoneCount === $scope.statPeriods.length;
-        }, function(error) {
-            $scope.error = error;
+    // Define scales
+    var xScale = d3.scaleTime().range([0, width]);
+    var yScale = d3.scaleLinear().range([height, 0]);
+    var color = d3.scaleOrdinal().range(["#e41a1c", "#377eb8"]);
+
+    // Define axes
+    var xAxis = d3.axisBottom().scale(xScale);
+    var yAxis = d3.axisLeft().scale(yScale);
+
+    // Define lines
+    var line = d3.line()
+    .x(function(d) { return xScale(d.date); })
+    .y(function(d) { return yScale(d.value); });
+
+    // Define svg canvas
+    var chart = d3.select("#chart")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom);
+
+    var svg = chart
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    var tooltip = svg.append("g");
+    tooltip.append("g");
+
+    var legend = tooltip.append("text")
+    .attr("transform", "translate(10,10)");
+
+    var chartDate = tooltip.append("text")
+    .attr('transform', 'translate(' + width + ',10)')
+    .style('text-anchor', 'end');
+
+    $http.get('/documents/stats.json')
+    .then(function(response, error) {
+        $scope.error = (response.headers('Content-Type').match(/html/));
+        if ($scope.error)
+            return;
+
+        var data = response.data;
+
+        data.forEach(function(dataset) {
+            dataset.datapoints.forEach(function(point) {
+                point.date = parseDate(point.date);
+            });
         });
+        color.domain(data.map(function(data) { return data.metric; }));
+
+        var yDomain = d3.extent(data[1].datapoints, function(d) { return d.value; });
+        yDomain[0] = 0;
+        yDomain[1] *= 1.2;
+
+        xScale.domain(d3.extent(data[1].datapoints, function(d) { return d.date; }));
+        yScale.domain(yDomain);
+
+        // Place the axes on the chart
+        svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+
+        svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
+        .append("text")
+        .attr("class", "label")
+        .attr("y", 6)
+        .attr("dy", ".71em")
+        .attr("dx", ".71em")
+        .style("text-anchor", "beginning")
+        .text("Product Concentration");
+
+        var metrics = svg.selectAll(".metric")
+        .data(data)
+        .enter().append("g")
+        .attr("class", "metric");
+
+        metrics.append("path")
+        .attr("class", "line")
+        .style("stroke", function(d) { return color(d.metric); })
+        .attr("d", function(d) { return line(d.datapoints); });
+
+        chart.on("mousemove", function() {
+            var point = d3.mouse(svg.node());
+
+            var date = parseDate(moment(xScale.invert(point[0])).format('YYYY-MM-DD'));
+            var dateValue = date.valueOf();
+            var slice = data.map(function(dataset) {
+                var idx = dataset.datapoints.findIndex(function(d) { return d.date.valueOf() === dateValue; });
+                var point = dataset.datapoints[idx] || {};
+                return {
+                    metric: dataset.metric,
+                    value: point.value,
+                };
+            });
+
+            _.remove(slice, function(d) { return ! _.isNumber(d.value); });
+            slice.sort(function(metric) { return metric.value; });
+
+            var nodes = tooltip
+            .select('g')
+            .selectAll('circle')
+            .data(slice);
+
+            nodes
+            .enter()
+            .append('circle')
+            .attr('r', 0);
+
+            nodes
+            .attr('r', function(d) { return _.isNumber(d.value) ? 5 : 0; })
+            .attr('cx', function(d) { return xScale(date); })
+            .attr('cy', function(d) { return yScale(d.value); })
+            .style("stroke", function(d) { return color(d.metric); })
+            .style("fill", function(d) { return color(d.metric); });
+
+            nodes
+            .exit()
+            .remove();
+
+            var names = {
+                visit: 'Visites',
+                simulation: 'Simulations',
+            };
+            var legendData = legend
+            .selectAll('tspan')
+            .data(slice);
+
+            legendData.enter()
+            .append('tspan')
+            .attr('x', '0')
+            .attr('dy', function(d, i) { return i * 20; });
+
+            legendData
+            .text(function(d) { return names[d.metric] + ' : ' + d.value; })
+            .style("fill", function(d) { return color(d.metric); });
+
+            legendData
+            .exit()
+            .remove();
+
+            var chartDateNode = chartDate
+            .selectAll('tspan')
+            .data(slice.length ? [date]: []);
+            chartDateNode.enter()
+            .append('tspan');
+
+            chartDateNode
+            .text(function(d) { return moment(d).format('dddd DD MMMM YYYY'); });
+
+            chartDateNode
+            .exit()
+            .remove();
+        });
+
+        $scope.allStatsRetrieved = true;
+    }, function(error) {
+        $scope.error = true;
     });
 });

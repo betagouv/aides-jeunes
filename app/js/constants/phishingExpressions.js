6aba8b7d4ce5ae25e992041f1261e7d228f2ab11
Thomas Guillet <guillet.thomas@gmail.com>
2018-01-09 18:50:44 +0100

    Allow phishing expression usage from NodeJS

--- app/js/constants/phishingExpressions.js
+++ app/js/constants/phishingExpressions.js
@@ -1,4 +1,15 @@
+(function() {
 'use strict';
 
-angular.module('ddsCommon').constant('phishingExpressions', [
-]);
+var phishingExpressions = [
+    /acusticanapoli\.com/,
+];
+
+/* Export either through Angular loader or CommonJS */
+if (typeof module != 'undefined') {  // we're in Node
+    module.exports = phishingExpressions;
+} else {  // we're in the browser
+    angular.module('ddsCommon').constant('phishingExpressions', phishingExpressions);
+}
+/* End of export */
+})();

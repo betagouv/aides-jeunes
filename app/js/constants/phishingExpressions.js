6b034a094a3dd4955c1463b94710d77722bb6492
Thomas Guillet <guillet.thomas@gmail.com>
2018-01-10 14:21:43 +0100

    Update phishing expressions

--- app/js/constants/phishingExpressions.js
+++ app/js/constants/phishingExpressions.js
@@ -3,6 +3,19 @@
 
 var phishingExpressions = [
     /acusticanapoli\.com/,
+    /archer\.fr/,
+    /assurance-ameli-recouvrement\.com/,
+    /assurance-ameli-recouvrement\.net/,
+    /assurance\.remboursement-amelifr\.kostidis\.gr/,
+    /depor420\.beget\.tech/,
+    /intercoined\.com/,
+    /liveone\.com\.br/,
+    /mannishtalk\.com/,
+    /remboursement\.ameliassurance\.contempora\.ro/,
+    /remboursement\.ameliassurance-fr36932693693269\.irds\.ro/,
+    /remboursement\.caisse-assurance\.irds\.ro/,
+    /remboursement\.ameliassurance\.marikala\.gr/,
+    /yzfg\.r\.bh\.d\.sendibt3\.com/
 ];
 
 /* Export either through Angular loader or CommonJS */

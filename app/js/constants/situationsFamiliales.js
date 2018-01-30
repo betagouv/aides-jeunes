d8084151bb9544e791da9824b88c8a6605d2d14f
Philippe Blayo <philippe.blayo@gmail.com>
2018-01-30 01:14:38 +0100

    [Fix] Migrate situationsFamiliales enum to new syntax

--- app/js/constants/situationsFamiliales.js
+++ app/js/constants/situationsFamiliales.js
@@ -2,15 +2,15 @@
 
 angular.module('ddsCommon').constant('situationsFamiliales', [
     {
-        value: 'Marié',  // Enum value 1 in OpenFisca
+        value: 'marie',  // Enum value 1 in OpenFisca
         label: 'Marié(e)',
     },
     {
-        value: 'Pacsé',  // Enum value 5 in OpenFisca
+        value: 'pacse',  // Enum value 5 in OpenFisca
         label: 'Pacsé(e)',
     },
     {
-        value: 'Célibataire',  // Enum value 2 in OpenFisca
+        value: 'celibataire',  // Enum value 2 in OpenFisca
         label: 'En union libre',
     }
 ]);

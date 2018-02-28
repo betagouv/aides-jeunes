d6489b5e785fb510f1276d9dec51bf195bcd13ca
Sylvain <sylvain.dermy@gmail.com>
2018-02-28 16:48:02 +0100

    Cheque Energie : droits.js
    Ajout de la description de l'aide

--- app/js/constants/droits.js
+++ app/js/constants/droits.js
@@ -267,6 +267,24 @@ var droitsDescription = {
                 },
             },
         },
+		'cheque_energie': {
+            'label': 'Chèque Énergie',
+            'imgSrc': 'logo_cheque_energie.png',
+            'prestations': {
+                'cheque_energie': {
+                    'label': 'Chèque Énergie',
+                    'shortLabel': 'cheque_energie',
+                    'description': 'Le Chèque Énergie peut être utilisé pour toutes les dépenses d’énergie (électricité, gaz, fioul, bois, etc.) et les travaux de rénovation énergétique. Il est envoyé automatiquement chaque année en fonction de votre situation fiscale et de la composition de votre foyer.',
+                    'conditions': [
+                        'Effectuer chaque année votre déclaration auprès des impôts.',
+						'Utiliser votre Chèque Énergie reçu en avril avant le 31 mars de l’année suivante'
+                    ],
+                    'link': 'https://www.chequeenergie.gouv.fr/',
+                    /* En principe, envoyé automatique : lien direct vers le formulaire d'utilisation en ligne */
+                    'instructions': 'https://www.chequeenergie.gouv.fr/beneficiaire/paiement',
+                },
+            },
+        },
         'departements': {
             label: 'Les conseils départementaux',
             'imgSrc': 'logo_cd_generique.png',

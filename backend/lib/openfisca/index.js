9c97780d0f1b4109b6bfd604711ce3d9febec2f4
Thomas Guillet <guillet.thomas@gmail.com>
2018-03-14 11:00:17 +0100

    Move migration systematic application

--- backend/lib/openfisca/index.js
+++ backend/lib/openfisca/index.js
@@ -2,14 +2,12 @@ var config = require('../../config/config');
 var mapping = require('./mapping');
 var rp = require('request-promise');
 
-var migrations = require('../migrations');
-
 var buildOpenFiscaRequest = exports.buildOpenFiscaRequest = mapping.buildOpenFiscaRequest;
 function sendToOpenfisca(endpoint) {
     return function(situation, callback) {
         var request;
         try {
-            request = buildOpenFiscaRequest(migrations.apply(situation));
+            request = buildOpenFiscaRequest(situation);
         } catch(e) {
             return callback({
                 message: e.message,

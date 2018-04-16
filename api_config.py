6bb8cd2a08f34e4a49239fa946f0eb4ea313eef2
Thomas Guillet <guillet.thomas@gmail.com>
2018-04-16 15:40:57 +0200

    Run OpenFisca directly with gunicorn

--- api_config.py
+++
@@ -1,6 +0,0 @@
-port = 2000
-workers = 4
-timeout = 60
-
-country_package = 'openfisca_france'
-extensions = ['openfisca_paris', 'openfisca_brestmetropole', 'openfisca_rennesmetropole']
---
+++ config.py
@@ -0,0 +1,3 @@
+bind = '127.0.0.1:2000'
+timeout = 60
+workers = 4

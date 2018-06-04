843c45d533269ffd6a799d725f79d8720b8d6f0b
Thomas Guillet <guillet.thomas@gmail.com>
2018-06-04 15:24:28 +0200

    Add dynamic action url

--- app/js/controllers/redirect.js
+++ app/js/controllers/redirect.js
@@ -1,10 +1,12 @@
 'use strict';
 
-angular.module('ddsApp').controller('RedirectCtrl', function($scope, SituationService) {
-  $scope.data = [{
+angular.module('ddsApp').controller('RedirectCtrl', function($scope, SituationService, $sce) {
+  $scope.endpoint = 'https://reflexe45-test.loiret.fr/public/requestv2/accountless/teleprocedure_id/92/';
+
+  $scope.fields = [{
     label: 'votre date de naissance',
     value: '1940-12-12',
-    formatter: function(value) { return moment(value).format('LL') },
+    formatter: function(value) { return moment(value).format('LL'); },
     key: 'date_naissance_dem'
-  }]
+  }];
 });

a8488ffc7af1de5f358b36f59f262f2f430a8d17
Thomas Guillet <guillet.thomas@gmail.com>
2018-06-06 17:22:25 +0200

    Add fields in redirection logic

--- app/js/controllers/redirection.js
+++ app/js/controllers/redirection.js
@@ -8,5 +8,25 @@ angular.module('ddsApp').controller('RedirectionCtrl', function($scope, Situatio
     value: '1940-12-12',
     formatter: function(value) { return moment(value).format('LL'); },
     key: 'date_naissance_dem'
+  }, {
+    label: 'votre situation familiale',
+    value: 0,
+    formatter: function(value) { return 'Célibataire'; },
+    key: 'situationfam_dem'
+  }, {
+    label: 'vos salaires (net) sur les 12 derniers mois',
+    value: 800*12,
+    formatter: function(value) { return value.toString() + ' €'; },
+    key: 'salaire_dem'
+  }, {
+    label: 'votre retraite (net) sur les 12 derniers mois',
+    value: 500*12,
+    formatter: function(value) { return value.toString() + ' €'; },
+    key: 'montantRetraite_dem'
+  }, {
+    label: 'vos allocations sur les 12 derniers mois',
+    value: 200*12,
+    formatter: function(value) { return value.toString() + ' €'; },
+    key: 'allocations_dem'
   }];
 });

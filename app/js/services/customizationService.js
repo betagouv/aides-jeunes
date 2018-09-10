b38b63697273ffa83ca646e1e90dbc24593c417b
charlottelecuit <42929880+charlottelecuit@users.noreply.github.com>
2018-09-10 10:23:31 +0200

    Add customization cd42

--- app/js/services/customizationService.js
+++ app/js/services/customizationService.js
@@ -19,6 +19,8 @@ angular.module('ddsCommon').factory('CustomizationService', function(lyonMetropo
                 return 'D33-GIRONDE';
             if (testCase.menages._.depcom[currentPeriod].match(/^34/))
                 return 'D34-HERAULT';
+            if (testCase.menages._.depcom[currentPeriod].match(/^42/))
+                return 'D42-LOIRE';
             if (testCase.menages._.depcom[currentPeriod].match(/^44/))
                 return 'D44-LOIRE_ATLANTIQUE';
             if (testCase.menages._.depcom[currentPeriod].match(/^59/))

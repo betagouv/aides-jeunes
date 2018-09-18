2595d8ca51c806955bde8f57d1f4487eef3dba8c
Thomas Guillet <guillet.thomas@gmail.com>
2018-09-18 14:26:33 +0200

    Move etablissement logic to directive and hide list by default

--- app/js/controllers/resultat.js
+++ app/js/controllers/resultat.js
@@ -65,16 +65,6 @@ angular.module('ddsApp').controller('ResultatCtrl', function($analytics, $http, 
                 $scope.shouldPatrimoineBeCaptured = function() {
                     return ! angular.isDefined(SituationService.hasPatrimoine($scope.situation));
                 };
-
-                CityService
-                    .getCities($scope.situation.menage.code_postal)
-                    .then(function(cities) { return _.find(cities, { codeInsee: $scope.situation.menage.depcom }); })
-                    .then(function(city) { return $http.get('https://etablissements-publics.api.gouv.fr/v3/communes/' + city.codeInsee + '/ccas+cdas+sdsei+msap'); })
-                    .then(function(response) { return response.data.features; }, function() { return []; })
-                    .then(function(etablissements) {
-                        $scope.etablissements = etablissements.map(normalizeEtablissement);
-
-                    });
             });
     }
 
@@ -106,42 +96,5 @@ angular.module('ddsApp').controller('ResultatCtrl', function($analytics, $http, 
         }).error(function(data) {
             $window.alert(data.error.apiError);
         });
-    };
-
-    function normalizeEtablissement(etablissementFeature) {
-        var etablissement = etablissementFeature.properties;
-
-        if (etablissement.url === 'https://www.maisondeservicesaupublic.fr') {
-            delete etablissement.url;
-        }
-
-        if (etablissement.horaires) {
-            var mapping = {
-                lundi: 1,
-                mardi: 2,
-                mercredi: 3,
-                jeudi: 4,
-                vendredi: 5,
-                samedi: 6,
-                dimanche: 7
-            };
-            etablissement.horaires = _.sortBy(etablissement.horaires, function(plage) {
-                return mapping[plage.du];
-            });
-        }
-
-        etablissement.adresse = _.find(etablissement.adresses, { type: 'physique' });
-        if (! etablissement.adresse) {
-            etablissement.adresse = _.find(etablissement.adresses, { type: 'géopostale' });
-        }
-        if (! etablissement.adresse) {
-            etablissement.adresse = etablissement.adresses[0];
-        }
-
-        return etablissement;
-    }
-
-    $scope.extractHHMM = function(dateString) {
-        return dateString.slice(0,5);
     };
 });
---
+++ app/js/directives/etablissementsList.js
@@ -0,0 +1,74 @@
+'use strict';
+
+function normalizeEtablissement(etablissementFeature) {
+    var etablissement = etablissementFeature.properties;
+
+    if (etablissement.url === 'https://www.maisondeservicesaupublic.fr') {
+        delete etablissement.url;
+    }
+
+    if (etablissement.horaires) {
+        var mapping = {
+            lundi: 1,
+            mardi: 2,
+            mercredi: 3,
+            jeudi: 4,
+            vendredi: 5,
+            samedi: 6,
+            dimanche: 7
+        };
+        etablissement.horaires = _.sortBy(etablissement.horaires, function(plage) {
+            return mapping[plage.du];
+        });
+    }
+
+    etablissement.adresse = _.find(etablissement.adresses, { type: 'physique' });
+    if (! etablissement.adresse) {
+        etablissement.adresse = _.find(etablissement.adresses, { type: 'géopostale' });
+    }
+    if (! etablissement.adresse) {
+        etablissement.adresse = etablissement.adresses[0];
+    }
+
+    return etablissement;
+}
+
+angular.module('ddsApp').directive('etablissementsList', function() {
+    return {
+        restrict: 'E',
+        templateUrl: 'partials/etablissements-list.html',
+        scope: {
+            codeInsee: '=',
+            codePostal: '='
+        },
+        controller: 'etablissementsListCtrl',
+    };
+});
+
+angular.module('ddsApp').controller('etablissementsListCtrl', function($http, $interval, $scope, CityService) {
+    function getEtablissements() {
+        if (! $scope.codePostal) {
+            return;
+        }
+
+        CityService
+            .getCities($scope.codePostal)
+            .then(function(cities) { return _.find(cities, { codeInsee: $scope.codeInsee }); })
+            .then(function(city) { return city; })
+            .then(function(city) { return $http.get('https://etablissements-publics.api.gouv.fr/v3/communes/' + city.codeInsee + '/ccas+cdas+sdsei+msap'); })
+            .then(function(response) { return response.data.features; }, function() { return []; })
+            .then(function(etablissements) {
+                $scope.etablissements = etablissements.map(normalizeEtablissement);
+            });
+    }
+
+    ['codeInsee', 'codePostal'].forEach(function(key) {
+        $scope.$watch(key, getEtablissements);
+    });
+
+    $scope.extractHHMM = function(dateString) {
+        return dateString.slice(0,5);
+    };
+
+    getEtablissements();
+});

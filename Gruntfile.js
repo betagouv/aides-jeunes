3d3664765306cd245e21ca0e6f647b3818ced8a7
Alexandre Segura <mex.zktk@gmail.com>
2018-10-23 13:46:32 +0200

    Use Webpack to build assets.
    - Remove Grunt tasks used to build assets.
    - Create entrypoints using require() notation.
    - Use PostCSS to add vendor prefixes.
    - Use Babel to support IE >= 11.

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,12 @@
 // Generated on 2014-06-19 using generator-angular-fullstack 1.4.3
 'use strict';
 
+// Webpack config is exported as a function
+var webpackConfig = require('./webpack.config');
+
+var webpackDevelopmentConfig = webpackConfig(process.env, { mode: 'development' });
+var webpackProductionConfig = webpackConfig(process.env, { mode: 'production' });
+
 // # Globbing
 // for performance reasons we're only matching one level down:
 // 'test/spec/{,*/}*.js'
@@ -19,15 +25,7 @@ module.exports = function (grunt) {
 
   // Define the configuration for all the tasks
   grunt.initConfig({
-    build: {
-      dist: 'dist',
-    },
-    // Project settings
-    yeoman: {
-      // configurable paths
-      app: 'app',
-      tmp: 'tmp'
-    },
+
     express: {
       options: {
         port: process.env.PORT || 9000
@@ -39,40 +37,20 @@ module.exports = function (grunt) {
         }
       }
     },
+
     open: {
       server: {
         url: 'http://localhost:<%= express.options.port %>'
       }
     },
+
     watch: {
-      js: {
-        files: ['<%= yeoman.app %>/js/**/*.js', '<%= yeoman.app %>/../backend/**/*.js'],
-        options: {
-          livereload: true
-        }
-      },
       jsTest: {
         files: ['test/spec/**/*.js'],
         tasks: ['karma']
       },
-      sass: {
-        files: ['<%= yeoman.app %>/styles/**/*.{scss,sass}'],
-        tasks: ['sass', 'autoprefixer']
-      },
       gruntfile: {
         files: ['Gruntfile.js']
-      },
-      livereload: {
-        files: [
-          '<%= yeoman.app %>/views/{,*//*}*.html',
-          '{.tmp,<%= yeoman.app %>}/styles/{,*//*}*.css',
-          '{.tmp,<%= yeoman.app %>}/js/{,*//*}*.js',
-          '{.tmp,<%= yeoman.app %>}/../backend/{,*//*}*.js',
-          '<%= yeoman.app %>/img/{,*//*}*.{png,jpg,jpeg,gif,webp,svg}'
-        ],
-        options: {
-          livereload: true
-        }
       },
       express: {
         files: [
@@ -84,42 +62,36 @@ module.exports = function (grunt) {
         tasks: ['express:dev', 'wait'],
         options: {
           livereload: true,
-          nospawn: true //Without this option specified express won't be reloaded
+          nospawn: true // Without this option specified express won't be reloaded
         }
       },
-      browserify: {
-        files: [ 'app/vendor.js' ],
-        tasks: [ 'browserify' ],
-        options: { livereload: true },
-      }
     },
 
-    // Empties folders to start fresh
-    clean: {
-      tmp: {
-        files: [{
-          dot: true,
-          src: [
-            '.tmp',
-            '<%= yeoman.tmp %>/*'
-          ]
-        }]
-      },
-      server: '.tmp'
+    webpack: {
+      prod: Object.assign(webpackProductionConfig, {
+        mode: 'production',
+      }),
+      dev: Object.assign(webpackDevelopmentConfig, {
+        mode: 'development',
+        keepalive: false,
+      })
     },
 
-    // Add vendor prefixed styles
-    autoprefixer: {
+    // @see https://github.com/webpack/docs/wiki/usage-with-grunt
+    // @see https://github.com/webpack/webpack-with-common-libs/blob/master/Gruntfile.js
+    'webpack-dev-server': {
       options: {
-        browsers: ['last 1 version', 'firefox >= 20']
+        webpack: Object.assign(webpackDevelopmentConfig, {
+          mode: 'development',
+        }),
+        // TODO Parameterize via env for Docker / local
+        host: '0.0.0.0',
       },
-      dist: {
-        files: [{
-          expand: true,
-          cwd: '.tmp/styles/',
-          src: '{,*/}*.css',
-          dest: '.tmp/styles/'
-        }]
+      start: {
+        keepalive: false,
+        webpack: {
+          mode: 'development'
+        }
       }
     },
 
@@ -157,159 +129,8 @@ module.exports = function (grunt) {
       }
     },
 
-    // Compiles Sass to CSS and generates necessary files if requested
-    sass: {
-      dist: {
-        files: [{
-          expand: true,
-          cwd: '<%= yeoman.app %>/styles',
-          src: '**/*.scss',
-          dest: '<%= yeoman.tmp %>/styles',
-          ext: '.css'
-        }],
-        options: {
-          includePaths: ['<%= yeoman.app %>/../node_modules']
-        }
-      }
-    },
-
-    browserify: {
-      vendor: {
-        src: './app/vendor.js',
-        dest: '<%= yeoman.tmp %>/js/vendor.js',
-        options: {
-          debug: true,
-        },
-      },
-      stats: {
-        src: './app/stats.js',
-        dest: '<%= yeoman.tmp %>/js/stats.js',
-        options: {
-          debug: true,
-        },
-      },
-      sentry: {
-        src: './app/sentry.js',
-        dest: '<%= yeoman.tmp %>/js/sentry.js',
-        options: {
-          debug: true,
-        },
-      },
-    },
-
-    // Renames files for browser caching purposes
-    rev: {
-      dist: {
-        files: {
-          src: [
-            '<%= yeoman.tmp %>/js/{,*/}*.js',
-            '<%= yeoman.tmp %>/styles/{,*/}*.css',
-            '<%= yeoman.tmp %>/styles/fonts/*'
-          ]
-        }
-      }
-    },
-
-    // Reads HTML for usemin blocks to enable smart builds that automatically
-    // concat, minify and revision files. Creates configurations in memory so
-    // additional tasks can operate on them
-    useminPrepare: {
-      html: ['<%= yeoman.app %>/views/front.html', '<%= yeoman.app %>/views/embed.html'],
-      options: {
-        dest: '<%= yeoman.tmp %>'
-      }
-    },
-
-    // Performs rewrites based on rev and the useminPrepare configuration
-    usemin: {
-      html: ['<%= yeoman.tmp %>/views/{,*/}*.html'],
-      options: {
-        assetsDirs: ['<%= yeoman.tmp %>']
-      }
-    },
-
-    htmlmin: {
-      dist: {
-        options: {
-          //collapseWhitespace: true,
-          //collapseBooleanAttributes: true,
-          //removeCommentsFromCDATA: true,
-          //removeOptionalTags: true
-        },
-        files: [{
-          expand: true,
-          cwd: '<%= yeoman.app %>/views',
-          src: ['**/*.html'],
-          dest: '<%= yeoman.tmp %>/views'
-        }]
-      }
-    },
-
-    // Allow the use of non-minsafe AngularJS files. Automatically makes it
-    // minsafe compatible so Uglify does not destroy the ng references
-    ngAnnotate: {
-      dist: {
-        files: [{
-          expand: true,
-          cwd: '.tmp/concat/js',
-          src: '*.js',
-          dest: '.tmp/concat/js'
-        }]
-      }
-    },
-
-    // Copies remaining files to places other tasks can use
-    copy: {
-      tmp: {
-        files: [{
-          expand: true,
-          dot: true,
-          cwd: '<%= yeoman.app %>',
-          dest: '<%= yeoman.tmp %>',
-          src: [
-            '*.{ico,png,txt}',
-            'img/**/*',
-            'fonts/**/*',
-            'resources/**/*',
-            'documents/**/*',
-            'styles/**/*.css'
-          ]
-        }, {
-          expand: true,
-          cwd: '.tmp/img',
-          dest: '<%= yeoman.tmp %>/img',
-          src: ['generated/*']
-        }, {
-          expand: true,
-          flatten: true,
-          cwd: '.',
-          dest: '<%= yeoman.tmp %>/fonts',
-          src: 'node_modules/font-awesome/fonts/*'
-        }]
-      },
-      dist: {
-        files: [{
-          expand: true,
-          dot: true,
-          cwd: 'tmp',
-          dest: 'dist',
-          src: [
-            '**/*'
-          ]
-        }]
-      }
-    },
-
     // Run some tasks in parallel to speed up the build process
     concurrent: {
-      server: [
-        'browserify',
-        'sass'
-      ],
-      test: [
-        'browserify',
-        'sass'
-      ],
       debug: {
         tasks: [
           'nodemon',
@@ -319,11 +140,6 @@ module.exports = function (grunt) {
           logConcurrentOutput: true
         }
       },
-      dist: [
-        'browserify',
-        'sass:dist',
-        'htmlmin'
-      ]
     },
 
     // Test settings
@@ -340,21 +156,9 @@ module.exports = function (grunt) {
       }
     },
 
-    // Uglify config for files compiled by Browserify
-    // Other files are uglified by grunt-usemin
-    uglify: {
-      dist: {
-        files: [{
-          expand: true,
-          src: ['./<%= yeoman.tmp %>/js/*.js'],
-          dest: './<%= yeoman.tmp %>',
-          rename: function (dst, src) {
-            return src;
-          }
-        }]
-      }
-    }
   });
+
+  grunt.loadNpmTasks('grunt-webpack');
 
   // Used for delaying livereload until after server has restarted
   grunt.registerTask('wait', function () {
@@ -371,20 +175,14 @@ module.exports = function (grunt) {
   grunt.registerTask('serve', function (target) {
     if (target === 'debug') {
       return grunt.task.run([
-        'clean:server',
-        'copy:tmp',
-        'concurrent:server',
-        'autoprefixer',
         'concurrent:debug'
       ]);
     }
 
     var tasks = [
-      'clean:server',
-      'copy:tmp',
-      'concurrent:server',
-      'autoprefixer',
+      'webpack:dev',
       'express:dev',
+      'webpack-dev-server:start',
       'open',
       'watch'
     ];
@@ -397,33 +195,12 @@ module.exports = function (grunt) {
     grunt.task.run(tasks);
   });
 
-  grunt.loadNpmTasks('grunt-browserify');
-
   grunt.registerTask('test', [
-    'clean:server',
     'karma'
   ]);
 
-  // WARNING
-  // grunt-usemin automatically creates subtasks for grunt-concat & grunt-uglify
-  // https://github.com/yeoman/grunt-usemin#tasks
-  grunt.registerTask('build', [
-    'clean:tmp',
-    'useminPrepare',
-    'concurrent:dist',
-    'autoprefixer',
-    'concat:generated',
-    'ngAnnotate',
-    'copy:tmp',
-    'uglify:generated',
-    'uglify:dist',
-    'rev',
-    'usemin',
-    'copy:dist'
-  ]);
-
   grunt.registerTask('default', [
+    'webpack:prod',
     'test',
-    'build'
   ]);
 };
---
+++ webpack.config.js
@@ -0,0 +1,174 @@
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var CleanWebpackPlugin = require('clean-webpack-plugin');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+var MiniCssExtractPlugin = require('mini-css-extract-plugin');
+var merge = require('webpack-merge');
+
+// @see https://github.com/preboot/angularjs-webpack
+
+var config = {
+    entry: {
+        sentry: [
+            './app/sentry.js'
+        ],
+        scripts: [
+            './app/scripts.js'
+        ],
+        'scripts.recapSituation': [
+            './app/scripts.recapSituation.js'
+        ],
+    },
+    output: {
+        filename: 'js/[name].js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/'
+    },
+    module: {
+        rules: [
+            // Angular 1.x doesn't support CommonJS
+            // @see https://github.com/webpack/webpack/issues/2049
+            {
+                test: require.resolve('angular'),
+                use: 'exports-loader?window.angular'
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: [
+                    { loader: 'babel-loader' },
+                    { loader: 'ng-annotate-loader' },
+                ]
+            },
+            {
+                test: /\.s?css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'postcss-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf|svg)$/,
+                use: [
+                    'file-loader?name=fonts/[name].[ext]',
+                ]
+            },
+            {
+                test: /\.(png)$/,
+                use: [
+                    'file-loader?name=img/[name].[ext]',
+                ]
+            }
+        ]
+    },
+    devServer: {
+        contentBase: './app',
+    },
+    plugins: [
+        new CopyWebpackPlugin([
+            { from: 'app/documents', to: 'documents/' },
+            { from: 'app/img', to: 'img/' },
+            { from: 'app/views', to: 'views/', ignore: [ 'front.html', 'embed.html' ] },
+        ]),
+        // Avoid bundling all Moment locales
+        // @see https://github.com/moment/moment/issues/2517
+        // @see https://webpack.js.org/plugins/context-replacement-plugin/
+        new webpack.ContextReplacementPlugin(
+            /moment[/\\]locale$/,
+            /fr/
+        ),
+        new webpack.ProvidePlugin({
+            angular: 'angular',
+            moment: 'moment',
+            _: 'lodash',
+            Cleave: 'cleave.js',
+            jsyaml: 'js-yaml',
+            d3: 'd3'
+        }),
+    ],
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                stats: {
+                    test: /[\\/]node_modules[\\/]d3/,
+                    priority: 20,
+                    chunks: 'initial',
+                    name: 'stats',
+                    enforce: true
+                },
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    priority: 10,
+                    chunks: 'initial',
+                    name: 'vendor',
+                    enforce: true
+                },
+                default: false
+            }
+        },
+    }
+};
+
+module.exports = function(env, argv) {
+
+    var htmlWebpackPluginConfigs = [
+        {
+            template: './app/views/front.html',
+            filename: 'views/front.html',
+            inject: false,
+        },
+        {
+            template: './app/views/embed.html',
+            filename: 'views/embed.html',
+            inject: false,
+        },
+    ];
+
+    var miniCssExtractPluginOptions = {
+        filename: "styles/[name].css",
+    };
+
+    var overrideConfig = {};
+
+    var plugins = [];
+
+    // When running with mode = development,
+    // assets will be served via webpack-dev-server
+    if (argv.mode === 'development') {
+        htmlWebpackPluginConfigs = htmlWebpackPluginConfigs.map(function(htmlWebpackPluginConfig) {
+            return Object.assign(htmlWebpackPluginConfig, {
+                devServer: 'http://localhost:8080'
+            });
+        });
+    }
+
+    if (argv.mode === 'production') {
+
+        plugins.push(new CleanWebpackPlugin([ 'dist' ]));
+        plugins.push(new webpack.HashedModuleIdsPlugin());
+
+        overrideConfig = {
+            output: {
+                filename: 'js/[name].[contenthash].js',
+            }
+        };
+
+        miniCssExtractPluginOptions = {
+            filename: "styles/[name].[contenthash].css",
+        };
+    }
+
+    htmlWebpackPluginConfigs.forEach(function(htmlWebpackPluginConfig) {
+        plugins.push(new HtmlWebpackPlugin(htmlWebpackPluginConfig));
+    });
+
+    plugins.push(new MiniCssExtractPlugin(miniCssExtractPluginOptions));
+
+    return merge(config, {
+        mode: argv.mode || 'none',
+        plugins: plugins,
+    }, overrideConfig);
+};

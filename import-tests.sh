9b20779f8bf5fae67d5c9be7d3526b1c48c04906
Thomas Guillet <guillet.thomas@gmail.com>
2018-11-22 16:42:06 +0100

    Refactor import script
    * Work in Ubuntu

--- import-tests.sh
+++ import-tests.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # Replaces the local database by the production one
 
 REMOTE=metal.mes-aides.gouv.fr
@@ -9,16 +10,16 @@ NOW=$(date +"%F-%H-%M-%S")
 DISTANT_DUMP_FOLDER="~/dumps/dump-$NOW"
 LOCAL_DUMP_FOLDER='./dump'
 
-
 cd `dirname $0`
 
-read -n 1 -p "Do you want to delete the content of the local $DATABASE database as part of this import?  [y/N]" drop
+export LC_ALL=C
 
+read -p "Do you want to delete the content of the local $DATABASE database as part of this import?  [y/N]" drop
 if [[ $drop == 'y' ]]
 then
     mongo $DATABASE --eval "db.dropDatabase()"
 else
-    read -n 1 -p "Are you POSITIVE you want to merge contents of the local $DATABASE database with contents from the distant $USER one?  [y/N]" sure
+    read -p "Are you POSITIVE you want to merge contents of the local $DATABASE database with contents from the distant $USER one?  [y/N]" sure
 
     if [[ $sure != 'y' ]]
     then
@@ -27,8 +28,7 @@ else
     fi
 fi
 
-
-read -n 1 -p "Do you want to LIMIT the imported situations to the one used in tests ONLY?  [y/N]" limited
+read -p "Do you want to LIMIT the imported situations to the one used in tests ONLY?  [y/N]" limited
 
 set -ex
 
@@ -37,7 +37,7 @@ ssh $USER@$REMOTE "mongodump --db $DATABASE"
 if [[ $limited == 'y' ]]
 then
     ssh $USER@$REMOTE "mongodump --db=dds --collection=situations --query='{status: \"test\"}'"
-    read -n 1 -p "Do you want to rename the local 'situations' collection in 'legacysituations' after the import?  [y/N]" rename
+    read -p "Do you want to rename the local 'situations' collection in 'legacysituations' after the import?  [y/N]" rename
 fi
 
 ssh $USER@$REMOTE "mkdir -p `dirname $DISTANT_DUMP_FOLDER` && mv dump $DISTANT_DUMP_FOLDER/ && gzip -rv $DISTANT_DUMP_FOLDER"

e3ecb7015d64701783a3f07dc3d948a74e7aba37
Thomas Guillet <guillet.thomas@gmail.com>
2018-12-27 11:44:40 +0100

    Add specific URL for tracer

--- backend/config/config.js
+++ backend/config/config.js
@@ -2,6 +2,7 @@ var env = process.env.NODE_ENV || 'development';
 
 var all = {
     openfiscaURL: process.env.OPENFISCA_URL || 'http://localhost:2000',
+    openfiscaPublicURL: 'https://openfisca.mes-aides.gouv.fr',
     openfiscaTracerURL: 'https://betagouv.github.io/openfisca-tracer',
     mongo: {
         uri: process.env.MONGODB_URL || 'mongodb://localhost/dds',
--- backend/config/development.js
+++ backend/config/development.js
@@ -1,6 +1,7 @@
 'use strict';
 
 module.exports = {
+    openfiscaPublicURL: 'http://localhost:2000',
     openfiscaTracerURL: 'http://localhost:3000',
     teleserviceAccessTokens: {
         loiret_APA_test: 'token',

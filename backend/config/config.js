c1664ff2ba1bc0e8a6f6f92605d138d0bb35ff9d
Thomas Guillet <guillet.thomas@gmail.com>
2018-12-27 11:16:38 +0100

    Limit production config
    * Add not sensitive value to default config

--- backend/config/config.js
+++ backend/config/config.js
@@ -8,7 +8,8 @@ var all = {
         options: {
             useMongoClient: true,
         },
-    }
+    },
+    openFiscaTracerURL: 'https://betagouv.github.io/openfisca-tracer'
 };
 
 var override = {};

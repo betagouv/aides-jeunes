95dfba21fe3170a9a57a7eae17544d409601c40f
Thomas Guillet <guillet.thomas@gmail.com>
2019-01-15 12:48:49 +0100

    Supprime la logique spécifique aux liens internes et l'indication de test pour l'AAH

--- test/backend/aidesDescription.js
+++ test/backend/aidesDescription.js
@@ -33,15 +33,9 @@ describe('aides descriptions', function() {
                         expect(aide.link).toMatch(/^https?:\/\//);
                     });
 
-                    if (aide.isExperimental) {
-                        it('should have an internal link', function() {
-                            expect(typeof aide.internalLink).toBe('string');
-                        });
-                    } else {
-                        it('should have a teleservice, a form, or instructions', function() {
-                            expect(typeof (aide.teleservice || aide.form || aide.instructions)).toBe('string');
-                        });
-                    }
+                    it('should have a teleservice, a form, or instructions', function() {
+                        expect(typeof (aide.teleservice || aide.form || aide.instructions)).toBe('string');
+                    });
                 });
             });
         });

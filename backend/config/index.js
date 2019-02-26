439b6fa3532472f675c364c29f415e141badf7c8
Thomas Guillet <guillet.thomas@gmail.com>
2019-02-26 11:06:18 +0100

    Rend accessible l'environnement (prod/dev) dans la config

--- backend/config/index.js
+++ backend/config/index.js
@@ -1,6 +1,7 @@
 var env = process.env.NODE_ENV || 'development';
 
 var all = {
+    env: env,
     openfiscaURL: process.env.OPENFISCA_URL || 'http://localhost:2000',
     openfiscaPublicURL: 'https://openfisca.mes-aides.gouv.fr',
     openfiscaTracerURL: 'https://betagouv.github.io/openfisca-tracer',

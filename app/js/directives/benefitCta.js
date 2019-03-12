00e083ffe3d0dd780fa4049c8d57ffc622953773
Thomas Guillet <guillet.thomas@gmail.com>
2019-03-12 18:12:23 +0100

    Sépare la logique des CTA

--- app/js/directives/benefitCta.js
+++ app/js/directives/benefitCta.js
@@ -1,20 +1,34 @@
 'use strict';
 
-angular.module('ddsApp').controller('benefitCtaCtrl', function($scope, $state, SituationService, TrampolineService) {
-    $scope.getActionURL = function(benefit) {
-        var action = benefit.teleservice || benefit.form || benefit.instructions;
+angular.module('ddsApp').controller('benefitCtaCtrl', function($scope) {
+    var types = ['teleservice', 'form', 'instructions'];
 
-        if (typeof action === 'object') {
-            return $state.href(action.state, action.params);
+    var type = types.reduce(function(value, type) {
+        return value || ($scope.benefit[type] && type);
+    }, false);
+    $scope.link = $scope.benefit[type];
+    $scope.type = type;
+});
+
+var typeLabels = {
+    teleservice: 'Faire une demande',
+    form: 'Accéder au formulaire',
+    instructions: 'Accéder aux instructions'
+};
+
+angular.module('ddsApp').controller('benefitCtaLinkCtrl', function($scope, $state, SituationService, TrampolineService) {
+    $scope.label = typeLabels[$scope.type];
+
+    $scope.getURL = function(link) {
+        if (typeof link === 'object') {
+            return $state.href(link.state, link.params);
         }
 
-        return action;
+        return link;
     };
 
-    $scope.onClick = function(benefit) {
-        var action = benefit.teleservice || benefit.form || benefit.instructions;
-
-        if (typeof action === 'object') {
+    $scope.onClick = function(link) {
+        if (typeof link === 'object') {
             var situation = SituationService.restoreLocal();
             TrampolineService.set({ situationId: situation._id });
         }
@@ -29,5 +43,18 @@ angular.module('ddsApp').directive('benefitCta', function() {
             benefit: '='
         },
         controller: 'benefitCtaCtrl',
+    };
+});
+
+angular.module('ddsApp').directive('benefitCtaLink', function() {
+    return {
+        restrict: 'E',
+        templateUrl: '/partials/benefit-cta-link.html',
+        scope: {
+            analyticsName: '=',
+            link: '=',
+            type: '=',
+        },
+        controller: 'benefitCtaLinkCtrl',
     };
 });

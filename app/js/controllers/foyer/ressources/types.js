30139f17d82f0d776ec32a4ab90aa6ba1095f5e5
Alexandre Segura <mex.zktk@gmail.com>
2019-03-20 18:38:19 +0100

    WIP Make button sticky on resources page.

--- app/js/controllers/foyer/ressources/types.js
+++ app/js/controllers/foyer/ressources/types.js
@@ -1,5 +1,15 @@
 'use strict';
 
+var scrollMonitor = require('scrollmonitor');
+
+function countSelectedRessourceTypes(selectedRessourceTypes) {
+    return _.reduce(selectedRessourceTypes, function(accumulator, value) {
+        return true === value ? accumulator + 1 : accumulator;
+    }, 0);
+}
+
+var elementWatcher;
+
 angular.module('ddsApp').controller('FoyerRessourceTypesCtrl', function($scope, $stateParams, ABTestingService, ressourceCategories, ressourceTypes, $state, RessourceService) {
 
     var momentDebutAnnee = moment($scope.situation.dateDeValeur).subtract(1, 'years');
@@ -10,6 +20,7 @@ angular.module('ddsApp').controller('FoyerRessourceTypesCtrl', function($scope, 
     var keyedRessourceTypes = _.keyBy(ressourceTypes, 'id');
     var filteredRessourceTypes = _.filter(ressourceTypes, RessourceService.isRessourceOnMainScreen);
     $scope.ressourceTypesByCategories = _.groupBy(filteredRessourceTypes, 'category');
+    $scope.selectedRessourceTypesCount = countSelectedRessourceTypes($scope.selectedRessourceTypes);
 
     var abtesting = ABTestingService.getEnvironment();
     $scope.hideHelp = abtesting && abtesting.resourceHelp && abtesting.resourceHelp.value === "Hide";
@@ -22,6 +33,47 @@ angular.module('ddsApp').controller('FoyerRessourceTypesCtrl', function($scope, 
             $scope.isOpen[keyedRessourceTypes[ressourceTypeId].category] = true;
         }
     });
+
+    var $scrollDetector = document.getElementById('scroll-detector');
+    if ($scrollDetector) {
+        // We need to wait for page to be loaded before initializing scrollmonitor,
+        // otherwise the element position may be wrong
+        angular.element(function() {
+            if (elementWatcher) {
+                elementWatcher.destroy();
+            }
+            elementWatcher = scrollMonitor.create($scrollDetector);
+            elementWatcher.stateChange(function() {
+                if (this.isBelowViewport) {
+                    document.getElementById('submit-button').classList.add('submit-button-nav--sticky');
+                } else {
+                    document.getElementById('submit-button').classList.remove('submit-button-nav--sticky');
+                }
+            });
+            if (elementWatcher.isBelowViewport) {
+                document.getElementById('submit-button').classList.add('submit-button-nav--sticky');
+            }
+        });
+        angular.element($scrollDetector).on('$destroy', function() {
+            elementWatcher.destroy();
+        });
+    }
+
+    // We force scrollmonitor to refresh when accordions are toggled
+    $scope.onClickHeading = function() {
+        // FIXME Find a better way than setTimeout
+        setTimeout(function() {
+            elementWatcher.recalculateLocation();
+            elementWatcher.update();
+            elementWatcher.triggerCallbacks();
+        }, 500);
+    };
+
+    // We can't use $scope.$watch('selectedRessourceTypes'),
+    // because the reference is untouched
+    $scope.onSelectedRessourceTypesChanged = function() {
+        $scope.selectedRessourceTypesCount = countSelectedRessourceTypes($scope.selectedRessourceTypes);
+    };
 
     function updateIndividuRessources(individu, selectedRessourceTypes) {
         Object.keys(selectedRessourceTypes).forEach(function(ressourceTypeId) {

8174edd9c76e3ef0a096454d9ab58aa5379c90af
Alexandre Segura <mex.zktk@gmail.com>
2019-03-20 15:19:18 +0100

    Add fulltext search endpoint with Fuse.js.

--- backend/controllers/outils.js
+++ backend/controllers/outils.js
@@ -1,3 +1,4 @@
+var Fuse = require('fuse.js');
 var communes = require('@etalab/decoupage-administratif/data/communes.json');
 var index = {};
 
@@ -19,10 +20,29 @@ communes.forEach(function(commune) {
     });
 });
 
+var options = {
+    shouldSort: true,
+    threshold: 0.3,
+    location: 0,
+    distance: 100,
+    maxPatternLength: 32,
+    minMatchCharLength: 1,
+    keys: [
+        "nom",
+        "codesPostaux"
+    ]
+};
+var fuse = new Fuse(communes, options);
+
 function find(postalCode) {
     return index[postalCode] || [];
 }
 
 exports.communes = function(req, res) {
     res.send(find(req.params.codePostal));
+};
+
+exports.searchCommunes = function(req, res) {
+    var matches = fuse.search(req.query.q);
+    res.send(matches.slice(0, 5));
 };
--- backend/routes/outils.js
+++ backend/routes/outils.js
@@ -2,6 +2,7 @@ var outils = require('../controllers/outils');
 
 module.exports = function(api) {
 
+    api.route('/outils/communes/search').get(outils.searchCommunes);
     api.route('/outils/communes/:codePostal').get(outils.communes);
 
 };

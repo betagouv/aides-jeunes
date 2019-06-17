14ee490ffd437e202d670afbc8f982aad9ca8107
Alexandre Segura <mex.zktk@gmail.com>
2019-06-17 11:29:42 +0200

    Deprecate time-grunt.

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,9 +23,6 @@ module.exports = function (grunt) {
   // Load grunt tasks automatically
   require('load-grunt-tasks')(grunt);
 
-  // Time how long tasks take. Can help when optimizing build times
-  require('time-grunt')(grunt);
-
   // Define the configuration for all the tasks
   grunt.initConfig({
 

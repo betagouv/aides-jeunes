3dca76b10456a394f5c21c1a84e34c7fa374b585
Thomas Guillet <guillet.thomas@gmail.com>
2019-08-02 18:07:00 +0200

    N'enregistre pas les followups sans emails

--- backend/controllers/situations.js
+++ backend/controllers/situations.js
@@ -106,6 +106,10 @@ exports.openfiscaTest = function(req, res) {
 };
 
 exports.followup = function(req, res) {
+    if (! req.body.email || ! req.body.email.length) {
+        return res.status(400).send({ result: 'KO' });
+    }
+
     Followup.create({
         email: req.body.email,
         situation: req.situation

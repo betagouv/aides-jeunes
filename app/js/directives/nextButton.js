0e48d459675fe0a919e8df3ac8aa6a2e5c5a3d5b
Alexandre Segura <mex.zktk@gmail.com>
2019-08-12 15:43:28 +0200

    WIP

--- app/js/directives/nextButton.js
+++ app/js/directives/nextButton.js
@@ -1,5 +1,9 @@
 'use strict';
 
+var scrollMonitor = require('scrollmonitor');
+var ResizeSensor = require('css-element-queries').ResizeSensor;
+var elementWatcher;
+
 angular.module('ddsApp').directive('nextButton', function() {
     return {
         restrict: 'E',
@@ -7,5 +11,40 @@ angular.module('ddsApp').directive('nextButton', function() {
         transclude: {
             message: '?message', // This slot is optional
         },
+        link(scope, element, attrs) {
+
+            var $offsetMarker = document.getElementById('offset-marker');
+            var $nextButton = document.getElementById('next-button');
+
+            var $content = document.querySelector('.content');
+            new ResizeSensor($content, function() {
+                if (elementWatcher) {
+                    elementWatcher.recalculateLocation();
+                    elementWatcher.update();
+                    elementWatcher.triggerCallbacks();
+                }
+            });
+
+            angular.element(function() {
+
+                if (elementWatcher) {
+                    elementWatcher.destroy();
+                }
+
+                elementWatcher = scrollMonitor.create($offsetMarker);
+                elementWatcher.stateChange(function() {
+                    if (this.isBelowViewport) {
+                        $nextButton.classList.add('next-button--sticky');
+                    } else {
+                        $nextButton.classList.remove('next-button--sticky');
+                    }
+                });
+
+                if (elementWatcher.isBelowViewport) {
+                    $nextButton.classList.add('next-button--sticky');
+                }
+            });
+
+        }
     };
 });

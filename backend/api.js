5ac95b7573f844ed36dab8c7fe0b597f2b058933
Alexandre Segura <mex.zktk@gmail.com>
2019-08-28 17:56:20 +0200

    Utilise le nouveau SDK Sentry (backend).

--- backend/api.js
+++ backend/api.js
@@ -8,22 +8,6 @@ require('./config/mongoose')(mongoose, config);
 // Setup Express
 var app = express();
 
-if (config.env.match(/production/) || (config.sentry && config.sentry.privateDsn)) {
-    if ((! config.sentry) || (! config.sentry.privateDsn)) {
-        throw new Error('Sentry monitoring in production is mandatory. Missing configuration details! Aborting...');
-    }
-    var raven = require('raven');
-    raven.config(config.sentry.privateDsn);
-    var installedValuePreInstall = raven.installed;
-    raven.install();
-    var installedValuePostInstall = raven.installed;
-    app.use(raven.requestHandler());
-
-    // Ensure installed property exists and behaves as expected
-    if (installedValuePreInstall || !installedValuePostInstall) {
-        throw new Error('raven.installed does not behave as expected! Aborting...');
-    }
-}
 app.use(require('./lib/ludwig')(mongoose, mongoose.model('Situation')));
 app.use(require('./config/api'));
 
--- server.js
+++ server.js
@@ -3,20 +3,23 @@
 var express = require('express');
 var morgan = require('morgan');
 var ludwigConfig = require('./ludwig/ui-config');
+var Sentry = require('@sentry/node');
 
 var port = process.env.PORT || 9000;
 
 ludwigConfig.mesAidesRootUrl = process.env.MES_AIDES_ROOT_URL || ('http://localhost:' + port);
 
+Sentry.init({
+    // Enable Sentry in production
+    // https://docs.sentry.io/development/sdk-dev/overview/#usage-for-end-users
+    dsn: process.env.NODE_ENV === 'production' ? 'https://fde1d4c9741e4ef3a3416e4e88b61392@sentry.data.gouv.fr/17' : null,
+});
+
 // Setup Express
 var app = express();
 
-if (app.get('env') == 'development') {
-    app.use(morgan('dev'));
-    app.use(require('errorhandler')());
-} else {
-    app.use(morgan('combined'));
-}
+// The request handler must be the first middleware on the app
+app.use(Sentry.Handlers.requestHandler());
 
 // Setup app
 app.use('/api', require('./backend/api'));
@@ -24,6 +27,16 @@ app.use(ludwigConfig.baseUrl, require('ludwig-ui')(ludwigConfig));
 app.use('/followups', require('./backend/followups'));
 
 require('./index.js')(app);
+
+// The error handler must be before any other error middleware and after all controllers
+app.use(Sentry.Handlers.errorHandler());
+
+if (app.get('env') == 'development') {
+    app.use(morgan('dev'));
+    app.use(require('errorhandler')());
+} else {
+    app.use(morgan('combined'));
+}
 
 // Start server
 app.listen(port, function () {

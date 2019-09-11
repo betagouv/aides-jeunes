0c6fa8f9e5d27715af92ab5ad6593822f8e75f0d
Alexandre Segura <mex.zktk@gmail.com>
2019-09-11 14:36:43 +0200

    Ajoute une page avec un questionnaire de suivi.

---
+++ app/js/controllers/suivi.js
@@ -0,0 +1,129 @@
+'use strict';
+
+var allBenefits = require('../constants/benefits');
+var SmoothScroll = require('smooth-scroll');
+
+var scroll = new SmoothScroll();
+
+var choices = [
+    { value: 'already', label: "J'en bénéficiais déjà" },
+    { value: 'asked',   label: "J'ai fait une demande" },
+    { value: 'failed',  label: "Je n'ai pas réussi à faire une demande" },
+    { value: 'nothing', label: "Je n'ai rien fait" },
+];
+
+function isNegative(value) {
+    return value === 'failed' || value === 'nothing';
+}
+
+angular.module('ddsApp').controller('SuiviCtrl', function($http, $scope, $stateParams, $timeout) {
+
+    $http.get('/api/followups/' + $stateParams.followupId)
+        .then(function(response) {
+
+            var followup = response.data;
+
+            var benefitsIds = followup.benefits.map(benefit => benefit.id);
+
+            var benefitsNormalized = [];
+
+            $scope.createdAt = moment(followup.createdAt).format('ll');
+
+            _.mapValues(allBenefits, function(aidesProviders) {
+                _.mapValues(aidesProviders, function(aidesProvider, aidesProviderId) {
+                    _.forEach(aidesProvider.prestations, function(aide, aideId) {
+
+                        if (! benefitsIds.includes(aideId)) {
+                            return;
+                        }
+
+                        var montant = _.find(followup.benefits, benefit => benefit.id === aideId).amount;
+
+                        benefitsNormalized.push(_.assign({},
+                            aide,
+                            {
+                                id: aideId,
+                                montant: montant,
+                                provider: aidesProvider,
+                                providerId: aidesProviderId,
+                                choices: choices,
+                                choiceValue: null,
+                                choiceComments: ''
+                            }
+                        ));
+                    });
+                });
+            });
+
+            $scope.droits = benefitsNormalized;
+        });
+
+    $scope.isComplete = false;
+    $scope.hiddenChoices = [];
+
+    $scope.checkComplete = function(droit, force) {
+
+        var choiceValues = _.map($scope.droits, droit => droit.choiceValue);
+        $scope.isComplete = (_.filter(choiceValues).length === $scope.droits.length);
+
+        if (! isNegative(droit.choiceValue) || true === force) {
+
+            $scope.hiddenChoices.push(droit.id);
+
+            var next = _.find($scope.droits, droit => droit.choiceValue === null);
+            if (next) {
+                // FIXME We use timeout so that scroll is calculated AFTER choices have been hidden
+                $timeout(function() {
+                    scroll.animateScroll(document.querySelector(`#${next.id}`), null, {
+                        updateURL: false,
+                        popstate: false,
+                        speed: 500,
+                        speedAsDuration: true,
+                        header: 'header.navbar-fixed-top'
+                    });
+                }, 80);
+            }
+        } else {
+            $timeout(() => document.querySelector(`#${droit.id} textarea`).focus(), 250);
+        }
+    };
+
+    $scope.isHidden = function(droit) {
+        return _.includes($scope.hiddenChoices, droit.id);
+    };
+
+    $scope.modify = function(e, droit) {
+        e.preventDefault();
+        $scope.hiddenChoices = _.filter($scope.hiddenChoices, item => item !== droit.id);
+    };
+
+    $scope.isNumber = _.isNumber;
+    $scope.isString = _.isString;
+    $scope.getFractionSize = function(droit) {
+        return droit.floorAt < 1 ? 2 : 0;
+    };
+
+    $scope.submitted = false;
+
+    $scope.submit = function() {
+
+        var answers = $scope.droits.map(droit => ({
+            id: droit.id,
+            value: droit.choiceValue,
+            comments: droit.choiceComments
+        }));
+
+        var survey = {
+            answers: answers
+        };
+
+        $http.post('/api/followups/' + $stateParams.followupId + '/surveys', survey)
+            .then(function(response) {
+                if (response.status === 201) {
+                    $scope.submitted = true;
+                }
+            });
+    };
+
+    $scope.isNegative = isNegative;
+});

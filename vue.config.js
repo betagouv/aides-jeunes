c52339eceb637ff86d9689248ca66c473763fcf5
Thomas Guillet <guillet.thomas@gmail.com>
2019-10-04 17:01:10 +0200

    Garde les espaces dans l'HTML

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,19 @@
 const configureAPI = require('./configure')
 
 module.exports = {
+  chainWebpack(config) {
+    config.module
+      .rule('vue')
+      .use('vue-loader')
+      .loader('vue-loader')
+      .tap(options => ({
+        ...options,
+        compilerOptions: {
+          ...options.compilerOptions,
+          preserveWhitespace: true,
+        },
+      }));
+  },
   devServer: {
     before: configureAPI
   }

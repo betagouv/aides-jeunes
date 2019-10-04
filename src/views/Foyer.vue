61164c394b351e740d441fd33fdf9dbb1f20ddd8
Thomas Guillet <guillet.thomas@gmail.com>
2019-10-04 09:52:06 +0200

    Utilise le fond gris clair de Mes Aides

--- src/views/Foyer.vue
+++ src/views/Foyer.vue
@@ -18,6 +18,12 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+
+.foyer {
+  background-color: whitesmoke;
+  padding-top: 1em;
+}
+
 pre {
   text-align: left;
 }

cc5a8095571af9a838b11818836f0e08605ddb04
Thomas Guillet <guillet.thomas@gmail.com>
2019-10-05 14:59:04 +0200

    Indique dans la config de CI les clés de prod

--- backend/config/continuous_integration.js
+++ backend/config/continuous_integration.js
@@ -1,6 +1,15 @@
 'use strict';
 
 module.exports = Object.assign(require('./development'), {
+    teleserviceAccessTokens: {
+        live_node_test: 'live_node_test',
+        loiret_APA_test: 'loiret_APA_test',
+        loiret_APA: 'loiret_APA',
+    },
+    mailjet: {
+        publicKey: 'publicKey',
+        privateKey: 'privateKey',
+    },
     sentry: {
         privateDsn: 'https://userID:password@sentry.data.gouv.fr/17'
     },

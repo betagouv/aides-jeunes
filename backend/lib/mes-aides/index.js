d0390d1d565b414cb46be859d194e21ccf5b9060
Alexandre Segura <mex.zktk@gmail.com>
2019-10-09 16:49:43 +0200

    Factorise le parcours de la structure des aides.

--- backend/lib/mes-aides/index.js
+++ backend/lib/mes-aides/index.js
@@ -38,36 +38,32 @@ function computeAides(situation, openfiscaResponse, showPrivate) {
         droitsInjectes: [], // declared by the user
     };
 
-    _.mapValues(droitsDescription, function(aidesProviders) {
-        _.mapValues(aidesProviders, function(aidesProvider, aidesProviderId) {
-            _.forEach(aidesProvider.prestations, function(aide, aideId) {
-                if ((! showPrivate) && aide.private) {
-                    return;
-                }
+    forEach((aide, aideId, aidesProvider, aidesProviderId) => {
+        if ((! showPrivate) && aide.private) {
+            return;
+        }
 
-                if (_.some(situation.individus, function(individu) { return valueAt(aideId, individu, period) !== undefined; })) {
-                    return result.droitsInjectes.push(aide);
-                }
+        if (_.some(situation.individus, function(individu) { return valueAt(aideId, individu, period) !== undefined; })) {
+            return result.droitsInjectes.push(aide);
+        }
 
-                var value = valueAt(aideId + '_non_calculable', computedRessources, period);
+        var value = valueAt(aideId + '_non_calculable', computedRessources, period);
 
-                if ((! value) || value === 'calculable') {
-                    value = round(valueAt(aideId, computedRessources, period), aide);
-                }
+        if ((! value) || value === 'calculable') {
+            value = round(valueAt(aideId, computedRessources, period), aide);
+        }
 
-                var dest = (value) ? result.droitsEligibles : result.droitsNonEligibles;
-                dest.push(_.assign({},
-                    aide,
-                    {
-                        id: aideId,
-                        montant: value,
-                        provider: aidesProvider,
-                        providerId: aidesProviderId,
-                    },
-                    customizationId && aide.customization && aide.customization[customizationId]
-                ));
-            });
-        });
+        var dest = (value) ? result.droitsEligibles : result.droitsNonEligibles;
+        dest.push(_.assign({},
+            aide,
+            {
+                id: aideId,
+                montant: value,
+                provider: aidesProvider,
+                providerId: aidesProviderId,
+            },
+            customizationId && aide.customization && aide.customization[customizationId]
+        ));
     });
 
     Object.keys(result).forEach(function(group) {
@@ -77,5 +73,24 @@ function computeAides(situation, openfiscaResponse, showPrivate) {
     return result;
 }
 
+/**
+ * This function iterates over the nested benefits, and executes a callback.
+ * The callback is called with 4 parameters:
+ * - benefit: the benefit object
+ * - benefitId: the benefit id
+ * - provider: the benefit provider id
+ * - providerId: the benefit provider id
+ */
+function forEach(cb) {
+    _.mapValues(droitsDescription, function(aidesProviders) {
+        _.mapValues(aidesProviders, function(aidesProvider, aidesProviderId) {
+            _.forEach(aidesProvider.prestations, function(aide, aideId) {
+                cb(aide, aideId, aidesProvider, aidesProviderId);
+            });
+        });
+    });
+}
+
 exports.computeAides = computeAides;
 exports.round = round;
+exports.forEach = forEach;

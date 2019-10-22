ea5d6109826056a7bd35dd34abf1e05c669ca01d
Thomas Guillet <guillet.thomas@gmail.com>
2019-10-22 17:24:06 +0200

    Implémente l'ajout d'enfant avec le formulaire d'individu

--- src/views/Foyer/Enfants/Ajouter.vue
+++ src/views/Foyer/Enfants/Ajouter.vue
@@ -1,37 +1,42 @@
 <template>
   <div class="container">
-    <div class="text-right">
-      <button class="button large" v-on:click="next">Ajouter</button>
+    <div class="frame-foyer">
+      <h1>Nouvel enfant</h1>
+      <IndividuForm v-model="individu" v-bind:date="situation.dateDeValeur" v-bind:existingIndividu="existingIndividu" v-on:input="emit()" />
     </div>
   </div>
 </template>
 
 <script>
+import IndividuForm from '@/components/IndividuForm'
 import Individu from '@/lib/Individu'
 import Situation from '@/lib/Situation'
 
 export default {
   name: 'enfants.ajouter',
   components: {
+    IndividuForm
   },
-  data () {    
-    return {
+  data () {
+    let situation = this.$SituationService.restoreLocal()
+    let enfants = Situation.getEnfants(situation)
+    let { existingIndividu, individu } = Individu.get(enfants, 'enfant', 1)
 
+    return {
+      existingIndividu,
+      individu,
+      situation
     }
   },
   methods: {
-    next: function() {
-      let s = this.$SituationService.restoreLocal()
-      let enfants = Situation.getEnfants(s)
-
-      let {individu} = Individu.get(enfants, 'enfant', 1)
-      individu.date_naissance = new Date('2010-12-12')
-      enfants.push(individu)
-      Situation.setEnfants(s, enfants)
+    emit: function() {
+      let enfants = Situation.getEnfants(this.situation)
+      enfants.push(this.individu)
+      Situation.setEnfants(this.situation, enfants)
 
       this.$SituationService.saveLocal()
       this.$router.push('/foyer/enfants')
-    },
+    }
   }
 }
 </script>

8457bf18951772faa692264351483a1a7e6dcbe4
Thomas Guillet <guillet.thomas@gmail.com>
2019-10-22 10:36:56 +0200

    Ajoute une librairie de gestion des nationalités

---
+++ src/components/NationalityChoice.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <label>
+      <input type="radio" name="nationalite" v-model="zone" value="fr" />
+      Française
+    </label>
+    <label>
+      <input type="radio" name="nationalite" v-model="zone" value="ue" />
+      Européenne
+    </label>
+    <label>
+      <input type="radio" name="nationalite" v-model="zone" value="autre" />
+      Non européenne
+    </label>
+  </div>
+</template>
+
+
+<script>
+
+import Nationality from '@/lib/Nationality'
+
+
+export default {
+  name: 'NationalityChoice',
+  components: {
+  },
+  props: {
+    value: String
+  },
+  computed: {
+    zone: {
+        get: function() {
+          return Nationality.getZone(this.value)
+        },
+        set: function(value) {
+          this.$emit('input', Nationality.getCountryCodeByNationality(value))
+        }
+    }
+  }
+}
+</script>
---
+++ src/lib/Nationality.js
@@ -0,0 +1,108 @@
+var Fuse = require('fuse.js');
+var worldCountries = require('world-countries');
+var countries = [];
+
+worldCountries.forEach(function(country) {
+    if (country.hasOwnProperty('demonyms')) {
+        countries.push({
+            code: country.cca2,
+            commonName: country.translations.fra.common,
+            demonym: country.demonyms.fra.f,
+            officialName: country.translations.fra.official,
+        });
+    }
+});
+
+var fuseOptions = {
+    shouldSort: true,
+    threshold: 0.5,
+    location: 0,
+    distance: 100,
+    maxPatternLength: 32,
+    minMatchCharLength: 1,
+    keys: [
+        "commonName",
+        "demonym",
+        "officialName"
+    ]
+};
+var fuse = new Fuse(countries, fuseOptions);
+
+var ZONE_LABEL = {
+    'fr': 'française',
+    'ue': 'UE',
+    'autre': 'hors UE'
+};
+
+var EEE_COUNTRY_CODES = [
+    'AT',
+    'BE',
+    'BG',
+    'CY',
+    'CZ',
+    'DE',
+    'DK',
+    'EE',
+    'ES',
+    'FI',
+    'FR',
+    'GR',
+    'HR',
+    'HU',
+    'IE',
+    'IS',
+    'IT',
+    'LI',
+    'LU',
+    'LV',
+    'MT',
+    'NL',
+    'NO',
+    'PL',
+    'PT',
+    'RO',
+    'SE',
+    'SI',
+    'SK',
+    'UK',
+];
+
+function getZone(countryCode) {
+
+    countryCode = countryCode.toUpperCase();
+
+    if (countryCode === 'FR') {
+        return 'fr';
+    }
+    if (EEE_COUNTRY_CODES.includes(countryCode) || countryCode === 'CH') {
+        return 'ue';
+    }
+
+    return 'autre';
+}
+
+const Nationality = {
+    toArray: function() {
+        return countries;
+    },
+    getLabel: function(nationality) {
+        return ZONE_LABEL[getZone(nationality)];
+    },
+    getZone: getZone,
+    getCountryCodeByNationality: function(nationality) {
+
+        switch (nationality) {
+        case 'ue':
+            return 'DE';
+        case 'autre':
+            return 'AF';
+        }
+
+        return 'FR';
+    },
+    search: function(q) {
+        return fuse.search(q).slice(0, 10);
+    }
+}
+
+export default Nationality

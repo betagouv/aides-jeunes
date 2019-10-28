9f411adb6c3cae43f704f13395e1998eeec5624a
Thomas Guillet <guillet.thomas@gmail.com>
2019-10-28 16:27:49 +0100

    Ajoute Sentry

--- src/main.js
+++ src/main.js
@@ -8,14 +8,22 @@ import ScrollService from './plugins/ScrollService'
 import SituationService from './plugins/SituationService'
 
 import AsyncComputed from 'vue-async-computed'
+import Raven from 'raven-js'
+import RavenVue from 'raven-js/plugins/vue'
 import Vuelidate from 'vuelidate'
 
 import 'template.data.gouv.fr/dist/main.css'
 import 'font-awesome/scss/font-awesome.scss'
 import '@/styles/main.scss'
 
+
 import MailDirective from './directives/mail'
 MailDirective(Vue)
+
+Raven
+  .config('https://d5f975565d1b46f8a90f968071422ba5@sentry.data.gouv.fr/39')
+  .addPlugin(RavenVue, Vue)
+  .install()
 
 Vue.use(AsyncComputed)
 Vue.use(ScrollService)

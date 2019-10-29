b99e93bcae56106b3eca6f4049cfbf58b90a77b2
Thomas Guillet <guillet.thomas@gmail.com>
2019-10-29 14:40:51 +0100

    Utilise 3 champs pour la saisie de date

--- src/components/InputDate.vue
+++ src/components/InputDate.vue
@@ -1,10 +1,37 @@
 <template>
   <div>
-    <input type="date" v-bind:value="item" v-on:input="emit($event)" >
+    <input
+      type="number"
+      autofocus
+      ref="day"
+      aria-label="Jour"
+      v-model="day"
+      placeholder="23"
+      min=1
+      max=31 />
+      /
+    <input
+      type="number"
+      ref="month"
+      aria-label="Mois"
+      v-model="month"
+      placeholder="02"
+      min=1
+      max=12 />
+      /
+    <input
+      type="number"
+      ref="year"
+      aria-label="Année"
+      v-model="year"
+      placeholder="1973"
+      min="1789"
+      max="2020">
   </div>
 </template>
 
 <script>
+import moment from 'moment'
 
 export default {
   name: 'InputDate',
@@ -13,7 +40,15 @@ export default {
   },
   data: function() {
     return {
-      item: this.value && this.value.toISOString().slice(0,10)
+      auto: !this.value,
+      day: this.value && moment(this.value).format('DD'),
+      month: this.value && moment(this.value).format('MM'),
+      year: this.value && moment(this.value).format('YYYY'),
+    }
+  },
+  computed: {
+    date: function() {
+      return `${this.year}-${this.month && this.month.padStart(2, '0')}-${this.day && this.day.padStart(2, '0')}`
     }
   },
   methods: {
@@ -22,7 +57,46 @@ export default {
       if (value) {
         this.$emit('input', value)
       }
+    },
+    update: function() {
+      const dt = moment(this.date, 'YYYY-MM-DD', true)
+      if (dt.isValid()) {
+        this.$emit('input', dt.toDate())
+      } else {
+        this.$emit('input', undefined)
+      }
     }
+  },
+  watch: {
+    day: function(to) {
+      if (to && to.length == 2 && this.auto) {
+        this.$refs.month.focus()
+      }
+      this.update()
+    },
+    month: function(to) {
+      if (to && to.length == 2 && this.auto) {
+        this.$refs.year.focus()
+      }
+      this.update()
+    },
+    year: function(to) {
+      if (to && to.length == 4 && this.auto) {
+        this.$refs.year.focus()
+      }
+      this.update()
+    },
   }
 }
 </script>
+
+<style scoped lang="css">
+div {
+  align-items: center;
+  display: flex;
+  flex-direction: row;
+}
+input {
+  width: 5em;
+}
+</style>

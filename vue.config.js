d53e89aef6f6c40b3af67fb0b5811cd223ebb54a
Jeremie Cook <hello@jeremiecook.com>
2019-11-07 14:07:09 +0100

    Prépare le futur d'une version front autonome

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,34 @@
 const configureAPI = require('./configure')
 
+
+
+var rp = require('request-promise')
+var outils = require('./backend/controllers/outils');
+var mapping = require('./backend/lib/openfisca/mapping');
+
+var buildOpenFiscaRequest = mapping.buildOpenFiscaRequest;
+function sendToOpenfisca (situation, callback) {
+    var request;
+    try {
+        request = buildOpenFiscaRequest(situation);
+    } catch(e) {
+        return callback({
+            message: e.message,
+            name: e.name,
+            stack: e.stack
+        });
+    }
+    rp({
+        uri: 'https://openfisca.mes-aides.gouv.fr/calculate',
+        method: 'POST',
+        body: request,
+        json: true,
+    })
+        .then(function(result) {
+            callback(null, result);
+        }).catch(callback);
+}
+
 module.exports = {
   chainWebpack(config) {
     config.module
@@ -15,6 +44,21 @@ module.exports = {
       }));
   },
   devServer: {
-    before: configureAPI
+    before: function(app) {
+
+      var cache;
+      app.route('/api/outils/communes/:codePostal').get(outils.communes);
+      app.post('/api/situations', function(req, res) {
+        cache = Object.assign({_id: 'yolo'}, req.body)
+
+        res.send(cache)
+      })
+
+      app.get('/api/situations/:id/openfisca-response', function(req, res) {
+        sendToOpenfisca(cache, function(err, result) {
+        res.send(Object.assign(result, { _id: 'yolo' }));
+        })
+      })
+    }
   }
 }

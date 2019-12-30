c3f332f65ee976387b887b22cbd1213b73b36a37
Thomas Guillet <guillet.thomas@gmail.com>
2019-12-30 16:48:35 +0100

    Facilite l'extraction de données mongo

---
+++ backend/lib/stats/cli.js
@@ -0,0 +1,41 @@
+/* eslint-disable no-console */
+var Promise = require('bluebird');
+var _ = require('lodash')
+var fs = Promise.promisifyAll(require('fs'))
+var mongodb = require('./mongodb')
+
+mongodb.connect()
+.then(db => {
+    const p = new Promise((resolve, reject) => {
+        db.collection('followups').find({ 'surveys.repliedAt': {$exists: true}}).toArray((err, items) => {
+            if (err) {
+                return reject(err)
+            }
+            var rows = []
+            items.forEach(i => {
+                const b = {
+                    situation: i.situation,
+                    date: i.createdAt.toISOString().slice(0,10)
+                }
+
+                i.surveys[0].answers.forEach(a => {
+                    rows.push(Object.assign({}, _.omit(a, '_id'), b))
+                })
+            })
+
+            resolve(rows)
+        })
+    })
+    return p
+})
+.then(r => {
+    var csv = r.map(i => Object.values(i).map(s => `"${s}"`).join(';'))
+    csv.unshift('benefit;result;comment;id;date')
+    return fs.writeFileAsync('data.csv', csv.join('\n'), 'utf-8')
+
+    //return fs.writeFileAsync('data.json', JSON.stringify(r, null, 2), 'utf-8')
+})
+.then(r => {
+    console.log(r)
+})
+.finally(mongodb.closeDb)

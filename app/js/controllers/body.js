a6974063cfb5151a556ca35ac4800307be5fb055
Thomas Guillet <guillet.thomas@gmail.com>
2020-01-13 21:54:08 +0100

    Supprime les fichiers Angular

--- app/js/controllers/body.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-angular.module('ddsApp').controller('BodyCtrl', function($scope, $timeout, SituationService) {
-    $scope.newSituation = function() {
-        SituationService.newSituation();
-    };
-});

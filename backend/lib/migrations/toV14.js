fb86895dffe1d04e4d79b839e1c962b210cad2e0
Thomas Guillet <guillet.thomas@gmail.com>
2020-01-20 13:40:53 +0100

    Supprime une représentation intermédiaire pour certains attributs booléens

---
+++ backend/lib/migrations/toV14.js
@@ -0,0 +1,37 @@
+/*
+ * Remove specific_situations
+ */
+var _ = require('lodash')
+
+var VERSION = 14
+
+var specificSituationValues = [
+    'chomeur', // Not passed to OpenFisca / Front only
+    'etudiant',
+    'retraite', // Not passed to OpenFisca / Front only
+    'handicap',
+    'inapte_travail'
+];
+
+function updatePerson(p) {
+    const s = p.specific_situations || (p.get && p.get('specific_situations')) || []
+    specificSituationValues.forEach(id => {
+        p[id] = s.indexOf(id) >= 0
+    })
+    return _.omit(p, 'specificSituations')
+}
+
+module.exports = {
+    function: function(situation) {
+
+        situation.demandeur = updatePerson(situation.demandeur)
+        situation.conjoint = updatePerson(situation.conjoint)
+
+        situation.enfants = situation.enfants.map(e => {
+            return updatePerson(e)
+        })
+
+        return situation
+    },
+    version: VERSION
+}

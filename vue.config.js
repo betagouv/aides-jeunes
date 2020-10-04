f03dccdc070071918889c7a163c9c4154012868c
Thomas Guillet <guillet.thomas@gmail.com>
2020-10-04 17:50:13 +0200

    Assure un décompte automatique des aides

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,13 @@
 const configureAPI = require('./configure')
 const mock = require('./mock')
 const before = process.env.NODE_ENV === 'front_only' ? mock : configureAPI
+var { forEach } = require('./app/js/constants/benefits/back')
 
-process.env.VUE_APP_BENEFIT_COUNT = 118
+let count = 0
+forEach(() => {
+  count = count + 1
+})
+process.env.VUE_APP_BENEFIT_COUNT = count
 
 module.exports = {
   configureWebpack: {

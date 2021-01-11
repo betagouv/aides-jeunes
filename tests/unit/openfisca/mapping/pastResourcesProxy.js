bf997c452acb85eb386e101a8f5f345b2826f461
Thomas Guillet <guillet.thomas@gmail.com>
2021-01-11 18:07:04 +0100

    Ajoute les tests migrés à Jest (#87)

---
+++ tests/unit/openfisca/mapping/pastResourcesProxy.js
@@ -0,0 +1,49 @@
+var subject = require('../../../../backend/lib/openfisca/mapping/individu/pastResourcesProxy');
+var expect = require('expect');
+
+describe('openfisca past resource proxy', function() {
+    var date = new Date('2019-02-14');
+
+    describe('proxyWithCurrentResources', function() {
+
+        describe('situation with 12 month rolling data', function() {
+            var individu = {
+                salaire_net: {
+                    '2019-01': 12000,
+                }
+            };
+
+            beforeEach(function() {
+                subject.proxyWithCurrentResources(individu, date);
+            });
+
+            it('populates current fiscal year', function() {
+                expect(individu.salaire_net['2017-01']).toEqual(1000);
+            });
+
+            it('populates the previous fiscal year', function() {
+                expect(individu.salaire_net['2016-01']).toEqual(1000);
+            });
+        });
+
+        describe('situation with fiscal data', function() {
+            var individu = {
+                salaire_imposable: {
+                    '2017': 12120
+                },
+                frais_reels: {
+                    '2017': 24240
+                },
+            };
+
+            beforeEach(function() {
+                subject.extendFiscalDataBackward(individu, date);
+            });
+
+            it('populates previous fiscal year', function() {
+                expect(individu.salaire_imposable['2016-01']).toEqual(1010);
+                expect(individu.frais_reels['2016']).toEqual(24240);
+            });
+        });
+    });
+});
---
+++ tests/unit/openfisca/mapping/propertyMove.js
@@ -0,0 +1,48 @@
+var subject = require('../../../../backend/lib/openfisca/mapping/propertyMove');
+var expect = require('expect');
+
+describe('openfisca mapping property move', function() {
+    describe('movePropertyValuesToGroupEntity', function() {
+        describe('situation with data', function() {
+            var testCase = {
+                individus: {
+                    bob: {
+                        aide_logement: {
+                            '2015-01': 12,
+                        },
+                        rsa: {
+                            '2015': 42,
+                        },
+                    },
+                },
+                familles: {
+                    _: {
+                        parents: ['bob'],
+                        enfants: [],
+                    },
+                },
+                foyers_fiscaux: {},
+            };
+
+            it('moves across ressources', function() {
+                subject.movePropertyValuesToGroupEntity(testCase);
+
+                expect(testCase.familles._.rsa).toBeTruthy();
+                expect(testCase.familles._.aide_logement).toBeTruthy();
+                expect(testCase.individus.bob.aide_logement).toBeFalsy();
+            });
+        });
+        describe('situation without data', function() {
+            var testCase = {
+                individus: {},
+                familles: { _: {}},
+                foyers_fiscaux: {},
+            };
+            subject.movePropertyValuesToGroupEntity(testCase);
+
+            it('runs completely', function() {
+                expect(testCase.familles._.rsa).toBeFalsy();
+            });
+        });
+    });
+});

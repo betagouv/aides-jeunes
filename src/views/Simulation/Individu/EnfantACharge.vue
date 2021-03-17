4223bf46d8196b03b49b0ff3482f894145dc329c
lucharrier <lucharrier@gmail.com>
2021-03-17 18:10:24 +0100

    Removed changes

--- src/views/Simulation/Individu/EnfantACharge.vue
+++ src/views/Simulation/Individu/EnfantACharge.vue
@@ -1,7 +1,8 @@
 <template>
   <form @submit.prevent='onSubmit'>
     <YesNoQuestion v-model="value">
-      <h1>{{ role === 'demandeur' ? `Figurez vous sur la dernière déclarion d'impôts de vos parents ?` : `Figure-t-il/elle sur votre dernière déclaration d'impôt sur le revenu&nbsp;?`}}</h1>    </YesNoQuestion>
+      <h1>{{ role === 'demandeur' ? `Figurez vous sur la dernière déclarion d'impôts de vos parents ?` : `Figure-t-il/elle sur votre dernière déclaration d'impôt sur le revenu&nbsp;?`}}</h1>
+    </YesNoQuestion>
     <Actions v-bind:onSubmit='onSubmit'/>
   </form>
 </template>

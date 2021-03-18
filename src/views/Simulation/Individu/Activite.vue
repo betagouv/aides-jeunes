9b7ef0974d81582c468eb487c429b1afe608a26c
lucharrier <lucharrier@gmail.com>
2021-03-18 16:58:19 +0100

    Removed compute date

--- src/views/Simulation/Individu/Activite.vue
+++ src/views/Simulation/Individu/Activite.vue
@@ -12,7 +12,6 @@
 <script>
 import Actions from '@/components/Actions'
 import Individu from '@/lib/Individu'
-import { datesGenerator } from '../../../../backend/lib/mes-aides'
 
 export default {
   name: 'SimulationActivite',
@@ -24,7 +23,6 @@ export default {
     const role = id.split('_')[0]
     const {individu} = Individu.get(this.$store.getters.peopleParentsFirst, role, this.$route.params.id, this.$store.state.dates)
     const value = individu.activite
-    let situation = this.$store.state.situation
     let ActiviteOptions = [
       {
         value: 'actif',
@@ -48,7 +46,7 @@ export default {
       },
     ]
     const RETRAITE_INDEX = 3
-    const age = Individu.age(individu, datesGenerator(situation.dateDeValeur).today.value);
+    const age = Individu.age(individu, this.$store.state.dates.today.value);
     if (age < 30) {
       ActiviteOptions.splice(RETRAITE_INDEX, 1)
     }

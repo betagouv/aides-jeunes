25c7617c5d66257009cd3e8c81fad306bb1b14b4
Thomas Guillet <guillet.thomas@gmail.com>
2021-03-19 19:38:53 +0100

    Affiche proprement la question sur le début du chômage (#1526)
    * Cassé suite à une erreur de gestion de conflits

--- src/views/Simulation/Individu/DateDebutChomage.vue
+++ src/views/Simulation/Individu/DateDebutChomage.vue
@@ -1,7 +1,7 @@
 <template>
   <form @submit.prevent='onSubmit'>
     <label>
-      <h1>`Quand ${getLabel('avoir')} commencé à être au chômage&nbsp;?`</h1>
+      <h1>Quand {{getLabel('avoir')}} commencé à être au chômage&nbsp;?</h1>
       <InputDate required id="date_debut_chomage" v-model="value" />
       <p class="notification warning" v-if="error">
         Ce champ est obligatoire.

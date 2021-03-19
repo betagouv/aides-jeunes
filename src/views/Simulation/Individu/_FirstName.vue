952fa6ffa764746fe2639f2097a3d1ac8329dd9f
Thomas Guillet <guillet.thomas@gmail.com>
2021-03-19 09:34:14 +0100

    Relie le label et l'input pour le prénom des enfants

--- src/views/Simulation/Individu/_FirstName.vue
+++ src/views/Simulation/Individu/_FirstName.vue
@@ -1,8 +1,10 @@
 <template>
   <form @submit.prevent='onSubmit'>
-    <h1>Quel est le prénom de votre enfant ?
-    <span>il servira uniquement à vous faciliter la saisie par la suite</span></h1>
-    <input type="text" v-model="value">
+    <label for="_firstName">
+      <h1>Quel est le prénom de votre enfant ?
+      <span>il servira uniquement à vous faciliter la saisie par la suite</span></h1>
+    </label>
+    <input type="text" id="_firstName" v-model="value">
     <p class="notification warning" v-if="error">
         Ce champ est obligatoire.
     </p>

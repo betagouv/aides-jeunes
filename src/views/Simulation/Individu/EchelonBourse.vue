ad86aade7356285bd99c8507a7671a117a803b53
Clément Lelong <lelong@cymit.fr>
2021-03-22 17:23:25 +0100

    Ajoute le style des input range & refactor le HTML de l'étape echelon bourse

--- src/views/Simulation/Individu/EchelonBourse.vue
+++ src/views/Simulation/Individu/EchelonBourse.vue
@@ -1,6 +1,6 @@
 <template>
   <form @submit.prevent='onSubmit'>
-    <h1>À quel échelon {{ getLabel('être') }} boursier ?</h1>
+    <legend>À quel échelon {{ getLabel('être') }} boursier ?</legend>
     <input id="echelon-bourse" v-model="value" type="range" min="-1" max="7">
     {{ value == -1 ? 'Non boursier': 'Boursier échelon ' + value }}
     <Actions v-bind:onSubmit='onSubmit'/>

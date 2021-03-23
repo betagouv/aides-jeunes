3c161db1fa8b0c5f407d1668534097823bc0b4f0
Thomas Guillet <guillet.thomas@gmail.com>
2021-03-23 07:58:08 +0100

    Extrait les classes Step dans un fichier à part

--- src/lib/State/blocks.js
+++ src/lib/State/blocks.js
@@ -1,48 +1,7 @@
 const Individu = require('@/lib/Individu').default;
 const Ressource = require('@/lib/Ressource').default;
 const { datesGenerator } = require('../../../backend/lib/mes-aides');
-
-function Step({key, entity, id, variable}) {
-  this.fullPath = entity ? `/simulation/${entity}${id ? `/${id}` : ''}${variable ? `/${variable}` : ''}` : '/'
-  this.key = key || this.fullPath
-  this.entity = entity
-  this.id = id
-  this.variable = variable
-}
-
-const updateMethods = {
-  individu: 'updateIndividu',
-  famille: 'updateFamille',
-  foyerFiscal: 'updateFoyerFiscal',
-  menage: 'updateMenage',
-}
-
-const internalUpdateMethods = {
-  individu: 'saveIndividu',
-  famille: 'saveFamille',
-  foyerFiscal: 'saveFoyerFiscal',
-  menage: 'saveMenage',
-}
-
-Step.prototype.clean = function({commit, dispatch, state}, storeInternal) {
-  const subject = state.situation[state.entity] || state.situation[this.id] || state.situation.enfants.find(enfant => enfant.id === this.id)
-  const result = {...subject, [this.variable]: undefined}
-  const updateMethod = updateMethods[this.entity]
-  const internalUpdateMethod = internalUpdateMethods[this.entity]
-  return storeInternal ? commit(internalUpdateMethod, result) : dispatch(updateMethod, result)
-}
-
-function ComplexStep({route, variables}) {
-  Step.call(this, {key: route})
-  this.fullPath = `/simulation/${route}`
-
-  this.substeps = variables ? variables.map(v => new Step(v)) : []
-}
-
-ComplexStep.prototype = Object.create(Step.prototype)
-ComplexStep.prototype.clean = function(store) {
-  this.substeps.forEach(s => s.clean(store))
-}
+const { Step, ComplexStep } = require('./steps');
 
 function individuBlockFactory(id) {
   const r = variable => new Step({entity: 'individu', id, variable})
@@ -228,6 +187,4 @@ function generateBlocks(situation) {
 
 module.exports = {
   generateBlocks,
-  Step,
-  ComplexStep,
 }

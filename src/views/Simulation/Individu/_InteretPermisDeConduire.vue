f48f08d1c905c972a79a0f96caa4fa2639d7efda
lucharrier <lucharrier@gmail.com>
2021-03-23 18:58:23 +0100

    Add file permis de conduire screen

---
+++ src/views/Simulation/Individu/_InteretPermisDeConduire.vue
@@ -0,0 +1,23 @@
+<template>
+  <form @submit.prevent='onSubmit'>
+    <YesNoQuestion v-model="value">
+      <h1>Prévoyez-vous de passer le permis de conduire ?</h1>
+    </YesNoQuestion>
+    <Actions v-bind:onSubmit='onSubmit'/>
+  </form >
+</template>
+
+<script>
+import Actions from '@/components/Actions'
+import YesNoQuestion from '@/components/YesNoQuestion'
+import { createIndividuMixin } from '@/mixins/IndividuMixin'
+
+export default {
+  name: 'SimulationIndividuInteretPermisDeConduire',
+  components: {
+    Actions,
+    YesNoQuestion
+  },
+  mixins: [createIndividuMixin('_interetPermisDeConduire')],
+}
+</script>

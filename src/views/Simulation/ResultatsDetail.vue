d98c4317bcdf039675605839404dc39ec3d1af15
lucharrier <lucharrier@gmail.com>
2021-03-24 11:25:27 +0100

    Use component DroitsDetails

--- src/views/Simulation/ResultatsDetail.vue
+++ src/views/Simulation/ResultatsDetail.vue
@@ -1,90 +1,18 @@
 <template>
   <div class="container">
-    <div class="droit-detail"
-      itemscope itemtype="http://schema.org/GovernmentService">
-
-      <div class="droit-detail-heading">
-        <h2 itemprop="name">{{ droit.label }}</h2>
-        <div class="dotted-line"></div>
-        <DroitMontant v-bind:droit="droit" unexpected v-if="droit.montant && (isString(droit.montant) || isNumber(droit.montant))" />
-      </div>
-
-      <div class="notification warning print-hidden" v-if="droit.isBaseRessourcesYearMinusTwo && ! ressourcesYearMinusTwoCaptured && ! isString(droit.montant)">
-        <span>
-          <i class="fa fa-warning" aria-hidden="true"></i>  Cette aide se base sur vos ressources de l'année {{ $store.state.dates.fiscalYear.label }}
-        </span>
-
-        <router-link class="button-outline warning text-center" to="ressources/fiscales">Déclarez vos ressources {{ $store.state.dates.fiscalYear.label }}</router-link>
-      </div>
-
-      <div class="notification warning print-hidden" v-if="droit.isBaseRessourcesPatrimoine && ! patrimoineCaptured && ! isString(droit.montant)">
-        <span>
-          <i class="fa fa-warning" aria-hidden="true"></i> Cette aide se base sur votre patrimoine. Vous avez un patrimoine immobilier, d'épargne, des revenus fonciers et/ou du capital ? Vous devez renseigner des informations complémentaires.
-        </span>
-
-        <router-link class="button-outline warning text-center" to="ressources/patrimoine">Déclarez votre patrimoine</router-link>
-      </div>
-
-      <div class="notification warning print-hidden" v-if="isString(droit.montant)">
-        <p>
-          L'application Mes Aides ne peut pas calculer le montant de cette prestation, car <span v-html="droit.uncomputability[droit.montant].reason.user"></span>.
-          <br>
-          <strong
-            v-if="droit.uncomputability[droit.montant].solution"
-            v-html="droit.uncomputability[droit.montant].solution"></strong>
-        </p>
-      </div>
-
-      <div class="droit-detail-description">
-        <p>
-          <span v-html="droit.description" itemprop="description"></span> <BenefitCtaLink
-            v-if="droit.link"
-            v-bind:analytics-name="droit.label"
-            v-bind:link="droit.link"
-            v-bind:benefit="droit"
-            type="link"
-            level="'inline'"
-            itemprop="termsOfService"
-          />
-        </p>
-        <div v-if="droit.conditions">
-          <p>Pour en bénéficier, vous devez également :</p>
-          <ul class="list-unstyled">
-            <li v-for="(condition, index) in droit.conditions" v-bind:key="index">
-              <i class="fa fa-check" aria-hidden="true"></i> <span v-html="condition"></span>
-            </li>
-          </ul>
-        </div>
-      </div>
-
-      <div class="droit-detail-buttons print-hidden">
-        <BenefitCta class="droit-detail-buttons-cta" v-bind:benefit="droit"></BenefitCta>
-        <a v-if="droit.msa"
-          target="_blank"
-          rel="noopener"
-          href="http://www.msa.fr/lfr/web/msa/espace-prive"
-          v-analytics="{ name:droit.label, action:'msa', category:'General'}"
-        >
-          Démarches pour les professions agricoles
-        </a>
-        <router-link
-          class="button-outline primary"
-          v-if="droit.provider.etablissements && droit.provider.etablissements.length > 0"
-          v-analytics="{ name:droit.label, action:'show-locations', category:'General'}"
-          v-bind:to="{ name: 'resultat/lieux', params: { id: droit.id }}">
-          <i class="fa fa-home"></i> Trouver une agence
-        </router-link>
-      </div>
-    </div>
+    <DroitsDetails
+        :droit="droit"
+        :city="situation.menage.depcom"
+        :droits="droits"
+        :patrimoine-captured="! shouldPatrimoineBeCaptured"
+        :ressources-year-minus-two-captured="ressourcesYearMinusTwoCaptured" />
   </div>
 </template>
 
 <script>
 import _ from 'lodash'
 
-import BenefitCta from '../../components/BenefitCta'
-import BenefitCtaLink from '../../components/BenefitCtaLink'
-import DroitMontant from '../../components/DroitMontant'
+import DroitsDetails from '../../components/DroitsDetails.vue'
 
 export default {
   name: 'SimulationResultats',
@@ -96,10 +24,8 @@ export default {
       showPrivate: false
     }
   },
-    components: {
-    BenefitCta,
-    BenefitCtaLink,
-    DroitMontant,
+  components: {
+    DroitsDetails,
   },
   computed: {
     droits: function() { return this.resultats && this.resultats.droitsEligibles },

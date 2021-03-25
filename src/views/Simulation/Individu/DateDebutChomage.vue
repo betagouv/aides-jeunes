5f9e751c9813020772a780a972e69d6ffb09a063
Clément Lelong <lelong@cymit.fr>
2021-03-25 16:55:00 +0100

    Rend obligatoire DateDebutChomage

--- src/views/Simulation/Individu/DateDebutChomage.vue
+++ src/views/Simulation/Individu/DateDebutChomage.vue
@@ -17,6 +17,6 @@ export default {
     Actions,
     InputDate,
   },
-  mixins: [createIndividuMixin('date_debut_chomage', true)],
+  mixins: [createIndividuMixin('date_debut_chomage')],
 }
 </script>

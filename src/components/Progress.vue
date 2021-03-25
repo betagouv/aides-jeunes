97d47ad237f95fc6263727f776336d99a3ffb086
Thomas Guillet <1410356+guillett@users.noreply.github.com>
2021-03-25 08:32:04 +0100

    Met en évidence les pages en attente de création (#1542)

--- src/components/Progress.vue
+++ src/components/Progress.vue
@@ -4,7 +4,7 @@
     <h3>Parcours complet</h3>
     <ul>
       <li v-for="(step) in full" v-bind:key="step.key || step.fullPath">
-        <router-link v-bind:class="{ inactive: !step.isActive, current: step.fullPath == current}" v-bind:to="step.fullPath" >{{step.fullPath}}</router-link>
+        <router-link v-bind:class="{ inactive: !step.isActive, current: step.fullPath == current}" v-bind:to="step.fullPath" >{{step.fullPath}}</router-link> <abbr v-if="step.missing" title="Cette page n'existe pas encore dans le router.">🚧</abbr>
       </li>
     </ul>
   </div>
@@ -15,7 +15,19 @@ export default {
   name: 'Progress',
   computed: {
     full: function() {
-      return this.$state.full(this.$store.state.situation)
+      return this.$state.full(this.$store.state.situation).map(s => {
+        if (process.env.NODE_ENV === 'production') {
+          return s
+        } else {
+          const {route} = this.$router.resolve(s.fullPath)
+          return {
+            key: s.key,
+            isActive: s.isActive,
+            fullPath: s.fullPath,
+            missing: route.matched[route.matched.length-1].path.match(/:property/)
+          }
+        }
+      })
     },
     current: function() {
       return this.$route.fullPath

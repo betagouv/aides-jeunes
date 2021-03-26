a78c9d08e6d00e428785af6e4ee2ffa846683604
lucharrier <lucharrier@gmail.com>
2021-03-26 11:20:55 +0100

    Permettre la validation de l'écran échelon avec Entrée quand le focus est sur l'input

--- src/views/Simulation/Individu/EchelonBourse.vue
+++ src/views/Simulation/Individu/EchelonBourse.vue
@@ -1,9 +1,13 @@
 <template>
   <form @submit.prevent='onSubmit'>
-    <h1>À quel échelon {{ getLabel('être') }} boursier ?</h1>
-    <input id="echelon-bourse" v-model="value" type="range" min="-1" max="7">
-    {{ value == -1 ? 'Non boursier': 'Boursier échelon ' + value }}
-    <Actions v-bind:onSubmit='onSubmit'/>
+      <fieldset>
+        <legend><h1>À quel échelon {{ getLabel('être') }} boursier ?</h1></legend>
+        <label>
+          <input id="echelon-bourse" v-model="value" type="range" min="-1" max="7">
+          {{ value == -1 ? 'Non boursier': 'Boursier échelon ' + value }}
+        </label>
+        <Actions v-bind:onSubmit='onSubmit'/>
+      </fieldset>
   </form>
 </template>
 

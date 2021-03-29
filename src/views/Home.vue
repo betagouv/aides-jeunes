ee761982280d8ace7494eb10bcdd2193c53ae81c
Clément Lelong <lelong@cymit.fr>
2021-03-29 14:21:17 +0200

    Intègre la home 1j1s

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,45 +1,70 @@
 <template>
   <div id="homepage">
-    <div class="container">
-      <main class="hero">
-        <img src="./../../public/img/5220.svg" alt="Illustration" />
-
-        <div class="hero__container text-center">
-          <h1>
-            Évaluez vos droits à <strong>{{ prestationsNationalesCount + partenairesLocauxCount }} aides sociales</strong>. <br />En moins de <strong>7 minutes</strong>.
-          </h1>
-
-          <div class="cta">
-            <router-link
-              to="/experimentations"
-              v-bind:class="`button ${ctaSize} secondary`"
-              v-if="showExperiment"
-            >
-              🚀 Continuer l'expérimentation 🚀
-            </router-link>
-            <button v-bind:class="`button ${ctaSize} primary`"
-              v-on:click="newSituation()"
-              v-analytics="{ action: ctaLabel, category:'Home'}"
-            >
-              {{ctaLabel}}
-            </button>
-            <button v-bind:class="`button ${ctaSize} secondary`"
-              v-on:click="next()"
-              v-analytics="{ action: 'Reprendre ma simulation', category:'Home'}"
-              v-if="hasExistingSituation"
-            >
-              Reprendre la simulation
-            </button>
-          </div>
-          <p>
-            Ce questionnaire en ligne simple vous donnera un <strong>montant</strong> et vous donnera <strong>accès aux démarches</strong> pour les <strong>{{ prestationsNationalesCount }}</strong> aides nationales et <strong>{{ partenairesLocauxCount }}</strong> aides locales évaluées par le simulateur.
-          </p>
-          <router-link class="" to="/lieux">
-            Trouver le bon interlocuteur près de chez moi
-          </router-link>
+     <div class="container">
+        <div class="aj-home-hero">
+            <div class="aj-home-hero-content">
+                <h1>Avec le simulateur,<br><small>je découvre toutes les aides</small></h1>
+                <div class="aj-home-hero-buttons-wrapper">
+                    <button v-bind:class="`button ${ctaSize} secondary`"
+                            v-on:click="next()"
+                            v-analytics="{ action: 'Reprendre ma simulation', category:'Home'}"
+                            v-if="hasExistingSituation"
+                    >
+                        Reprendre ma simulation
+                    </button>
+                    <button v-bind:class="`button ${ctaSize} primary`"
+                    v-on:click="newSituation()"
+                    v-analytics="{ action: ctaLabel, category:'Home'}"
+                    >
+                        {{ctaLabel}}
+                    </button>
+                </div>
+            </div>
         </div>
-      </main>
+        <div class="aj-home-illustration">
+            <img src="@/assets/images/home_illustration.png" />
+        </div>
     </div>
+    <!--<div class="container">-->
+      <!--<main class="hero">-->
+        <!--<img src="./../../public/img/5220.svg" alt="Illustration" />-->
+
+        <!--<div class="hero__container text-center">-->
+          <!--<h1>-->
+            <!--Évaluez vos droits à <strong>{{ prestationsNationalesCount + partenairesLocauxCount }} aides sociales</strong>. <br />En moins de <strong>7 minutes</strong>.-->
+          <!--</h1>-->
+
+          <!--<div class="cta">-->
+            <!--<router-link-->
+              <!--to="/experimentations"-->
+              <!--v-bind:class="`button ${ctaSize} secondary`"-->
+              <!--v-if="showExperiment"-->
+            <!--&gt;-->
+              <!--🚀 Continuer l'expérimentation 🚀-->
+            <!--</router-link>-->
+            <!--<button v-bind:class="`button ${ctaSize} primary`"-->
+              <!--v-on:click="newSituation()"-->
+              <!--v-analytics="{ action: ctaLabel, category:'Home'}"-->
+            <!--&gt;-->
+              <!--{{ctaLabel}}-->
+            <!--</button>-->
+            <!--<button v-bind:class="`button ${ctaSize} secondary`"-->
+              <!--v-on:click="next()"-->
+              <!--v-analytics="{ action: 'Reprendre ma simulation', category:'Home'}"-->
+              <!--v-if="hasExistingSituation"-->
+            <!--&gt;-->
+              <!--Reprendre la simulation-->
+            <!--</button>-->
+          <!--</div>-->
+          <!--<p>-->
+            <!--Ce questionnaire en ligne simple vous donnera un <strong>montant</strong> et vous donnera <strong>accès aux démarches</strong> pour les <strong>{{ prestationsNationalesCount }}</strong> aides nationales et <strong>{{ partenairesLocauxCount }}</strong> aides locales évaluées par le simulateur.-->
+          <!--</p>-->
+          <!--<router-link class="" to="/lieux">-->
+            <!--Trouver le bon interlocuteur près de chez moi-->
+          <!--</router-link>-->
+        <!--</div>-->
+      <!--</main>-->
+    <!--</div>-->
   </div>
 </template>
 
@@ -83,7 +108,7 @@ export default {
       return this.$store.getters.passSanityCheck
     },
     ctaLabel: function() {
-      return this.hasExistingSituation ? 'Commencer une nouvelle simulation' : 'Évaluer mes droits'
+      return this.hasExistingSituation ? 'Commencer une nouvelle simulation' : 'Je commence'
     },
     ctaSize: function() {
       return this.hasExistingSituation ? 'large' : 'xlarge'

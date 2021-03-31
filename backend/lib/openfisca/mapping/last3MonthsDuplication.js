dd31b9f362c5053d7b3f4541ad8ac5bd88a2eddc
Thomas Guillet <thomas+github@palya.fr>
2021-03-31 14:02:12 +0200

    fixup! Regroupe les calculs des dates

--- backend/lib/openfisca/mapping/last3MonthsDuplication.js
+++ backend/lib/openfisca/mapping/last3MonthsDuplication.js
@@ -30,7 +30,7 @@ var forDuplication = types.reduce((accum, type) => {
 }, {})
 
 function copyTo3PreviousMonths(testCase, dateDeValeur) {
-    var periodKeys = ['thisMonth', 'oneMonthsAgo', 'twoMonthsAgo', 'threeMonthsAgo'];
+    var periodKeys = ['thisMonth', 'oneMonthAgo', 'twoMonthsAgo', 'threeMonthsAgo'];
     var periods = common.getPeriods(dateDeValeur);
 
     Object.keys(forDuplication).forEach(function(entityName) {

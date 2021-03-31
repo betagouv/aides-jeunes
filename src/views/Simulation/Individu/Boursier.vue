80d0e0b1f5dfe69e76671f6e66f0f7fc5fa84d0c
lucharrier <lucharrier@gmail.com>
2021-03-31 16:31:24 +0200

    Added new sreen Alternant, Boursier, AideMobiliteMasterSortieAcademie

---
+++ src/views/Simulation/Individu/Boursier.vue
@@ -0,0 +1,23 @@
+<template>
+  <form @submit.prevent='onSubmit'>
+    <YesNoQuestion v-model="value">
+      {{ getLabel('avoir') | capitalize }} boursier&nbsp;?
+    </YesNoQuestion>
+    <Actions v-bind:onSubmit='onSubmit'/>
+  </form>
+</template>
+
+<script>
+import Actions from '@/components/Actions'
+import YesNoQuestion from '@/components/YesNoQuestion'
+import { createIndividuMixin } from '@/mixins/IndividuMixin'
+
+export default {
+  name: 'SimulationIndividuBoursier',
+  components: {
+    Actions,
+    YesNoQuestion
+  },
+  mixins: [createIndividuMixin('boursier')],
+}
+</script>

3d4dc1faca940113689174f822fa3b945bb38cf8
lucharrier <lucharrier@gmail.com>
2021-04-02 13:26:55 +0200

    Fixed compute age

--- src/lib/State/blocks.js
+++ src/lib/State/blocks.js
@@ -175,8 +175,8 @@ function housingBlock() {
           new Step({entity: 'menage', variable: 'participation_frais'}),
           {
             isActive: (subject, situation) => {
-              const age = Individu.age(subject, datesGenerator(situation.dateDeValeur).today.value);
-              return age <= 30
+              const age = Individu.age(situation.demandeur, datesGenerator(situation.dateDeValeur).today.value);
+              return age < 30
             },
             steps: [
               new Step({entity: 'individu', id: 'demandeur', variable: 'habite_chez_parents'}),

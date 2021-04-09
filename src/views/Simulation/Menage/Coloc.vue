c12d5acaeffc5800b06bb72f60fa56316e3856ff
Clément Lelong <lelong@cymit.fr>
2021-04-09 17:01:36 +0200

    Corrige Enfants sur mobile & les hint

--- src/views/Simulation/Menage/Coloc.vue
+++ src/views/Simulation/Menage/Coloc.vue
@@ -60,12 +60,6 @@
 </script>
 
 <style scoped lang="scss">
-    span.help {
-        font-style: italic;
-        display: block;
-        font-size: 0.8em;
-    }
-
     fieldset {
         margin-bottom: 2em;
     }

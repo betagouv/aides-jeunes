961e2504e0be14b75f9294d2b2b3a4e71a01219e
Clément Lelong <lelong@cymit.fr>
2021-04-14 16:26:50 +0200

    Route dynamique pour EnSavoirPlus

--- src/router.js
+++ src/router.js
@@ -41,13 +41,7 @@ const router = new Router({
         {
           name: 'handicap',
           path: 'handicap',
-          component: () => import(/* webpackChunkName: "individu" */ './views/Simulation/Individu/Handicap.vue'),
-          children: [{
-            name: 'handicap_en_savoir_plus',
-            path: 'en_savoir_plus',
-            props: true,
-            component: () => import(/* webpackChunkName: "individu" */ './components/EnSavoirPlusContent.vue')
-          }]
+          component: () => import(/* webpackChunkName: "individu" */ './views/Simulation/Individu/Handicap.vue')
         },
         {
           name: 'taux_incapacite',
@@ -62,13 +56,7 @@ const router = new Router({
         {
           name: 'inapte_travail',
           path: 'inapte_travail',
-          component: () => import(/* webpackChunkName: "individu" */ './views/Simulation/Individu/InapteTravail.vue'),
-          children: [{
-            name: 'inapte_en_savoir_plus',
-            path: 'en_savoir_plus',
-            props: true,
-            component: () => import(/* webpackChunkName: "individu" */ './components/EnSavoirPlusContent.vue')
-          }]
+          component: () => import(/* webpackChunkName: "individu" */ './views/Simulation/Individu/InapteTravail.vue')
         },
         {
           name: 'date_debut_chomage',
@@ -213,12 +201,6 @@ const router = new Router({
       {
         path: 'enfants',
         component: () => import(/* webpackChunkName: "individu" */ './views/Simulation/Enfants.vue'),
-        children: [{
-          name: 'enfants_en_savoir_plus',
-          path: 'en_savoir_plus',
-          props: true,
-          component: () => import(/* webpackChunkName: "individu" */ './components/EnSavoirPlusContent.vue')
-        }]
       },
       {
         path: 'enfants/ressources',
@@ -307,6 +289,11 @@ const router = new Router({
       {
         path: ':id/:property',
         component: () => import(/* webpackChunkName: "individu" */ './views/Simulation/Property.vue'),
+      }, {
+          path: ':element/en_savoir_plus',
+          name: 'en_savoir_plus',
+          props: true,
+          component: () => import(/* webpackChunkName: "individu" */ './components/EnSavoirPlusContent.vue')
       }]
     },
     {
@@ -351,7 +338,7 @@ const router = new Router({
         meta: {
           title: 'Votre patrimoine'
         }
-      }, 
+      },
       {
         path: 'recapitulatif',
         component: () => import(/* webpackChunkName: "recapitulatif" */ './views/Foyer/Recapitulatif.vue'),

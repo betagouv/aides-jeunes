63d24ce5ef460c50df99c16990301026b7de4862
Clément Lelong <lelong@cymit.fr>
2021-04-15 17:23:27 +0200

    Termine l'implémentation du wizard simple

--- src/components/Wizard.vue
+++ src/components/Wizard.vue
@@ -1,21 +1,23 @@
 <template>
     <div class="aj-wizard-container">
-        <h1>Ma simulation</h1>
-        <div class="aj-progressBar-container">
-            <div class="aj-step-container">
-                <div v-for="(chapter,index) in chapters" :key="index" class="aj-step">
-                    <div class="aj-step-icon" :class="{'aj-step-done': chapter.done, 'aj-step-inactive': !chapter.done, 'aj-step-active' : index === currentStep}">
-                        <img v-if="chapter.done" src="../assets/images/done.svg" class="aj-check-icon">
+        <div class="aj-wizard-content">
+            <h1>Ma simulation</h1>
+            <div class="aj-progressBar-container">
+                <div class="aj-step-container">
+                    <div v-for="(chapter,index) in chapters" :key="index" class="aj-step">
+                        <div class="aj-step-icon" :class="{'aj-step-done': chapter.done, 'aj-step-inactive': !chapter.done, 'aj-step-active' : index === currentStep}">
+                            <img v-if="chapter.done" src="../assets/images/done.svg" class="aj-check-icon">
+                        </div>
+                        <router-link :to="getRootChapterPath(chapter.value)" class="aj-step-title" :class="{'aj-active-title' : index === currentStep, 'aj-disabled-title' : disabledLink(chapter, index)}">
+                            {{ chapter.title }}
+                        </router-link>
                     </div>
-                    <router-link :to="getRootChapterPath(chapter.value)" class="aj-step-title" :class="{'aj-active-title' : index === currentStep, 'aj-disabled-title' : disabledLink(chapter, index)}">
-                        {{ chapter.title }}
-                    </router-link>
                 </div>
+                <div class="aj-progressBar"></div>
             </div>
-            <div class="aj-progressBar"></div>
-        </div>
-        <div class="aj-btn-container">
-            <button :disabled="currentStep < (chapters.length + 1)" class="button">Voir les résultats</button>
+            <div class="aj-btn-container">
+                <router-link to="/simulation/resultats" :class="{'disabled': disableResults}" class="button">Voir les résultats</router-link>
+            </div>
         </div>
     </div>
 </template>
@@ -31,26 +33,31 @@ export default {
                 {id: 2, title: 'Mon foyer', value: 'foyer', done: false},
                 {id: 3, title: 'Mon logement', value: 'logement', done: false},
                 {id: 4, title: 'Mes revenus', value: 'revenus', done: false},
+                {id: 5, title: 'Mes projets', value: 'projets', done: false},
             ]
         }
     },
     computed: {
         currentStep() {
             return this.getStepByRoute(this.$route)
+        },
+        disableResults() {
+            return this.currentStep < 5
         }
     },
     methods: {
         setDoneChapters(step) {
           const currentChapter = this.chapters.find(c => c.value === step.chapter)
           this.chapters = this.chapters.map((c) => {
-              c.done = c.id < currentChapter.id
-              return c
+            c.done = !currentChapter || currentChapter && c.id < currentChapter.id
+            return c
           })
         },
         getStepByRoute(route) {
             const step = this.$state.current(route.path, this.$store.state.situation)
             const currentChapter = step && step.chapter || ''
             this.setDoneChapters(step)
+            console.log(step.chapter)
             switch (currentChapter) {
                 case 'profil':
                     return 0;
@@ -60,8 +67,10 @@ export default {
                     return 2;
                 case 'revenus':
                     return 3;
-                default:
+                case 'projets':
                     return 4;
+                default:
+                    return 5;
             }
         },
         getRootChapterPath(chapter) {

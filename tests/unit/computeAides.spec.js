e3c205e99f1d1bf3d1239da345eaaa10dff8a817
Thomas Guillet <thomas@palya.fr>
2021-04-26 21:32:05 +0200

    Adapte les tests au passage de logement_social en privé

--- tests/unit/computeAides.spec.js
+++ tests/unit/computeAides.spec.js
@@ -50,9 +50,6 @@ describe('computeAides', function () {
                 demandeur: {
                     css_participation_forfaitaire: {
                         '2014-11': 10
-                    },
-                    logement_social: {
-                        '2014-11': false
                     }
                 }
             },
@@ -96,17 +93,13 @@ describe('computeAides', function () {
             expect(plf).toBeTruthy()
             expect(plf.provider.label).toEqual('Ville de Paris')
             expect(plf.montant).toEqual(10)
-
-            var logement_social = droits.droitsNonEligibles.find(function(element) { return element.id === 'logement_social' })
-            expect(logement_social).toBeTruthy()
-            expect(logement_social.provider.label).toEqual('Ministère de la Cohésion des territoires')
         })
     })
 
     describe('computeAides of true boolean values', function() {
         beforeEach(function() {
-            openfiscaResult.individus.demandeur.logement_social['2014-11'] = true
-            droits = compute(situation, openfiscaResult)
+            openfiscaResult.individus.demandeur.logement_social = {'2014-11': true}
+            droits = compute(situation, openfiscaResult, true)
         })
 
         it('should extract eligibles droits from openfisca result', function() {

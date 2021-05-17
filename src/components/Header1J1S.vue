ca709b306f7c64f430ab81d24efe1625e8a7db01
antoine <antoine@telescoop.fr>
2021-05-17 12:02:40 +0200

    ajoute un lien vers 1j1s sur le svg république française

--- src/components/Header1J1S.vue
+++ src/components/Header1J1S.vue
@@ -2,9 +2,11 @@
     <header class="aj-1j1s-header">
         <div class="aj-1j1s-header-logo-container">
             <div class="container">
-                <svg width="100%" height="100%" viewBox="0 0 26 18" fill="none" xmlns="http://www.w3.org/2000/svg" fit="" preserveAspectRatio="xMidYMid meet" focusable="false" @click="toggleShowMenu()">
-                    <rect x="26" width="2" height="26" rx="1" transform="rotate(90 26 0)" fill="currentColor"></rect><rect x="26" y="8" width="2" height="26" rx="1" transform="rotate(90 26 8)" fill="currentColor"></rect><rect x="26" y="16" width="2" height="26" rx="1" transform="rotate(90 26 16)" fill="currentColor"></rect>
-                </svg>
+                <router-link to="/">
+                    <svg width="100%" height="100%" viewBox="0 0 26 18" fill="none" xmlns="http://www.w3.org/2000/svg" fit="" preserveAspectRatio="xMidYMid meet" focusable="false" @click="toggleShowMenu()">
+                        <rect x="26" width="2" height="26" rx="1" transform="rotate(90 26 0)" fill="currentColor"></rect><rect x="26" y="8" width="2" height="26" rx="1" transform="rotate(90 26 8)" fill="currentColor"></rect><rect x="26" y="16" width="2" height="26" rx="1" transform="rotate(90 26 16)" fill="currentColor"></rect>
+                    </svg>
+                </router-link>
                 <router-link to="/">
                     <img class="rf" src="@/../public/img/logo_rf.svg"><img class="fr" src="@/../public/img/logo1j1s-france-relance.svg">
                 </router-link>

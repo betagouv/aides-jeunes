cba68cc579dec934dc29d20cf2a9c7ca1dc0fc5d
Quentin Madura <quentin.madura@gmail.com>
2021-05-17 15:27:06 +0200

    [+] <- Ajout prettier pour vue

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,26 @@
 module.exports = {
-    "env": {
-        "node": true,
-        "es6": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:vue/essential",
-        "prettier",
-        "plugin:cypress/recommended"
-    ],
-    "plugins": ["prettier", "vue"],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly",
-
-    },
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "module",
-        "parser": "babel-eslint",
-    },
-    "rules": {
-        "no-irregular-whitespace": 0
-    }
+  env: {
+    node: true,
+    es6: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:vue/essential",
+    "prettier",
+    "plugin:cypress/recommended",
+    "plugin:prettier-vue/recommended",
+  ],
+  plugins: ["prettier", "vue"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module",
+    parser: "babel-eslint",
+  },
+  rules: {
+    "no-irregular-whitespace": 0,
+  },
 };

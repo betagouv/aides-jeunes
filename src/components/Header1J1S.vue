bc704e5f4571668a0369fee86be75750fae1fa7e
antoine <antoine@telescoop.fr>
2021-05-17 13:44:39 +0200

    ajoute un titre au svg pour l'accessibilité

--- src/components/Header1J1S.vue
+++ src/components/Header1J1S.vue
@@ -4,6 +4,7 @@
             <div class="container">
                 <router-link to="/">
                     <svg width="100%" height="100%" viewBox="0 0 26 18" fill="none" xmlns="http://www.w3.org/2000/svg" fit="" preserveAspectRatio="xMidYMid meet" focusable="false" @click="toggleShowMenu()">
+                        <title>Logo république française</title>
                         <rect x="26" width="2" height="26" rx="1" transform="rotate(90 26 0)" fill="currentColor"></rect><rect x="26" y="8" width="2" height="26" rx="1" transform="rotate(90 26 8)" fill="currentColor"></rect><rect x="26" y="16" width="2" height="26" rx="1" transform="rotate(90 26 16)" fill="currentColor"></rect>
                     </svg>
                 </router-link>

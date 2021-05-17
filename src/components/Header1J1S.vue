1308a1c38c9b09ade7beb69930e010b354da6331
antoine <antoine@telescoop.fr>
2021-05-17 11:12:42 +0200

    ajoute un bouton vers la home 1j1s en desktop

--- src/components/Header1J1S.vue
+++ src/components/Header1J1S.vue
@@ -8,6 +8,9 @@
                 <router-link to="/">
                     <img class="rf" src="@/../public/img/logo_rf.svg"><img class="fr" src="@/../public/img/logo1j1s-france-relance.svg">
                 </router-link>
+                <div class="aj-1j1s-header-right-links">
+                    <router-link class="button outline" to="/" v-if="$route.name !== 'home'">Retourner à l'accueil du simulateur</router-link>
+                </div>
             </div>
         </div>
         <nav class="aj-1j1s-header-menu" :class="{'show': showMenu}">

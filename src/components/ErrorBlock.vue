e61492ee9e90f2652df4c505ce35bcad5676129e
Thomas Guillet <thomas@palya.fr>
2021-05-21 07:33:56 +0200

    fixup! Améliore le message d'erreur en cas de timeout

--- src/components/ErrorBlock.vue
+++ src/components/ErrorBlock.vue
@@ -80,7 +80,7 @@ export default {
         : JSON.stringify(value, null, 2)
     },
     isTimeoutError: function () {
-      return this.errorText && this.errorText.match(/toObject/) //(/time.?out/i)
+      return this.errorText && this.errorText.match(/time.?out/i)
     },
     resultats: function () {
       return this.$store.state.calculs.resultats

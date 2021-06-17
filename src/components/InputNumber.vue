4afb23792cd2dd5ca7421c3b4ebe823259df09c3
kout95 <antoine.bernier@outlook.fr>
2021-06-17 10:29:29 +0200

    Change le type input number par text

--- src/components/InputNumber.vue
+++ src/components/InputNumber.vue
@@ -1,33 +1,21 @@
 <template>
-  <input type="number" v-select-on-click v-model.number="result" />
+  <input type="text" v-select-on-click v-model="result" />
 </template>
 
 <script>
-const isNumber = require("lodash/isNumber")
-
 export default {
   name: "InputNumber",
   props: ["value"],
-  data() {
-    return {
-      valueHistory: null,
-    }
-  },
   computed: {
     result: {
       get: function () {
-        return this.value || this.valueHistory
+        return this.value
       },
       set: function (value) {
-        let errorMessage
+        const floatValue = parseFloat(value)
         let emittedValue
-        if (isNumber(value)) {
-          emittedValue = value
-        } else if (value !== undefined) {
-          errorMessage = "Veuillez renseigner un nombre valide."
-        }
+        if (value) emittedValue = floatValue
         this.$emit("input", emittedValue)
-        this.$store.dispatch("updateError", errorMessage)
       },
     },
   },

7942643d798027babb193f0a30c840eda917b3df
Quentin Madura <quentin.madura@gmail.com>
2021-07-13 12:31:55 +0200

    Corrige les calculs de distances pour les villes de Marseille, Lyon et Paris

--- cli.js
+++ cli.js
@@ -5,13 +5,26 @@ var communesMap = require("communes-lonlat").reduce((map, item) => {
   return map
 }, {})
 
-
-console.log(process.argv)
-var origin = communesMap[process.argv[2]]
-var destination = communesMap[process.argv[3]]
-
-console.log('origin', origin)
-console.log('destination', destination)
+const processing = {
+  arrondissements(inseeCode) {
+    if (inseeCode) {
+      if (inseeCode.startsWith("132")) {
+        return 13055
+      }
+      if (inseeCode.startsWith("693")) {
+        return 69123
+      }
+      if (inseeCode.startsWith("751")) {
+        return 75056
+      }
+    }
+    return inseeCode
+  },
+}
+var origin = communesMap[processing.arrondissements(process.argv[2])]
+var destination = communesMap[processing.arrondissements(process.argv[3])]
+console.log(origin)
+console.log(destination)
 if (origin && destination) {
   distance = haversine(
     origin.centre.coordinates,

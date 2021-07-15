265e6fdd692a49bbe51766afff849eb0c7e2e4cd
kout95 <antoine.bernier@outlook.fr>
2021-07-15 10:52:36 +0200

    Ajout un bouton lien invalide pour envoyer un event matomo lorsqu'un lien est cassé

--- src/components/DroitsDetails.vue
+++ src/components/DroitsDetails.vue
@@ -126,6 +126,18 @@
             class="aj-droit-content-buttons-cta"
             v-bind:benefit="droit"
           ></BenefitCta>
+          <div class="is-align-vertically-center">
+            <a
+              class="text-center"
+              @click="alertBrokenLink()"
+              v-if="showBrokenLinkButton"
+              >Lien(s) invalide(s) ? Cliquer ici pour nous notifier !</a
+            >
+            <span class="text-center" v-else-if="showThanksMessage"
+              >Merci pour votre aide ! Nous réglerons ce problème très
+              prochainement.</span
+            >
+          </div>
         </div>
 
         <a
@@ -165,12 +177,28 @@ export default {
     BenefitCtaLink,
     DroitMontant,
   },
+  data() {
+    return {
+      showBrokenLinkButton: true,
+      showThanksMessage: false,
+    }
+  },
   methods: {
     isEmpty: (array) => array.length === 0,
     isNotEmpty: (array) => array.length !== 0,
     isBoolean: (val) => typeof val === "boolean",
     isNumber: (val) => typeof val === "number",
     isString: (val) => typeof val === "string",
+    alertBrokenLink() {
+      this.showBrokenLinkButton = false
+      this.showThanksMessage = true
+      setTimeout(() => (this.showThanksMessage = false), 5000)
+      this.$matomo.trackEvent(
+        "General",
+        "Erreur lien aide invalide",
+        this.droit.label
+      )
+    },
   },
 }
 </script>

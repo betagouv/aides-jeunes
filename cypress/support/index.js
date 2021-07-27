63de6c3e535c683ffa55506502c8ca135663af9f
Thomas Guillet <thomas@palya.fr>
2021-07-27 15:31:25 +0200

    Étend le délai d'attente de la page de résultats dans les tests E2E

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -324,7 +324,7 @@ export function hasPrimeActivite(position) {
   const name = /prime d’activité/
   const id = "ppa"
   const description = /revenus/
-  cy.get("#print-disclaimer", { timeout: 15000 })
+  cy.get("#print-disclaimer", { timeout: 20000 })
     .invoke("text")
     .should("contain", "engagement")
   cy.get(
@@ -360,7 +360,7 @@ export function hasAAH() {
   const name = /allocation aux adultes handicapés/
   const id = "aah"
   const description = "AAH"
-  cy.get("#print-disclaimer", { timeout: 15000 })
+  cy.get("#print-disclaimer", { timeout: 20000 })
     .invoke("text")
     .should("contain", "engagement")
   cy.get(
@@ -396,7 +396,7 @@ export function hasLogementSocial() {
   const name = /logement social/
   const id = "logement"
   const description = /revenus/
-  cy.get("#print-disclaimer", { timeout: 15000 })
+  cy.get("#print-disclaimer", { timeout: 20000 })
     .invoke("text")
     .should("contain", "engagement")
   cy.get(
@@ -427,7 +427,7 @@ export function hasBourseCriteresSociaux(position) {
   const name = /bourse sur critères sociaux/
   const id = "bourse_criteres_sociaux"
   const description = /BCS/
-  cy.get("#print-disclaimer", { timeout: 15000 })
+  cy.get("#print-disclaimer", { timeout: 20000 })
     .invoke("text")
     .should("contain", "engagement")
   cy.get(

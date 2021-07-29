d290acb4e06c43f4ba73dfcc23e471bc9812b762
Thomas Guillet <thomas@palya.fr>
2021-07-29 15:09:44 +0200

    fixup! Rétablit les tests de génération des tests OpenFisca en Yaml

--- app/js/constants/benefits/utils.js
+++ app/js/constants/benefits/utils.js
@@ -24,7 +24,7 @@ function transformInstitutions(collection) {
         data.testing_benefits || []
       ),
       national: data.national,
-      repository: data.repository || (!data.national && "france-local"),
+      repository: data.repository || (data.national ? null : "france-local"),
     }
     result[data.slug] = item
     return result

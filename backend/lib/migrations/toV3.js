2cef5f7c2adb42b7bc7220fc3b9eb4978a9210fd
kout95 <antoine.bernier@outlook.fr>
2021-08-05 13:53:30 +0200

    Corrige la migration

--- backend/lib/migrations/toV3.js
+++ backend/lib/migrations/toV3.js
@@ -21,12 +21,12 @@ function updatePerson(p) {
     p[renames[prop]] = p[prop]
   })
 
-  extras = [
+  const extras = [
     "tns_auto_entrepreneur_type_activite",
     "tns_micro_entreprise_type_activite",
   ]
 
-  AE_map = {
+  const AE_map = {
     achat_revente: "rpns_auto_entrepreneur_CA_achat_revente",
     bic: "rpns_auto_entrepreneur_CA_bic",
     bnc: "rpns_auto_entrepreneur_CA_bnc",
@@ -38,19 +38,14 @@ function updatePerson(p) {
     }
   }
 
-  micro_map = {
-    achat_revente: "rpns_micro_entreprise_CA_bic_vente_imp",
-    bic: "rpns_micro_entreprise_CA_bic_service_imp",
-    bnc: "rpns_micro_entreprise_CA_bnc_imp",
-  }
   if (p.tns_micro_entreprise_chiffre_affaires) {
-    var dest_field = AE_map[p.tns_micro_entreprise_type_activite]
+    let dest_field = AE_map[p.tns_micro_entreprise_type_activite]
     if (dest_field) {
       p[dest_field] = p.tns_micro_entreprise_chiffre_affaires
     }
   }
 
-  old_CA = [
+  const old_CA = [
     "tns_auto_entrepreneur_chiffre_affaires",
     "tns_micro_entreprise_chiffre_affaires",
   ]

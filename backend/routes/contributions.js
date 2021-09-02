9a6381221bba8a79b456ba9e87ebeb37fc4905e5
Quentin Madura <quentin.madura@gmail.com>
2021-09-02 14:28:08 +0200

    wip

--- backend/routes/contributions.js
+++ backend/routes/contributions.js
@@ -2,10 +2,15 @@ var cors = require("cors")
 const AuthContribution = require("../controllers/contributions")
 const { urlencoded } = require("body-parser")
 
+const ALLOWED_URLS = [
+  "https://test-contribution.netlify.app",
+  "https://contribuer-aides-jeunes.netlify.app",
+]
+
 module.exports = function (api) {
   // parse application/x-www-form-urlencoded
   api.use(urlencoded({ extended: false }))
   api
     .route("/contribution/auth")
-    .post(cors({ origin: "*" }), AuthContribution.verify)
+    .post(cors({ origin: ALLOWED_URLS }), AuthContribution.verify)
 }

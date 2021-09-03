34084a2757b3658112c9b2a6e2893566a17b9913
kout95 <antoine.bernier@outlook.fr>
2021-09-03 10:26:10 +0200

    Envoie un event matomo lors de l'error router navigation cancelled

--- src/plugins/StateService.js
+++ src/plugins/StateService.js
@@ -13,11 +13,18 @@ const StateService = {
       const nextStep = next(this.$route, this.$store.getters.getAllSteps)
       this.$router.push(nextStep.path).catch((failure) => {
         if (
-          !VueRouter.isNavigationFailure(
+          VueRouter.isNavigationFailure(
             failure,
             VueRouter.NavigationFailureType.cancelled
           )
         ) {
+          this.$matomo &&
+            this.$matomo.trackEvent(
+              "Parcours",
+              "Navigation cancelled",
+              failure.toString()
+            )
+        } else {
           throw new Error(failure)
         }
       })

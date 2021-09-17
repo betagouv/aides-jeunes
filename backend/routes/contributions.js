173a794371cf1792e71e66c2c5f0dec172a18ce0
Quentin Madura <quentin.madura@gmail.com>
2021-09-17 11:21:42 +0200

    Simplifie la route d'origine

--- backend/routes/contributions.js
+++ backend/routes/contributions.js
@@ -1,16 +1,15 @@
 var cors = require("cors")
 const AuthContribution = require("../controllers/contributions")
 const { urlencoded } = require("body-parser")
-
-const ALLOWED_URLS = [
-  "https://test-contribution.netlify.app",
-  "https://contribuer-aides-jeunes.netlify.app",
-]
+const config = require("../config/index")
 
 module.exports = function (api) {
   // parse application/x-www-form-urlencoded
   api.use(urlencoded({ extended: false }))
   api
     .route("/contribution/auth")
-    .post(cors({ origin: ALLOWED_URLS }), AuthContribution.verify)
+    .post(
+      cors({ origin: config.netlifyContributionURL }),
+      AuthContribution.verify
+    )
 }

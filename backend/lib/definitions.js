c42f759add8b3a6b97148cd961f41ec8ac68c049
kout95 <antoine.bernier@outlook.fr>
2021-09-20 10:43:01 +0200

    Change le nom des variables et améliore la question _nombreMoisDebutContratDeTravail

--- backend/lib/definitions.js
+++ backend/lib/definitions.js
@@ -167,6 +167,7 @@ var menage = {
   charges_locatives: Number,
   _codePostal: String,
   coloc: Boolean,
+  date_entree_logement: Date,
   depcom: String,
   logement_chambre: Boolean,
   loyer: Number,

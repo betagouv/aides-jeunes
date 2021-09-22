46e60ed09b33fb1cd8a8efcf040dbd3cfcdb0758
Quentin Madura <quentin.madura@gmail.com>
2021-09-22 17:06:32 +0200

    Ajoute des tests et de la logique

--- backend/lib/mes-aides/computeFrontEndBenefits.js
+++ backend/lib/mes-aides/computeFrontEndBenefits.js
@@ -1,21 +1,49 @@
+const moment = require("moment")
+const { isEqual } = require("lodash")
+function computeFrontEndBenefits(
+  droitsDescription,
+  situation,
+  openfiscaResponse
+) {
+  droitsDescription.forEach(function (
+    benefit,
+    benefitId,
+    provider,
+    providerID
+  ) {
+    if (benefitId == "a") {
+      const period = "2014-11" // getPeriods(situation.dateDeValeur).currentMonth.id
 
-function computeFrontEndBenefits(droitsDescription, situation, openfiscaResponse) {
-  droitsDescription.forEach(function(benefit, benefitId, provider, providerID) {
-    if (benefitId == 'a') {
-      const period = '2014-11' // getPeriods(situation.dateDeValeur).currentMonth.id
+      const elegibilite_age =
+        moment().diff(situation.demandeur.date_naissance, "years") >=
+          benefit.age_min &&
+        moment().diff(situation.demandeur.date_naissance, "years") <=
+          benefit.age_max
 
-      const age = 21 // date diff
-      const eligibilite_geo = true // region il faut un mapping
-      //const communes = require('@etalab/decoupage-administratif/data/communes.json')
+      const communes = require("@etalab/decoupage-administratif/data/communes.json")
+      const listCommunes = communes.filter(
+        (commune) =>
+          (benefit.departements &&
+            benefit.departements.includes(commune.departement)) ||
+          (benefit.region && benefit.region.includes(commune.region)) ||
+          (benefit.communes && benefit.communes.includes(commune.code))
+      )
+      const depcom = situation.menage.depcom
+      const eligibilite_geo = listCommunes.some(
+        (commune) =>
+          depcom === commune.code || depcom.startsWith(commune.departement)
+      )
 
-      const eligibilite_statuts = true //
-      //benefits.statuts.map(s => true).reduce((a, v) => a || v, false) // any([true, true, false]) => false
+      const eligibilite_statuts = benefit.statuts
+        .map((statut) => statut === situation.demandeur.activite)
+        .reduce((a, v) => a || v, false) // any([true, true, false])
+
       const montant = 200
-
-      const eligibilite = openfiscaResponse.individus.demandeur.activite[period] == 'chomeur'
+      const eligibilite =
+        eligibilite_statuts && elegibilite_age && period && eligibilite_geo
 
       const result = eligibilite ? montant || true : montant ? 0 : false
-      openfiscaResponse.individus.demandeur[benefitId] = {[period]: result }
+      openfiscaResponse.individus.demandeur[benefitId] = { [period]: result }
     }
   })
 }

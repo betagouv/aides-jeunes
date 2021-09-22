4f0af8e5cf0bc8647f9af8c14107fb10e3f9d48a
Quentin Madura <quentin.madura@gmail.com>
2021-09-22 17:06:32 +0200

    Ajoute des tests et de la logique

--- tests/unit/computeFrontEndBenefits.spec.js
+++ tests/unit/computeFrontEndBenefits.spec.js
@@ -1,44 +1,78 @@
 var expect = require("expect")
 var { buildOpenFiscaRequest } = require("@/../backend/lib/openfisca/mapping")
-var { computeFrontEndBenefits } = require("@/../backend/lib/mes-aides/computeFrontEndBenefits")
+var {
+  computeFrontEndBenefits,
+} = require("@/../backend/lib/mes-aides/computeFrontEndBenefits")
 import droitsDescription from "@/../app/js/constants/benefits/back"
-
 
 describe("computeAides", function () {
   it("adds the benefit amount when eligible", function () {
     var situation = {
       dateDeValeur: "2014-11-01",
       demandeur: {
-        id: 'demandeur',
-        date_naissance: '2000-01-01',
-        activite: 'chomeur',
+        id: "demandeur",
+        date_naissance: "2000-01-01",
+        activite: "chomeur",
       },
       famille: {},
       menage: {
-        depcom: '64201',
+        depcom: "64201",
       },
     }
     var openfiscaRequest = buildOpenFiscaRequest(situation)
     computeFrontEndBenefits(droitsDescription, situation, openfiscaRequest)
-    expect(openfiscaRequest.individus.demandeur.a['2014-11']).toBe(200)
+    expect(openfiscaRequest.individus.demandeur.a["2014-11"]).toBe(200)
   })
 
-  it("adds 0 when ineligible", function () {
+  it("adds 0 when ineligible statut", function () {
     var situation = {
       dateDeValeur: "2014-11-01",
       demandeur: {
-        id: 'demandeur',
-        date_naissance: '2000-01-01',
-        activite: 'actif',
+        id: "demandeur",
+        date_naissance: "2000-01-01",
+        activite: "actif",
       },
       famille: {},
       menage: {
-        depcom: '64201',
+        depcom: "64201",
       },
     }
     var openfiscaRequest = buildOpenFiscaRequest(situation)
     computeFrontEndBenefits(droitsDescription, situation, openfiscaRequest)
-    expect(openfiscaRequest.individus.demandeur.a['2014-11']).toBe(0)
+    expect(openfiscaRequest.individus.demandeur.a["2014-11"]).toBe(0)
   })
-
+  it("adds 0 when ineligible city", function () {
+    var situation = {
+      dateDeValeur: "2014-11-01",
+      demandeur: {
+        id: "demandeur",
+        date_naissance: "2000-01-01",
+        activite: "chomeur",
+      },
+      famille: {},
+      menage: {
+        depcom: "95201",
+      },
+    }
+    var openfiscaRequest = buildOpenFiscaRequest(situation)
+    computeFrontEndBenefits(droitsDescription, situation, openfiscaRequest)
+    expect(openfiscaRequest.individus.demandeur.a["2014-11"]).toBe(0)
+  })
+  it("adds 0 when ineligible age", function () {
+    var situation = {
+      dateDeValeur: "2014-11-01",
+      demandeur: {
+        id: "demandeur",
+        date_naissance: "1988-01-01",
+        activite: "chomeur",
+      },
+      famille: {},
+      menage: {
+        depcom: "64201",
+      },
+    }
+    var openfiscaRequest = buildOpenFiscaRequest(situation)
+    computeFrontEndBenefits(droitsDescription, situation, openfiscaRequest)
+    expect(openfiscaRequest.individus.demandeur.a["2014-11"]).toBe(0)
+  })
 })

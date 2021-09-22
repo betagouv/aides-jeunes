56eca99bd1a0873906baf9887b014f68c6521667
kout95 <antoine.bernier@outlook.fr>
2021-09-22 12:54:18 +0200

    Créer un middleware pour vérifier que le contributeur est bien connecté à netlify

--- backend/controllers/contributions.js
+++ backend/controllers/contributions.js
@@ -2,6 +2,12 @@ const axios = require("axios")
 const config = require("../config/index")
 
 const AuthContribution = {
+  notAuthorized(res) {
+    return res.send({
+      msg: "Not authorized",
+      statusCode: 401,
+    })
+  },
   verifyIdentity(token) {
     return axios.get(
       `${config.netlifyContributionURL}/.netlify/identity/user`,
@@ -15,12 +21,18 @@ const AuthContribution = {
   async verify(req, res) {
     const verifyIdentity = await AuthContribution.verifyIdentity(req.body.token)
     if (verifyIdentity.status !== 200)
-      return res.send({
-        msg: "Not found",
-        statusCode: 404,
-      })
+      return AuthContribution.notAuthorized(res)
     res.cookie("contribution_token", req.body.token)
     return res.redirect(301, "/")
+  },
+  isLogged(req, res, next) {
+    AuthContribution.verifyIdentity(req.body.token).then((verifyIdentity) => {
+      if (verifyIdentity.status !== 200) {
+        AuthContribution.notAuthorized(res)
+      } else {
+        next()
+      }
+    })
   },
 }
 

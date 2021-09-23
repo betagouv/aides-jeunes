a016ef5c4565adcfee0ef42297f4f8014741c2c7
Quentin Madura <quentin.madura@gmail.com>
2021-09-23 15:27:15 +0200

    Répare les tests dans la CI

--- backend/lib/openfisca/mapping/menage/index.js
+++ backend/lib/openfisca/mapping/menage/index.js
@@ -2,13 +2,10 @@ const cloneDeep = require("lodash/cloneDeep")
 const moment = require("moment")
 
 function buildOpenFiscaMenage(situation) {
-  console.log(situation)
   var openFiscaMenage = cloneDeep(situation.menage)
-  console.log(openFiscaMenage)
-  if (openFiscaMenage._nombreMoisEntreeLogement)
-    openFiscaMenage.date_entree_logement = moment(situation.dateDeValeur)
-      .subtract(openFiscaMenage._nombreMoisEntreeLogement ? 2 : 12, "months")
-      .format("YYYY-MM-DD")
+  openFiscaMenage.date_entree_logement = moment(situation.dateDeValeur)
+    .subtract(openFiscaMenage._nombreMoisEntreeLogement ? 2 : 12, "months")
+    .format("YYYY-MM-DD")
   return openFiscaMenage
 }
 

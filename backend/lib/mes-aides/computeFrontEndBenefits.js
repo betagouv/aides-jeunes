51eff46e7fd57ddc7afd7a41156683567aa16a38
kout95 <antoine.bernier@outlook.fr>
2021-09-27 16:51:25 +0200

    Ne rend pas les critères obligatoires

--- backend/lib/mes-aides/computeFrontEndBenefits.js
+++ backend/lib/mes-aides/computeFrontEndBenefits.js
@@ -14,11 +14,15 @@ function computeFrontEndBenefits(
 
   droitsDescription.forEach(function (benefit, benefitId) {
     if (benefitId === "benefit_front_test") {
-      const elegibiliteAge = age >= benefit.age_min && age <= benefit.age_max
+      const elegibiliteAge =
+        (benefit.age_min === undefined || age >= benefit.age_min) &&
+        (benefit.age_max === undefined || age <= benefit.age_max)
 
-      const eligibiliteStatuts = benefit.statuts.some(
-        (statut) => statut === situation.demandeur.activite
-      )
+      const eligibiliteStatuts =
+        benefit.statuts === undefined ||
+        benefit.statuts.some(
+          (statut) => statut === situation.demandeur.activite
+        )
 
       let eligibiliteGeo = true
 
@@ -37,12 +41,12 @@ function computeFrontEndBenefits(
           eligibiliteGeo && benefit.regions.includes(commune.region)
       }
 
-      const montant = 200
+      const montant = benefit.montant
 
       const eligibilite =
         eligibiliteStatuts && elegibiliteAge && period && eligibiliteGeo
 
-      const result = montant ? eligibilite * montant : eligibilite
+      const result = montant !== undefined ? eligibilite * montant : eligibilite
 
       openfiscaResponse.individus.demandeur[benefitId] = { [period]: result }
     }

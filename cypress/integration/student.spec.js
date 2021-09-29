1cca569ca514ae86e2b8b4bef34ec809f11f3cc2
Thomas Guillet <thomas@palya.fr>
2021-09-29 12:17:18 +0200

    Sépare les tests d'intégration E2E dans des fichiers distincts

---
+++ cypress/integration/student.spec.js
@@ -0,0 +1,48 @@
+/// <reference types="Cypress" />
+import * as steps from "../support"
+
+context("Full simulation", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:8080/init-ci")
+  })
+
+  it("accept a student situation", () => {
+    steps.home()
+    steps.etudiant_public()
+
+    cy.get("div").find('input[type="radio"][value="true"]').first().check()
+    cy.get('button[type="submit"]').click()
+
+    steps.zeroEnfants()
+    steps.celibataire()
+    steps.parentsSepares()
+    steps.unEnfantSuperieur()
+    steps.heberge()
+    steps.neParticipePasLogement()
+    steps.hebergeParents()
+
+    cy.get('input[type="number"').type("45200")
+    steps.submit()
+    // Ressources
+    steps.submit()
+    cy.get(".aj-tooltip")
+    cy.get(".aj-question")
+      .invoke("text")
+      .should("contain", "revenu brut global")
+    cy.get('input[type="number"]').type("17860.35")
+    steps.submit()
+    cy.get('input[type="number"]').type("1")
+    steps.submit()
+    steps.checkRadio("false")
+    steps.submit()
+    steps.checkRadio("true")
+    steps.submit()
+    steps.checkRadio("false")
+    steps.submit()
+    steps.checkRadio("true")
+    steps.submit()
+    cy.get('input[type="number"]').type("2")
+    steps.submit()
+    steps.hasBourseCriteresSociaux(3)
+  })
+})

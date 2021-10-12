aae51f2b60a8cc1b95eae6bb7efad119e87fe8d0
Quentin Madura <quentin.madura@gmail.com>
2021-10-12 11:55:19 +0200

    Simplifie la page des resultats inattendu

--- src/views/Foyer/ResultatInattendu.vue
+++ src/views/Foyer/ResultatInattendu.vue
@@ -11,17 +11,11 @@
 
     <h3>Le montant indiqué pour {{ longLabel }} vous semble inexact&nbsp;?</h3>
 
-    <ResultatInattenduPpa v-if="droit.id === 'ppa'"></ResultatInattenduPpa>
-    <ResultatInattenduAideLogement
-      v-else-if="droit.id === 'aide_logement'"
-    ></ResultatInattenduAideLogement>
-    <ResultatInattenduGarantieJeune
-      v-else-if="droit.id === 'garantie_jeunes'"
-    ></ResultatInattenduGarantieJeune>
-    <ResultatInattenduYearMinusTwo
-      :droit="droit"
-      v-else
-    ></ResultatInattenduYearMinusTwo>
+    <component
+      v-if="resultatsInattendus[droit.id]"
+      :is="resultatsInattendus[droit.id]"
+    />
+    <ResultatInattenduYearMinusTwo :droit="droit" v-else />
   </form>
 </template>
 
@@ -31,6 +25,12 @@ import ResultatInattenduAideLogement from "@/components/ResultatInattendu/AideLo
 import ResultatInattenduGarantieJeune from "@/components/ResultatInattendu/GarantieJeune"
 import ResultatInattenduPpa from "@/components/ResultatInattendu/Ppa"
 import ResultatInattenduYearMinusTwo from "@/components/ResultatInattendu/YearMinusTwo"
+
+const RESULTATS_INATTENDUS = {
+  aide_logement: ResultatInattenduAideLogement,
+  garantie_jeunes: ResultatInattenduGarantieJeune,
+  ppa: ResultatInattenduPpa,
+}
 
 export default {
   name: "resultat-inattendu",
@@ -56,6 +56,7 @@ export default {
     )
     return {
       benefitKeyed,
+      resultatsInattendus: RESULTATS_INATTENDUS,
     }
   },
   computed: {

1b419190afdef04e50e876e2406ea635dae2c0c4
Quentin Madura <quentin.madura@gmail.com>
2021-10-25 10:46:14 +0200

    Répare les emails suite au changement des légendes dynamiques

--- backend/lib/mes-aides/emails/initial.js
+++ backend/lib/mes-aides/emails/initial.js
@@ -7,19 +7,23 @@ const assign = require("lodash/assign")
 
 const mustache = require("consolidate").mustache
 const config = require("../../../config")
+const openfiscaController = require("../../openfisca/parameters")
+const { formatDroitEstime } = require("../../../../lib/benefits")
 const { getBenefitLegend } = require("../../../../lib/benefits")
 const { mjml } = require(".")
 
-function basicBenefitText(b) {
-  if (b.labelFunction) {
-    return b.labelFunction(b)
+function basicBenefitText(droit, parameters) {
+  if (droit.labelFunction) {
+    return droit.labelFunction(droit)
   }
 
-  if (b.type === "bool") {
-    return b.label
+  const droitEstime = formatDroitEstime(droit, parameters)
+
+  if (droitEstime.type === "bool") {
+    return droit.label
   }
 
-  return `${b.label} pour un montant de ${b.montant} ${getBenefitLegend(b)}`
+  return `${droitEstime.label} pour un montant de ${droitEstime.value} ${droitEstime.legend}`
 }
 
 var textTemplate = fs.readFileSync(
@@ -31,22 +35,22 @@ var mjmlTemplate = fs.readFileSync(
   "utf8"
 )
 
-function renderAsText(followup, benefits) {
+function renderAsText(followup, benefits, parameters) {
   var data = {
-    benefitTexts: benefits.map(basicBenefitText),
+    benefitTexts: benefits.map(basicBenefitText, parameters),
     returnURL: `${config.baseURL}${followup.returnPath}`,
   }
 
   return mustache.render(textTemplate, data)
 }
 
-function renderAsHtml(followup, benefits) {
-  var droits = map(benefits, function (droit) {
-    var montant = ""
-    if (isNumber(droit.montant)) {
-      var unit = droit.unit || "€"
-      var legend = getBenefitLegend(droit)
-      montant = `${droit.montant.toFixed(0)} ${unit} ${legend}`
+function renderAsHtml(followup, benefits, parameters) {
+  let droits = map(benefits, function (droit) {
+    let value = ""
+    let droitEstime = formatDroitEstime(droit, parameters)
+
+    if (droitEstime.type === "float") {
+      value = `${droitEstime.value} ${droitEstime.legend}`
     }
 
     var ctaLink = ""
@@ -67,56 +71,56 @@ function renderAsHtml(followup, benefits) {
 
     return assign({}, droit, {
       imgSrc: "/img/" + droit.provider.imgSrc,
-      montant: montant,
+      montant: value,
       ctaLink: ctaLink,
       ctaLabel: ctaLabel,
       droitLabel: capitalize(droit.label),
     })
   })
 
-  var data = {
-    droits: droits,
-    baseURL: config.baseURL,
-    returnURL: `${config.baseURL}${followup.returnPath}`,
-  }
-
-  return mustache.render(mjmlTemplate, data).then(function (templateString) {
-    const output = mjml(templateString)
-    return {
-      html: output.html,
-    }
-  })
+  return mustache
+    .render(mjmlTemplate, {
+      droits: droits,
+      baseURL: config.baseURL,
+      returnURL: `${config.baseURL}${followup.returnPath}`,
+    })
+    .then(function (templateString) {
+      const output = mjml(templateString)
+      return {
+        html: output.html,
+      }
+    })
 }
 
-function render(followup) {
-  var p = followup.populated("situation")
-    ? Promise.resolve(followup)
+async function render(followup) {
+  var populated = followup.populated("situation")
+    ? await followup
     : followup.populate("situation").execPopulate()
 
-  return p
-    .then((f) => f.situation.compute())
-    .then(function (results) {
-      return results.droitsEligibles
-    })
-    .then(function (benefits) {
-      followup.benefits = benefits.map((benefit) => ({
-        id: benefit.id,
-        amount: benefit.montant,
-      }))
-      followup.save()
+  const parameters = await openfiscaController.getParameters(
+    populated.situation.dateDeValeur
+  )
 
-      return Promise.all([
-        renderAsText(followup, benefits),
-        renderAsHtml(followup, benefits),
-      ]).then(function (values) {
-        return {
-          subject: `[${followup.situation._id}] Récapitulatif de votre simulation sur 1jeune1solution.gouv.fr`,
-          text: values[0],
-          html: values[1].html,
-          attachments: values[1].attachments,
-        }
-      })
-    })
+  const situationsResults = await populated.situation.compute()
+  const droitsEligibles = situationsResults.droitsEligibles
+  followup.benefits = droitsEligibles.map((benefit) => ({
+    id: benefit.id,
+    amount: benefit.montant,
+    unit: benefit.unit,
+  }))
+  followup.save()
+
+  return Promise.all([
+    renderAsText(followup, droitsEligibles, parameters),
+    renderAsHtml(followup, droitsEligibles, parameters),
+  ]).then(function (values) {
+    return {
+      subject: `[${followup.situation._id}] Récapitulatif de votre simulation sur 1jeune1solution.gouv.fr`,
+      text: values[0],
+      html: values[1].html,
+      attachments: values[1].attachments,
+    }
+  })
 }
 
 exports.render = render

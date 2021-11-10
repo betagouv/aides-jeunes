30e0aaaaba2f1146ae503d710aabcb8781ffd0cc
kout95 <antoine.bernier@outlook.fr>
2021-11-10 10:51:02 +0100

    Corrige le Recapitulatif.js

--- src/lib/Recapitulatif.js
+++ src/lib/Recapitulatif.js
@@ -41,7 +41,7 @@ export const SIMPLE_STEPS = {
     ]
   },
   depcom() {
-    const answer = getStepAnswer(
+    const answer = getAnswer(
       this.$store.state.answers.current,
       "menage",
       "depcom",
@@ -58,14 +58,19 @@ export const SIMPLE_STEPS = {
     ]
   },
 
-  _bourseCriteresSociauxCommuneDomicileFamilial(step) {
-    const individu = getIndividuByStep(step, this)
+  _bourseCriteresSociauxCommuneDomicileFamilial() {
+    const answer = getAnswer(
+      this.$store.state.answers.current,
+      "individu",
+      "_bourseCriteresSociauxCommuneDomicileFamilial",
+      "demandeur"
+    )
     return [
       {
         label: "Quel est le code postal de la commune de vos parents ?",
         value: displayDepcomValue(
-          individu._bourseCriteresSociauxCommuneDomicileFamilialCodePostal,
-          individu._bourseCriteresSociauxCommuneDomicileFamilialNomCommune
+          answer._bourseCriteresSociauxCommuneDomicileFamilialCodePostal,
+          answer._bourseCriteresSociauxCommuneDomicileFamilialNomCommune
         ),
       },
     ]

59cbad7d0a07eb468cd4322bc6d5e5c409475ce3
Kout95 <65901733+Kout95@users.noreply.github.com>
2021-11-17 11:51:13 +0100

    Update src/lib/Recapitulatif.js
    Co-authored-by: Thomas Guillet <guillet.thomas@gmail.com>

--- src/lib/Recapitulatif.js
+++ src/lib/Recapitulatif.js
@@ -36,7 +36,7 @@ export const SIMPLE_STEPS = {
                     ).label
                 )
                 .join(", ")
-            : "Aucuns",
+            : "Aucuns revenus",
       },
     ]
   },

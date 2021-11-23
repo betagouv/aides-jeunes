74ec831ed8bd2690ff81e828820f4af8ba01ee45
kout95 <antoine.bernier@outlook.fr>
2021-11-23 14:23:22 +0100

    Simplifie test-benefits-urls

--- testBenefitsUrls.js
+++ testBenefitsUrls.js
@@ -4,21 +4,17 @@ const subject = require("./data/all")
 let links = []
 let done = 0
 
-Object.keys(subject.groupByInstitution).forEach(function (providerName) {
-  const provider = subject[providerName]
-  Object.keys(provider.prestations).forEach(function (aideName) {
-    const aide = provider.prestations[aideName]
-    links = links.concat(
-      [aide.link, aide.instructions, aide.form, aide.teleservice]
-        .filter((link) => link)
-        .map((link) => {
-          return {
-            aideName: aideName,
-            url: link,
-          }
-        })
-    )
-  })
+subject.all.forEach((benefit) => {
+  links = links.concat(
+    [benefit.link, benefit.instructions, benefit.form, benefit.teleservice]
+      .filter((link) => link)
+      .map((link) => {
+        return {
+          benefitId: benefit.id,
+          url: link,
+        }
+      })
+  )
 })
 
 links.forEach((link) => {
@@ -26,12 +22,12 @@ links.forEach((link) => {
     .get(link.url)
     .then(() => {
       done += 1
-      console.log(`[${done} / ${links.length}] Ok ${link.aideName}`)
+      console.log(`[${done} / ${links.length}] Ok ${link.benefitId}`)
     })
     .catch(() => {
       done += 1
       console.log(
-        `[${done} / ${links.length}] Warning ${link.aideName} lien potentiellement invalide: ${link.url}`
+        `[${done} / ${links.length}] Warning ${link.benefitId} lien potentiellement invalide: ${link.url}`
       )
     })
 })

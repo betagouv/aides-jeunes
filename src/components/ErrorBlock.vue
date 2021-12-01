27545ddb4ea1d9d01ef8f5fc80451ebd642b0fad
kout95 <antoine.bernier@outlook.fr>
2021-12-01 14:47:01 +0100

    test

--- src/components/ErrorBlock.vue
+++ src/components/ErrorBlock.vue
@@ -37,6 +37,7 @@
         Afficher les détails techniques
       </button>
     </div>
+    <pre v-html="errorText"></pre>
     <small v-if="showDetails">
       Informations techniques :
       <pre v-html="errorText"></pre>

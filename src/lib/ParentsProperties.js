a8b01d69ea4983b84353e00c1c3a19c20abc7971
Cugniere <16650011+Cugniere@users.noreply.github.com>
2022-01-05 16:53:11 +0100

    update: utilisation d'abTesting au sein de la fonction items

--- src/lib/ParentsProperties.js
+++ src/lib/ParentsProperties.js
@@ -1,33 +1,47 @@
+import ABTestingService from "@/plugins/ABTestingService"
+import { isRelevant } from "@/lib/Utils"
+
 const STEPS = {
   _situation: {
     question: "Quelle est la situation de vos parents ?",
     questionType: "enum",
-    items: [
-      {
-        label: "En couple",
-        value: "en_couple",
-      },
-      {
-        label: "Séparés",
-        value: "separes",
-      },
-      {
-        label: "Célibataire",
-        value: "celibataire",
-      },
-      {
-        label: "Veuf ou veuve",
-        value: "veuve",
-      },
-      {
-        label: "Décédés",
-        value: "decedes",
-      },
-      {
-        label: "Sans autorité parentale",
-        value: "sans_autorite",
-      },
-    ],
+    items: (component) => {
+      let items = [
+        {
+          label: "En couple",
+          value: "en_couple",
+        },
+        {
+          label: "Séparés",
+          value: "separes",
+        },
+        {
+          label: "Célibataire",
+          value: "celibataire",
+          isRelevant: (component) => {
+            const abTesting = ABTestingService.getEnvironment()
+            return !(
+              abTesting &&
+              abTesting.parentCelibataire &&
+              !abTesting.parentCelibataire.value
+            )
+          },
+        },
+        {
+          label: "Veuf ou veuve",
+          value: "veuve",
+        },
+        {
+          label: "Décédés",
+          value: "decedes",
+        },
+        {
+          label: "Sans autorité parentale",
+          value: "sans_autorite",
+        },
+      ]
+      return isRelevant(items, component)
+    },
   },
   _en_france: {
     question: "Vos parents habitent-ils en France ?",

56ad585a94a7d1772bf07cc7a103bc588eafebb6
Cugniere <16650011+Cugniere@users.noreply.github.com>
2022-01-05 14:23:31 +0100

    chore: évite réassignation

--- src/mixins/ABTestingMixin.js
+++ src/mixins/ABTestingMixin.js
@@ -4,10 +4,9 @@ const abTesting = ABTestingService.getEnvironment()
 export default {
   methods: {
     abTestingFilter: function (items) {
-      items = items.filter((item) => {
+      return items.filter((item) => {
         return !item.abTesting || item.abTesting(abTesting)
       })
-      return items
     },
   },
 }

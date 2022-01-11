8ad71bc0ded16d3b81fb75dc0de1f869eb176aae
Vanessa-D <vanessa.ducloux@laposte.net>
2022-01-11 11:55:11 +0100

    Ajoute une fonctionqui vérifie si une commune est inclue dans la zone géographique d'une institution

--- tools/geographical-benefit-count.js
+++ tools/geographical-benefit-count.js
@@ -1,7 +1,36 @@
 const communes = require("@etalab/decoupage-administratif/data/communes.json")
 
+const epcis = require("@etalab/decoupage-administratif/data/epci.json")
+
 function isGeographicallyIncluded(codeInsee, institution) {
-  return true // false
+  // Trouve dans la liste "communes" l'élément où la valeur de la clé code vaut codeInsee
+  const communeInfo = communes.find((element) => element.code === codeInsee)
+
+  const typeInstitution = institution.type
+  const idInstitution = institution.id
+  const codeRegion = communeInfo.region // On récupère le code région dans laquelle la commune se trouve
+  const codeDepartement = communeInfo.departement
+
+  // Compare l'Id de l'institution avec le code région/departement/epci/commune
+  if (typeInstitution == "national") {
+    return true
+  } else if (typeInstitution == "region") {
+    return idInstitution == codeRegion
+  } else if (typeInstitution == "departement") {
+    return idInstitution == codeDepartement
+  } else if (typeInstitution == "epci") {
+    // Trouve dans la liste "epcis" l'élément où la valeur de la clé membres trouve elle-même l'élément
+    // où la valeur de la clé code vaut codeInsee
+    const epciInfo = epcis.find((element) =>
+      element.membres.find((commune) => commune.code === codeInsee)
+    )
+    const codeEpci = epciInfo.code
+    return idInstitution == codeEpci
+  } else if (typeInstitution == "commune") {
+    return idInstitution == codeInsee
+  } else {
+    return false
+  }
 }
 
 module.exports = {

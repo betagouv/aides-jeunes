1f7ab18a8c23a2aac567cd89ba21410e9b2efd5e
Cugniere <16650011+Cugniere@users.noreply.github.com>
2022-01-13 13:50:16 +0100

    chore: réactive scroll behavior

--- src/router.js
+++ src/router.js
@@ -348,6 +348,14 @@ const router = createRouter({
       },
     },
   ],
+  scrollBehavior(to /*, from, savedPosition*/) {
+    if (to.hash) {
+      return {
+        selector: to.hash,
+      }
+    }
+    return { left: 0, top: 0 }
+  },
 })
 
 router.beforeEach((to, from, next) => {

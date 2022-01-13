a28aaa767c520a9553406070ce0acfd4cdf71d0a
Vanessa-D <vanessa.ducloux@laposte.net>
2022-01-13 17:23:47 +0100

    Ajoute une fonction qui précise le nombre d'aides par commune

--- tools/geographical-benefit-count.js
+++ tools/geographical-benefit-count.js
@@ -19,11 +19,15 @@ function isGeographicallyIncluded(codeInsee, institution) {
   } else if (typeInstitution == "departement") {
     return idInstitution == codeDepartement
   } else if (typeInstitution == "epci") {
-    // Trouve dans la liste "epcis" l'élément où la valeur de la clé membres trouve elle-même l'élément
-    // où la valeur de la clé code vaut codeInsee
+    // Trouve dans la liste "epcis" l'élément où la propriété membres trouve elle-même l'élément
+    // où la propriété code vaut codeInsee
     const epciInfo = epcis.find((element) =>
       element.membres.find((commune) => commune.code === codeInsee)
     )
+    // Si ne trouve pas d'epci --> renvoie false
+    if (epciInfo === undefined) {
+      return false
+    }
     const codeEpci = epciInfo.code
     return idInstitution == codeEpci
   } else if (typeInstitution == "commune") {
@@ -33,6 +37,19 @@ function isGeographicallyIncluded(codeInsee, institution) {
   }
 }
 
+function computeInterestingBenefitsCount(codeInsee) {
+  let total = 0
+  institutionIds.forEach((id) => {
+    const institution = institutionsMap[id]
+    const result = isGeographicallyIncluded(codeInsee, institution)
+    if (result) {
+      total = total + institution.benefitsIds.length
+    }
+  })
+  return total
+}
+
 module.exports = {
   isGeographicallyIncluded,
+  computeInterestingBenefitsCount,
 }

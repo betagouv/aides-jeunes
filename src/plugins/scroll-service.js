4fe98d3b7e8035e1c4664ef5e7dd408828a48c42
CUGNIERE <16650011+Cugniere@users.noreply.github.com>
2022-01-13 16:45:04 +0100

    Revert "[469] Vérifier si scrollBehavior correspond à du code mort (#2313)"
    This reverts commit 75e4616639b21b1d62947d3dc66b871696187c68.

---
+++ src/plugins/scroll-service.js
@@ -0,0 +1,22 @@
+import SmoothScroll from "smooth-scroll"
+
+const ScrollService = {
+  install(app) {
+    app.scroll = new SmoothScroll()
+
+    function go(event, destination, offset) {
+      event.preventDefault()
+      app.scroll.animateScroll(destination, event.target, {
+        updateURL: false,
+        offset: function () {
+          return offset || 0
+        },
+      })
+    }
+    app.config.globalProperties.$ScrollService = {
+      go: go,
+    }
+  },
+}
+
+export default ScrollService

6a58433ca3d63df41fe7bbdf6c9d38ad7c8440be
Thomas Guillet <thomas@palya.fr>
2022-01-14 10:53:45 +0100

    Déplacer les fonctions utilitaires de dénombrement d'aides pertinentes

--- tests/unit/geographical-beenfit-count.spec.js
+++ tests/unit/geographical-beenfit-count.spec.js
@@ -1,7 +1,7 @@
 const expect = require("expect")
 const {
   isGeographicallyIncluded,
-} = require("../../tools/geographical-benefit-count")
+} = require("../../lib/benefits/geographical-count-utils")
 
 describe("geographical benefit count", function () {
   // Arcachon
@@ -26,6 +26,65 @@ describe("geographical benefit count", function () {
       const result = isGeographicallyIncluded(arcachon, {
         type: "region",
         id: "11",
+      })
+      expect(result).toEqual(false)
+    })
+  })
+
+  describe("logic for departemental institution", function () {
+    it("works for relevant departemental", function () {
+      // 33 = Gironde
+      const result = isGeographicallyIncluded(arcachon, {
+        type: "departement",
+        id: "33",
+      })
+      expect(result).toEqual(true)
+    })
+
+    it("works for irrelevant departement", function () {
+      // 75 = Paris
+      const result = isGeographicallyIncluded(arcachon, {
+        type: "departement",
+        id: "75",
+      })
+      expect(result).toEqual(false)
+    })
+  })
+
+  describe("logic for epci institution", function () {
+    it("works for relevant epci", function () {
+      // 243300563 = CA Bassin d'Arcachon Sud-Pôle Atlantique (COBAS)
+      const result = isGeographicallyIncluded(arcachon, {
+        type: "epci",
+        id: "243300563",
+      })
+      expect(result).toEqual(true)
+    })
+
+    it("works for irrelevant epci", function () {
+      // 200035319 = CA Bassin d'Arcachon Sud-Pôle Atlantique (COBAS)
+      const result = isGeographicallyIncluded(arcachon, {
+        type: "epci",
+        id: "200035319",
+      })
+      expect(result).toEqual(false)
+    })
+  })
+
+  describe("logic for communal institution", function () {
+    it("works for relevant commune", function () {
+      const result = isGeographicallyIncluded(arcachon, {
+        type: "commune",
+        id: arcachon,
+      })
+      expect(result).toEqual(true)
+    })
+
+    it("works for irrelevant commune", function () {
+      // 75056 = Paris
+      const result = isGeographicallyIncluded(arcachon, {
+        type: "commune",
+        id: "75056",
       })
       expect(result).toEqual(false)
     })

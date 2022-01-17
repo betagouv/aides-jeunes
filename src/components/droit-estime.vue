114c0e2952a95e6b63a44b3ad3b53ee804149278
kout95 <antoine.bernier@outlook.fr>
2022-01-17 14:22:26 +0100

    Renomme la logique isType... pour éviter la confusion

--- src/components/droit-estime.vue
+++ src/components/droit-estime.vue
@@ -1,7 +1,7 @@
 <template>
   <div v-if="droit.type" class="aj-droit-estime">
     <div>
-      <template v-if="isNumber(droitEstime.type) || isString(droitEstime.type)">
+      <template v-if="isBenefitTypeNumber || isBenefitTypeString">
         <span>
           <span class="aj-droit-estime-label font-normal font-base">
             {{ droitEstime.label }}
@@ -15,7 +15,7 @@
           {{ droitEstime.legend }}
         </span>
       </template>
-      <span v-else-if="isBoolean(droitEstime.type)">
+      <span v-else-if="isBenefitTypeBoolean">
         <i
           :data-testid="`aj-droit-estime-icon-${
             droitEstime.icon ? droitEstime.icon : 'fa-check-circle'
@@ -45,24 +45,28 @@ export default {
     droit: Object,
   },
   computed: {
-    droitEstime: function () {
+    droitEstime() {
       return formatDroitEstime(
         this.droit,
         this.$store.state.openFiscaParameters
       )
     },
-    showUnexpected: function () {
+    isBenefitTypeBoolean() {
+      return this.droit.type === "bool"
+    },
+    isBenefitTypeNumber() {
+      return this.droit.type === "float"
+    },
+    isBenefitTypeString() {
+      return this.droit.type === "string"
+    },
+    showUnexpected() {
       return (
         (this.droit.isBaseRessourcesYearMinusTwo &&
           !this.$store.getters.ressourcesYearMinusTwoCaptured) ||
         this.droit.showUnexpectedAmount
       )
     },
-  },
-  methods: {
-    isBoolean: (type) => type === "bool",
-    isNumber: (type) => type === "float",
-    isString: (type) => type === "string",
   },
 }
 </script>

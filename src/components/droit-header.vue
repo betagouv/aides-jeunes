7a911200f1a7e6d7f194b7f0afed56788058fa4f
Kout95 <65901733+Kout95@users.noreply.github.com>
2022-01-17 09:55:26 +0100

    Simplifie le header de la page d'aide et corrige le bug en moyenne écran (#2312)
    * Première passe de simplification
    
    * fix le header en mode contribution
    
    * Retire flex-direction
    
    * Supprime flex-direction: row
    
    * Update src/styles/partials/mobile/_resultats.scss
    
    Co-authored-by: CUGNIERE <16650011+Cugniere@users.noreply.github.com>
    
    * Simplifie le chemin de l'image
    
    * Apply suggestions from code review
    
    Co-authored-by: CUGNIERE <16650011+Cugniere@users.noreply.github.com>
    
    * Corrige l'affichage du montant en mobile
    
    Co-authored-by: CUGNIERE <16650011+Cugniere@users.noreply.github.com>

---
+++ src/components/droit-header.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="aj-droit-header">
+    <img
+      class="aj-droit-illustration"
+      :src="require(`/public/img/${droit.institution.imgSrc}`)"
+      :alt="`Image pour ${droit.label}`"
+    />
+
+    <div class="aj-droit-identity">
+      <h2 class="aj-question aj-benefit-label" itemprop="name">{{
+        $filters.capitalize(droit.label)
+      }}</h2>
+      <div class="aj-institution-label">{{
+        $filters.capitalize(droit.institution.label)
+      }}</div>
+    </div>
+
+    <DroitEstime :droit="droit" />
+  </div>
+</template>
+
+<script>
+import DroitEstime from "./droit-estime"
+
+export default {
+  name: "DroitHeader",
+  components: {
+    DroitEstime,
+  },
+  props: {
+    droit: Object,
+  },
+}
+</script>

764db9af76e77736a97016242c4420d00faf418b
CUGNIERE <16650011+Cugniere@users.noreply.github.com>
2022-01-21 14:22:17 +0100

    Revert "Revert "[448] Ajouter une question à choix multiple pour les aides au financement de vélo (#2333)" (#2348)"
    This reverts commit bd69c62f638fd09a5ac069f694c95d46669ddfce.

---
+++ src/components/multiple-answers.vue
@@ -0,0 +1,61 @@
+<template>
+  <div><i>Plusieurs choix possibles</i></div>
+  <br />
+  <fieldset>
+    <div
+      v-for="(item, value) in itemsObject"
+      :key="value"
+      class="aj-selection-wrapper"
+    >
+      <input
+        :id="value"
+        type="checkbox"
+        :checked="item.checked"
+        @input="update"
+      />
+      <label :for="value">{{ item.label }}</label>
+    </div>
+  </fieldset>
+</template>
+
+<script>
+export default {
+  name: "MultipleAnswers",
+  props: {
+    modelValue: {
+      type: Array,
+      default: () => [],
+    },
+    items: [Object],
+  },
+  emits: ["update:modelValue"],
+  data() {
+    return {
+      itemsObject: this.generateAnswers(this.items),
+    }
+  },
+  methods: {
+    generateAnswers(items) {
+      const itemsObject = {}
+      for (let key in items) {
+        itemsObject[items[key].value] = {
+          label: items[key].label,
+          checked: this.modelValue.includes(items[key].value),
+        }
+      }
+      this.$emit("update:modelValue", this.modelValue)
+      return itemsObject
+    },
+    update(e) {
+      if (!e.target.checked) {
+        this.$emit(
+          "update:modelValue",
+          this.modelValue.filter((value) => value != e.target.id)
+        )
+      } else if (!this.modelValue.includes(e.target.id)) {
+        this.$emit("update:modelValue", [...this.modelValue, e.target.id])
+      }
+    },
+  },
+}
+</script>

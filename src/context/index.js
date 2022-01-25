d1028c7a590ecbbeb02f5bc1ec6234469d35d836
Thomas Guillet <thomas@palya.fr>
2022-01-25 10:04:35 +0100

    Adapte à la nouvelle nomenclature

--- src/context/index.js
+++ src/context/index.js
@@ -1,11 +1,2 @@
-import unJeune1solution from "./1jeune1solution"
 import mesAides from "./mes-aides.org"
-
-let variation
-if (process.env.VUE_APP_VARIATION === "1jeune1solution") {
-  variation = unJeune1solution
-} else {
-  variation = mesAides
-}
-
-export default variation
+export default mesAides
--- src/context/mes-aides.org/index.js
+++ src/context/mes-aides.org/index.js
@@ -11,7 +11,7 @@ export default {
       name: "a-propos",
       component: () =>
         import(
-          /* webpackChunkName: "static" */ "@/variations/mes-aides.org/views/a-propos.vue"
+          /* webpackChunkName: "static" */ "@/context/mes-aides.org/views/a-propos.vue"
         ),
     },
     {
@@ -19,7 +19,7 @@ export default {
       name: "ameliorer",
       component: () =>
         import(
-          /* webpackChunkName: "static" */ "@/variations/mes-aides.org/views/ameliorer.vue"
+          /* webpackChunkName: "static" */ "@/context/mes-aides.org/views/ameliorer.vue"
         ),
     },
     {
@@ -27,7 +27,7 @@ export default {
       name: "liens-utiles",
       component: () =>
         import(
-          /* webpackChunkName: "static" */ "@/variations/mes-aides.org/views/liens-utiles.vue"
+          /* webpackChunkName: "static" */ "@/context/mes-aides.org/views/liens-utiles.vue"
         ),
     },
   ],

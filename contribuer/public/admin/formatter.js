0544ef85689a103fcb526dca3d62f57c92743870
Cugniere <16650011+Cugniere@users.noreply.github.com>
2022-02-01 13:31:06 +0100

    feature: formatte les champs textes avant submissions

---
+++ contribuer/public/admin/formatter.js
@@ -0,0 +1,20 @@
+const StringControl = CMS.getWidget("string").control
+
+class CustomStringControl extends StringControl {
+  constructor(props) {
+    super(props)
+    this.state = {
+      value: this.props.value,
+    }
+  }
+  format = (value) => {
+    return value.trim().replace(/ +(?= )/g, "")
+  }
+  isValid = () => {
+    this.props.onChange(this.format(this.props.value))
+    return (
+      !this.props.field.get("required", true) || this.props.value.length > 0
+    )
+  }
+}
+CMS.registerWidget("string", CustomStringControl)

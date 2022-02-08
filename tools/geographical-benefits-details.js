afd32e5e08adcdda1129468423760654f493a99e
Vanessa-D <vanessa.ducloux@laposte.net>
2022-02-08 17:05:24 +0100

    Rend généréque la logique d'itération en prenant en compte la pertinence géographique

--- tools/geographical-benefits-details.js
+++ tools/geographical-benefits-details.js
@@ -1,4 +1,3 @@
-/* eslint-disable prettier-vue/prettier */
 const fs = require("fs")
 const ProgressBar = require("progress")
 const communes = require("@etalab/decoupage-administratif/data/communes.json")
@@ -7,67 +6,79 @@ const epcis = require("@etalab/decoupage-administratif/data/epci.json")
 const { institutionsMap } = require("../data/all")
 const {
   isGeographicallyIncluded,
-  computeInterestingBenefitCounts,
 } = require("../lib/benefits/geographical-count-utils")
 
 const communeMap = {}
 communes.forEach((commune) => {
   communeMap[commune.code] = commune
+  commune.count = 0
+  commune.institutions = []
 })
 
 const institutionIds = Object.keys(institutionsMap)
-const bar = new ProgressBar(":bar :elapsed :eta", {
-  total: institutionIds.length,
-})
 
-institutionIds.forEach((id) => {
-  const institution = institutionsMap[id]
-  switch (institution.type) {
-    case "national":
-      communes.forEach((commune) => {
-        const previousValue = communeMap[commune.code].count || 0
-        communeMap[commune.code].count =
-          previousValue + institution.benefitsIds.length
-      })
-      break
-    case "region":
-    case "departement":
-      communes.forEach((commune) => {
-        const included = isGeographicallyIncluded(commune, institution)
-        const previousValue = communeMap[commune.code].count || 0
-        communeMap[commune.code].count =
-          previousValue + (included ? institution.benefitsIds.length : 0)
-      })
-      break
-    case "epci":
-      const epciInfo = epcis.find((element) => element.code === institution.id)
-      if (!epciInfo) {
-        console.log(institution)
-        process.exit(1)
-      }
-      epciInfo.membres.forEach((commune) => {
-        const previousValue = communeMap[commune.code].count || 0
-        communeMap[commune.code].count =
-          previousValue + institution.benefitsIds.length
-      })
-      break
-    case "commune":
-      if (!communeMap[institution.id]) {
-        console.log(institution.id)
+function listInterestingInstitutions(commune, institution) {
+  commune.institutions.push(institution.id)
+}
+
+function incrementCount(commune, institution) {
+  commune.count += institution.benefitsIds.length
+}
+
+function iterateGivenGeographicalRelevancy(apply) {
+  const bar = new ProgressBar(":bar :elapsed :eta", {
+    total: institutionIds.length,
+  })
+
+  institutionIds.forEach((id) => {
+    const institution = institutionsMap[id]
+    switch (institution.type) {
+      case "national":
+        communes.forEach((commune) => {
+          apply(communeMap[commune.code], institution)
+        })
+        break
+      case "region":
+      case "departement":
+        communes.forEach((commune) => {
+          const included = isGeographicallyIncluded(commune, institution)
+          if (included) {
+            apply(communeMap[commune.code], institution)
+          }
+        })
+        break
+      case "epci": {
+        const epciInfo = epcis.find(
+          (element) => element.code === institution.id
+        )
+        if (!epciInfo) {
+          console.log(institution)
+          process.exit(1)
+        }
+        epciInfo.membres.forEach((commune) => {
+          apply(communeMap[commune.code], institution)
+        })
         break
       }
-      const previousValue = communeMap[institution.id].count || 0
-      communeMap[institution.id].count =
-        previousValue + institution.benefitsIds.length
-      break
-    case "caf":
-      break
-    default:
-      console.log("Dont know how to deal with " + typeInstitution)
-  }
+      case "commune": {
+        if (!communeMap[institution.id]) {
+          console.log(institution.id)
+          break
+        }
+        apply(communeMap[institution.id], institution)
+        break
+      }
+      case "caf":
+        break
+      default:
+        console.log("Dont know how to deal with " + institution.type)
+    }
 
-  bar.tick()
-})
+    bar.tick()
+  })
+}
+iterateGivenGeographicalRelevancy(listInterestingInstitutions)
+iterateGivenGeographicalRelevancy(incrementCount)
 
 fs.writeFileSync(
   "geographical-benefits-details.json",

3ff5aca0d82e3d89759bfd138cd29600bda1f24d
kout95 <antoine.bernier@outlook.fr>
2022-02-08 12:03:07 +0100

    Corrige tools

--- tools/get-all-steps.js
+++ tools/get-all-steps.js
@@ -1,9 +1,28 @@
 const { generateBlocks } = require("../lib/state/blocks")
 
+const getStepOrSubSteps = (accum, step) => {
+  if (step.steps) {
+    return getStepOrSubSteps(accum, step.steps)
+  } else if (step.length) {
+    step.forEach((s) => {
+      getStepOrSubSteps(accum, s)
+    })
+    return accum
+  }
+  accum.push(step)
+
+  return accum
+}
+
 const emptySimulation = {
   individus: {
     demandeur: {},
   },
   menage: {},
 }
-console.log(generateBlocks(emptySimulation))
+
+const steps = generateBlocks(emptySimulation)
+
+const result = []
+getStepOrSubSteps(result, steps)
+console.log(result)

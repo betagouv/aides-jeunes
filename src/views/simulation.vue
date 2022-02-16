c530be89714907d40c3a3657f95c168c5e1b378a
Kout95 <65901733+Kout95@users.noreply.github.com>
2022-02-16 17:15:10 +0100

    Simplifie la réutilisation de la barre de progression (#2447)

--- src/views/simulation.vue
+++ src/views/simulation.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="aj-simulation">
-    <div v-if="showProgress" class="aj-complete-progress-bar">
-      <div :style="currentProgressStyle" />
-    </div>
+    <ProgressBar></ProgressBar>
     <div
       class="container aj-layout-container"
       :class="{ 'aj-debug-container': debug }"
@@ -31,11 +29,12 @@
 import TitreChapitre from "@/components/titre-chapitre"
 import Progress from "@/components/progress"
 import Summary from "@/components/summary"
-import { isStepAnswered } from "../../lib/answers"
+import ProgressBar from "@/components/progress-bar"
 
 export default {
   name: "Simulation",
   components: {
+    ProgressBar,
     TitreChapitre,
     Progress,
     Summary,
@@ -51,36 +50,6 @@ export default {
     },
     debug() {
       return this.$store.getters.getDebug
-    },
-    showProgress() {
-      return "resultatsAttendus" !== this.$route.name
-    },
-    progress() {
-      const cleanPath = this.$route.path.replace(/\/en_savoir_plus$/, "")
-      const allSteps = this.$store.getters.getAllSteps.filter(
-        (step) => !["/", "/simulation/resultats"].includes(step.path)
-      )
-      const activeSteps = allSteps.filter((step) => step.isActive)
-
-      // Use anwers as basis when you are not in journey
-      if (!allSteps.some((step) => step.path === cleanPath)) {
-        const answeredSteps = activeSteps.filter((step) =>
-          isStepAnswered(this.$store.state.answers.all, step)
-        )
-        return answeredSteps.length / activeSteps.length
-      } else {
-        const stepIndex = allSteps.findIndex((item) => item.path === cleanPath)
-        const previousStep = allSteps
-          .slice(0, stepIndex)
-          .filter((step) => step.isActive)
-
-        return previousStep.length / activeSteps.length
-      }
-    },
-    currentProgressStyle() {
-      return {
-        width: `${this.progress * 100}%`,
-      }
     },
   },
   methods: {

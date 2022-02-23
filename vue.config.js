dc744c9c7af95b5c6d48369f2d6b961d15480e5d
Cugniere <16650011+Cugniere@users.noreply.github.com>
2022-02-23 07:27:22 +0100

    chore: met à jour le nombre d'aides disponibles

--- vue.config.js
+++ vue.config.js
@@ -13,6 +13,7 @@ process.env.VUE_APP_MATOMO_ID = matomo.id
 process.env.VUE_APP_VALIDATION_DELAY = animation?.delay || 0
 process.env.VUE_APP_CONTACT_EMAIL = "aides-jeunes@beta.gouv.fr"
 process.env.VUE_APP_CONTEXT_NAME = "1jeune1solution"
+process.env.VUE_APP_NOMBRE_AIDES = 400
 process.env.VUE_APP_BASE_URL = baseURL
 process.env.VUE_APP_CONTEXT = process.env.CONTEXT
 process.env.VUE_APP_PR_URL = `${process.env.REPOSITORY_URL}/pull/${process.env.REVIEW_ID}`

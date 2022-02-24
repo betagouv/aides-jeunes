c7263a9668c5b5e7d8b794332c5987614d64e978
Cugniere <16650011+Cugniere@users.noreply.github.com>
2022-02-24 11:03:20 +0100

    update: ajoute le nombre d'aides sur la page d'accueil

--- src/views/home.vue
+++ src/views/home.vue
@@ -5,7 +5,7 @@
         <div class="aj-home-hero-content">
           <h1>
             <span class="hightlight"
-              >Évaluez vos droits à<br />plus de 20 aides
+              >Évaluez vos droits à<br />{{ benefitsNumber }} aides
             </span>
             <br />en moins de 5 minutes.
           </h1>
@@ -57,6 +57,11 @@ export default {
     },
     ctaSize: function () {
       return this.hasExistingSituation ? "large" : "xlarge"
+    },
+    benefitsNumber: function () {
+      return process.env.VUE_APP_BENEFIT_COUNT
+        ? process.env.VUE_APP_BENEFIT_COUNT
+        : "plus de 400"
     },
   },
   methods: {

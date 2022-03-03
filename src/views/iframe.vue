e31caebcf58f81ea3d0e0a8687f377ce9b56f05c
Thomas Guillet <guillet.thomas@gmail.com>
2022-03-03 14:08:40 +0100

    Améliore la gestion d'affichage en iframe (#2463)

---
+++ src/views/iframe.vue
@@ -0,0 +1,30 @@
+<template>
+  <iframe
+    id="iframe"
+    :src="`/?iframe`"
+    scrolling="no"
+    @load="iframeLoaded"
+  ></iframe>
+</template>
+
+<script>
+import { iframeResize } from "iframe-resizer"
+
+export default {
+  name: "IFrame",
+  methods: {
+    iframeLoaded() {
+      iframeResize({ log: false }, "#iframe")
+    },
+  },
+}
+</script>
+
+<style scoped>
+#iframe {
+  margin: 0 auto;
+  width: 100%;
+  border: none;
+  min-height: 700px;
+}
+</style>

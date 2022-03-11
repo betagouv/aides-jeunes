e9900b403b91cc9b90e5e496a1a9e35eb055ebf3
Cugniere <16650011+Cugniere@users.noreply.github.com>
2022-03-11 14:07:15 +0100

    update: github api

--- src/components/bandeau-demo.vue
+++ src/components/bandeau-demo.vue
@@ -3,15 +3,23 @@
     <a class="bandeau-demo" target="_blank" rel="noopener" :href="link">
       Démo
     </a>
-    <a v-if="debugLink !== 'undefined'" class="lien-debug" :href="debugLink"
+    <a v-if="benefitId" class="lien-debug" :href="benefitLink"
       >Accéder à l'aide créée</a
     >
   </div>
 </template>
 
 <script>
+import axios from "axios"
+
 export default {
   name: "BandeauDemo",
+  data: function () {
+    return {
+      benefitId: null,
+      benefitLink: null,
+    }
+  },
   computed: {
     afficheBandeau() {
       return true || process.env.VUE_APP_CONTEXT === "deploy-preview"
@@ -19,12 +27,34 @@ export default {
     link() {
       return process.env.VUE_APP_PR_URL
     },
-    debugLink() {
-      //process.env.VUE_APP_NETLIFY_PR
-      //return "/simulation/resultats?debug"
-      ///simulation/resultats?debug=aide-nationale-au-brevet-daptitude-aux-fonctions-danimateur-bafa
-      return process.env.VUE_APP_NETLIFY_PR
-    },
+    async debugLink() {},
+  },
+  mounted: async function () {
+    let pr = "pull/2530/head".split("/")
+    if (process.env.VUE_APP_NETLIFY_PR !== "undefined") {
+      pr = process.env.VUE_APP_NETLIFY_PR.split("/")
+    }
+    if (pr.length < 3) return
+    const url = `https://api.github.com/repos/betagouv/aides-jeunes/pulls/${pr[1]}/files`
+    //process.env.VUE_APP_NETLIFY_PR
+    //return "/simulation/resultats?debug"
+    ///simulation/resultats?debug=aide-nationale-au-brevet-daptitude-aux-fonctions-danimateur-bafa
+    //return process.env.VUE_APP_NETLIFY_PR
+    axios
+      .get(url)
+      .then((response) => {
+        console.log("ok", response)
+        for (let entry of response.data) {
+          if (entry.filename.match(/(\.yml|.yaml)$/i)) {
+            this.benefitId = entry.filename
+          }
+        }
+        return
+      })
+      .catch((e) => {
+        console.log("nok", e)
+        return
+      })
   },
 }
 </script>
@@ -64,7 +94,7 @@ export default {
     color: #ffffff;
     z-index: 5000000;
     pointer-events: all;
-    border: 1px solid #fff;
+    border: 1px solid #ffffff11;
   }
 }
 </style>

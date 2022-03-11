8d7425c5ebc1b58bec4335de4d836fee4093dcd2
CUGNIERE <16650011+Cugniere@users.noreply.github.com>
2022-03-11 17:13:32 +0100

    [440] Ajouter un lien sur la home des preview app lors de l'ajout ou modification d'une aide dans netlify (#2530)
    * update: récupère le nom de la branche via netlify
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update: github api
    
    * update: github api
    
    * update
    
    * chore: cleanup
    
    * chore: more cleanup
    
    * update: ajout du support de plusieurs aides
    
    * fix
    
    * fix again
    
    * chore: cleanup yaml files

--- src/components/bandeau-demo.vue
+++ src/components/bandeau-demo.vue
@@ -3,12 +3,22 @@
     <a class="bandeau-demo" target="_blank" rel="noopener" :href="link">
       Démo
     </a>
+    <a v-if="benefitLink" class="lien-debug" :href="benefitLink"
+      >Accéder aux aides ajoutées</a
+    >
   </div>
 </template>
 
 <script>
+import axios from "axios"
+
 export default {
   name: "BandeauDemo",
+  data: function () {
+    return {
+      benefitLink: null,
+    }
+  },
   computed: {
     afficheBandeau() {
       return process.env.VUE_APP_CONTEXT === "deploy-preview"
@@ -16,6 +26,30 @@ export default {
     link() {
       return process.env.VUE_APP_PR_URL
     },
+  },
+  mounted: async function () {
+    const pr = process.env.VUE_APP_NETLIFY_PR.split("/")
+    if (pr.length < 3) return
+    const url = `https://api.github.com/repos/betagouv/aides-jeunes/pulls/${pr[1]}/files`
+    axios
+      .get(url)
+      .then((response) => {
+        const benefits = []
+        for (let entry of response.data) {
+          const match = entry.filename.match(
+            /data\/benefits\/(?:openfisca|javascript)\/(.*)(?:\.yml|\.yaml)$/i
+          )
+          if (match) {
+            benefits.push(match[1])
+          }
+        }
+        if (benefits.length) {
+          this.benefitLink = `/simulation/resultats?debug=${benefits.join(",")}`
+        }
+      })
+      .catch((e) => {
+        console.log("Failed to find a benefit file name from this PR", e)
+      })
   },
 }
 </script>
@@ -43,6 +77,19 @@ export default {
     color: white;
     transform: rotate(45deg);
     pointer-events: all;
+  }
+
+  .lien-debug {
+    position: fixed;
+    inset: auto 30px 50px auto;
+    display: block;
+    background: #5770be;
+    padding: 5px 10px;
+    border-radius: 15px;
+    color: #ffffff;
+    z-index: 5000000;
+    pointer-events: all;
+    border: 1px solid #ffffff11;
   }
 }
 </style>

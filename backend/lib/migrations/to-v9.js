f065805b560baa72629945fb0b1100d31c82ff3c
kout95 <antoine.bernier@outlook.fr>
2022-03-25 09:05:30 +0100

    Ajoute une migration pour migrer enceinte

---
+++ backend/lib/migrations/to-v9.js
@@ -0,0 +1,31 @@
+/*
+ * Migre enceinte
+ */
+
+const VERSION = 9
+
+function isEnceinteAnswer(answer) {
+  return answer.entityName === "individu" && answer.fieldName === "enceinte"
+}
+
+function updateEnceinte(answers) {
+  const enceinteAnswerIds = answers.reduce((accum, answer, index) => {
+    return accum.concat(isEnceinteAnswer(answer) ? index : [])
+  }, [])
+
+  enceinteAnswerIds.forEach((enceinteAnswerId) => {
+    const newValue = answers[enceinteAnswerId].value
+      ? "enceinte"
+      : "pas_enceinte"
+    answers[enceinteAnswerId].set("value", newValue, { strict: false })
+  })
+}
+
+module.exports = {
+  function: function (simulation) {
+    updateEnceinte(simulation.answers.all)
+    updateEnceinte(simulation.answers.current)
+    return simulation
+  },
+  version: VERSION,
+}

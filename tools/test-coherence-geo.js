466b663242c4a70097822df0f7a7af648eab5bf0
Thomas Guillet <thomas@palya.fr>
2022-04-05 17:18:42 +0200

    Regroupe la vérification des contraintes géo dans un seul script

--- tools/test-coherence-geo.js
+++ tools/test-coherence-geo.js
@@ -1,9 +1,30 @@
-const subject = require("../data/all")
+const benefits = require("../data/all")
 
-console.log(subject)
-
-console.log(
-  subject.all.find((benefit) => {
-    return benefit.institution.type === "region"
+const jsBenefits = benefits.all
+  .filter((benefit) => {
+    return (
+      ["region", "departement", "commune", "epci"].includes(
+        benefit.institution.type
+      ) && benefit.source === "javascript"
+    )
   })
-)
+  .forEach((benefit) => {
+    if (!testGeoRelevancy(benefit)) {
+      console.log(benefit.id)
+    }
+  })
+
+function testGeoRelevancy(benefit) {
+  const conditionGeo = benefit.conditions_generales.find((condition) => {
+    return (
+      condition.type === "regions" ||
+      condition.type === "departements" ||
+      condition.type === "communes" ||
+      condition.type === "epcis"
+    )
+  })
+  if (!conditionGeo) {
+    return false
+  }
+  return conditionGeo.values[0] === benefit.institution.code_insee
+}

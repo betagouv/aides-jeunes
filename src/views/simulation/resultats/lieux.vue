700458ba0203beeef1a14419711904802304eb96
kout95 <antoine.bernier@outlook.fr>
2022-04-11 10:17:03 +0200

    Use BackButton button

--- src/views/simulation/resultats/lieux.vue
+++ src/views/simulation/resultats/lieux.vue
@@ -1,24 +1,10 @@
 <template>
   <div class="aj-unbox">
-    <button
-      class="aj-etablissements-back-button button outline small with-icon"
-      type="button"
-      @click="window?.history.back()"
+    <BackButton
+      class="aj-etablissements-back-button small"
+      @click="goToBenefitDetail()"
+      >Retour à l'aide</BackButton
     >
-      <svg
-        width="12"
-        height="12"
-        viewBox="0 0 12 12"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M11.25 5.24998H2.87249L6.53249 1.58998C6.82499 1.29748 6.82499 0.817478 6.53249 0.524978C6.23999 0.232478 5.76749 0.232478 5.47499 0.524978L0.532485 5.46748C0.239985 5.75998 0.239985 6.23248 0.532485 6.52498L5.47499 11.4675C5.76749 11.76 6.23999 11.76 6.53249 11.4675C6.82499 11.175 6.82499 10.7025 6.53249 10.41L2.87249 6.74998H11.25C11.6625 6.74998 12 6.41248 12 5.99998C12 5.58748 11.6625 5.24998 11.25 5.24998Z"
-          fill="#030F8F"
-        />
-      </svg>
-      Retour à l'aide
-    </button>
     <p class="aj-etablissements-intro">
       Vous pouvez y être accompagné·e pour faire votre demande et poser toutes
       vos questions.
@@ -52,14 +38,17 @@ import Institution from "@/lib/institution"
 import Etablissement from "@/components/etablissement"
 import EtablissementLib from "@/lib/etablissement"
 import { getEtablissements } from "@/../lib/benefits/etablissements"
+import BackButton from "@/components/buttons/back-button"
 
 export default {
   name: "Lieux",
   components: {
+    BackButton,
     Etablissement,
   },
   data: function () {
     return {
+      benefit: null,
       list: [],
       updating: true,
       window,
@@ -67,10 +56,10 @@ export default {
   },
   mounted: function () {
     const city = this.$store.getters.situation.menage.depcom
-    const benefit = Institution.benefits.all.find(
+    this.benefit = Institution.benefits.all.find(
       (benefit) => benefit.id === this.$route.params.benefit_id
     )
-    const types = getEtablissements(benefit)
+    const types = getEtablissements(this.benefit)
 
     axios
       .get(
@@ -95,6 +84,12 @@ export default {
       .finally(() => {
         this.updating = false
       })
+  },
+  methods: {
+    goToBenefitDetail() {
+      const path = `/simulation/resultats/${this.benefit.id}`
+      this.$router.push(path)
+    },
   },
 }
 </script>

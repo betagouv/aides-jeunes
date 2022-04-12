37c66cdcba4ca7c1bc657d72b254b9f6023b3952
Vanessa-D <vanessa.ducloux@laposte.net>
2022-04-12 13:44:20 +0200

    Ajoute un test qui vérifie la présence d'un code Siren pour les institutions de type Epci

--- tests/unit/benefits-description.spec.js
+++ tests/unit/benefits-description.spec.js
@@ -23,6 +23,13 @@ describe("benefit descriptions", function () {
         })
       }
 
+      if (institution.type == "epci") {
+        it("should have a code_siren", function () {
+          expect(typeof institution.code_siren).toBe("string")
+          expect(institution.code_siren.length).toBeGreaterThan(1)
+        })
+      }
+
       it("should have an id", function () {
         expect(typeof institution.id).toBe("string")
         expect(institution.id.length).toBeGreaterThan(1)

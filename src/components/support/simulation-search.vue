97a8219214d1419091459fcf37d9fc3f2dea16d0
Kout95 <65901733+Kout95@users.noreply.github.com>
2022-04-14 15:39:41 +0200

    Créer un champ pour accéder à une simulation dans l'outil d'accompagnement (#2755)
    * Créer une route pour accéder à la simulation
    
    * Améliore le design
    
    * Ajoute la regex
    
    * Ajoute une redirection vers la simulation
    
    * Affiche uniquement si l'utilisateur est connecté

---
+++ src/components/support/simulation-search.vue
@@ -0,0 +1,50 @@
+<template>
+  <form class="simulation-search" @submit="onSubmit">
+    <label for="simulationId" class="aj-question"
+      >Accéder à une simulation :</label
+    >
+    <div class="simulation-search-input">
+      <input
+        id="simulationId"
+        v-model="situationId"
+        type="text"
+        placeholder="ID de la simulation"
+        required
+      />
+      <button class="button primary">Accéder</button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "SimulationSearch",
+  data() {
+    return {
+      situationId: "",
+    }
+  },
+  methods: {
+    onSubmit() {
+      const situationId = /([a-z0-9]{24})/.exec(this.situationId)[0]
+      window?.open(`/api/support/simulation/${situationId}`, "_blank").focus()
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.simulation-search {
+  margin-bottom: 24px;
+
+  .simulation-search-input {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 16px;
+
+    input {
+      max-width: 400px;
+    }
+  }
+}
+</style>

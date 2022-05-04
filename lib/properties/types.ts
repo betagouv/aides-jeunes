b2edb54be31ea582d031beba98896fc05c168840
kout95 <antoine.bernier@outlook.fr>
2022-05-04 18:43:21 +0200

    Commence le typage

--- lib/properties/types.ts
+++ lib/properties/types.ts
@@ -1,1 +1,72 @@
-export interface Property {}
+interface PropertyData {
+  openFiscaParameters?: any
+  simulation?: any
+  individu?: any
+  periods?: any
+}
+
+export class Property {
+  question: string | ((data: PropertyData) => string)
+  questionType?: string
+  help?: string
+  moreInfo?: string
+
+  constructor(
+    question: string | ((data: PropertyData) => string),
+    questionType?: string,
+    help?: string,
+    moreInfo?: string
+  ) {
+    this.question = question
+    this.questionType = questionType
+    this.help = help
+    this.moreInfo = moreInfo
+  }
+
+  getValueOrExecuteFunction(key: string, propertyData: PropertyData): any {
+    // @ts-ignore
+    const value = this[key]
+    return typeof value === "function" ? value(propertyData) : value
+  }
+
+  getQuestion(propertyData: PropertyData) {
+    return this.getValueOrExecuteFunction("question", propertyData)
+  }
+}
+
+export class EnumProperty extends Property {
+  items?: [{}] | ((data: PropertyData) => [{}])
+
+  constructor(
+    question: string | ((data: PropertyData) => string),
+    questionType?: string,
+    help?: string,
+    moreInfo?: string,
+    items?: [{}] | ((data: PropertyData) => [{}])
+  ) {
+    super(question, questionType, help, moreInfo)
+    this.items = items
+  }
+
+  getItems(propertyData: PropertyData) {
+    return this.getValueOrExecuteFunction("items", propertyData)
+  }
+}
+
+export class NumberProperty extends Property {
+  types: string
+  unit?: string
+
+  constructor(
+    question: string | ((data: PropertyData) => string),
+    questionType?: string,
+    help?: string,
+    moreInfo?: string,
+    types: string = "amount",
+    unit?: string
+  ) {
+    super(question, questionType, help, moreInfo)
+    this.types = types
+    this.unit = unit
+  }
+}

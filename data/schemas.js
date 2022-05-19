ae71dd81ea431ead4b39ac2fc9886a5f15c59ec5
Cugniere <16650011+Cugniere@users.noreply.github.com>
2022-05-19 09:59:50 +0200

    chore: cleanup et commentaires

--- data/schemas.js
+++ data/schemas.js
@@ -15,26 +15,16 @@ const typesMap = {
   hidden: "hidden",
 }
 
-function getFieldType(field) {
-  return typesMap[field.widget] ? typesMap[field.widget] : field.widget
-}
-
 function generateSchema(fields) {
   let schema = {}
   for (let field of fields) {
     let line = {
-      type: getFieldType(field),
+      type: typesMap[field.widget] ? typesMap[field.widget] : field.widget,
       required: field.required === false ? false : true,
     }
     if (field.fields) {
       schema[field.name] = generateSchema(field.fields)
-    } else if (
-      field.widget == "list" ||
-      field.widget == "object" ||
-      field.types ||
-      field.type ||
-      (field.widget == "hidden" && field.type == "list")
-    ) {
+    } else if (["list", "object"].includes(field.widget)) {
       if (field.types) {
         schema[field.name] = generateSchema(field.types)
       } else if (field.fields) {
@@ -51,42 +41,43 @@ function generateSchema(fields) {
   return schema
 }
 
-function errorLogger(field, value, expectedType, depth) {
-  return {
-    path: `${depth.join(".")}${depth.length ? "." : ""}${field}`,
-    message: `${field} is of type ${typeof value}, ${expectedType} expected in schema`,
+function errorLogger(field, depth = [], value, expectedType) {
+  if (value && expectedType) {
+    return {
+      path: `${depth.join(".")}${depth.length ? "." : ""}${field}`,
+      message: `${field} is of type ${typeof value}, ${expectedType} expected in schema`,
+    }
+  } else {
+    return {
+      path: `${depth.join(".")}${depth.length ? "." : ""}${field}`,
+      message: `${field} is not present in schema`,
+    }
   }
 }
 
 function compareSchema(data, schema, output, depth = []) {
-  /*
-    compareSchema works as follow:
-    - first loop:
-        - makes sure that every key in the data is in the schema
-        - don't take into account "hidden" type key, since their format isn't controlled
-        - if the type of the data key is an object, recursively call compareSchema
-    - second loop : make sure that the data is not missing a field
-  */
+  // Check that every field in the schema is in the data and of the specified type
   const schemaKeys = Object.keys(schema)
   for (let key in data) {
-    if (schemaKeys.includes(key)) {
+    if (schemaKeys.includes(key) && schema[key].type != "hidden") {
       if (
-        typeof data[key] !== schema[key].type &&
-        data[key] !== null &&
-        schema[key].type != "hidden" &&
-        typeof schema[key].type !== "undefined" &&
-        typeof schema[key].type !== "object"
+        typeof data[key] !== schema[key].type && // compare type with expected type
+        data[key] !== null && // in case the field was unset in netlify CMS
+        schema[key].type != "hidden" && // don't check hidden fields
+        typeof schema[key].type !== "undefined" && // skip array check
+        typeof schema[key].type !== "object" // don't check object
       ) {
-        output.push(errorLogger(key, data[key], schema[key].type, depth))
-      }
-      if (typeof data[key] == "object" && schema[key].type != "hidden") {
+        output.push(errorLogger(key, depth, data[key], schema[key].type))
+      } else if (typeof data[key] == "object") {
+        // if the field contains other field, check them recursively
+        // if the field is an array, check each item against a list of possible fields
         if (data[key] instanceof Array) {
           if (schema[key] instanceof Array) {
             const types = schema[key].map((field) => field.type)
             for (let i in data[key]) {
               if (!types.includes(typeof data[key][i])) {
                 output.push(
-                  errorLogger(i, data[key][i], types.join(", "), depth)
+                  errorLogger(i, depth, data[key][i], types.join(", "))
                 )
               }
             }
@@ -99,17 +90,17 @@ function compareSchema(data, schema, output, depth = []) {
               }
             }
           }
+          // if the field is an object, check each key separately
         } else if (data[key] != null) {
           compareSchema(data[key], schema[key], output, [...depth, key])
         }
       }
-    } else {
-      output.push({
-        path: `${depth.join(".")}${depth.length ? "." : ""}${key}`,
-        message: `${key} is not present in schema`,
-      })
+      // if the field is not in the schema
+    } else if (typeof schema[key]?.type === "undefined") {
+      output.push(errorLogger(key, depth))
     }
   }
+
   const dataKeys = Object.keys(data)
   for (let key in schema) {
     if (

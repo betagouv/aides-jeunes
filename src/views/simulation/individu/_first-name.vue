21c7cb6a75c5d10e856560d1c4c7cfda458361df
vanessa <vanessa.ducloux@laposte.net>
2022-06-02 18:35:43 +0200

    Ajoute des rôles titre h2 sur des questions n'ayant pas de balise titre

--- src/views/simulation/individu/_first-name.vue
+++ src/views/simulation/individu/_first-name.vue
@@ -1,6 +1,6 @@
 <template>
   <form @submit.prevent="onSubmit">
-    <label for="_firstName" class="aj-question"
+    <label role="heading" aria-level="2" for="_firstName" class="aj-question"
       >Quel est le prénom de votre enfant ? Il servira uniquement à vous
       faciliter la saisie par la suite.</label
     >

84162c7f7da17cec72c22bc51f322a17929b8436
vanessa <vanessa.ducloux@laposte.net>
2022-06-02 13:42:03 +0200

    Ajoute un type d'aide à la liste des aides

--- src/views/liste-aides.vue
+++ src/views/liste-aides.vue
@@ -38,6 +38,7 @@ const TYPES = {
   national: "Aides nationales",
   region: "Aides régionales",
   departement: "Aides départementales",
+  autre: "Collectivités territoriales",
   epci: "EPCI (Métropole, inter-communauté, etc.)",
   caf: "CAF Locales",
   commune: "Aides communales",

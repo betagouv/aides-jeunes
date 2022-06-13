fbbd3663649601f7da808568bdb452b5723e5e9b
Kout95 <65901733+Kout95@users.noreply.github.com>
2022-06-13 10:55:22 +0200

    Revert "Ajoute le système de vérification des liens de aides-velo (#2942)"
    This reverts commit d9696a40b699e212496b64926145a9936e78ed16.

---
+++ tools/test-benefits-urls.js
@@ -0,0 +1,33 @@
+const axios = require("axios")
+const subject = require("../data/all")
+
+let links = []
+let done = 0
+
+subject.all.forEach((benefit) => {
+  links = links.concat(
+    [benefit.link, benefit.instructions, benefit.form, benefit.teleservice]
+      .filter((link) => link)
+      .map((link) => {
+        return {
+          benefitId: benefit.id,
+          url: link,
+        }
+      })
+  )
+})
+
+links.forEach((link) => {
+  axios
+    .get(link.url)
+    .then(() => {
+      done += 1
+      console.log(`[${done} / ${links.length}] Ok ${link.benefitId}`)
+    })
+    .catch(() => {
+      done += 1
+      console.log(
+        `[${done} / ${links.length}] Warning ${link.benefitId} lien potentiellement invalide: ${link.url}`
+      )
+    })
+})

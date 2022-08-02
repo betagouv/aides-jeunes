4ebe8a25d8f119bbfc85b43c0ef0834529956980
Thomas Guillet <thomas@palya.fr>
2022-08-02 20:07:22 +0200

    fixup! Rend plus actionnable la page iframe pour favoriser les réutilisations

--- public/documents/iframe-integration.js
+++ public/documents/iframe-integration.js
@@ -1,7 +1,7 @@
 ;(function () {
   "use strict"
   const script = document.currentScript
-  const src = new URL(window.location.href.slice(0, -6) + "simulation")
+  const src = new URL(window.location.href.slice(0, -7) + "/simulation")
 
   src.searchParams.set("iframe", true)
   src.searchParams.set(
@@ -11,7 +11,7 @@
 
   const iframe = document.createElement("iframe")
   const iframeAttributes = {
-    id: "simulateurEmbauche",
+    id: "simulateur",
     src: src.toString(),
     style: "border: none; width: 100%; display: block; height: 700px",
     allow: "clipboard-write",
@@ -24,16 +24,4 @@
   }
 
   script.before(iframe)
-  window.addEventListener("message", function (evt) {
-    if (evt.data.kind === "resize-height") {
-      iframe.style.height = evt.data.value + "px"
-    }
-    if (evt.data.kind === "get-offset") {
-      const iframePosition = iframe.getBoundingClientRect()
-      iframe.contentWindow?.postMessage(
-        { kind: "offset", value: Math.max(iframePosition.top * -1, 0) },
-        "*"
-      )
-    }
-  })
 })()

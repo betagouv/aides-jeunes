befd48ceb0c263d20086a1374eca7ff9217ea593
Cugniere <16650011+Cugniere@users.noreply.github.com>
2022-08-04 09:16:33 +0200

    chore: minimize import

--- data/all.ts
+++ data/all.ts
@@ -1,11 +1,10 @@
-import path from "path"
-import fs from "fs"
+import { existsSync } from "node:fs"
 import base from "./index.js"
 import jamstackLoader from "jamstack-loader"
 
 let configFile = "contribuer/public/admin/config.yml"
 let jamstack
-if (fs.existsSync(configFile)) {
+if (existsSync(configFile)) {
   jamstack = jamstackLoader.get(configFile)
 } else {
   jamstack = jamstackLoader.get(`../${configFile}`)

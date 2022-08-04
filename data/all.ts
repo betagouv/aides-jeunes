fa7f1430451869ef87c111b03e7f91d76c47d592
Cugniere <16650011+Cugniere@users.noreply.github.com>
2022-08-04 09:18:19 +0200

    chore: lint

--- data/all.ts
+++ data/all.ts
@@ -2,7 +2,7 @@ import { existsSync } from "node:fs"
 import base from "./index.js"
 import jamstackLoader from "jamstack-loader"
 
-let configFile = "contribuer/public/admin/config.yml"
+const configFile = "contribuer/public/admin/config.yml"
 let jamstack
 if (existsSync(configFile)) {
   jamstack = jamstackLoader.get(configFile)

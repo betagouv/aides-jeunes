8b87c555ce7147ec1ebf6a58258dfeb6462ac175
kout95 <antoine.bernier@outlook.fr>
2022-08-09 16:37:15 +0200

    Corrige le lien

--- iframes.config.js
+++ iframes.config.js
@@ -1,11 +1,17 @@
 import path from "path"
 import { fileURLToPath } from "url"
-import config from "./dist-server/backend/config/index.js"
 import webpack from "webpack"
+import config from "./dist-server/backend/config/index.js"
 
 const dirname = path.dirname(fileURLToPath(import.meta.url))
 
-process.env.BASE_URL = config.baseURL
+if (process.env.NODE_ENV === "production") {
+  const port = process.env.PORT || 8080
+  process.env.BASE_URL =
+    process.env.MES_AIDES_ROOT_URL || `http://localhost:${port}`
+} else {
+  process.env.BASE_URL = config.baseURL
+}
 
 export default {
   mode: "production",

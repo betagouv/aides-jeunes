4fb456c2ee0b77940f9fb275ff26c1123242d1a8
kout95 <antoine.bernier@outlook.fr>
2022-08-11 15:49:48 +0200

    Ajoute deux urls pour récupérer des data consolidées sur les aides vélos

--- backend/controllers/data.ts
+++ backend/controllers/data.ts
@@ -1,4 +1,5 @@
 import data from "../../data/all.js"
+import Simulation from "../models/simulation.js"
 
 export function benefits(req, res) {
   res.send(data.all)
@@ -7,6 +8,86 @@ export function benefits(req, res) {
 const institutionList = Object.values(data.institutionsMap)
 export function institutions(req, res) {
   res.send(institutionList)
+}
+
+export async function countSimulationByBikeTypeNumber(req, res) {
+  const total: number = await Simulation.count({
+    "answers.all.fieldName": "_interetsAidesVelo",
+  })
+  const aggregateResult: any[] = await Simulation.aggregate([
+    { $unwind: "$answers.all" },
+    { $match: { "answers.all.fieldName": "_interetsAidesVelo" } },
+    {
+      $group: {
+        _id: {
+          bikeTypeNumber: { $size: "$answers.all.value" },
+        },
+        count: {
+          $sum: 1,
+        },
+      },
+    },
+    {
+      $sort: { count: -1 },
+    },
+  ])
+  const result = {
+    total: total,
+    countByBikeTypesNumber: aggregateResult.reduce((accum, item) => {
+      accum[item._id.bikeTypeNumber] = item.count
+      return accum
+    }, {}),
+  }
+  res.send(result)
+}
+
+export async function countSimulationByBikeTypeNumberByBikeType(req, res) {
+  const aggregateResult: any[] = await Simulation.aggregate([
+    { $unwind: "$answers.all" },
+    { $match: { "answers.all.fieldName": "_interetsAidesVelo" } },
+    {
+      $addFields: {
+        bikeTypeNumber: { $size: "$answers.all.value" },
+      },
+    },
+    { $unwind: "$answers.all.value" },
+    {
+      $group: {
+        _id: {
+          bikeTypeNumber: "$bikeTypeNumber",
+          bikeType: "$answers.all.value",
+        },
+        count: {
+          $sum: 1,
+        },
+      },
+    },
+    {
+      $sort: { "_id.bikeType": -1 },
+    },
+  ])
+  let total = 0
+
+  const countByBikeTypeByBikeTypesNumber = aggregateResult.reduce(
+    (accum, item) => {
+      total += item.count
+      if (!accum[item._id.bikeTypeNumber]) {
+        accum[item._id.bikeTypeNumber] = {
+          count: 0,
+        }
+      }
+      accum[item._id.bikeTypeNumber][item._id.bikeType] = item.count
+      accum[item._id.bikeTypeNumber].count += item.count
+
+      return accum
+    },
+    {}
+  )
+  const result = {
+    total: total,
+    countByBikeTypeByBikeTypesNumber,
+  }
+  res.send(result)
 }
 
 export default { benefits, institutions }
--- backend/routes/data.ts
+++ backend/routes/data.ts
@@ -1,8 +1,19 @@
 import cors from "cors"
-import { benefits, institutions } from "../controllers/data.js"
+import {
+  benefits,
+  countSimulationByBikeTypeNumber,
+  countSimulationByBikeTypeNumberByBikeType,
+  institutions,
+} from "../controllers/data.js"
 
 const dataRoutes = function (api) {
   api.route("/benefits").get(cors({ origin: "*" }), benefits)
   api.route("/institutions").get(cors({ origin: "*" }), institutions)
+  api
+    .route("/count-by-bike-types-number")
+    .get(cors({ origin: "*" }), countSimulationByBikeTypeNumber)
+  api
+    .route("/count-simulation-by-bike-type-number-by-bike-type")
+    .get(cors({ origin: "*" }), countSimulationByBikeTypeNumberByBikeType)
 }
 export default dataRoutes

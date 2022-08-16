5bfc6d016c7cc0fec4cec510b421d2d1768565b9
Thomas Guillet <guillet.thomas@gmail.com>
2022-08-16 12:03:21 +0200

    Ordonne les liens défectuels par importance et groupés par aide (#3135)

--- tools/check-links-validity.ts
+++ tools/check-links-validity.ts
@@ -1,53 +1,30 @@
 import Benefits from "../data/all.js"
 import axios from "axios"
+import fs from "fs"
+import Bluebird from "bluebird"
 
-// Extrait la liste des liens référencés dans la base de règles
-let links = []
-Benefits.all.forEach((benefit) => {
-  links = links.concat(
-    ["link", "instructions", "form", "teleservice"]
-      .filter(
-        (linkType) => benefit[linkType] && typeof benefit[linkType] === "string"
-      )
-      .map((linkType) => {
-        const link = benefit[linkType]
-        return {
-          title: `${benefit.label} (${benefit.institution.label})`,
-          link,
-          type: linkType,
-          editLink: ["openfisca", "javascript"].includes(benefit.source)
-            ? `https://contribuer-aides-jeunes.netlify.app/admin/#/collections/benefits_${benefit.source}/entries/${benefit.id}`
-            : undefined,
-        }
-      })
+async function checkURL(benefit) {
+  const results = await Bluebird.map(benefit.links, fetchStatus)
+  const errors = results.filter((r) => !r.ok)
+  console.log(
+    `${benefit.label} (${benefit.institution})\n${results
+      .map((e) => (e.ok ? `- ✅ ${e.type}` : `- ❌ ${e.type} ${e.link}`))
+      .join("\n")}`
   )
-})
 
-// Certains sites référencés ont des problèmes de certificats, mais ce n'est pas
-// ce que nous cherchons à détecter ici.
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
-
-// Création d'une queue permettant de paralléliser la vérification des liens
-const queue = [...links]
-const detectedErrors = []
-const simultaneousItems = 5
-
-async function processNextQueueItem() {
-  if (queue.length !== 0) {
-    await fetchAndReport(queue.shift())
-    await processNextQueueItem()
-  }
+  return { ...benefit, errors }
 }
 
-async function fetchAndReport({ link, title, editLink, type }) {
-  let status = await getHTTPStatus(link)
+async function fetchStatus(ressource) {
+  ressource.status = await getHTTPStatus(ressource.link)
 
   // Retry one time in case of timeout
-  if (status === 499) {
+  if (ressource.status === 499) {
     await sleep(10000)
-    status = await getHTTPStatus(link)
+    ressource.status = await getHTTPStatus(ressource.link)
   }
-  report({ status, link, title, editLink, type })
+  ressource.ok = ressource.status === 200
+  return ressource
 }
 
 async function getHTTPStatus(link) {
@@ -61,52 +38,121 @@ async function getHTTPStatus(link) {
   }
 }
 
-async function report({ status, link, title, editLink, type }) {
-  console.log(status === 200 ? "✅" : "❌", status, link)
-  if (status !== 200) {
-    detectedErrors.push({ status, link, title, editLink, type })
-  }
-}
-
 function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms))
 }
 
-;(async () => {
-  await Promise.allSettled(
-    Array.from({ length: simultaneousItems }).map(processNextQueueItem)
-  )
-  if (detectedErrors.length > 0) {
-    // Formattage spécifique pour récupérer le résultat avec l'action Github
-    if (process.argv.slice(2).includes("--ci")) {
-      const message = `
-
-			Certains liens référencés ne semblent plus fonctionner :
-
-			| Aide | Type | Status HTTP | Lien de modification |
-			|---|---|---|---|
-			${detectedErrors
-        .map(
-          ({ status, title, link, editLink, type }) =>
-            `| [${title}](${link}) | ${type} | ${status} | ${
-              editLink ? `[✎ Modifier](${editLink})` : ""
-            } |`
+async function getPriorityStats() {
+  const stats = await axios
+    .get("https://aides-jeunes-stats-recorder.osc-fr1.scalingo.io/statistics")
+    .then((r) => r.data)
+  const statTotal = stats.map((v) => {
+    const p = v.events?.showDetails || {}
+    const totals = Object.keys(p)
+    return {
+      benefit: v.benefit,
+      count: totals.reduce((at, t) => {
+        const indexes = Object.keys(p[t])
+        return (
+          at +
+          indexes.reduce((ai, i) => {
+            return ai + p[t][i]
+          }, 0)
         )
-        .join("\n")}`
-
-      const format = (msg) =>
-        msg
-          .trim()
-          .split("\n")
-          .map((line) => line.trim())
-          .join("<br />")
-      console.log(`::set-output name=comment::${format(message)}`)
-    } else if (detectedErrors) {
-      console.log(
-        `Liens invalides : ${detectedErrors.map(({ link }) => `\n- ${link}`)}`
-      )
+      }, 0),
     }
+  })
 
-    console.log("Terminé")
+  const statByBenefitId = statTotal.reduce((a, v) => {
+    a[v.benefit] = v.count
+    return a
+  }, {})
+
+  return statByBenefitId
+}
+
+async function getBenefitData() {
+  const priorityMap = await getPriorityStats()
+
+  const data = Benefits.all.map((benefit) => {
+    const linkMap = ["link", "instructions", "form", "teleservice"]
+      .filter(
+        (linkType) => benefit[linkType] && typeof benefit[linkType] === "string"
+      )
+      .reduce((a, linkType) => {
+        const link = benefit[linkType]
+        a[link] = a[link] || { link, types: [] }
+        a[link].types.push(linkType)
+
+        return a
+      }, {})
+
+    const links = Object.values(linkMap).map((v: any) => {
+      return {
+        link: v.link,
+        type: v.types.join(" / "),
+      }
+    })
+
+    return {
+      id: benefit.id,
+      label: benefit.label,
+      institution: benefit.institution.label,
+      priority: priorityMap[benefit.id] || 0,
+      links,
+      editLink: ["openfisca", "javascript"].includes(benefit.source)
+        ? `https://contribuer-aides-jeunes.netlify.app/admin/#/collections/benefits_${benefit.source}/entries/${benefit.id}`
+        : undefined,
+    }
+  })
+  return data.sort((a, b) => +(a.priority - b.priority))
+}
+
+function githubRowFormat(data) {
+  const errors = data.errors.map(
+    (e) => ` - [${e.type}](${e.link}) (${e.status})`
+  )
+  const details = `- [ ] [Modifier](${data.editLink}) ${data.label} / ${data.institution} (${data.priority} affichages)`
+  return [details, ...errors].join("\n")
+}
+
+function basicFormat(data) {
+  const errors = data.errors.map((e) => ` - ${e.status} ${e.type} ${e.link}`)
+  const details = `- ${data.label} / ${data.institution} (${data.priority} affichages)`
+  return [details, ...errors].join("\n")
+}
+
+function buildMessage(benefitWithErrors, rowFormat) {
+  return `Certaines aides référencées (${
+    benefitWithErrors.length
+  }) ont des liens dysfonctionnels :
+
+${benefitWithErrors.map(rowFormat).join("\n")}`
+}
+
+function buildGitHubIssueCommentText(benefitWithErrors) {
+  return `::set-output name=comment::${buildMessage(
+    benefitWithErrors,
+    githubRowFormat
+  )}`
+    .split("\n")
+    .join("<br />")
+}
+
+async function main() {
+  const benefitData = await getBenefitData()
+  const results = await Bluebird.map(benefitData, checkURL, { concurrency: 3 })
+  const detectedErrors = results
+    .filter((i) => i.errors.length)
+    .sort((a, b) => -(a.priority - b.priority))
+  if (detectedErrors.length > 0) {
+    if (process.argv.slice(2).includes("--ci")) {
+      console.log(buildGitHubIssueCommentText(detectedErrors))
+    } else if (detectedErrors) {
+      console.log(buildMessage(detectedErrors, basicFormat))
+    }
   }
-})()
+  console.log("Terminé")
+}
+
+main()

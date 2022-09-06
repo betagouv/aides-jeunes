058118bacf879d63ab42de388aeb94b43afc6807
Shamzic <shamery@free.fr>
2022-09-06 18:06:39 +0200

    Iframe: ajout d'un lien dans le footer de l'iframe

--- src/components/iframe-layout.vue
+++ src/components/iframe-layout.vue
@@ -1,5 +1,11 @@
 <template>
   <slot />
+  <div id="iframe-footer">
+    <p>Simulateur propulsé par&nbsp;</p>
+    <a href="https://www.1jeune1solution.gouv.fr/" target="_blank">
+      la plateforme 1jeune1solution
+    </a>
+  </div>
 </template>
 
 <script>
@@ -12,5 +18,12 @@ export default {
 html,
 body {
   height: unset !important;
+}
+
+#iframe-footer {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 13px;
 }
 </style>

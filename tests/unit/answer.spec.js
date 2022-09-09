a3463a77daba61aa864a9c59767febb08f1ae12a
Shamzic <shamery@free.fr>
2022-09-09 11:58:52 +0200

    Correction prettier

--- tests/unit/answer.spec.js
+++ tests/unit/answer.spec.js
@@ -6,30 +6,35 @@ const initMockStore = () => {
   store.calculs = { dirty: false }
   store.simulation = {
     answers: {
-      all: [{
-        id: "demandeur",
-        entityName: "individu",
-        fieldName: "date_naissance",
-        value: "Thu Jul 06 1995 00:00:00 GMT+0200 (heure d’été d’Europe centrale)"
-      }, {
-        id: "demandeur",
-        entityName: "individu",
-        fieldName: "nationalite",
-        value: "FR"
-      },
-      {
-        id: "demandeur",
-        entityName: "individu",
-        fieldName:"activite",
-        value:"independant"
-      }, {
-        entityName:"individu",
-        fieldName:"handicap",
-        id:"demandeur",
-        value: false
-      }],
+      all: [
+        {
+          id: "demandeur",
+          entityName: "individu",
+          fieldName: "date_naissance",
+          value:
+            "Thu Jul 06 1995 00:00:00 GMT+0200 (heure d’été d’Europe centrale)",
+        },
+        {
+          id: "demandeur",
+          entityName: "individu",
+          fieldName: "nationalite",
+          value: "FR",
+        },
+        {
+          id: "demandeur",
+          entityName: "individu",
+          fieldName: "activite",
+          value: "independant",
+        },
+        {
+          entityName: "individu",
+          fieldName: "handicap",
+          id: "demandeur",
+          value: false,
+        },
+      ],
       current: [],
-    }
+    },
   }
 }
 
@@ -38,59 +43,59 @@ describe("Store answers tests", () => {
     initMockStore()
   })
 
-  it('Store should not be dirty when the answer value is the same', () => {
-    const newAnswer =  {
-      id: "demandeur",
-      entityName: "individu",
-      fieldName: "nationalite",
-      value: "FR"
-    }
-    addAnswer(newAnswer, store.simulation, store.calculs)
-    expect(store.calculs.dirty).toEqual(false)
-  })
-
-  it('Store should not be dirty when multiple answers values are the same', () => {
-    let newAnswer = {
-      id: "demandeur",
-      entityName: "individu",
-      fieldName: "nationalite",
-      value: "FR"
-    }
-    addAnswer(newAnswer, store.simulation, store.calculs)
-    newAnswer = {
-      entityName:"individu",
-      fieldName:"handicap",
-      id:"demandeur",
-      value: false
-    }
-    addAnswer(newAnswer, store.simulation, store.calculs)
-    expect(store.calculs.dirty).toEqual(false)
-  })
-
-  it('Store should be dirty when the answer value is different', () => {
+  it("Store should not be dirty when the answer value is the same", () => {
     const newAnswer = {
       id: "demandeur",
       entityName: "individu",
       fieldName: "nationalite",
-      value: "EN"
+      value: "FR",
+    }
+    addAnswer(newAnswer, store.simulation, store.calculs)
+    expect(store.calculs.dirty).toEqual(false)
+  })
+
+  it("Store should not be dirty when multiple answers values are the same", () => {
+    let newAnswer = {
+      id: "demandeur",
+      entityName: "individu",
+      fieldName: "nationalite",
+      value: "FR",
+    }
+    addAnswer(newAnswer, store.simulation, store.calculs)
+    newAnswer = {
+      entityName: "individu",
+      fieldName: "handicap",
+      id: "demandeur",
+      value: false,
+    }
+    addAnswer(newAnswer, store.simulation, store.calculs)
+    expect(store.calculs.dirty).toEqual(false)
+  })
+
+  it("Store should be dirty when the answer value is different", () => {
+    const newAnswer = {
+      id: "demandeur",
+      entityName: "individu",
+      fieldName: "nationalite",
+      value: "EN",
     }
     addAnswer(newAnswer, store.simulation, store.calculs)
     expect(store.calculs.dirty).toEqual(true)
   })
 
-  it('Store should be dirty when multiple answers values are different', () => {
+  it("Store should be dirty when multiple answers values are different", () => {
     let newAnswer = {
       id: "demandeur",
       entityName: "individu",
       fieldName: "nationalite",
-      value: "EN"
+      value: "EN",
     }
     addAnswer(newAnswer, store.simulation, store.calculs)
-    newAnswer =  {
-      entityName:"individu",
-      fieldName:"handicap",
-      id:"demandeur",
-      value: true
+    newAnswer = {
+      entityName: "individu",
+      fieldName: "handicap",
+      id: "demandeur",
+      value: true,
     }
     addAnswer(newAnswer, store.simulation, store.calculs)
     expect(store.calculs.dirty).toEqual(true)

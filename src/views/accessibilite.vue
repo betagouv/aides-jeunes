23f971aa77cd7c275e3cb9201977e479777e2d93
Thomas Guillet <guillet.thomas@gmail.com>
2022-09-15 10:19:29 +0200

    Met à jour la déclaration d'accessibilité (#3219)

--- src/views/accessibilite.vue
+++ src/views/accessibilite.vue
@@ -1,55 +1,211 @@
 <template>
   <article class="text container aj-text-container">
-    <h1>Déclaration d'accessibilité</h1>
+    <h1>Déclaration d’accessibilité</h1>
 
     <p>
-      L’accessibilité est prise en compte dans la conception de ce projet mais
-      nous n’avons pas encore passé d’audit de conformité.
+      L’entité s’engage à rendre son service accessible, conformément à
+      l’article 47 de la loi n°2005-102 du 11 février 2005.
     </p>
+
+    <p> Cette déclaration d’accessibilité s’applique à {{ baseURL }}. </p>
+
     <p>
-      À ce jour, la navigation au clavier et la lecture d'écran est possible.
+      À ce jour, la navigation au clavier et la lecture d’écran est possible.
     </p>
 
     <h2>État de conformité</h2>
 
     <p>
-      Le service est <strong>non conforme avec le RGAA 4.1</strong> : le site
-      n'a pas encore été audité.
+      Le service est
+      <strong
+        >non conforme avec le référentiel général d’amélioration de
+        l’accessibilité (RGAA), version 4.1</strong
+      >
+      en raison des non-conformités et des dérogations énumérées ci-dessous.
     </p>
 
-    <h2>Établissement de cette déclaration d'accessibilité</h2>
+    <h2>Résultats des tests</h2>
 
-    <p>Cette déclaration a été établie le 9 avril 2021.</p>
+    <p
+      >L’audit de conformité réalisé par une experte en accessibilité web de
+      l’entreprise SMILE révèle que 42,55% des critères du RGAA version 4.1 sont
+      respectés.</p
+    >
+
+    <h2>Contenus non accessibles</h2>
+
+    <h3>Non-conformité</h3>
+
+    <ul>
+      <li
+        >De nombreuses anomalies vont pénaliser les utilisateurs en situation de
+        handicap, naviguant ou non à l’aide de technologies d’assistance.</li
+      >
+      <li
+        >Les non-conformités les plus bloquantes pour les utilisateurs
+        concernent :
+        <ul>
+          <li
+            >Certaines images de décoration ne sont pas correctement ignorée aux
+            outils d’assistance.</li
+          >
+          <li
+            >Certains texte et composant d’interface de contraste
+            insuffisants.</li
+          >
+          <li
+            >Des composants d’interface riches ne respectant pas les
+            recommandations de structuration pour l’accessibilité.</li
+          >
+          <li
+            >Problème de manipulation au clavier sur les composants
+            d’interface.</li
+          >
+          <li>Changement de contexte sans avertir l’utilisateur.</li>
+          <li>Erreurs de validation de code.</li>
+          <li>Manque d’information sur le title de la page.</li>
+          <li>Manque de structuration dans la page.</li>
+          <li>Certaines balises sont utilisées à des fins de présentation.</li>
+          <li
+            >Lorsque les feuilles de styles sont désactivées, le contenu de la
+            page devient incohérent.</li
+          >
+          <li
+            >Des contenus avec une structuration (titres et intertitres,
+            paragraphes, liste) incorrecte, ou insuffisante pour une navigation
+            efficace avec un lecteur d’écran</li
+          >
+          <li>Contenu de page est inséré en css.</li>
+          <li>En zoom 200% le menu principal est tronqué.</li>
+          <li>L’ordre de tabulation n’est pas traité correctement.</li>
+          <li
+            >Au niveau formulaire, étiquettes mal relié aux champs, absence de
+            l’aide et de contrôle à la saisie.</li
+          >
+          <li>Parfois regroupement manquant.</li>
+          <li
+            >L’attribut « autocomplete » manquant sur les champs de saisie
+            personnels.</li
+          >
+          <li>Liens d’évitements manquant sur le site.</li>
+        </ul>
+      </li>
+    </ul>
+
+    <h3>Établissement de cette déclaration d’accessibilité</h3>
+
+    <p>Cette déclaration a été établie le 8 septembre 2022.</p>
+
+    <h3>Technologies utilisées pour la réalisation du service</h3>
+
+    <ul>
+      <li>HTML5</li>
+      <li>CSS</li>
+      <li>JavaScript</li>
+    </ul>
+
+    <h3>Environnement de test</h3>
+
+    <p
+      >Les vérifications de restitution de contenus ont été réalisées sur la
+      base de la combinaison fournie par la base de référence du RGAA 4.1, avec
+      les versions suivantes :</p
+    >
+
+    <ul>
+      <li>Agent utilisateur : Firefox</li>
+      <li>Technologie d’assistance : NVDA</li>
+    </ul>
+
+    <h3>Outils pour évaluer l’accessibilité</h3>
+
+    <ul>
+      <li>Wave</li>
+      <li>WCAG color contrast checker</li>
+      <li>Headings maps</li>
+      <li>web developer toolbar</li>
+    </ul>
+
+    <h3>Pages du site ayant fait l’objet de la vérification de conformité</h3>
+    <ul>
+      <li>Page d’accueil : {{ baseURL }}/</li>
+      <li>Page accessibilité : {{ baseURL }}/accessibilite</li>
+      <li>Page contact : {{ baseURL }}/contact</li>
+      <li>Page mentions légales : {{ baseURL }}/cgu</li>
+      <li>Page aides : {{ baseURL }}/aides</li>
+      <li
+        >Simulateur « Mon profil » :
+        {{ baseURL }}/simulation/individu/demandeur/date_naissance</li
+      >
+      <li>Simulateur « Mon Foyer » : {{ baseURL }}/simulation/enfants</li>
+      <li
+        >Simulateur « Mon Logement » :
+        {{ baseURL }}/simulation/menage/statut_occupation_logement</li
+      >
+      <li
+        >Simulateur « Mes revenus » :
+        {{ baseURL }}/simulation/individu/demandeur/ressources/types</li
+      >
+      <li>Page résultat simulateur : {{ baseURL }}/simulation/resultats</li>
+      <li
+        >Page récapitulative simulateur :
+        {{ baseURL }}/simulation/recapitulatif</li
+      >
+    </ul>
 
     <h2>Signaler un dysfonctionnement</h2>
 
-    <div>
-      <p>
-        Si vous rencontrez un défaut d’accessibilité vous empêchant d’accéder à
-        un contenu ou une fonctionnalité du site, merci de nous en faire part.
-      </p>
-      <ul>
-        <li>
-          <a
-            v-mail="{
-              subject: 'Problème d\'accessibilité',
-            }"
-            >{{ contactEmail }}</a
-          >
-        </li>
-      </ul>
-    </div>
+    <p>
+      Si vous rencontrez un défaut d’accessibilité vous empêchant d’accéder à un
+      contenu ou une fonctionnalité du site, merci de nous en faire part.
+    </p>
+    <ul>
+      <li>
+        <a
+          v-mail="{
+            subject: 'Problème d\'accessibilité',
+          }"
+          >{{ contactEmail }}</a
+        >
+      </li>
+    </ul>
+
+    <h2>Voies de recours</h2>
 
     <p>
       Si vous n’obtenez pas de réponse rapide de notre part, vous êtes en droit
-      de faire parvenir vos doléances ou une demande de saisine au
-      <a
-        href="https://formulaire.defenseurdesdroits.fr/code/afficher.php?ETAPE=accueil_2016"
-        target="_blank"
-        title="Défenseur des droits - Nouvelle fenêtre"
-        >Défenseur des droits</a
-      >.
+      de faire parvenir vos doléances ou une demande de saisine au Défenseur des
+      droits :
     </p>
+    <ul>
+      <li
+        ><a
+          href="https://formulaire.defenseurdesdroits.fr/code/afficher.php?ETAPE=accueil_2016"
+          target="_blank"
+          title="Défenseur des droits - Nouvelle fenêtre"
+          >avec le formulaire en ligne</a
+        ></li
+      >
+      <li
+        >en prenant contact avec
+        <a
+          href="https://www.defenseurdesdroits.fr/saisir/delegues"
+          target="_blank"
+          title="Défenseur des droits - Nouvelle fenêtre"
+          >une personne déléguée dans votre région</a
+        >
+        ou</li
+      >
+      <li
+        >en envoyant un courrier par la poste (gratuit, ne pas mettre de
+        timbre), à l’adresse suivante :
+        <address>
+          Défenseur des droits<br />
+          Libre réponse 71120<br />
+          75342 Paris CEDEX 07
+        </address></li
+      >
+    </ul>
   </article>
 </template>
 
@@ -59,5 +215,10 @@ import ContactEmailMixin from "@/mixins/contact-email"
 export default {
   name: "Accessibilite",
   mixins: [ContactEmailMixin],
+  setup() {
+    return {
+      baseURL: process.env.VITE_BASE_URL,
+    }
+  },
 }
 </script>

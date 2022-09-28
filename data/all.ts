29ad9e6e5dc26865c97b0abb7eeb570fb9c6654f
Cugniere <16650011+Cugniere@users.noreply.github.com>
2022-09-28 16:34:40 +0200

    fix: l'envoi d'email ne se faisait pas si la commande était lancé en dehors du repository

--- data/all.ts
+++ data/all.ts
@@ -1,13 +1,16 @@
 import { existsSync } from "node:fs"
+import path from "path"
 import base from "./index.js"
 import jamstackLoader from "jamstack-loader"
 
-const configFile = "contribuer/public/admin/config.yml"
+const __dirname = new URL(".", import.meta.url).pathname
+const configFile = path.join(__dirname, "../contribuer/public/admin/config.yml")
+
 let jamstack
 if (existsSync(configFile)) {
   jamstack = jamstackLoader.get(configFile)
 } else {
-  jamstack = jamstackLoader.get(`../${configFile}`)
+  jamstack = jamstackLoader.get(path.join(__dirname, "../../contribuer/public/admin/config.yml"))
 }
 
 export default base.generate(jamstack)

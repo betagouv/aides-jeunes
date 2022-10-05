78693ba6020dc23fb2a282321250d67499ab7cfd
Cugniere <16650011+Cugniere@users.noreply.github.com>
2022-10-05 09:52:44 +0200

    test: vérifie la consistence des aides CAF

---
+++ tests/integration/caf-benefits.spec.js
@@ -0,0 +1,24 @@
+import data from "@root/data/all"
+
+for (let benefit of data.all) {
+  if (benefit.institution.type == "caf") {
+    describe(`CAF benefit ${benefit.slug}`, () => {
+      it("should be restricted to exactly one departement", () => {
+        expect(
+          benefit.conditions_generales.some((condition) => {
+            return (
+              condition.type === "departements" && condition.values.length === 1
+            )
+          })
+        )
+      })
+      if (benefit.label.match(/bafa/i)) {
+        it("is related to BAFA and should have a corresponding flag", () => {
+          expect(
+            benefit.interestFlag && benefit.interestFlag === "_interetBafa"
+          )
+        })
+      }
+    })
+  }
+}

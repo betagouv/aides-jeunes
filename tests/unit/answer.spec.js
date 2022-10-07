346d7a4bfe106e6e3b507293d80ae9bb869e39fb
Shamzic <shamery@free.fr>
2022-10-07 15:13:36 +0200

    Fix: ajout des imports par ordre alphabétique

--- tests/unit/answer.spec.js
+++ tests/unit/answer.spec.js
@@ -1,4 +1,4 @@
-import { setActivePinia, createPinia } from "pinia"
+import { createPinia, setActivePinia } from "pinia"
 import { useStore } from "@root/src/stores"
 
 const initMock = (store) => {

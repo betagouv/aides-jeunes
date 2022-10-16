227c784056ae3f665f62e0e8daf9c6ced3e92eb1
Cugniere <16650011+Cugniere@users.noreply.github.com>
2022-10-16 09:11:36 +0200

    chore: clean test

--- tests/unit/components/input-date.spec.js
+++ tests/unit/components/input-date.spec.js
@@ -1,7 +1,3 @@
-/**
- * @jest-environment jsdom
- */
-//import { mount } from '@vue/test-utils'
 import InputDate from "@/components/input-date.vue"
 
 describe("input-date.vue", () => {
@@ -22,7 +18,11 @@ describe("input-date.vue", () => {
   })
 
   it("accept valid dates", async () => {
-    const testSet = [{ date: "2001-12-14", result: new Date("2001-12-14") }]
+    const testSet = [
+      { date: "2001-12-14", result: new Date("2001-12-14") },
+      { date: "1800-01-01", result: undefined },
+      { date: "2100-01-01", result: undefined },
+    ]
     for (const test of testSet) {
       let emitted
       InputDate.methods.update.call({
@@ -32,40 +32,4 @@ describe("input-date.vue", () => {
       expect(emitted.value).toEqual(test.result)
     }
   })
-
-  /*
-  it('accept valid dates', async () => {
-    const modelValue = '2007-12-12'
-    const wrapper = mount(InputDate, {
-      //props: { model: modelValue },
-      directives: {
-        "select-on-click"() { }
-      }
-    })
-
-    const testSet = {
-      day: 12,
-      month: 12,
-      year: 2001
-    }
-    const elements = {
-
-    }
-
-    console.log(wrapper.methods)
-    
-    //await day.setValue('12')
-    //await month.setValue('12')
-    //await year.setValue('year')
-    //expect(input.element.value).toBe('12')
-    
-    expect(wrapper.emitted()).toHaveProperty('modelValue')
-    //update:modelValue
-
-
-
-    //console.log(wrapper)
-    //expect(wrapper.text()).toMatch(modelValue)
-  })
-  */
 })

87768ecf0f45378ca00d8706d980d38df1b633ed
Valandr <valerie.andriani@gmail.com>
2022-11-22 09:23:32 +0100

    Footer iframe rendu compact

--- src/components/iframe-layout.vue
+++ src/components/iframe-layout.vue
@@ -4,15 +4,19 @@
   <div class="aj-iframe-footer-body">
     <div class="aj-iframe-footer">
       <p>
-        Simulateur propulsé par&nbsp;
+        Simulateur propulsé par
         <a href="https://www.1jeune1solution.gouv.fr/" target="_blank">
           la plateforme 1jeune1solution
         </a>
       </p>
-    </div>
-    <div class="aj-iframe-footer">
-      <p>Accédez à nos&nbsp;</p
-      ><a href="https://mes-aides.1jeune1solution.beta.gouv.fr/cgu">CGU</a>
+      <p>-</p>
+      <p>
+        Accédez à nos<a
+          href="https://mes-aides.1jeune1solution.beta.gouv.fr/cgu"
+        >
+          CGU</a
+        ></p
+      >
     </div>
   </div>
 </template>

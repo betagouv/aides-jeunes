6b16061a8cfea7500c77cdfded8a5d24118fcf48
Thomas Guillet <thomas@palya.fr>
2022-12-16 14:08:11 +0100

    Déplace la commande pour l'envoi d'email dans un script

--- backend/lib/email.sh
+++ backend/lib/email.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-echo "send benefit-action-survey --multiple 1000 >> /var/log/main/emails.log"
+cd `dirname $0`
+
+node ../../dist-server/backend/lib/email.js send benefit-action-survey --multiple 1000 # >> - #/var/log/main/emails.log

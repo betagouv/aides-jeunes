13dfa24621741a332c9880cfe50a9110a7c59132
Shamzic <shamery@free.fr>
2022-12-26 18:38:40 +0100

    Supprime une condition inutile

--- backend/lib/teleservices/openfisca-axe.ts
+++ backend/lib/teleservices/openfisca-axe.ts
@@ -30,23 +30,12 @@ function fetch(s) {
   const os = require("os")
   const path = require("path")
   const cachePath = path.join(os.tmpdir(), `simulation_${s.source._id}_${base}`)
-  /* eslint-disable */
-  if (false && fs.existsSync(cachePath)) {
-    // eslint-disable-line no-constant-condition
-    return fs
-      .readFileAsync(cachePath)
-      .then((data) => {
-        s.response = JSON.parse(data)
-      })
-      .then(() => s)
-  } else {
-    return request(s.request)
-      .then((payload) => {
-        s.response = payload
-        return fs.writeFileAsync(cachePath, JSON.stringify(payload, null, 2))
-      })
-      .then(() => s)
-  }
+  return request(s.request)
+    .then((payload) => {
+      s.response = payload
+      return fs.writeFileAsync(cachePath, JSON.stringify(payload, null, 2))
+    })
+    .then(() => s)
 }
 
 OpenFiscaAxe.prototype.toExternal = function () {

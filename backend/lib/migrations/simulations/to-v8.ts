737a1ea0107e3776f1ae5043e684c96fb803772a
Shamzic <shamery@free.fr>
2022-12-26 18:30:11 +0100

    Corrige eslint no-constant-condition

--- backend/lib/migrations/simulations/to-v8.ts
+++ backend/lib/migrations/simulations/to-v8.ts
@@ -7,20 +7,18 @@ import { ANSWER_BASIC_IDS } from "../../definitions"
 const VERSION = 8
 
 function removeIds(answers) {
-  // eslint-disable-next-line no-constant-condition
-  while (true) {
-    const index = answers.findIndex(
+  let index = null
+  while (!index || index > -1) {
+    index = answers.findIndex(
       (answer) =>
         !(
           ANSWER_BASIC_IDS.includes(answer.id) ||
           answer.id.match(/^enfant_\d+$/)
         )
     )
-    if (index > -1) {
+    if (index) {
       const result = omit(answers[index].toObject(), ["id", "_id"])
       answers[index] = result
-    } else {
-      break
     }
   }
 }

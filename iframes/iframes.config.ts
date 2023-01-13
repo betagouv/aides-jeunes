ffec7f58d7a2acbe7617401e713eb37b5ee5e5ba
cugniere <16650011+Cugniere@users.noreply.github.com>
2023-01-13 15:31:57 +0100

    update: config

--- iframes/iframes.config.ts
+++ iframes/iframes.config.ts
@@ -1,5 +1,5 @@
 import path from "path"
-import config from "../backend/config/index.ts"
+import config from "../backend/config/index.js"
 import webpack from "webpack"
 
 const __dirname = new URL(".", import.meta.url).pathname

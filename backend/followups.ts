0334f06a0881bfc5c98c1633b76b2f62c606f231
Thomas Guillet <thomas@palya.fr>
2023-01-17 17:50:07 +0100

    Supprime du code pas testé et pasa fonctionnel

--- backend/followups.ts
+++ backend/followups.ts
@@ -8,30 +8,6 @@ const router = express.Router()
 
 router.param("followupId", followup)
 router.get("/:followupId", resultRedirect)
-
-if (app.get("env") === "development") {
-  router.get("/:followupId/simulation-results.html", (req: ajRequest, res) => {
-    req.followup
-      .renderSimulationResultsEmail()
-      .then((render: any) => res.send(render.html))
-  })
-  router.get(
-    "/:followupId/simulation-usefulness-survey.html",
-    (req: ajRequest, res) => {
-      req.followup
-        .renderSimulationUsefulnessSurveyEmail({ returnPath: "/returnPath" })
-        .then((render: any) => res.send(render.html))
-    }
-  )
-  router.get(
-    "/:followupId/benefit-action-survey.html",
-    (req: ajRequest, res) => {
-      req.followup
-        .renderBenefitActionSurveyEmail({ returnPath: "/returnPath" })
-        .then((render: any) => res.send(render.html))
-    }
-  )
-}
 app.use(router)
 
 export default app

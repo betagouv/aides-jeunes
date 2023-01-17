875f7b6334d671c3897ec689748bd76598567284
Valandr <valerie.andriani@gmail.com>
2023-01-17 10:20:03 +0100

    Mise à jour du test du build

--- tests/integration/iframe-build.spec.js
+++ tests/integration/iframe-build.spec.js
@@ -4,14 +4,11 @@ const fs = require("fs")
 const filename = "../../dist/documents/iframe-integration.js"
 
 const buildOutput = fs.readFileSync(path.join(__dirname, `${filename}`), "utf8")
-console.log()
 describe("Test iframe build output", function () {
   it("File is not empty", function () {
     expect(buildOutput).not.toBe("")
   })
-  if (process.env.NODE_ENV === "production") {
-    it("Does not contain localhost", function () {
-      expect(buildOutput).not.toContain("localhost")
-    })
-  }
+  it("Does not contain localhost", function () {
+    expect(buildOutput).not.toContain("localhost")
+  })
 })

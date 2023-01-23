d9997a6c1e4af2b6af8356d79c4ff501ba7979b6
Shamzic <shamery@free.fr>
2023-01-23 22:00:37 +0100

    Supprime createSurvey (obsolète)

--- backend/models/followup.ts
+++ backend/models/followup.ts
@@ -111,15 +111,6 @@ FollowupSchema.method("addSurveyIfMissing", async function (type: SurveyType) {
   return survey
 })
 
-FollowupSchema.method("addSurveyIfMissing", async function (type: SurveyType) {
-  let survey = this.surveys.find((survey) => survey.type === type)
-  if (!survey) {
-    survey = await this.surveys.create({ type })
-    this.surveys.push(survey)
-  }
-  return survey
-})
-
 FollowupSchema.method("sendSurvey", function (surveyType: SurveyType) {
   const followup = this
   return this.addSurveyIfMissing(surveyType).then((survey: SurveyLayout) => {

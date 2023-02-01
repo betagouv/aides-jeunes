7be163b1d26d008b6c38a38bf5db3b01a9ff5c5c
ANDRIANI Valérie <52297439+Valandr@users.noreply.github.com>
2023-02-01 16:15:12 +0100

    Amélioration affichage nombre aides page toutes les aides (#3475)
    * Amélioration affichage nombre aides page toutes les aides
    
    * Création d'une institution pour chaque msa et création d'une msa locale
    
    * Ajoute un test en amount dans la CI
    
    * Évite le lancement du test OpenFisca sans validation de schéma
    
    * Modification noms msa
    
    * 2 itération modification noms msa
    
    * Remplacement mutualite_sociale_agricole par msa
    
    * Résolution problème de build du front
    
    * Suppression de msa de l'outil de contribution
    
    ---------
    
    Co-authored-by: Thomas Guillet <thomas@palya.fr>

--- tests/integration/schema-validation-config.spec.js
+++ tests/integration/schema-validation-config.spec.js
@@ -8,8 +8,33 @@ const jamstack = jamstackLoader.get(
   )
 )
 
-describe("Test admin config", function () {
-  it("should not have local_backend true", function () {
-    expect(Boolean(jamstack.local_backend)).toBe(false)
+describe("Schema validation", function () {
+  describe("config", function () {
+    it("should not have local_backend true", function () {
+      expect(Boolean(jamstack.local_backend)).toBe(false)
+    })
+  })
+
+  describe("benefits", function () {
+    const institutions = jamstack.collections.institutions.items
+    const institutionMap = institutions.reduce((result, item) => {
+      result[item.slug] = item
+      return result
+    }, {})
+
+    const benefits = [
+      ...jamstack.collections.benefits_openfisca.items,
+      ...jamstack.collections.benefits_javascript.items,
+    ]
+
+    benefits.forEach((benefit) => {
+      describe(benefit.slug, function () {
+        describe(`institution value '${benefit.institution}'`, function () {
+          it("should refer to the slug/filename of an existing institution", function () {
+            expect(institutionMap[benefit.institution]).toBeTruthy()
+          })
+        })
+      })
+    })
   })
 })

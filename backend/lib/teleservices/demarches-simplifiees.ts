5dc4c4d7e025dc58210d7a691e9e2cf0a9cd762c
Thomas Guillet <thomas@palya.fr>
2023-02-02 00:13:29 +0100

    Ajouter une aide et du pré-remplissage DS

---
+++ backend/lib/teleservices/demarches-simplifiees.ts
@@ -0,0 +1,32 @@
+import openfisca from "../openfisca/index.js"
+
+function DemarchesSimplifiees(simulation) {
+  this.simulation = simulation
+}
+
+DemarchesSimplifiees.prototype.toInternal = function () {
+  return this.simulation.answers.current.map((item) => {
+    return {
+      label: item.fieldName || item.entityName,
+      value: JSON.stringify(item.value),
+    }
+  })
+}
+
+DemarchesSimplifiees.prototype.toExternal = function () {
+  this.simulation.dateDeValeur.toISOString()
+  const text = `Parce que c'est l'heure (de la simulation ${this.simulation.dateDeValeur
+    .toISOString()
+    .slice(0, -5)})`
+  const number = Math.round(Math.random() * 100).toString()
+  const boolean = Math.random() > 0.5 ? "true" : "false"
+  const options = Math.random() > 0.5 ? "fromage" : "dessert"
+  return {
+    "champ_Q2hhbXAtMzAxNjg5Ng==": text,
+    "champ_Q2hhbXAtMzAxNjg5OA==": number,
+    "champ_Q2hhbXAtMzAxNjg5OQ==": boolean,
+    "champ_Q2hhbXAtMzAxNjkwMA==": options,
+  }
+}
+
+export default DemarchesSimplifiees

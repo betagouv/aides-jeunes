63a0e52bf7f89147c2373f569631c6e5c88a5d7c
Thomas Guillet <thomas@palya.fr>
2023-02-03 15:36:46 +0100

    Expérimente l'utilisation de JSON Schéma pour valider les fichies YAML

--- tests/integration/benefits-description.spec.js
+++ tests/integration/benefits-description.spec.js
@@ -21,45 +21,21 @@ describe("benefit descriptions", function () {
     describe(institutionSlug, function () {
       const institution = subject.institutionsMap[institutionSlug]
 
-      it("should have a correct slug", function () {
-        expect(Boolean(institutionSlug.match(/[a-z A-Z \-_]*/))).toBe(true)
-      })
-
       if (
         institution.type == "departement" ||
         institution.type == "region" ||
         institution.type == "commune"
       ) {
-        it("should have a code_insee", function () {
-          expect(typeof institution.code_insee).toBe("string")
-          expect(institution.code_insee.length).toBeGreaterThan(1)
-        })
-
         it("should have a relevant code_insee", function () {
           expect(codesInstitutions[institution.type]).toContain(
             institution.code_insee
           )
         })
-
-        it("should have a code_siren", function () {
-          expect(typeof institution.code_siren).toBe("string")
-          expect(institution.code_siren).toMatch(/^(1|2){1}[0-9]{8}$/)
-        })
       } else if (institution.type == "epci") {
-        it("should have a code_siren", function () {
-          expect(typeof institution.code_siren).toBe("string")
-          expect(institution.code_siren).toMatch(/^(1|2){1}[0-9]{8}$/)
-        })
-
         it("should have a relevant code_siren", function () {
           expect(codesInstitutions[institution.type]).toContain(
             institution.code_siren
           )
-        })
-      } else {
-        it("should have a code_siren", function () {
-          expect(typeof institution.code_siren).toBe("string")
-          expect(institution.code_siren).toMatch(/^[1-9]{1}[0-9]{8}$/)
         })
       }
 
@@ -68,18 +44,9 @@ describe("benefit descriptions", function () {
         expect(institution.id.length).toBeGreaterThan(1)
       })
 
-      it("should have a label", function () {
-        expect(typeof institution.label).toBe("string")
-        expect(institution.label.length).toBeGreaterThan(1)
-      })
-
       it("should refer to a img file that exists", function () {
         const path = `${__dirname}/../../public/${institution.imgSrc}`
         expect(fs.existsSync(path)).toBe(true)
-      })
-
-      it("should have a type", function () {
-        expect(typeof institution.type).toBe("string")
       })
 
       institution.benefitsIds.forEach(function (benefitId) {
@@ -92,15 +59,6 @@ describe("benefit descriptions", function () {
               expect(fs.existsSync(path)).toBe(true)
             })
           }
-
-          it("should have a correct id", function () {
-            expect(Boolean(benefitId.match(/[a-z A-Z \-_]*/))).toBe(true)
-          })
-
-          it("should have a label", function () {
-            expect(typeof benefit.label).toBe("string")
-            expect(benefit.label.length).toBeGreaterThan(1)
-          })
 
           it("should have a description", function () {
             expect(typeof benefit.description).toBe("string")
@@ -119,11 +77,6 @@ describe("benefit descriptions", function () {
               )
             })
           }
-
-          it("should have a link", function () {
-            expect(typeof benefit.link).toBe("string")
-            expect(benefit.link).toMatch(/^https?:\/\//)
-          })
 
           if (benefit.conditions) {
             describe("conditions", function () {

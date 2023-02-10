0c74d8a79b8e1d3902f5f422c27c778b607e3337
Thomas Guillet <thomas@palya.fr>
2023-02-10 14:12:47 +0100

    Expérimente une refonte de parcours

--- lib/state/blocks.ts
+++ lib/state/blocks.ts
@@ -473,6 +473,40 @@ function resourceBlocks(situation) {
 }
 
 export function generateBlocks(situation): BlockLayout[] {
+  if (true) {
+    return [
+      { steps: [new Step({})] },
+      new Step({
+        entity: "individu",
+        id: "demandeur",
+        variable: "date_naissance",
+        chapter: "profil",
+      }),
+      new Step({ entity: "menage", variable: "depcom" }),
+      new Step({
+        entity: "individu",
+        id: "demandeur",
+        variable: "_bourseCriteresSociauxCommuneDomicileFamilial",
+      }),
+      new Step({
+        entity: "famille",
+        variable: "bourse_criteres_sociaux_nombre_enfants_a_charge",
+      }),
+      new Step({
+        entity: "famille",
+        variable:
+          "bourse_criteres_sociaux_nombre_enfants_a_charge_dans_enseignement_superieur",
+      }),
+      new Step({
+        entity: "individu",
+        id: "demandeur",
+        variable: "bourse_criteres_sociaux_base_ressources_parentale",
+      }),
+      new Step({ entity: "resultats", chapter: "resultats" }),
+      new Step({ entity: "resultats", chapter: "resultats" }),
+    ]
+  }
+
   return [
     { steps: [new Step({})] },
     individuBlockFactory("demandeur"),

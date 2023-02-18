248f002fab02deec34583650729717d39edd2cce
cugniere <16650011+Cugniere@users.noreply.github.com>
2023-02-18 06:49:27 +0100

    update: catch erreur

--- tools/iframe-build-control.sh
+++ tools/iframe-build-control.sh
@@ -1,25 +1,24 @@
 FILE=./public/documents/iframe-integration.js
 
-Set -e
 
 echo "Testing iframe build"
 if [ -f "$FILE" ]; then
     echo "✅ $FILE exists"
-    exit 1
 else
     echo "❌ $FILE does not exists"
+    exit 1
 fi
 
 if [ -s "$FILE" ]; then
     echo "✅ $FILE is not empty"
-    exit 1
 else
     echo "❌ $FILE is empty"
+    exit 1
 fi
 
 if ! grep -q localhost "$FILE"; then
     echo "✅ $FILE does not contains string localhost"
-    exit 1
 else
     echo "❌ $FILE contains string localhost"
+    exit 1
 fi

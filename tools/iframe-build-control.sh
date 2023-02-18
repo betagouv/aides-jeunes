826b80d7fe0361c136290af31887459eed64e16c
cugniere <16650011+Cugniere@users.noreply.github.com>
2023-02-18 06:36:06 +0100

    update: utilise un script bash

---
+++ tools/iframe-build-control.sh
@@ -0,0 +1,24 @@
+FILE=./public/documents/iframe-integration.js
+
+
+echo "Testing iframe build"
+if [ -f "$FILE" ]; then
+    echo "✅ $FILE exists"
+else
+    echo "❌ $FILE does not exists"
+    exit 0
+fi
+
+if [ -s "$FILE" ]; then
+    echo "✅ $FILE is not empty"
+else
+    echo "❌ $FILE is empty"
+    exit 0
+fi
+
+if ! grep -q localhost "$FILE"; then
+    echo "✅ $FILE does not contains string localhost"
+else
+    echo "❌ $FILE contains string localhost"
+    exit 0
+fi

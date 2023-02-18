64a0ca47059811b1909c1f6300bb805b4f15d596
cugniere <16650011+Cugniere@users.noreply.github.com>
2023-02-18 06:42:11 +0100

    update: change code erreur

--- tools/iframe-build-control.sh
+++ tools/iframe-build-control.sh
@@ -6,19 +6,19 @@ if [ -f "$FILE" ]; then
     echo "✅ $FILE exists"
 else
     echo "❌ $FILE does not exists"
-    exit 0
+    exit 1
 fi
 
 if [ -s "$FILE" ]; then
     echo "✅ $FILE is not empty"
 else
     echo "❌ $FILE is empty"
-    exit 0
+    exit 1
 fi
 
 if ! grep -q localhost "$FILE"; then
     echo "✅ $FILE does not contains string localhost"
 else
     echo "❌ $FILE contains string localhost"
-    exit 0
+    exit 1
 fi

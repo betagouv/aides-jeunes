49d7c505e95e97b9dc831265a7bedd162606f295
cugniere <16650011+Cugniere@users.noreply.github.com>
2023-02-27 14:47:36 +0100

    update: ajout de la commune mornantais

--- tests/unit/compute-aides-velo-benefits.spec.js
+++ tests/unit/compute-aides-velo-benefits.spec.js
@@ -3,14 +3,14 @@ import { computeAidesVeloBenefits } from "@root/lib/benefits/compute-aides-velo"
 
 describe("computeAidesVeloBenefits", function () {
   it("matches EPCI data", function () {
-    const benefits = [{ external_id: "aides . pays basque" }]
+    const benefits = [{ external_id: "aides . vallée d'ossau" }]
     const situation = {
       dateDeValeur: "2021-01-01",
       menage: {
-        _codePostal: "64100",
-        depcom: "64102",
+        _codePostal: "64260",
+        depcom: "64062",
         _departement: "64",
-        _epci: "200067106",
+        _epci: "246400337",
         _region: "75",
       },
       demandeur: {

85eeb22d3e2e4800dc6f01b853e14faec61fad5d
Shamzic <shamery@free.fr>
2023-03-03 18:29:26 +0100

    Refacto '!important' non utiles dans le css

--- src/components/yes-no-question.vue
+++ src/components/yes-no-question.vue
@@ -13,7 +13,7 @@
     <div class="fr-container fr-px-0">
       <div class="fr-grid-row">
         <div class="fr-col-12 fr-col-sm-6 fr-col-md-6 fr-col-lg-6">
-          <div class="fr-radio-group fr-radio-rich fr-mb-2w">
+          <div class="fr-radio-group fr-radio-rich fr-mb-2w fr-mt-1w">
             <input
               :id="`yes-${uniqueFieldName}`"
               v-model="model"

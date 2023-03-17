179ff3779dfa16cbc27e2a2a0e99dc62d7d2cf99
Shamzic <shamery@free.fr>
2023-03-17 14:52:50 +0100

    Ajout du plugin des thèmes css

---
+++ src/plugins/theme-service.js
@@ -0,0 +1,27 @@
+import bordeauxMetropole from "@/styles/themes/bordeaux-metropole.css?inline"
+import lightBlue from "@/styles/themes/light-blue.css?inline"
+import defaultTheme from "@/styles/themes/default.css?inline"
+
+const themes = {
+  "bordeaux-metropole": bordeauxMetropole,
+  "light-blue": lightBlue,
+  default: defaultTheme,
+}
+
+const ThemeService = {
+  install: function (app) {
+    const styleElement = document.createElement("style")
+    document.head.appendChild(styleElement)
+
+    app.config.globalProperties.$theme = {
+      current: "default",
+      values: Object.keys(themes),
+      update(newTheme) {
+        app.config.globalProperties.$theme.current = newTheme
+        styleElement.textContent = themes[newTheme] || defaultTheme
+      },
+    }
+  },
+}
+
+export default ThemeService

eb83b5d4d1c04b47d261fa72f0432bff35020ae2
Shamzic <shamery@free.fr>
2023-03-24 17:35:24 +0100

    Refacto iframe integration

--- iframes/iframe-integration.js
+++ iframes/iframe-integration.js
@@ -14,19 +14,11 @@ if (script.getAttribute("data-with-logo") !== null) {
 }
 
 const currentScript = document.currentScript
-let selectedTheme = "theme-default-dsfr"
-if (
-  currentScript &&
-  currentScript.attributes &&
-  currentScript.attributes.length > 0
-) {
-  const themeAttribute = Array.from(currentScript.attributes).find((attr) =>
-    attr.name.startsWith("theme-")
-  )
-  if (themeAttribute) {
-    selectedTheme = themeAttribute.name
-  }
-}
+const themeAttribute = Array.from(currentScript.attributes).find((attribute) =>
+  attribute.name.startsWith("theme-")
+)
+const selectedTheme = themeAttribute?.name || "theme-default-dsfr"
+
 src.searchParams.set(selectedTheme, true)
 
 const iframe = document.createElement("iframe")

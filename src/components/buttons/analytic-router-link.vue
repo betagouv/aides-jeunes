f2cefb365334e17151c81a4c05cad78d4d204d28
Valandr <valerie.andriani@gmail.com>
2023-03-29 15:47:40 +0200

    Création d'un nouveau composant router-link pour l'enregistrement des événements

---
+++ src/components/buttons/analytic-router-link.vue
@@ -0,0 +1,41 @@
+<template>
+  <router-link @click="toto" :to="{}"> Montant inattendu ? </router-link>
+</template>
+
+<script>
+import { trackMatomoEvent } from "../../../src/directives/analytics.js"
+import sendStatistics from "../../../src/directives/analytics.js"
+import { useStore } from "@/stores/index.ts"
+
+export default {
+  name: "AnalyticRouterLink",
+  components: {
+    sendStatistics,
+    trackMatomoEvent,
+  },
+  props: {
+    name: String,
+    category: String,
+    action: String,
+    value: String,
+  },
+  setup() {
+    return {
+      store: useStore(),
+    }
+  },
+  computed: {
+    droits() {
+      const resultats =
+        !this.store.calculs.dirty && this.store.calculs.resultats
+
+      return resultats?.droitsEligibles
+    },
+  },
+  methods: {
+    toto() {
+      this.droits, this.name, this.action
+    },
+  },
+}
+</script>

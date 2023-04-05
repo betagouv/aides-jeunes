5abb5d1f07df806fa27d1f15e2b13594bfd0bbc2
Shamzic <shamery@free.fr>
2023-04-05 11:54:36 +0200

    Fix import layout header mes-aides

--- src/context/index.js
+++ src/context/index.js
@@ -1,3 +1,4 @@
 import baseContext from "./base.js"
 import mesAidesContext from "./mes-aides.js"
+
 export default { ...baseContext, ...mesAidesContext }
--- src/context/mes-aides.js
+++ src/context/mes-aides.js
@@ -1,3 +1,7 @@
 import MesAidesLayout from "@/components/mes-aides-layout.vue"
+import Home from "@/views/home.vue"
 
-export default MesAidesLayout
+export default {
+  Home,
+  MesAidesLayout,
+}

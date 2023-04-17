3d2bddcf309a9083fcb1b46610d83f810c623830
Thomas Guillet <thomas@palya.fr>
2023-04-17 18:41:16 +0200

    Corrige le calcul de la distance au foyer familial pour les étudiants orphelins

--- backend/lib/openfisca/mapping/individu/index.ts
+++ backend/lib/openfisca/mapping/individu/index.ts
@@ -51,6 +51,12 @@ const individuSchema: individuGeneratorLayout = {
   },
   bourse_criteres_sociaux_distance_domicile_familial: {
     fn: function (individu, situation) {
+      if (
+        ["decedes", "sans_autorite"].indexOf(situation.parents._situation) >= 0
+      ) {
+        return 0
+      }
+
       if (individu.habite_chez_parents) {
         return 0
       }

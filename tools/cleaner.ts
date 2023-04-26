bb4452ab6d1ed9af922fb0936fe700b532cc8fcd
Baptiste Maurel <baptou12@gmail.com>
2023-04-26 15:20:28 +0200

    Fix and refacto le script d'anonymisation (#3661)
    * fix: réparer le script d'anonymisation
    
    - Suppression des process.exit qui faisait s'arrêter le script en cours de traitement
    - Utilisation d'async/await pour facilité la lecture
    - Ajout d'une déconnexion de la DB à la fin du script
    
    * feat: replace new Date instanciation by `Date.now()`
    
    * feat: erase followup error that could contains email address
    
    * chore: sortir les fonction qui anonymise les followup et simulation
    
    * feat: add tests
    
    * feat: create enum for simulation statuses

--- tools/cleaner.ts
+++ tools/cleaner.ts
@@ -1,174 +1,80 @@
-/* eslint-disable no-console */
-import es from "event-stream"
+import mongoose from "mongoose"
 
-// Loads
-// import expect from "expect"
-import("../backend/lib/mongo-connector")
-import Simulation from "../backend/models/simulation"
+import config from "../backend/config/index.js"
+import mongooseConfig from "../backend/config/mongoose.js"
+import Simulation, {
+  SimulationStatusEnum,
+} from "../backend/models/simulation.js"
 import Followup from "../backend/models/followup"
+import {
+  anonymizeSimulation,
+  anonymizeFollowup,
+} from "../lib/cleaner-functions"
 
-function getAnonymizedAnswer(answer, simulation) {
-  switch (answer.entityName) {
-    case "famille": {
-      switch (answer.fieldName) {
-        case "en_couple": {
-          return answer
-        }
-        default: {
-          return null
-        }
-      }
-    }
-    case "menage": {
-      switch (answer.fieldName) {
-        case "depcom":
-        case "loyer":
-        case "statut_occupation_logement":
-        case "_logementType":
-        case "_primoAccedant":
-        case "_locationType": {
-          return answer
-        }
-        default: {
-          return null
-        }
-      }
-    }
-    case "individu": {
-      if (answer.id == "demandeur" || answer.id == "conjoint") {
-        switch (answer.fieldName) {
-          case "_contrat_alternant":
-          case "_hasRessources":
-          case "_interetAidesSanitaireSocial":
-          case "_interetBafa":
-          case "_interetEtudesEtranger":
-          case "_interetPermisDeConduire":
-          case "_interetsAidesVelo":
-          case "activite":
-          case "age":
-          case "alternant":
-          case "boursier":
-          case "depcom":
-          case "enfant_a_charge":
-          case "nationalite":
-          case "ressources": {
-            return answer
-          }
-          case "date_naissance": {
-            const dt = new Date(simulation.dateDeValeur)
-            const dob = new Date(answer.value)
-            return {
-              id: answer.id,
-              entityName: answer.entityName,
-              fieldName: "age",
-              value: Math.round(
-                (dt.getTime() - dob.getTime()) / 365.25 / 24 / 60 / 60 / 1000
-              ),
-            }
-          }
-          default: {
-            return null
-          }
-        }
-      } else {
-        return null
-      }
-    }
-    default: {
-      return null
-    }
-  }
-}
-
-function generateNewAll(answers, simulation) {
-  return answers.map((a) => getAnonymizedAnswer(a, simulation)).filter((a) => a)
-}
-
-function main() {
-  const aMonthAgo = new Date().getTime() - 31 * 24 * 60 * 60 * 1000
-  const aWeekAgo = new Date().getTime() - 7 * 24 * 60 * 60 * 1000
+async function main() {
+  const aMonthAgo = Date.now() - 31 * 24 * 60 * 60 * 1000
+  const aWeekAgo = Date.now() - 7 * 24 * 60 * 60 * 1000
 
   let followup_count = 0
-  Followup.find({
+  const followupsCursor = await Followup.find({
     createdAt: { $lt: aMonthAgo },
     email: { $exists: true },
   })
     .sort({ _id: -1 })
     .cursor()
-    .pipe(
-      es.map(function (model, done) {
-        model.email = undefined
 
-        model.save(function (err) {
-          if (err) {
-            console.log(
-              `Cannot save ${model.constructor.modelName} ${model.id}`
-            )
-            console.trace(err)
-          }
-          followup_count += 1
-          done()
-        })
-      })
-    )
-    .on("end", function () {
-      console.log(["Terminé", "Followup", followup_count].join(";"))
-      process.exit()
-    })
-    .on("error", function (err) {
+  for await (const followup of followupsCursor) {
+    const followupAnonymized = anonymizeFollowup(followup)
+
+    try {
+      await followupAnonymized.save()
+      followup_count += 1
+    } catch (err) {
+      console.error(`Cannot save followup: ${followupAnonymized.id}`)
       console.trace(err)
-      process.exit()
-    })
-    .resume()
+    }
+  }
+
+  console.log(["Terminé", "Followup", followup_count].join(";"))
 
   let simulation_count = 0
-  Simulation.find({
+  const simulationsCursor = await Simulation.find({
     $or: [
       {
         dateDeValeur: { $lt: aMonthAgo },
-        status: "new",
+        status: SimulationStatusEnum.NEW,
         hasFollowup: { $exists: true },
       },
       {
         dateDeValeur: { $lt: aWeekAgo },
-        status: "new",
+        status: SimulationStatusEnum.NEW,
         hasFollowup: { $exists: false },
       },
     ],
   })
     .sort({ dateDeValeur: 1 })
     .cursor()
-    .pipe(
-      es.map(function (model, done) {
-        model.status = "anonymized"
 
-        const newAll = generateNewAll(model.answers.all, model)
-        model.answers = {
-          all: newAll,
-          current: [],
-        }
+  for await (const simulation of simulationsCursor) {
+    const anonymizedSimulation = anonymizeSimulation(simulation)
 
-        model.save(function (err) {
-          if (err) {
-            console.log(
-              `Cannot save ${model.constructor.modelName} ${model.id}`
-            )
-            console.trace(err)
-          }
-          simulation_count += 1
-          done()
-        })
-      })
-    )
-    .on("end", function () {
-      console.log(["Terminé", "Simulation", simulation_count].join(";"))
-      process.exit()
-    })
-    .on("error", function (err) {
-      console.trace(err)
-      process.exit()
-    })
-    .resume()
+    try {
+      await anonymizedSimulation.save()
+      simulation_count += 1
+    } catch (err) {
+      console.error(`Cannot save simulation: ${anonymizedSimulation.id}`)
+    }
+  }
+
+  console.log(["Terminé", "Simulation", simulation_count].join(";"))
 }
 
-main()
+try {
+  mongooseConfig(mongoose, config)
+  await main()
+} catch (err) {
+  console.error(err)
+} finally {
+  await mongoose.connection.close()
+  console.log("DB disconnected")
+}

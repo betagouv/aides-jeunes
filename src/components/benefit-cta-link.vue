e9d7461599db7b1d95e7eaa65179450f2f581d46
Shamzic <shamery@free.fr>
2023-04-28 11:08:18 +0200

    revert benefit-cta-link

--- src/components/benefit-cta-link.vue
+++ src/components/benefit-cta-link.vue
@@ -1,50 +1,24 @@
 <template>
-  <div class="fr-container fr-px-0 fr-mb-0 fr-py-2w">
-    <div class="fr-grid-row fr-grid-row--gutters">
-      <ul
-        v-for="(cta, index) in ctas"
-        :key="index"
-        class="fr-col-6 fr-btns-group fr-mx-0 fr-py-0 fr-px-0"
-      >
-        <li>
-          <BenefitCtaLink
-            :analytics-name="benefit.id"
-            :benefit="benefit"
-            :link="cta.link"
-            :type="cta.type"
-            :level="levels[index]"
-          /> </li
-      ></ul>
-      <ul
-        v-if="showProximityCta"
-        class="fr-col fr-btns-group fr-mx-0 fr-py-0 fr-px-0"
-      >
-        <li>
-          <AnalyticRouterLink
-            id="cta-proximity"
-            class="fr-btn"
-            :analytics="{
-              name: benefit.id,
-              action: eventTypeShowLocations,
-              category: 'General',
-            }"
-            :to="{
-              name: 'resultatsLieuxDedies',
-              params: { benefit_id: benefit.id },
-            }"
-            >À proximité de chez vous
-          </AnalyticRouterLink>
-        </li>
-      </ul>
-    </div>
-  </div>
+  <a
+    :id="`cta-${type}`"
+    v-analytics="{ name: analyticsName, action: type, category: 'General' }"
+    :aria-label="longLabel"
+    :class="{
+      'fr-btn fr-btn--secondary': level === 'default',
+      'fr-btn': level === 'success',
+    }"
+    :href="getURL(link)"
+    class="text-center"
+    rel="noopener"
+    target="_blank"
+    @click="onClick(link)"
+    v-html="label"
+  />
 </template>
 
 <script>
-import BenefitCtaLink from "./benefit-cta-link.vue"
-import { hasEtablissements } from "@lib/benefits/etablissements.ts"
-import AnalyticRouterLink from "@/components/buttons/analytic-router-link.vue"
-import { BehaviourEventTypes } from "@lib/enums/behaviour-event-types.ts"
+import ResultatsMixin from "@/mixins/resultats.js"
+import { useStore } from "@/stores/index.ts"
 import storageService from "@/lib/storage-service.ts"
 
 let typeLabels = {
@@ -60,19 +34,23 @@ let longLabels = {
 }
 
 export default {
-  name: "BenefitCta",
-  components: {
-    BenefitCtaLink,
-    AnalyticRouterLink,
-  },
+  name: "BenefitCtaLink",
+  components: {},
+  mixins: [ResultatsMixin],
   props: {
+    analyticsName: String,
     benefit: Object,
+    level: String,
+    type: String,
+    link: [String, Object],
   },
-  data: function () {
+  setup() {
     return {
-      levels: ["success", "default"],
-      eventTypeShowLocations: BehaviourEventTypes.showLocations,
+      store: useStore(),
     }
+  },
+  data() {
+    return {}
   },
   computed: {
     label() {
@@ -89,6 +67,7 @@ export default {
       if (typeof link === "object") {
         return this.$router.resolve(link).href
       }
+
       return link
     },
     onClick(link) {
@@ -97,29 +76,6 @@ export default {
           simulationId: this.store.calculs.resultats._id,
         })
       }
-    },
-    ctas() {
-      const ctaBehaviourTypes = [
-        BehaviourEventTypes.teleservice,
-        BehaviourEventTypes.form,
-        BehaviourEventTypes.instructions,
-      ]
-      return ctaBehaviourTypes
-        .map((type) => {
-          const linkGenerator = this.benefit[`${type}Generator`]
-          const link = this.benefit[type] || (linkGenerator && linkGenerator())
-          return {
-            type,
-            link,
-          }
-        })
-        .filter(function (item) {
-          return item.link
-        })
-        .slice(0, 2)
-    },
-    showProximityCta() {
-      return hasEtablissements(this.benefit) && this.$route.name !== "aide"
     },
   },
 }

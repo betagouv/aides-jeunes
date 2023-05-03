812dd586fbe1b9112c0ce5a0f67a84c3ef33c4e3
Shamzic <shamery@free.fr>
2023-05-03 16:21:50 +0200

    Fix updating

--- src/components/droits-details-lieux.vue
+++ src/components/droits-details-lieux.vue
@@ -45,7 +45,7 @@ const etablissementsPreview = computed(() => {
         <span v-else>Afficher tous les lieux</span>
       </button>
     </div>
-    <div v-if="etablissements.length === 0">
+    <div v-if="etablissements.length === 0 && !updating">
       <p class="fr-italic fr-my-2w">
         <cite>Aucun lieu n'a pu être trouvé à proximité de chez vous.</cite>
       </p>

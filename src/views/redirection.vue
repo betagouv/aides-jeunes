34f2ac26adadc2d8fa2edc26531ca122afc72773
Valandr <valerie.andriani@gmail.com>
2023-05-23 15:55:57 +0200

    Suppression code inutile

--- src/views/redirection.vue
+++ src/views/redirection.vue
@@ -106,11 +106,10 @@ export default {
       const route = this.$route.query.vers
       const [teleservice, procedure] = route.split("?procedure=")
       if (teleservice === "ds") {
-        document.location = `https://www.demarches-simplifiees.fr/commencer/${procedure}`
+        this.$router.push = `https://www.demarches-simplifiees.fr/commencer/${procedure}`
       } else {
         this.error = "Identifiant de simulation absent… Arrêt."
         this.updating = false
-        return
       }
     },
     fetchSimulationId() {

651fb0eb6369e326b3a9294d983e8043b07b7a07
Thomas Guillet <thomas@palya.fr>
2023-06-06 17:24:21 +0200

    Ajoute une interface pour l'envoi d'email

--- backend/lib/smtp.ts
+++ backend/lib/smtp.ts
@@ -1,9 +1,10 @@
 import config from "../config/index.js"
 import nodemailer from "nodemailer"
+import { EmailLayout } from "../../lib/types/email.js"
 
 const transporter = nodemailer.createTransport(config.smtp)
 
-export function sendMail(email) {
+export function sendMail(email: EmailLayout) {
   return transporter.sendMail({
     from: '"Équipe du simulateur 1jeune1solution.gouv.fr" <aides-jeunes@beta.gouv.fr>"',
     ...email,

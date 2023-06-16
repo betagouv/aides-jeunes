8168996358d7c80f4d5de464e0a2cd5569f304f1
Thomas Guillet <thomas@palya.fr>
2023-06-16 16:07:32 +0200

    Met à jour le test

--- tests/unit/tools/new.spec.ts
+++ tests/unit/tools/new.spec.ts
@@ -1,7 +1,26 @@
+import { process } from "../../../../lib/benefits/link-validity.js"
+
+function makeBenefitData(props) {
+  return {
+    id: "benefit_id",
+    label: "Texte de l'aide",
+    institution: "institution_label",
+    priority: 0,
+    links: [
+      {
+        link: "https://li.nk",
+        type: "institution",
+      },
+    ],
+    editLink: "https://edit.link",
+    ...props,
+  }
+}
+
 describe("process for new link results", () => {
   describe("process for new link results", () => {
     it("should anonymize followup", () => {
-      const operations = process([], {})
+      const operations = process([], makeBenefitData())
 
       expect(operations).toHaveLength(1)
     })

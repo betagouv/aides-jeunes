66384e7bfce5de63bec4d2040cb9688682ff3a94
CUGNIERE <16650011+Cugniere@users.noreply.github.com>
2023-07-03 12:21:24 +0200

    Update/aides velo 3.0.13 (#3808)
    * update: change la version du package aides vélos en 3.0.13
    
    * update: ajoute les institutions et logos manquants
    
    * chore: optimise la taille des images
    
    * update: modifie un test pour autoriser le digramme e dans l'o

--- tests/unit/aides-velo-benefit-generator.spec.ts
+++ tests/unit/aides-velo-benefit-generator.spec.ts
@@ -8,7 +8,7 @@ describe("aides velo benefit generator", function () {
 
   list.forEach((benefit) => {
     it("generates simple benefit ids", function () {
-      expect(benefit.id).toMatch(/^[0-9a-zA-Z_éàèëçâôû-]+$/)
+      expect(benefit.id).toMatch(/^[0-9a-zA-Z_éàèëçâôûœ-]+$/)
     })
 
     it("verify description exists", function () {

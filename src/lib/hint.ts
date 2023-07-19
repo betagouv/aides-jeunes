8728da3ccbed9235c5c9dcd547a9baefad64b532
CUGNIERE <16650011+Cugniere@users.noreply.github.com>
2023-07-19 14:07:47 +0200

    Affiche un message pour le code postal des parents (#3855)
    * update: affiche un message pour le code postal des parents
    
    * chore: corrige la phrase

--- src/lib/hint.ts
+++ src/lib/hint.ts
@@ -4,6 +4,9 @@ const texts = {
   depcom: (): string => {
     return "Le simulateur n'accepte que les codes postaux français. Si vous vivez à l'étranger, ce simulateur n'est pas encore adapté à votre situation."
   },
+  _bourseCriteresSociauxCommuneDomicileFamilial: (): string => {
+    return "Lorsque vos parents sont séparés, il faut indiquer le code postal du parent qui vous a à charge. Si vous êtes encore mineur et en garde alternée, il faut indiquer le code postal du parent dont le domicile est le plus proche du votre."
+  },
   enfants: (): string => {
     return `« Un enfant à charge » est un enfant dont vous êtes responsable et dont vous vous occupez, qu'il soit votre enfant naturel ou non.`
   },

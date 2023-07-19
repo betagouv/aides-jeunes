1bb518e9909f98af4d75d0056caef5938e4f3110
cugniere <16650011+Cugniere@users.noreply.github.com>
2023-07-19 11:33:37 +0200

    update: affiche un message pour le code postal des parents

--- src/lib/hint.ts
+++ src/lib/hint.ts
@@ -4,6 +4,9 @@ const texts = {
   depcom: (): string => {
     return "Le simulateur n'accepte que les codes postaux français. Si vous vivez à l'étranger, ce simulateur n'est pas encore adapté à votre situation."
   },
+  _bourseCriteresSociauxCommuneDomicileFamilial: (): string => {
+    return "Lorsque les parents sont séparés, il faut prendre les ressources du parent ayant à la charge l'étudiant. Si l'étudiant est en garde alternée, il faut faire la somme des ressources des deux foyers fiscaux des parents séparés."
+  },
   enfants: (): string => {
     return `« Un enfant à charge » est un enfant dont vous êtes responsable et dont vous vous occupez, qu'il soit votre enfant naturel ou non.`
   },

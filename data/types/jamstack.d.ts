58384d76f552ad3494caa733a57d131ee10b7056
cugniere <16650011+Cugniere@users.noreply.github.com>
2023-08-08 07:50:53 +0200

    update: ajoute un type au fichiers jamstack

---
+++ data/types/jamstack.d.ts
@@ -0,0 +1,12 @@
+export interface JamstackLayout {
+  locale: any
+  backend: any
+  media_folder: any
+  public_folder: any
+  slug: any
+  publish_mode: any
+  root: any
+  patterns: any
+  fields: any
+  collections: any
+}

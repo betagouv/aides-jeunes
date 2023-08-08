2ac295972407900bd63e635aae2038cc2aa41ac7
cugniere <16650011+Cugniere@users.noreply.github.com>
2023-08-08 07:16:39 +0200

    update: déclare l'interface ComponentCustomProperties

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -14,3 +14,21 @@ declare module "generator:institutions" {
   const institutionMap: RollupInstitutionMapInterface
   export default institutionMap
 }
+
+declare module "vue" {
+  interface ComponentCustomProperties {
+    $theme: {
+      current: string
+      options: {
+        title: string
+        label: string
+        value: string
+      }[]
+      update(string): void
+    }
+    $filters: {
+      capitalize(string): string
+    }
+  }
+}
+export {}

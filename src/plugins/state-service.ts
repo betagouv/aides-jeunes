fb743df5dac7a37267b0014cfa18647e6e334828
baptou12 <baptou12@gmail.com>
2023-08-30 15:24:30 +0200

    feat: tout les `trackEvent` sont maintenant fait avec `Piwik.getTracker`

--- src/plugins/state-service.ts
+++ src/plugins/state-service.ts
@@ -24,7 +24,7 @@ const StateService = {
               action: "Navigation cancelled",
               label: failure.toString(),
             },
-            this.$matomo
+            window.Piwik?.getTracker()
           )
         } else {
           throw new Error(failure)

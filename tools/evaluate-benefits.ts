bd026581715e5b33674bb318d7f1867654d2b473
cugniere <16650011+Cugniere@users.noreply.github.com>
2023-08-31 11:16:56 +0200

    chore: infère le type situation layout

--- tools/evaluate-benefits.ts
+++ tools/evaluate-benefits.ts
@@ -4,6 +4,7 @@ import { computeAides } from "../lib/benefits/compute.js"
 import benefits from "../data/all.js"
 import("../backend/lib/mongo-connector.js")
 import Simulation from "../backend/models/simulation.js"
+import { situationsLayout } from "../lib/types/situations.js"
 
 function main() {
   const simulationId = process.argv[2]
@@ -17,7 +18,7 @@ function main() {
   Simulation.findById(simulationId, (err, simulation) => {
     if (err) return process.exit(1)
 
-    const situation = generateSituation(simulation)
+    const situation = generateSituation(simulation) as situationsLayout
 
     calculate(situation, function (err, result) {
       const openfiscaResponse = Object.assign(

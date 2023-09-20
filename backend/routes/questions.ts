ab79e2be07da57f42fd51dd127385effaeccec57
baptou12 <baptou12@gmail.com>
2023-09-20 15:43:32 +0200

    chore: Enlève la route `api/questions`
    Note:
    - ça n'est apparement plus utilisé
    - enlève le code associé

--- backend/routes/questions.ts
+++
@@ -1,9 +0,0 @@
-import express, { Express } from "express"
-import { getQuestions } from "../controllers/questions.js"
-
-export default function (api: Express) {
-  const route = express.Router({ mergeParams: true })
-  route.get("", getQuestions)
-
-  api.use("/questions", route)
-}

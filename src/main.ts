a0783d666389e4e5d879656c07bd0f1dc7488f3f
Shamzic <shamery@free.fr>
2023-09-28 15:07:26 +0200

    Fix prettier

--- src/main.ts
+++ src/main.ts
@@ -64,7 +64,7 @@ if (navigator.cookieEnabled && process.env.VITE_CONTEXT === "production") {
 }
 
 app.config.globalProperties.$filters = {
-  capitalize(value = "") {
+  capitalize(value: string = "") {
     return value.charAt(0).toUpperCase() + value.slice(1)
   },
 }

2f1878796ca914b93b5cc0955b1c60bd448de094
baptou12 <baptou12@gmail.com>
2023-09-28 10:16:12 +0200

    hotfix: change way we send email
    More to follow

--- backend/lib/email.sh
+++ backend/lib/email.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-cd `dirname $0`
-
-node ../../dist-server/backend/lib/email.js send initial-survey --multiple 1000
+node ./dist-server/backend/lib/email.js send initial-survey --multiple 1000

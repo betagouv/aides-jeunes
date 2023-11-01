64199a2dfa10daa5eef62a57891d1ad22189460e
Shamzic <shamery@free.fr>
2023-11-01 18:44:14 +0100

    Ajoute le bouton de partage d'une aide par facebook et par email

--- src/components/droits-details.vue
+++ src/components/droits-details.vue
@@ -111,6 +111,32 @@
           </div>
         </div>
       </div>
+      <div class="fr-print-hidden">
+        <div class="fr-share fr-mt-2w">
+          <p class="fr-share__title">Partager l'aide :</p>
+          <ul class="fr-share__group">
+            <li>
+              <a
+                class="fr-share__link fr-share__link--facebook"
+                title="Partager sur Facebook - nouvelle fenêtre"
+                :href="`https://www.facebook.com/sharer.php?u=${sharingLinkUrl}`"
+                target="_blank"
+                rel="noopener"
+                onclick="window.open(this.href,'Partager sur Facebook','toolbar=no,location=yes,status=no,menubar=no,scrollbars=yes,resizable=yes,width=600,height=450'); event.preventDefault();"
+                >Partager sur Facebook</a
+              >
+            </li>
+            <li>
+              <button
+                class="fr-share__link fr-share__link--copy"
+                title="Copier dans le presse-papier"
+                :onclick="copyToClipboard"
+                >Copier dans le presse-papier</button
+              >
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -160,6 +186,15 @@ export default {
     }
   },
   computed: {
+    sharingLinkUrl() {
+      return `https://mes-aides.1jeune1solution.beta.gouv.fr/aides/${this.droit.slug}`
+    },
+    sharingLinkTitle() {
+      const prefix = `${this.droit.prefix}${
+        this.droit.prefix?.endsWith("’") ? "" : " "
+      }`
+      return `Accédez aux informations sur l'aide financière : ${prefix}${this.droit.label}`
+    },
     aCharge() {
       return SituationMethods.aCharge(this.store.situation)
     },
@@ -183,6 +218,12 @@ export default {
     },
     showVoluntaryConditions() {
       return (droit) => droit.voluntary_conditions?.length
+    },
+  },
+  methods: {
+    async copyToClipboard() {
+      await navigator.clipboard.writeText(this.sharingLinkUrl)
+      alert("Adresse copiée dans le presse papier." + this.sharingLinkUrl)
     },
   },
 }

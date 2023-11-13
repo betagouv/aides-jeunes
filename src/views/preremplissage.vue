8fb62937f88de3b692a5b6e95edd3f4dfca3a411
Shamzic <shamery@free.fr>
2023-11-13 14:57:52 +0100

    refactor: supprime headers inutiles dans le post (preremplissage)

--- src/views/preremplissage.vue
+++ src/views/preremplissage.vue
@@ -94,8 +94,6 @@ const submitPrefillData = async () => {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        "Access-Control-Allow-Headers": "Content-Type",
-        "Access-Control-Allow-Origin": "*",
       },
 
       body: JSON.stringify(formData.value),

c06f0f8cf11287cb7e0f1eed9fd9ef4f72563009
Shamzic <shamery@free.fr>
2023-11-16 11:51:08 +0100

    refactor : renderAndSendEmail en sendEmail + sendMail en sendEmailSmtp

--- backend/lib/messaging/email/email-service.ts
+++ backend/lib/messaging/email/email-service.ts
@@ -1,11 +1,11 @@
-import { sendMail } from "../../smtp.js"
+import { sendEmailSmtp } from "../../smtp.js"
 import emailRender from "../../mes-aides/emails/email-render.js"
 import { EmailType } from "../../../../lib/enums/messaging.js"
 
-export async function renderAndSendEmail(category: EmailType, followup) {
+export async function sendEmail(category: EmailType, followup) {
   try {
     const render: any = await emailRender(category, followup)
-    const response = await sendMail({
+    const response = await sendEmailSmtp({
       to: followup.email,
       subject: render.subject,
       text: render.text,

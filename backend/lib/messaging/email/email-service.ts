5644f9454b13f1f921830facde00ec64663dcc9c
Shamzic <shamery@free.fr>
2023-11-20 14:21:04 +0100

    refactor: enum EmailCategory -> EmailType

--- backend/lib/messaging/email/email-service.ts
+++ backend/lib/messaging/email/email-service.ts
@@ -2,15 +2,15 @@ import { sendEmailSmtp } from "../../smtp.js"
 import emailRender from "../../mes-aides/emails/email-render.js"
 import { EmailType } from "../../../../lib/enums/messaging.js"
 
-export async function sendEmail(category: EmailType, followup) {
+export async function sendEmail(emailType: EmailType, followup) {
   try {
-    const render: any = await emailRender(category, followup)
+    const render: any = await emailRender(emailType, followup)
     const response = await sendEmailSmtp({
       to: followup.email,
       subject: render.subject,
       text: render.text,
       html: render.html,
-      tags: [category],
+      tags: [emailType],
     })
     followup.sentAt = Date.now()
     followup.messageId = response.messageId

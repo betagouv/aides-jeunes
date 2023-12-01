f83f3d2221ff4a033520686dc68fbaa5a55d3218
Shamzic <shamery@free.fr>
2023-12-01 18:22:35 +0100

    chore: fix ci test unitaire

--- tests/unit/lib/state/messaging/messaging-sms.spec.ts
+++ tests/unit/lib/state/messaging/messaging-sms.spec.ts
@@ -4,6 +4,8 @@ import {
   filterInitialSurveySms,
 } from "@backend/lib/messaging/sending.js"
 import dayjs from "dayjs"
+import utc from "dayjs/plugin/utc"
+dayjs.extend(utc)
 
 describe("shouldSendSurveyBySms condition tests", () => {
   let followup
@@ -84,10 +86,13 @@ describe("shouldSendSurveyBySms condition tests", () => {
 })
 
 describe("filterInitialSurveySms tests", () => {
-  it("should filter followup with the correct condition for sending initial survey", async () => {
-    const limit = 10
+  let mockFollowups
+  const limit = 10
+
+  beforeEach(() => {
     jest.useFakeTimers().setSystemTime(new Date("2023-11-25"))
-    const mockFollowups = [
+
+    mockFollowups = [
       {
         // should not be included because it has already a sms survey
         _id: "mock-0",
@@ -110,15 +115,15 @@ describe("filterInitialSurveySms tests", () => {
         surveys: [
           {
             type: "track-click-on-simulation-usefulness-email",
-            createdAt: dayjs("2023-11-22").toDate(),
+            createdAt: dayjs("2023-11-21").toDate(),
             answers: [],
           },
         ],
         phone: "0600000000",
         email: "test@test.fr",
-        createdAt: dayjs("2023-11-17").toDate(),
-        sentAt: dayjs("2023-11-17").toDate(),
-        smsSentAt: dayjs("2023-11-17").toDate(),
+        createdAt: dayjs("2023-11-15").toDate(),
+        sentAt: dayjs("2023-11-15").toDate(),
+        smsSentAt: dayjs("2023-11-15").toDate(),
       },
       {
         // should not be included because it has no sms survey and has a phone
@@ -148,18 +153,40 @@ describe("filterInitialSurveySms tests", () => {
         sentAt: dayjs("2023-11-17").toDate(),
       },
     ]
+  })
 
-    const result = await filterInitialSurveySms(mockFollowups, limit)
+  afterEach(() => {
+    jest.useRealTimers()
+  })
 
+  it("should not include followup with sms survey", async () => {
+    const result = await filterInitialSurveySms([mockFollowups[0]], limit)
+    expect(result).not.toContainEqual(
+      expect.objectContaining({ _id: "mock-0" })
+    )
+  })
+
+  it("should include followup with no sms survey and has a phone and respects the email delay", async () => {
+    const result = await filterInitialSurveySms([mockFollowups[1]], limit)
     expect(result).toContainEqual(expect.objectContaining({ _id: "mock-1" }))
+  })
+
+  it("should not include followup with no sms survey and has a phone but does not respect the email delay", async () => {
+    const result = await filterInitialSurveySms([mockFollowups[2]], limit)
     expect(result).not.toContainEqual(
       expect.objectContaining({ _id: "mock-2" })
     )
+  })
+
+  it("should not include followup without a phone", async () => {
+    const result = await filterInitialSurveySms([mockFollowups[3]], limit)
     expect(result).not.toContainEqual(
       expect.objectContaining({ _id: "mock-3" })
     )
+  })
+
+  it("should include followup with a phone, no sms survey, and no email", async () => {
+    const result = await filterInitialSurveySms([mockFollowups[4]], limit)
     expect(result).toContainEqual(expect.objectContaining({ _id: "mock-4" }))
-    expect(result).toHaveLength(2)
-    jest.useRealTimers()
   })
 })

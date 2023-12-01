6e578ba13c85548e612a888cfabbd420e96bed6b
baptou12 <baptou12@gmail.com>
2023-12-01 11:08:27 +0100

    feat: Fait un redirect si il y a une erreur

--- src/composables/use-followup-survey-data.ts
+++ src/composables/use-followup-survey-data.ts
@@ -2,15 +2,19 @@ import { ref, onMounted } from "vue"
 import axios from "axios"
 import dayjs from "dayjs"
 import * as Sentry from "@sentry/vue"
+import { useRouter } from "vue-router"
 
 import { FetchSurvey } from "@lib/types/survey.d.js"
 import { StandardBenefit, BenefitWithChoice } from "@data/types/benefits.d.js"
 import { getBenefit } from "@/lib/benefits.js"
+import { useStore } from "@/stores/index.js"
 
 export function useFollowupSurveyData(token: string) {
   const followupCreatedAt = ref<string>("")
   const benefitsWithChoice = ref<BenefitWithChoice[]>([])
   const loading = ref<boolean>(true)
+  const store = useStore()
+  const router = useRouter()
 
   const getFollowupSurveyData = async () => {
     const { data: followupSurveyData } = (await axios.get(
@@ -41,6 +45,7 @@ export function useFollowupSurveyData(token: string) {
   onMounted(() => {
     getFollowupSurveyData().catch((err) => {
       Sentry.captureException(err)
+      store.redirection((route) => router.push(route))
     })
   })
 

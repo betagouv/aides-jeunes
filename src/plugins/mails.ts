e36b9cfcad445c1dfa67293a03fb2d056ac92f54
Thomas Guillet <thomas@palya.fr>
2023-12-14 17:22:35 +0100

    Ajoute des espaces insécables

--- src/plugins/mails.ts
+++ src/plugins/mails.ts
@@ -14,10 +14,10 @@ export const sendEcartInstructions = (simulationId, droitsEligibles) => {
     subject: `Montants inattendus [${simulationId}]`,
     body: `Bonjour,
 
-    En effectuant une simulation sur votre simulateur, j'ai obtenu le résultat suivant :
+    En effectuant une simulation sur votre simulateur, j'ai obtenu le résultat suivant :
     ${generateResultLines(droitsEligibles)}
 
-    Mais en effectuant une demande auprès de l'administration XXX, j'ai obtenu le résultat suivant :
+    Mais en effectuant une demande auprès de l'administration XXX, j'ai obtenu le résultat suivant :
     - XXX
 
     J'ai bien compris que vous n'étiez pas décisionnaires et ne pourrez pas intervenir en ma faveur.
@@ -26,8 +26,8 @@ export const sendEcartInstructions = (simulationId, droitsEligibles) => {
     Bonne journée,
 
     ————
-    ID : ${simulationId} (à conserver impérativement pour traitement de votre demande)
-            ————`,
+    ID : ${simulationId} (à conserver impérativement pour traitement de votre demande)
+    ————`,
   }
 }
 export const sendEcartSimulation = (simulationId, droitsEligibles) => {
@@ -38,10 +38,10 @@ export const sendEcartSimulation = (simulationId, droitsEligibles) => {
     subject: `Montants inattendus [${simulationId}]`,
     body: `Bonjour,
 
-    En effectuant une simulation sur votre simulateur, j'ai obtenu le résultat suivant :
+    En effectuant une simulation sur votre simulateur, j'ai obtenu le résultat suivant :
     ${generateResultLines(droitsEligibles)}
 
-    Mais en effectuant la même simulation sur le site XXX, j'ai obtenu le résultat suivant :
+    Mais en effectuant la même simulation sur le site XXX, j'ai obtenu le résultat suivant :
     - XXX
 
     Vous pouvez me joindre par téléphone au XX XX XX XX XX (de préférence en semaine) pour une dizaine de minutes d'échange afin de comprendre d'où provient cet écart.
@@ -49,8 +49,8 @@ export const sendEcartSimulation = (simulationId, droitsEligibles) => {
     Bonne journée,
 
     ————
-    ID : ${simulationId} (à conserver impérativement pour traitement de votre demande)
-            ————`,
+    ID : ${simulationId} (à conserver impérativement pour traitement de votre demande)
+    ————`,
   }
 }
 
@@ -75,8 +75,8 @@ export const sendError = (simulationId, error) => {
   Je vous joins également une capture d'écran pour faciliter la compréhension du problème.
 
   ————
-  ID : ${simulationId}
-  Erreur : ${error}
+  ID : ${simulationId}
+  Erreur : ${error}
   ————`,
   }
 }

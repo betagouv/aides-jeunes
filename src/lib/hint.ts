5b8d533697d4ae43920f9e03b138f6aea88ced2d
Simon Hamery <shamery@free.fr>
2023-12-15 11:38:57 +0100

    Update src/lib/hint.ts
    Co-authored-by: Baptiste Maurel <baptou12@gmail.com>

--- src/lib/hint.ts
+++ src/lib/hint.ts
@@ -10,7 +10,7 @@ const texts = {
   enfants: (): string => {
     return `« Un enfant à charge » est un enfant dont vous êtes responsable et dont vous vous occupez, qu'il soit votre enfant naturel ou non.`
   },
-  types: (): string => {
+  types: (): string => { // Step: resources/types
     return `Lorsque vos parents sont séparés, il faut indiquer les types de ressources perçues par le parent qui vous a à charge. Si vous êtes mineur et en garde alternée, il faut indiquer les types de ressources perçues par les deux foyers fiscaux des parents séparés.`
   },
 }

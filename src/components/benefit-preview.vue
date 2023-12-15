d03d63365923a0cec289d7fbba73fce19f0e98dc
Shamzic <shamery@free.fr>
2023-12-15 16:59:37 +0100

    refactor: crée un composant spécifique à la preview d'une aide (benefit-preview dans benefits-list)

---
+++ src/components/benefit-preview.vue
@@ -0,0 +1,76 @@
+<script setup lang="ts">
+import { defineProps } from "vue"
+import { getBenefitImage } from "@lib/benefits/details.js"
+import DroitEstime from "./droit-estime.vue"
+import WarningMessage from "@/components/warning-message.vue"
+import useUtils from "@/composables/use-utils.js"
+import { StandardBenefit } from "@data/types/benefits"
+
+const { capitalizeString, isBoolean } = useUtils()
+const props = defineProps({
+  benefit: Object,
+})
+
+const askBenefitLabelButton = (benefit) => {
+  return `Demander ${benefit.prefix}${benefit.prefix == "l’" ? "" : " "}${
+    benefit.label
+  }`
+}
+</script>
+
+<template>
+  <div v-if="props.benefit">
+    <router-link
+      class="fr-tile"
+      :to="`/simulation/resultats/${props.benefit.id}`"
+      itemscope
+      itemtype="http://schema.org/GovernmentService"
+      :data-testid="props.benefit.id"
+      :aria-label="askBenefitLabelButton(props.benefit)"
+    >
+      <div class="fr-p-4w">
+        <div class="aj-benefit-header fr-mb-4w">
+          <img
+            class="aj-institution-icon"
+            :src="getBenefitImage(props.benefit as StandardBenefit)"
+            :alt="`Logo ${props.benefit.institution.label}`"
+          />
+          <div class="aj-benefit-name">
+            <h2 class="fr-text--lead" itemprop="name">{{
+              capitalizeString(props.benefit.label)
+            }}</h2>
+            <div class="aj-benefit-institution"
+              >{{ capitalizeString(props.benefit.institution.label) }}
+            </div>
+            <div>
+              <p class="fr-text--justify" v-html="props.benefit.description" />
+              <WarningMessage
+                v-if="
+                  benefit?.montant &&
+                  isBoolean(benefit.montant) &&
+                  benefit.warning === true
+                "
+              >
+                <img src="@/assets/images/warning.svg" alt="" /> Attention,
+                cette aide vous est accessible sous certaines conditions
+                supplémentaires.
+              </WarningMessage>
+            </div>
+          </div>
+          <DroitEstime :droit="(props.benefit as StandardBenefit)" />
+        </div>
+        <ul class="fr-btns-group fr-btns-group--inline-sm fr-btns-group--right">
+          <li>
+            <router-link
+              :to="`/simulation/resultats/${props.benefit.id}`"
+              class="fr-btn fr-my-0"
+              data-testid="aide-cta"
+            >
+              Demander cette aide
+            </router-link>
+          </li>
+        </ul>
+      </div>
+    </router-link>
+  </div>
+</template>
--- src/components/benefits-list.vue
+++ src/components/benefits-list.vue
@@ -1,16 +1,10 @@
 <script setup lang="ts">
 import { computed, defineProps } from "vue"
-import { getBenefitImage } from "@lib/benefits/details.js"
-import DroitEstime from "./droit-estime.vue"
-import WarningMessage from "@/components/warning-message.vue"
 import { Benefit } from "@data/types/benefits"
-import useUtils from '@/composables/use-utils.js';
-
-const { capitalizeString, isBoolean } = useUtils()
+import BenefitPreview from "@/components/benefit-preview.vue"
 
 const props = defineProps({
   benefits: Array<Benefit>,
-  ineligible: Boolean,
   filter: Array<any>,
 })
 
@@ -19,67 +13,10 @@ const list: any = computed(() => {
     return !props.filter || props.filter.includes(value.id)
   })
 })
-
-const askBenefit = (benefit) => {
-  return `Demander ${benefit.prefix}${benefit.prefix == "l’" ? "" : " "}${
-    benefit.label
-  }`
-}
 </script>
 
 <template>
   <div v-for="(benefit, index) in list" :key="index" class="fr-mb-5w">
-    <router-link
-      class="fr-tile"
-      :to="`/simulation/resultats/${benefit.id}`"
-      itemscope
-      itemtype="http://schema.org/GovernmentService"
-      :data-testid="benefit.id"
-      :aria-label="askBenefit(benefit)"
-    >
-      <div class="fr-p-4w">
-        <div class="aj-benefit-header fr-mb-4w">
-          <img
-            class="aj-institution-icon"
-            :src="getBenefitImage(benefit)"
-            :alt="`Logo ${benefit.institution.label}`"
-          />
-          <div class="aj-benefit-name">
-            <h2 class="fr-text--lead" itemprop="name">{{
-              capitalizeString(benefit.label)
-            }}</h2>
-            <div class="aj-benefit-institution"
-              >{{ capitalizeString(benefit.institution.label) }}
-            </div>
-            <div>
-              <p class="fr-text--justify" v-html="benefit.description" />
-              <WarningMessage
-                v-if="
-                  benefit.montant &&
-                  isBoolean(benefit.montant) &&
-                  benefit.warning === true
-                "
-              >
-                <img src="@/assets/images/warning.svg" alt="" /> Attention,
-                cette aide vous est accessible sous certaines conditions
-                supplémentaires.
-              </WarningMessage>
-            </div>
-          </div>
-          <DroitEstime :droit="benefit" />
-        </div>
-        <ul class="fr-btns-group fr-btns-group--inline-sm fr-btns-group--right">
-          <li>
-            <router-link
-              :to="`/simulation/resultats/${benefit.id}`"
-              class="fr-btn fr-my-0"
-              data-testid="aide-cta"
-            >
-              Demander cette aide
-            </router-link>
-          </li>
-        </ul>
-      </div>
-    </router-link>
+    <benefit-preview :benefit="benefit" />
   </div>
 </template>

43758308ce6c2071a52dca71020d606ef266ae68
Simon Hamery <shamery@free.fr>
2024-03-07 12:08:10 +0100

    fix: capitalize non souhaité (#4233)

--- lib/properties/others/simple-properties.ts
+++ lib/properties/others/simple-properties.ts
@@ -3,15 +3,10 @@ import IndividuMethods from "../../individu.js"
 import { ressourceTypes } from "../../resources.js"
 import { capitalize } from "../../../lib/utils.js"
 
-function perceivedLabel(individu) {
-  const label = IndividuMethods.label(individu, "percevoir")
-  return individu.id.startsWith("enfant") ? capitalize(label) : label
-}
-
 export default {
   ressources: new MultipleProperty({
     question: ({ individu }) =>
-      `Quel type de revenu ${perceivedLabel(individu)} ?`,
+      `Quel type de revenu ${IndividuMethods.label(individu, "percevoir")} ?`,
     items: ressourceTypes.map((resource: any) => {
       return { value: resource.id, label: resource.label }
     }),

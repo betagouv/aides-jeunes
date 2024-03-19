25b6265c65e57e5d118fead82b98713abdc8031b
Simon Hamery <shamery@free.fr>
2024-03-19 12:31:18 +0100

    Update src/components/1j1s/footer-1j1s.vue
    Co-authored-by: Thomas Guillet <thomas.guillet@beta.gouv.fr>

--- src/components/1j1s/footer-1j1s.vue
+++ src/components/1j1s/footer-1j1s.vue
@@ -109,7 +109,7 @@ const parameters = {
     { label: "Données personnelles", to: "/confidentialite" },
     { label: "Statistiques", to: "/stats" },
     { label: "Toutes les aides", to: "/aides" },
-    { label: "Intégration iframe", to: "/iframe" },
+    { label: "Intégrer notre simulateur", to: "/iframe" },
     { label: "Nous contacter", to: "/contact" },
   ],
 }

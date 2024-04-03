32b7f2c082eba3791bf1a9de4e4d85c34cbb9d96
Thomas Guillet <thomas.guillet@beta.gouv.fr>
2024-04-03 13:08:06 +0200

    Fix le glitch d'affichage sur la page de résultats (#4267)

--- src/stores/results.ts
+++ src/stores/results.ts
@@ -79,7 +79,10 @@ export const useResultsStore = defineStore("results", {
       return useStore().saveSituationError
     },
     shouldDisplayResults() {
-      return !(this.updating || this.hasWarning || this.error) && this.benefits
+      return (
+        !(this.updating || this.hasWarning || this.error) &&
+        this.resultats?.droitsEligibles
+      )
     },
     simulationAnonymized() {
       return useStore().simulationAnonymized

37c13678d82c80e86ac97174c18528048bfa6c6f
Thomas Guillet <thomas.guillet@beta.gouv.fr>
2024-04-05 09:53:43 +0200

    Update backend/lib/migrations/followups/to-v5.ts
    Co-authored-by: Simon Hamery <shamery@free.fr>

--- backend/lib/migrations/followups/to-v5.ts
+++ backend/lib/migrations/followups/to-v5.ts
@@ -4,6 +4,7 @@ export default {
   apply(followup) {
     followup.surveys = followup.surveys.map((survey) => {
       survey.touchedAts = [survey.openedAt]
+      survey.openedAt = undefined
       return survey
     })
     return followup

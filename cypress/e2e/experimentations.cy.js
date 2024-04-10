e410f8b8fe4c26ea815369565b81a19977519901
Thomas Guillet <thomas@palya.fr>
2024-04-10 17:09:05 +0200

    Simplifie le test

--- cypress/e2e/experimentations.cy.js
+++ cypress/e2e/experimentations.cy.js
@@ -1,12 +1,7 @@
 /// <reference types="Cypress" />
-import navigate from "../utils/navigate.js"
 import "cypress-axe"
 
 context("Test du site d'expérimentations", () => {
-  beforeEach(() => {
-    navigate.init()
-  })
-
   it("Go to the recap during a basic situation and modify/continue the simulation", () => {
     expect(process.env.GITHUB_HEAD_REF).to.eq("test")
     expect(process.env.GITHUB_RUN_ID).to.eq("test")

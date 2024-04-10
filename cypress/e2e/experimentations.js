94e23ba4896c88139e70e70496d990a389e5bd4b
Thomas Guillet <thomas@palya.fr>
2024-04-10 16:56:21 +0200

    Ajoute une étape de CI pour le site d'expérimentations

---
+++ cypress/e2e/experimentations.js
@@ -0,0 +1,15 @@
+/// <reference types="Cypress" />
+import navigate from "../utils/navigate.js"
+import "cypress-axe"
+
+context("Test du site d'expérimentations", () => {
+  beforeEach(() => {
+    navigate.init()
+  })
+
+  it("Go to the recap during a basic situation and modify/continue the simulation", () => {
+    expect(process.env.GITHUB_HEAD_REF).to.eq("test")
+    expect(process.env.GITHUB_RUN_ID).to.eq("test")
+    expect(100).to.eq(200)
+  })
+})

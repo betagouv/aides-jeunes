bf632acf1c797763e41b65af266d7b9b7fba8bc3
Thomas Guillet <thomas@palya.fr>
2024-04-10 18:30:29 +0200

    Simplifie la route de collecte d'une simulation de démo

--- backend/controllers/simulation-demo.ts
+++ backend/controllers/simulation-demo.ts
@@ -2,6 +2,8 @@ import axios from "axios"
 
 let data = null
 
+const artifactName = "cypress-json-base"
+
 async function getArtifactMetadata() {
   const artifactListResponse = await axios.get(
     "https://api.github.com/repos/betagouv/aides-jeunes/actions/artifacts"
@@ -9,7 +11,7 @@ async function getArtifactMetadata() {
   const artifactListPayload = artifactListResponse.data
   const releventArtifacts = artifactListPayload.artifacts.filter((artifact) => {
     return (
-      artifact.name === "cypress-json-base" &&
+      artifact.name === artifactName &&
       artifact.workflow_run.head_branch === "main"
     )
   })
@@ -26,7 +28,7 @@ async function fetchArtifactZip() {
   }
 
   // CORS + anonymous access issues on GitHub API directly
-  const url = `https://nightly.link/betagouv/aides-jeunes/actions/runs/${metadata.workflow_run.id}/${metadata.name}.zip`
+  const url = `https://nightly.link/betagouv/aides-jeunes/actions/runs/${metadata.workflow_run.id}/${artifactName}.zip`
   const artifact = await axios.get(url, {
     responseType: "arraybuffer",
   })

99a68b80d67117b9fa730926e4ed7e0402420d19
Thomas Guillet <thomas@palya.fr>
2024-04-10 17:17:55 +0200

    Corrige le partage de envvar

--- cypress/e2e/experimentations.cy.js
+++ cypress/e2e/experimentations.cy.js
@@ -3,7 +3,6 @@ import "cypress-axe"
 
 context("Test du site d'expérimentations", () => {
   it("Go to the recap during a basic situation and modify/continue the simulation", () => {
-    expect(process.env.GITHUB_HEAD_REF).to.eq("test")
     expect(process.env.GITHUB_RUN_ID).to.eq("test")
     expect(100).to.eq(200)
   })

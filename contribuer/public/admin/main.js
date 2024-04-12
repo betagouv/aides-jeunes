c81bc8e00ee0abec51053b09e20596730b8345d5
Thomas Guillet <thomas@palya.fr>
2024-04-12 17:25:39 +0200

    Permet la prévisualisation dans Decap avec une iframe

--- contribuer/public/admin/main.js
+++ contribuer/public/admin/main.js
@@ -78,167 +78,35 @@ function updateInstitutionsList(institutions) {
   }
 }
 
-const Conditions = ({ conditions }) => {
-  if (!conditions || !conditions.length) {
-    return <div></div>
+
+class DroitPreviewTemplate extends React.Component {
+  constructor(props) {
+    super(props)
+    this.iframe = React.createRef()
+    this.checkReadiness = this.checkReadiness.bind(this)
+    this.send = this.send.bind(this)
+    addEventListener("message", this.checkReadiness)
   }
-
-  let conditionsList = conditions.map((condition, index) => {
-    return <li key={index} dangerouslySetInnerHTML={{ __html: condition }}></li>
-  })
-  return (
-    <div className="aj-content-conditions">
-      <p className="aj-content-conditions-title">
-        Pour en bénéficier, vous devez également :
-      </p>
-      <ul className="list-unstyled">{conditionsList}</ul>
-    </div>
-  )
-}
-
-const Description = ({ description, link }) => {
-  const linkHTML = link ? (
-    <a href={link}>Plus d&apos;informations</a>
-  ) : (
-    <span></span>
-  )
-
-  if (!description) {
-    return <p>{linkHTML}</p>
+  checkReadiness(event) {
+    if (event.data.source == "aides-jeunes" && event.data.value == "ready") {
+      removeEventListener("message", this.checkReadiness)
+      this.send()
+    }
   }
-
-  return (
-    <div className="aj-content-description">
-      <p className="aj-droit-description-text">
-        <span
-          dangerouslySetInnerHTML={{
-            __html: description,
-          }}
-        ></span>
-        &nbsp;
-        {linkHTML}
-      </p>
-    </div>
-  )
-}
-
-const DroitEstime = ({ droit }) => {
-  if (!droit.type) {
-    return <span></span>
+  send() {
+    const benefit = this.props.entry.get("data").toJS()
+    const institution = benefit.institution && institutionsMap[benefit.institution] || {}
+    this.iframe.current.contentWindow.postMessage({
+      source: 'decap',
+      value: {...benefit, "institution": {"label": "institution", ...institution}}
+    }, 'http://localhost:8080')
   }
-
-  let droitEstime
-  switch (droit.type) {
-    case "bool":
-      droitEstime = <span className="aj-droit-eligible">✅</span>
-      break
-    case "float":
-      const montant = droit.montant || ""
-      const unit = droit.montant ? droit.unit : ""
-      const legend =
-        droit.legend && LEGENDE_PERIODICITE_AIDE_ENUM[droit.periodicite]
-          ? `${droit.legend} ${
-              LEGENDE_PERIODICITE_AIDE_ENUM[droit.periodicite]
-            }`
-          : droit.legend ||
-            LEGENDE_PERIODICITE_AIDE_ENUM[droit.periodicite] ||
-            ""
-      droitEstime = (
-        <span className="aj-droit-value">{`${montant} ${unit} ${legend}`}</span>
-      )
-      break
-    default:
-      droitEstime = <span></span>
-      break
+  componentDidUpdate(prevProps) {
+    this.send()
   }
-  return droitEstime
-}
-
-const CTA = ({ droit }) => {
-  if (!droit.teleservice && !droit.form && !droit.instructions) {
-    return <span></span>
+  render() {
+    return <iframe ref={this.iframe} class="iframe" src="http://localhost:8080/preview?iframe"/>;
   }
-
-  const ctas = [
-    {
-      test: Boolean(droit.teleservice),
-      ctaLink: droit.teleservice,
-      ctaLabel: "Faire une demande en ligne",
-    },
-    {
-      test: Boolean(droit.form),
-      ctaLink: droit.form,
-      ctaLabel: "Accéder au formulaire papier",
-    },
-    {
-      test: Boolean(droit.instructions),
-      ctaLink: droit.instructions,
-      ctaLabel: "Lien vers des instructions à suivre",
-    },
-  ]
-
-  return ctas
-    .filter((cta) => cta.test)
-    .slice(0, 2)
-    .map((cta, index) => (
-      <a href={cta.ctaLink} key={index} className="aj-droit-cta button cta">
-        {cta.ctaLabel}
-      </a>
-    ))
-}
-
-const DroitPreviewTemplate = ({ entry }) => {
-  const droit = entry.get("data").toJS()
-  const institution =
-    droit.institution && institutionsMap[droit.institution]
-      ? institutionsMap[droit.institution].name
-      : ""
-  const institutionImage =
-    droit.institution && institutionsMap[droit.institution]
-      ? institutionsMap[droit.institution].imgSrc
-      : undefined
-  return (
-    <div className="aj-main-container">
-      <h1 className="aj-preview">Aperçu</h1>
-      <div className="aj-results-details">
-        <div className="aj-droit-detail">
-          <div className="aj-droit-header">
-            {(droit.imgSrc && (
-              <img
-                className="aj-droit-illustration"
-                src={`${config.institutionRessourcesURL}/${droit.imgSrc}`}
-              />
-            )) ||
-              (institutionImage && (
-                <img
-                  className="aj-droit-illustration"
-                  src={`${config.institutionRessourcesURL}/${institutionImage}`}
-                />
-              ))}
-            <div>
-              <div className="aj-droit-identity">{droit.label}</div>
-              <div className="aj-institution-label">{institution}</div>
-            </div>
-
-            <div className="aj-droit-montant">
-              <DroitEstime droit={droit} />
-            </div>
-          </div>
-          <div className="aj-droit-content">
-            <div className="aj-droit-content-description">
-              <Description description={droit.description} link={droit.link} />
-            </div>
-            <div className="aj-droit-conditions">
-              <Conditions conditions={droit.conditions} />
-            </div>
-            <div className="aj-droit-content-buttons-cta">
-              <CTA droit={droit} />
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  )
 }
 
 CMS.registerPreviewTemplate("benefits_openfisca", DroitPreviewTemplate)

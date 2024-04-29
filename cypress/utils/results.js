09c30a29dcb1066d76e35154ff5be3b9673c8c2d
Jeremy PASTOURET <pastouret.jeremy@gmail.com>
2024-04-29 17:32:05 +0200

    fix: echap la modal de saisie d'e-mail

--- cypress/utils/results.js
+++ cypress/utils/results.js
@@ -256,20 +256,6 @@ const receiveResultsEmail = () => {
 
 const receiveResultsSms = () => {
   const phone = "0600000000"
-  cy.intercept(
-    {
-      method: "POST",
-      url: "/api/simulation/*/followup",
-    },
-    {
-      statusCode: 200,
-      body: {
-        surveyOptin: true,
-        phone: "0600000000",
-      },
-    }
-  ).as("post-receive-results-sms")
-
   cy.get("[data-testid='send-email-and-sms-button']", {
     timeout: 20000,
   })
@@ -278,6 +264,19 @@ const receiveResultsSms = () => {
   // scroll to input#phone
   const ABTestingEnvironment = storageService.local.getItem("ABTesting") || {}
   if (ABTestingEnvironment.Followup_SMS.value === "show") {
+    cy.intercept(
+      {
+        method: "POST",
+        url: "/api/simulation/*/followup",
+      },
+      {
+        statusCode: 200,
+        body: {
+          surveyOptin: true,
+          phone: "0600000000",
+        },
+      }
+    ).as("post-receive-results-sms")
     cy.get("input#phone").scrollIntoView().should("be.visible").type(phone)
     cy.get(".fr-btn:contains(J'accepte d'être recontacté ou recontactée)")
       .should("be.visible")
@@ -288,9 +287,9 @@ const receiveResultsSms = () => {
     })
   } else if (ABTestingEnvironment.Followup_SMS.value === "hide") {
     cy.get("input#phone").should("not.exist")
-    cy.get('button.fr-btn[title:"Fermer la fenêtre modale"]')
-      .should("be.visible")
-      .click()
+    cy.get(".fr-btn:contains(J'accepte d'être recontacté ou recontactée)").type(
+      "{esc}"
+    )
   }
 }
 

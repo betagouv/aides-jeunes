93b2db91c96b449351701c1404e87b01728925dd
Shamzic <shamery@free.fr>
2024-04-29 17:17:45 +0200

    fix: npm run format

--- cypress/utils/results.js
+++ cypress/utils/results.js
@@ -12,7 +12,7 @@ const back = () => cy.get('[data-testid="back-button"]').click()
 const IdentifyBenefit = (id, name) => {
   cy.get(
     `[itemtype="http://schema.org/GovernmentService"][data-testid="${id}"]`,
-    { timeout: 10000 },
+    { timeout: 10000 }
   ).as(`${id}-summary`)
   cy.checkA11y()
   getBenefitSummary(id)
@@ -30,7 +30,7 @@ const hasBafaGroupPreviewBenefit = (mustBeDisplay) => {
   } else {
     cy.get(
       `[itemtype="http://schema.org/GovernmentService"][data-testid="${bafaGroupPreviewId}"]`,
-      { timeout: 10000 },
+      { timeout: 10000 }
     ).should("not.exist")
   }
 }
@@ -71,11 +71,11 @@ const hasPrimeActiviteNearbyPlaces = () => {
   cy.get('[data-testid="nearby-places"]').should("be.visible")
   cy.get('[data-testid="lieu-title"]').should(
     "contain",
-    "Caisse d'allocations familiales",
+    "Caisse d'allocations familiales"
   )
   cy.get('[data-testid="lieu-informations-link"]').should(
     "contain",
-    "Voir les informations",
+    "Voir les informations"
   )
 }
 
@@ -206,7 +206,7 @@ const hasVeloGroupPreviewBenefit = (mustBeDisplay) => {
   } else {
     cy.get(
       `[itemtype="http://schema.org/GovernmentService"][data-testid="${veloGroupPreviewId}"]`,
-      { timeout: 10000 },
+      { timeout: 10000 }
     ).should("not.exist")
   }
 }
@@ -267,7 +267,7 @@ const receiveResultsSms = () => {
         surveyOptin: true,
         phone: "0600000000",
       },
-    },
+    }
   ).as("post-receive-results-sms")
 
   cy.get("[data-testid='send-email-and-sms-button']", {
@@ -298,7 +298,7 @@ const checkResultsRequests = () => {
   cy.wait("@post-simulation").then(({ request, response }) => {
     cy.writeFile(
       `cypress/payloads/${Cypress.spec.fileName}-simulation.json`,
-      response.body,
+      response.body
     )
     expect(request.method).to.equal("POST")
     expect(response.statusCode).to.equal(200)

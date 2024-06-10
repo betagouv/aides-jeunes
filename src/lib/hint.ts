64fa10a4e67db58c3b6ecb8d23d4b25dfd66c34e
Jeremy PASTOURET <pastouret.jeremy@gmail.com>
2024-06-10 11:06:34 +0200

    feat: ajout du bouton en savoir plus pour le service civique

--- src/lib/hint.ts
+++ src/lib/hint.ts
@@ -10,6 +10,9 @@ const texts = {
   enfants: (): string => {
     return `« Un enfant à charge » est un enfant dont vous êtes responsable et dont vous vous occupez, qu'il soit votre enfant naturel ou non.`
   },
+  service_civique: (): string => {
+    return `Un Service Civique est un engagement volontaire au service de l'intérêt général. Depuis le 1er juillet 2022, le Service Civique est indemnisé 601 euros net par mois environ. Vous pouvez trouver des missions sur le site d'1jeune1solution.`
+  },
 }
 
 // Retrieve `moreInfo` field of each mutualized step

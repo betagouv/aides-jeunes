4f9a77b10262e6da3a1a60b980f64cfce32888cc
Shamzic <shamery@free.fr>
2024-06-18 17:15:05 +0200

    refactor: script setup & code reordering

--- src/components/step-view.vue
+++ src/components/step-view.vue
@@ -1,70 +1,58 @@
+<script setup lang="ts">
+import { ref, computed } from "vue"
+import StepView from "./step-view.vue"
+
+const props = defineProps({
+  step: Object,
+  depth: Number,
+})
+
+const open = ref(false)
+const itemCount = computed(() => counter(props.step))
+const nextStep = computed(() => next(props.step))
+const label = computed(() => {
+  const count = itemCount.value
+  return `${nextStep.value}${count > 1 ? `(+${count - 1})` : ""}`
+})
+
+const counter = (step) => {
+  return step.steps ? step.steps.map(counter).reduce((a, v) => a + v, 0) : 1
+}
+
+const next = (step) => {
+  return step.path || (step.steps ? next(step.steps[0]) : "GGG")
+}
+
+const toggle = () => {
+  open.value = !open.value
+}
+</script>
+
 <template>
   <div class="step">
     <button
-      :disabled="item_count == 1"
+      :disabled="itemCount == 1"
       class="fr-btn fr-btn--secondary"
-      @click="toggle()"
+      @click="toggle"
     >
-      {{ item_count == 1 ? "" : open ? "-" : "+" }}
+      {{ itemCount == 1 ? "" : open ? "-" : "+" }}
     </button>
     <span
-      :class="!step.isActive && 'hidden'"
+      :class="!step?.isActive && 'hidden'"
       class="fr-icon-question-line"
       aria-hidden="true"
     />
-    {{ label(step) }}
+    {{ label }}
 
     <div v-if="open">
       <StepView
-        v-for="(substep, index) in step.steps"
+        v-for="(substep, index) in step?.steps"
         :key="index"
         :step="substep"
       />
     </div>
   </div>
 </template>
-
-<script>
-import { ref } from "vue"
-
-function counter({ steps }) {
-  return steps ? steps.map(counter).reduce((a, v) => a + v, 0) : 1
-}
-
-function next(step) {
-  return step.path || (step.steps ? next(step.steps[0]) : "GGG")
-}
-
-export default {
-  name: "StepView",
-  props: {
-    step: Object,
-    depth: Number,
-  },
-  setup() {
-    return {
-      open: ref(false),
-    }
-  },
-  computed: {
-    item_count() {
-      return counter(this.step)
-    },
-    next() {
-      return next(this.step)
-    },
-  },
-  methods: {
-    toggle() {
-      this.open = !this.open
-    },
-    label() {
-      const count = this.item_count
-      return `${this.next}${count > 1 ? `(+${count - 1})` : ""}`
-    },
-  },
-}
-</script>
 
 <style type="text/css" scoped>
 .step {

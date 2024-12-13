f555fb1edab1a92ffa70236fd126e7879a71c676
Shamzic <shamery@free.fr>
2024-12-13 10:59:38 +0100

    refactor: home page

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,3 +1,12 @@
+<script setup lang="ts">
+import HeroSection from "@/components/home/hero-section.vue"
+import FeaturesSection from "@/components/home/features-section.vue"
+import HowItWorksSection from "@/components/home/how-it-works-section.vue"
+import BenefitsShowcaseSection from "@/components/home/benefits-showcase-section.vue"
+import PartnersSection from "@/components/home/partners-section.vue"
+import FaqSection from "@/views/faq.vue"
+</script>
+
 <template>
   <div data-testid="home-page">
     <HeroSection />
@@ -20,12 +29,3 @@
     </div>
   </div>
 </template>
-
-<script setup lang="ts">
-import HeroSection from "@/components/home/hero-section.vue"
-import FeaturesSection from "@/components/home/features-section.vue"
-import HowItWorksSection from "@/components/home/how-it-works-section.vue"
-import BenefitsShowcaseSection from "@/components/home/benefits-showcase-section.vue"
-import PartnersSection from "@/components/home/partners-section.vue"
-import FaqSection from "@/views/faq.vue"
-</script>

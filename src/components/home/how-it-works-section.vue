aa9be837c8a01893e7453474b39146f9e726b5ca
Shamzic <shamery@free.fr>
2024-12-13 15:26:21 +0100

    fix: accessibilité - axe

--- src/components/home/how-it-works-section.vue
+++ src/components/home/how-it-works-section.vue
@@ -2,7 +2,7 @@
   <div class="fr-container fr-mt-8w">
     <div class="fr-grid-row fr-grid-row--center">
       <div class="fr-col-12">
-        <h3 class="fr-h3 fr-mb-4w">Comment ça marche ?</h3>
+        <h2 class="fr-h3 fr-mb-4w">Comment ça marche ?</h2>
         <div class="aj-home-steps">
           <div
             v-for="(step, index) in steps"
@@ -11,7 +11,7 @@
           >
             <div class="aj-home-step__number">{{ index + 1 }}</div>
             <div>
-              <h4 class="fr-text--lg fr-mb-2w">{{ step.title }}</h4>
+              <h3 class="fr-text--lg fr-mb-2w">{{ step.title }}</h3>
               <p class="aj-home-step__description fr-text--sm fr-mb-0">
                 {{ step.description }}
               </p>

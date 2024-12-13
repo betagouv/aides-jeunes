4155b8cf810e2d86fce0fb9d80e5b00cce2b6a2c
Shamzic <shamery@free.fr>
2024-12-13 11:11:54 +0100

    refactor: déplace et refacto du bouton d'accès à la liste des aides

--- src/views/home.vue
+++ src/views/home.vue
@@ -5,6 +5,9 @@ import HowItWorksSection from "@/components/home/how-it-works-section.vue"
 import BenefitsShowcaseSection from "@/components/home/benefits-showcase-section.vue"
 import PartnersSection from "@/components/home/partners-section.vue"
 import FaqSection from "@/views/faq.vue"
+import { useRouter } from "vue-router"
+
+const router = useRouter()
 </script>
 
 <template>
@@ -15,17 +18,5 @@ import FaqSection from "@/views/faq.vue"
     <BenefitsShowcaseSection />
     <PartnersSection />
     <FaqSection :showBackButton="false" />
-
-    <div class="fr-container fr-mt-4w fr-mb-8w">
-      <div class="fr-grid-row fr-grid-row--center">
-        <div class="fr-col-12 fr-col-md-8 fr-col-lg-6 fr-text--center">
-          <router-link
-            to="/aides"
-            class="fr-link fr-fi-arrow-right-line fr-link--icon-right"
-            >Accéder à la liste complète des aides</router-link
-          >
-        </div>
-      </div>
-    </div>
   </div>
 </template>

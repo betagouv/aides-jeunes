fb02ca108e7aa61c595638bae68c266090604d80
Shamzic <shamery@free.fr>
2025-01-01 17:41:29 +0100

    feat: cache certaines sections de la page d'accueil dans l'iframe

--- src/views/home.vue
+++ src/views/home.vue
@@ -5,15 +5,23 @@ import HowItWorksSection from "@/components/home/how-it-works-section.vue"
 import BenefitsShowcaseSection from "@/components/home/benefits-showcase-section.vue"
 import PartnersSection from "@/components/home/partners-section.vue"
 import FaqSection from "@/views/faq.vue"
+import { useIframeStore } from "@/stores/iframe.js"
+import { computed } from "vue"
+
+const iframeStore = useIframeStore()
+
+const isIframe = computed(() => iframeStore.inIframe)
 </script>
 
 <template>
   <div data-testid="home-page">
     <HeroSection />
-    <FeaturesSection />
-    <HowItWorksSection />
-    <BenefitsShowcaseSection />
-    <PartnersSection />
-    <FaqSection :show-back-button="false" />
+    <span v-if="!isIframe">
+      <FeaturesSection />
+      <HowItWorksSection />
+      <BenefitsShowcaseSection />
+      <PartnersSection />
+      <FaqSection :show-back-button="false" />
+    </span>
   </div>
 </template>

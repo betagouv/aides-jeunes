52677d9dd6e06229fd5bf40e043320e5d4c7454b
Jeremy PASTOURET <pastouret.jeremy@gmail.com>
2025-01-29 15:15:47 +0100

    feat: mise à jour de la pertinance des tests

--- tests/unit/backend/survey.spec.ts
+++ tests/unit/backend/survey.spec.ts
@@ -1,5 +1,7 @@
 import { expect, jest } from "@jest/globals"
 import { SurveyType } from "../../../lib/enums/survey"
+import { Survey } from "@lib/types/survey"
+import FollowupModel from "../../../backend/models/followup.js"
 
 describe("Survey methods", () => {
   let mockFollowup
@@ -9,77 +11,60 @@ describe("Survey methods", () => {
       surveys: [],
       save: jest.fn(),
       addSurveyIfMissing: jest.fn(),
-      updateSurvey: jest.fn(),
+      updateSurvey: FollowupModel.prototype.updateSurvey,
     }
   })
 
   describe("addSurveyIfMissing", () => {
     it("should add survey if not exists", async () => {
-      const expectedSurvey = {
+      const existingSurvey: Survey = {
+        _id: "test-id",
         type: SurveyType.TrackClickOnSimulationUsefulnessEmail,
+        answers: [],
+        touchedAts: [],
       }
+      mockFollowup.surveys = [existingSurvey]
+      const newAnswers = [{ id: "wasUseful", value: true }]
+      await mockFollowup.updateSurvey(
+        SurveyType.TrackClickOnSimulationUsefulnessEmail,
+        newAnswers
+      )
+      expect(mockFollowup.surveys[0].answers).toEqual(newAnswers)
+      expect(mockFollowup.surveys[0].touchedAts).toHaveLength(1)
+    })
 
-      mockFollowup.addSurveyIfMissing.mockResolvedValue(expectedSurvey)
-
+    it("should not add survey if it already exists", async () => {
+      const existingSurvey: Survey = {
+        _id: "test-id",
+        type: SurveyType.TrackClickOnSimulationUsefulnessEmail,
+        answers: [],
+        touchedAts: [],
+      }
+      mockFollowup.surveys = [existingSurvey]
+      mockFollowup.addSurveyIfMissing.mockResolvedValue(existingSurvey)
       const result = await mockFollowup.addSurveyIfMissing(
         SurveyType.TrackClickOnSimulationUsefulnessEmail
       )
-
-      expect(mockFollowup.addSurveyIfMissing).toHaveBeenCalledWith(
-        SurveyType.TrackClickOnSimulationUsefulnessEmail
-      )
-      expect(result).toEqual(expectedSurvey)
+      expect(result).toEqual(existingSurvey)
     })
   })
 
   describe("updateSurvey", () => {
     it("should update survey with answers", async () => {
-      const answers = [
-        {
-          id: "wasUseful",
-          value: true,
-        },
-      ]
-
-      mockFollowup.updateSurvey.mockResolvedValue(undefined)
-
-      await mockFollowup.updateSurvey(
-        SurveyType.TrackClickOnSimulationUsefulnessEmail,
-        answers
-      )
-
-      expect(mockFollowup.updateSurvey).toHaveBeenCalledWith(
-        SurveyType.TrackClickOnSimulationUsefulnessEmail,
-        answers
-      )
-    })
-  })
-
-  describe("route handler", () => {
-    it("should handle survey update from request", async () => {
-      const req = {
-        query: { wasuseful: "true" },
-        followup: mockFollowup,
+      const existingSurvey: Survey = {
+        _id: "test-id",
+        type: SurveyType.TrackClickOnSimulationUsefulnessEmail,
+        answers: [],
+        touchedAts: [],
       }
-
-      const expectedAnswers = [
-        {
-          id: "wasUseful",
-          value: true,
-        },
-      ]
-
-      mockFollowup.updateSurvey.mockResolvedValue(undefined)
-
+      mockFollowup.surveys = [existingSurvey]
+      const newAnswers = [{ id: "wasUseful", value: true }]
       await mockFollowup.updateSurvey(
         SurveyType.TrackClickOnSimulationUsefulnessEmail,
-        expectedAnswers
+        newAnswers
       )
-
-      expect(mockFollowup.updateSurvey).toHaveBeenCalledWith(
-        SurveyType.TrackClickOnSimulationUsefulnessEmail,
-        expectedAnswers
-      )
+      expect(mockFollowup.surveys[0].answers).toEqual(newAnswers)
+      expect(mockFollowup.surveys[0].touchedAts).toHaveLength(1)
     })
   })
 })

58451bc5f012114be4cc2930c4f64b6c6d9947f6
Jeremy PASTOURET <pastouret.jeremy@gmail.com>
2025-02-24 10:56:02 +0100

    fix: suppression de texte devenu obsolete et amélioration du bouton de reception de résultat

--- src/components/recap-email-and-sms-form.vue
+++ src/components/recap-email-and-sms-form.vue
@@ -177,11 +177,6 @@ const ctaText = ref(computeCtaText())
 
 <template>
   <div class="fr-modal__content">
-    <p>
-      Si vous le souhaitez nous pouvons vous recontacter à deux reprises pour
-      faire le point sur les démarches que vous avez faites et les blocages que
-      vous avez rencontrés.
-    </p>
     <form class="fr-form fr-my-2w" @submit.prevent="sendRecap(true)">
       <div class="fr-form-group">
         <label class="fr-label" for="email"
@@ -267,7 +262,7 @@ const ctaText = ref(computeCtaText())
           class="fr-btn fr-btn--secondary"
           @click.prevent="sendRecap(false)"
         >
-          Je souhaite juste le récapitulatif
+          Je souhaite recevoir mon récapitulatif
         </button>
       </li>
     </ul>

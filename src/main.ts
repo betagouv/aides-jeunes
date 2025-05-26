06cccc16776e97c294c7980df56cdfbb2aae0399
Shamzic <shamery@free.fr>
2025-05-26 14:21:45 +0200

    feat: amélioration config ssr/ssg

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,4 @@
 import { ViteSSG } from "vite-ssg"
-import "@gouvfr/dsfr/dist/dsfr.min.css"
-import "@gouvfr/dsfr/dist/utility/utility.min.css"
-import "@gouvfr/dsfr/dist/dsfr.module.min.js"
 import App from "./app.vue"
 
 import router from "./router.js"
@@ -10,7 +7,6 @@ import StateService from "./plugins/state-service.js"
 import ThemeService from "./plugins/theme-service.js"
 
 import * as Sentry from "@sentry/vue"
-import VueMatomo from "vue-matomo"
 import VueCookies from "vue-cookies"
 
 import "@/styles/aides-jeunes.css"
@@ -35,6 +31,20 @@ const Resizer = {
   },
 }
 
+declare module "vue" {
+  export interface ComponentCustomProperties {
+    $theme: {
+      current: string
+      options: {
+        title: string
+        label: string
+        value: string
+      }[]
+      update(string): void
+    }
+  }
+}
+
 dayjs.locale("fr")
 dayjs.extend(customParseFormat)
 
@@ -42,73 +52,78 @@ const initApp = ({ app, router, initialState, onSSRAppRendered }) => {
   const pinia = createPinia()
   app.use(pinia)
 
-  if (process.env.SSR) {
+  if (import.meta.env.SSR) {
     onSSRAppRendered(() => {
       initialState.pinia = pinia.state.value
     })
   } else {
     pinia.state.value = initialState.pinia || {}
-  }
 
-  app.directive("analytics", AnalyticsDirective)
-  app.directive("mail", MailDirective)
-  app.directive("selectOnClick", SelectOnClickDirective)
-
-  if (process.env?.VITE_SENTRY_FRONTEND_DSN) {
-    Sentry.init({
-      app,
-      dsn: process.env.VITE_SENTRY_FRONTEND_DSN,
-      environment: process.env.VITE_CONTEXT,
-      integrations: [Sentry.browserTracingIntegration({ router })],
-      tracesSampleRate: 0.1,
-      debug: "development" === process.env.VITE_CONTEXT,
-      ignoreErrors: ["Can't find variable: gmo", /change_ua/],
+    // Import DSFR CSS and JS only on client-side
+    import("@gouvfr/dsfr/dist/dsfr.min.css")
+    import("@gouvfr/dsfr/dist/utility/utility.min.css")
+    import("@gouvfr/dsfr/dist/dsfr.module.min.js").then(() => {
+      console.log("DSFR loaded")
     })
-  }
-
-  app.use(Resizer)
-  app.use(StateService)
-  app.use(ThemeService)
-  app.use(VueCookies)
-
-  if (
-    !process.env.SSR &&
-    navigator.cookieEnabled &&
-    process.env?.VITE_MATOMO_URL
-  ) {
-    app.use(VueMatomo, {
-      host: process.env.VITE_MATOMO_URL,
-      trackerFileName: "piwik",
-      siteId: process.env.VITE_MATOMO_ID,
-      router: router,
-      preInitActions: [
-        [
-          "setExcludedQueryParams",
-          [
-            "token",
-            "situationId",
-            "simulationId",
-            "_ga",
-            "surveyEmail",
-            "code",
-            "state",
+    if (
+      typeof navigator !== "undefined" &&
+      navigator.cookieEnabled &&
+      process.env?.VITE_MATOMO_URL
+    ) {
+      import("vue-matomo").then(({ default: VueMatomo }) => {
+        app.use(VueMatomo, {
+          host: process.env.VITE_MATOMO_URL,
+          trackerFileName: "piwik",
+          siteId: process.env.VITE_MATOMO_ID,
+          router: router,
+          preInitActions: [
+            [
+              "setExcludedQueryParams",
+              [
+                "token",
+                "situationId",
+                "simulationId",
+                "_ga",
+                "surveyEmail",
+                "code",
+                "state",
+              ],
+            ],
           ],
-        ],
-      ],
+        })
+      })
+    }
+    if (process.env?.VITE_SENTRY_FRONTEND_DSN) {
+      Sentry.init({
+        app,
+        dsn: process.env.VITE_SENTRY_FRONTEND_DSN,
+        environment: process.env.VITE_CONTEXT,
+        integrations: [Sentry.browserTracingIntegration({ router })],
+        tracesSampleRate: 0.1,
+        debug: "development" === process.env.VITE_CONTEXT,
+        ignoreErrors: ["Can't find variable: gmo", /change_ua/],
+      })
+    }
+
+    app.use(ThemeService)
+    app.use(Resizer)
+    app.directive("analytics", AnalyticsDirective)
+    app.directive("mail", MailDirective)
+    app.directive("selectOnClick", SelectOnClickDirective)
+    app.use(StateService)
+    app.use(VueCookies)
+    router.beforeEach((to, from, next) => {
+      const store = useStore(pinia)
+      store.$onAction(persistDataOnSessionStorage)
+      store.initialize()
+      store.setOpenFiscaParameters()
+
+      if (to.query.debug === "parcours") {
+        store.setDebug(true)
+      }
+      next()
     })
   }
-
-  router.beforeEach((to, from, next) => {
-    const store = useStore(pinia)
-    store.$onAction(persistDataOnSessionStorage)
-    store.initialize()
-    store.setOpenFiscaParameters()
-
-    if (to.query.debug === "parcours") {
-      store.setDebug(true)
-    }
-    next()
-  })
 }
 
 export const createApp = ViteSSG(

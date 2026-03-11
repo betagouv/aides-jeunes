7b08351d461a7b084a9da709dd5b5e60d7357e5d
Jérémy PASTOURET <pastouret.jeremy@gmail.com>
2026-03-11 17:35:17 +0100

    fix(next): mise à jour des dépendances mais aussi correction d'un warning next lorsqu'il y a plusieurs lockfile dans un projet (#5075)

--- contribuer/next.config.js
+++ contribuer/next.config.js
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
+const path = require("path")
 const nextConfig = {
   output: "export",
   reactStrictMode: true,
   images: {
     unoptimized: true,
+  },
+  turbopack: {
+    root: path.join(__dirname, ".."),
   },
 }
 
